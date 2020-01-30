b9b3c996ec3da1b3a75bba80f577fbe63f3edb82
Janos Guljas <janos@resenje.org>
2020-01-30 12:14:01 +0100

    test p2p/protobuf package

---
+++ pkg/p2p/protobuf/internal/pb/doc.go
@@ -0,0 +1,9 @@
+// Copyright 2020 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:generate sh -c "protoc -I . -I \"$(go list -f '{{ .Dir }}' -m github.com/gogo/protobuf)/protobuf\" --gogofaster_out=. test.proto"
+
+// Package pb holds only Protocol Buffer definitions and generated code for
+// testing purposes.
+package pb
