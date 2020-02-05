aa6d77110620e9426b52a78de9efafe07833631a
Petar Radovic <petar.radovic@gmail.com>
2020-02-05 19:54:24 +0100

    update subsribeHandler and peerTracker

--- pkg/hive/pb/hive.pb.go
+++ pkg/hive/pb/hive.pb.go
@@ -67,7 +67,7 @@ func (m *Subscribe) GetDepth() uint32 {
 }
 
 type SubscribeResponse struct {
-	Peers []*Peer `protobuf:"bytes,1,rep,name=Peers,proto3" json:"Peers,omitempty"`
+	Peers *Peers `protobuf:"bytes,1,opt,name=Peers,proto3" json:"Peers,omitempty"`
 }
 
 func (m *SubscribeResponse) Reset()         { *m = SubscribeResponse{} }
@@ -103,29 +103,29 @@ func (m *SubscribeResponse) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_SubscribeResponse proto.InternalMessageInfo
 
-func (m *SubscribeResponse) GetPeers() []*Peer {
+func (m *SubscribeResponse) GetPeers() *Peers {
 	if m != nil {
 		return m.Peers
 	}
 	return nil
 }
 
-type Peer struct {
-	BzzAddress *BzzAddress `protobuf:"bytes,1,opt,name=BzzAddress,proto3" json:"BzzAddress,omitempty"`
+type Peers struct {
+	BzzAddress []*BzzAddress `protobuf:"bytes,1,rep,name=BzzAddress,proto3" json:"BzzAddress,omitempty"`
 }
 
-func (m *Peer) Reset()         { *m = Peer{} }
-func (m *Peer) String() string { return proto.CompactTextString(m) }
-func (*Peer) ProtoMessage()    {}
-func (*Peer) Descriptor() ([]byte, []int) {
+func (m *Peers) Reset()         { *m = Peers{} }
+func (m *Peers) String() string { return proto.CompactTextString(m) }
+func (*Peers) ProtoMessage()    {}
+func (*Peers) Descriptor() ([]byte, []int) {
 	return fileDescriptor_d635d1ead41ba02c, []int{2}
 }
-func (m *Peer) XXX_Unmarshal(b []byte) error {
+func (m *Peers) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
 }
-func (m *Peer) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+func (m *Peers) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	if deterministic {
-		return xxx_messageInfo_Peer.Marshal(b, m, deterministic)
+		return xxx_messageInfo_Peers.Marshal(b, m, deterministic)
 	} else {
 		b = b[:cap(b)]
 		n, err := m.MarshalToSizedBuffer(b)
@@ -135,19 +135,19 @@ func (m *Peer) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 		return b[:n], nil
 	}
 }
-func (m *Peer) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Peer.Merge(m, src)
+func (m *Peers) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Peers.Merge(m, src)
 }
-func (m *Peer) XXX_Size() int {
+func (m *Peers) XXX_Size() int {
 	return m.Size()
 }
-func (m *Peer) XXX_DiscardUnknown() {
-	xxx_messageInfo_Peer.DiscardUnknown(m)
+func (m *Peers) XXX_DiscardUnknown() {
+	xxx_messageInfo_Peers.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_Peer proto.InternalMessageInfo
+var xxx_messageInfo_Peers proto.InternalMessageInfo
 
-func (m *Peer) GetBzzAddress() *BzzAddress {
+func (m *Peers) GetBzzAddress() []*BzzAddress {
 	if m != nil {
 		return m.BzzAddress
 	}
@@ -253,7 +253,7 @@ func (m *Depth) GetDepth() uint32 {
 func init() {
 	proto.RegisterType((*Subscribe)(nil), "pb.Subscribe")
 	proto.RegisterType((*SubscribeResponse)(nil), "pb.SubscribeResponse")
-	proto.RegisterType((*Peer)(nil), "pb.Peer")
+	proto.RegisterType((*Peers)(nil), "pb.Peers")
 	proto.RegisterType((*BzzAddress)(nil), "pb.BzzAddress")
 	proto.RegisterType((*Depth)(nil), "pb.Depth")
 }
@@ -261,21 +261,21 @@ func init() {
 func init() { proto.RegisterFile("hive.proto", fileDescriptor_d635d1ead41ba02c) }
 
 var fileDescriptor_d635d1ead41ba02c = []byte{
-	// 216 bytes of a gzipped FileDescriptorProto
+	// 213 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0xca, 0xc8, 0x2c, 0x4b,
 	0xd5, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x2a, 0x48, 0x52, 0x52, 0xe4, 0xe2, 0x0c, 0x2e,
 	0x4d, 0x2a, 0x4e, 0x2e, 0xca, 0x4c, 0x4a, 0x15, 0x12, 0xe1, 0x62, 0x75, 0x49, 0x2d, 0x28, 0xc9,
-	0x90, 0x60, 0x54, 0x60, 0xd4, 0xe0, 0x0d, 0x82, 0x70, 0x94, 0x8c, 0xb9, 0x04, 0xe1, 0x4a, 0x82,
-	0x52, 0x8b, 0x0b, 0xf2, 0xf3, 0x8a, 0x53, 0x85, 0xe4, 0xb8, 0x58, 0x03, 0x52, 0x53, 0x8b, 0x8a,
-	0x25, 0x18, 0x15, 0x98, 0x35, 0xb8, 0x8d, 0x38, 0xf4, 0x0a, 0x92, 0xf4, 0x40, 0x02, 0x41, 0x10,
-	0x61, 0x25, 0x33, 0x2e, 0x16, 0x10, 0x43, 0x48, 0x8f, 0x8b, 0xcb, 0xa9, 0xaa, 0xca, 0x31, 0x25,
-	0xa5, 0x28, 0xb5, 0xb8, 0x18, 0x6c, 0x2e, 0xb7, 0x11, 0x1f, 0x48, 0x31, 0x42, 0x34, 0x08, 0x49,
-	0x85, 0x92, 0x13, 0xb2, 0x7a, 0x21, 0x09, 0x2e, 0x76, 0xff, 0xb2, 0xd4, 0xa2, 0x9c, 0xc4, 0x4a,
-	0xb0, 0x56, 0x9e, 0x20, 0x18, 0x57, 0x48, 0x8a, 0x8b, 0x23, 0x34, 0x2f, 0x05, 0x22, 0xc5, 0x04,
-	0x96, 0x82, 0xf3, 0x95, 0x64, 0xa1, 0xde, 0xc0, 0xee, 0x1f, 0x27, 0x89, 0x13, 0x8f, 0xe4, 0x18,
-	0x2f, 0x3c, 0x92, 0x63, 0x7c, 0xf0, 0x48, 0x8e, 0x71, 0xc2, 0x63, 0x39, 0x86, 0x0b, 0x8f, 0xe5,
-	0x18, 0x6e, 0x3c, 0x96, 0x63, 0x48, 0x62, 0x03, 0x87, 0x8b, 0x31, 0x20, 0x00, 0x00, 0xff, 0xff,
-	0xb1, 0x04, 0x2b, 0x51, 0x25, 0x01, 0x00, 0x00,
+	0x90, 0x60, 0x54, 0x60, 0xd4, 0xe0, 0x0d, 0x82, 0x70, 0x94, 0x4c, 0xb8, 0x04, 0xe1, 0x4a, 0x82,
+	0x52, 0x8b, 0x0b, 0xf2, 0xf3, 0x8a, 0x53, 0x85, 0xe4, 0xb9, 0x58, 0x03, 0x52, 0x53, 0x8b, 0x8a,
+	0xc1, 0x4a, 0xb9, 0x8d, 0x38, 0xf5, 0x0a, 0x92, 0xf4, 0xc0, 0x02, 0x41, 0x10, 0x71, 0x25, 0x73,
+	0xa8, 0x02, 0x21, 0x3d, 0x2e, 0x2e, 0xa7, 0xaa, 0x2a, 0xc7, 0x94, 0x94, 0xa2, 0xd4, 0x62, 0x90,
+	0x72, 0x66, 0x0d, 0x6e, 0x23, 0x3e, 0x90, 0x72, 0x84, 0x68, 0x10, 0x92, 0x0a, 0x25, 0x27, 0x64,
+	0xf5, 0x42, 0x12, 0x5c, 0xec, 0xfe, 0x65, 0xa9, 0x45, 0x39, 0x89, 0x95, 0x60, 0x9b, 0x78, 0x82,
+	0x60, 0x5c, 0x21, 0x29, 0x2e, 0x8e, 0xd0, 0xbc, 0x14, 0x88, 0x14, 0x13, 0x58, 0x0a, 0xce, 0x57,
+	0x92, 0x85, 0x7a, 0x04, 0xbb, 0x8f, 0x9c, 0x24, 0x4e, 0x3c, 0x92, 0x63, 0xbc, 0xf0, 0x48, 0x8e,
+	0xf1, 0xc1, 0x23, 0x39, 0xc6, 0x09, 0x8f, 0xe5, 0x18, 0x2e, 0x3c, 0x96, 0x63, 0xb8, 0xf1, 0x58,
+	0x8e, 0x21, 0x89, 0x0d, 0x1c, 0x32, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0x45, 0xd0, 0x07,
+	0x51, 0x27, 0x01, 0x00, 0x00,
 }
 
 func (m *Subscribe) Marshal() (dAtA []byte, err error) {
@@ -326,24 +326,22 @@ func (m *SubscribeResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
-	if len(m.Peers) > 0 {
-		for iNdEx := len(m.Peers) - 1; iNdEx >= 0; iNdEx-- {
-			{
-				size, err := m.Peers[iNdEx].MarshalToSizedBuffer(dAtA[:i])
-				if err != nil {
-					return 0, err
-				}
-				i -= size
-				i = encodeVarintHive(dAtA, i, uint64(size))
+	if m.Peers != nil {
+		{
+			size, err := m.Peers.MarshalToSizedBuffer(dAtA[:i])
+			if err != nil {
+				return 0, err
 			}
-			i--
-			dAtA[i] = 0xa
+			i -= size
+			i = encodeVarintHive(dAtA, i, uint64(size))
 		}
+		i--
+		dAtA[i] = 0xa
 	}
 	return len(dAtA) - i, nil
 }
 
-func (m *Peer) Marshal() (dAtA []byte, err error) {
+func (m *Peers) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
 	dAtA = make([]byte, size)
 	n, err := m.MarshalToSizedBuffer(dAtA[:size])
@@ -353,27 +351,29 @@ func (m *Peer) Marshal() (dAtA []byte, err error) {
 	return dAtA[:n], nil
 }
 
-func (m *Peer) MarshalTo(dAtA []byte) (int, error) {
+func (m *Peers) MarshalTo(dAtA []byte) (int, error) {
 	size := m.Size()
 	return m.MarshalToSizedBuffer(dAtA[:size])
 }
 
-func (m *Peer) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+func (m *Peers) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	i := len(dAtA)
 	_ = i
 	var l int
 	_ = l
-	if m.BzzAddress != nil {
-		{
-			size, err := m.BzzAddress.MarshalToSizedBuffer(dAtA[:i])
-			if err != nil {
-				return 0, err
+	if len(m.BzzAddress) > 0 {
+		for iNdEx := len(m.BzzAddress) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.BzzAddress[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintHive(dAtA, i, uint64(size))
 			}
-			i -= size
-			i = encodeVarintHive(dAtA, i, uint64(size))
+			i--
+			dAtA[i] = 0xa
 		}
-		i--
-		dAtA[i] = 0xa
 	}
 	return len(dAtA) - i, nil
 }
@@ -472,24 +472,24 @@ func (m *SubscribeResponse) Size() (n int) {
 	}
 	var l int
 	_ = l
-	if len(m.Peers) > 0 {
-		for _, e := range m.Peers {
-			l = e.Size()
-			n += 1 + l + sovHive(uint64(l))
-		}
+	if m.Peers != nil {
+		l = m.Peers.Size()
+		n += 1 + l + sovHive(uint64(l))
 	}
 	return n
 }
 
-func (m *Peer) Size() (n int) {
+func (m *Peers) Size() (n int) {
 	if m == nil {
 		return 0
 	}
 	var l int
 	_ = l
-	if m.BzzAddress != nil {
-		l = m.BzzAddress.Size()
-		n += 1 + l + sovHive(uint64(l))
+	if len(m.BzzAddress) > 0 {
+		for _, e := range m.BzzAddress {
+			l = e.Size()
+			n += 1 + l + sovHive(uint64(l))
+		}
 	}
 	return n
 }
@@ -659,8 +659,10 @@ func (m *SubscribeResponse) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			m.Peers = append(m.Peers, &Peer{})
-			if err := m.Peers[len(m.Peers)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+			if m.Peers == nil {
+				m.Peers = &Peers{}
+			}
+			if err := m.Peers.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex
@@ -688,7 +690,7 @@ func (m *SubscribeResponse) Unmarshal(dAtA []byte) error {
 	}
 	return nil
 }
-func (m *Peer) Unmarshal(dAtA []byte) error {
+func (m *Peers) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
 	iNdEx := 0
 	for iNdEx < l {
@@ -711,10 +713,10 @@ func (m *Peer) Unmarshal(dAtA []byte) error {
 		fieldNum := int32(wire >> 3)
 		wireType := int(wire & 0x7)
 		if wireType == 4 {
-			return fmt.Errorf("proto: Peer: wiretype end group for non-group")
+			return fmt.Errorf("proto: Peers: wiretype end group for non-group")
 		}
 		if fieldNum <= 0 {
-			return fmt.Errorf("proto: Peer: illegal tag %d (wire type %d)", fieldNum, wire)
+			return fmt.Errorf("proto: Peers: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		case 1:
@@ -746,10 +748,8 @@ func (m *Peer) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			if m.BzzAddress == nil {
-				m.BzzAddress = &BzzAddress{}
-			}
-			if err := m.BzzAddress.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+			m.BzzAddress = append(m.BzzAddress, &BzzAddress{})
+			if err := m.BzzAddress[len(m.BzzAddress)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex
