d7395c5d27bc25e710fcc5ba3dee1b7037f35f91
Petar Radovic <petar.radovic@gmail.com>
2020-02-06 18:15:17 +0100

    hive init & start prototype

--- pkg/hive/hive.go
+++ pkg/hive/hive.go
@@ -19,39 +19,45 @@ import (
 )
 
 const (
-	protocolName           = "hive"
-	depthStreamName        = "hive_depth"
-	depthStreamVersion     = "1.0.0"
-	peersStreamName        = "hive_peers"
-	peersStreamVersion     = "1.0.0"
-	subscribeStreamName    = "hive_subscribe"
-	subscribeStreamVersion = "1.0.0"
-	maxPeersCount          = 50
+	protocolName      = "hive"
+	initStreamName    = "hive_init"
+	initStreamVersion = "1.0.0"
+	//maxPeersCount     = 50
 )
 
-type PeerTracker interface {
+type AddressBook interface {
+	// Add peer to knows peers (address book)
 	AddPeer(overlay, underlay []byte) error // todo: introduce bzz address if needed
-	SuggestPeers() (peers []p2p.Peer, err error)
-	Subscribe() (c <-chan p2p.Peer, unsubscribe func())
-	ConnectedPeers() (peers []Peer)
+	GetPeer(overlay []byte) ([]byte, []byte)
+	ConnectedPeers() (peers []p2p.Peer)
 }
 
 // SaturationTracker tracks weather the saturation has changed
 type SaturationTracker interface {
 	Subscribe() (c <-chan struct{}, unsubscribe func())
-	Depth() (uint32, error)
+	Depth() (uint8, error)
 }
 
-type Peer struct {
-	po       uint32
-	overlay  []byte
-	underlay []byte
-}
+//type Peer struct {
+//	seenPeers map[string]bool
+//	lock      sync.Mutex // guards seen
+//	po        uint32
+//	overlay   []byte
+//	underlay  []byte
+//}
+//
+//func (p *Peer) seen(peer *Peer) (seen bool) {
+//	p.lock.Lock()
+//	defer p.lock.Unlock()
+//	_, ok := p.seenPeers[string(peer.overlay)]
+//	p.seenPeers[string(peer.overlay)] = true
+//	return ok
+//}
 
 type Service struct {
 	streamer          p2p.Streamer
 	logger            logging.Logger
-	peerTracker       PeerTracker
+	addressBook       AddressBook
 	saturationTracker SaturationTracker
 	quit              chan struct{}
 }
@@ -59,7 +65,7 @@ type Service struct {
 type Options struct {
 	Streamer          p2p.Streamer
 	Logger            logging.Logger
-	PeerTracker       PeerTracker
+	PeerTracker       AddressBook
 	SaturationTracker SaturationTracker
 	TickInterval      time.Duration
 }
@@ -69,7 +75,7 @@ func New(o Options) *Service {
 		streamer:          o.Streamer,
 		logger:            o.Logger,
 		saturationTracker: o.SaturationTracker,
-		peerTracker:       o.PeerTracker,
+		addressBook:       o.PeerTracker,
 		quit:              make(chan struct{}),
 	}
 }
@@ -79,19 +85,9 @@ func (s *Service) Protocol() p2p.ProtocolSpec {
 		Name: protocolName,
 		StreamSpecs: []p2p.StreamSpec{
 			{
-				Name:    depthStreamName,
-				Version: depthStreamVersion,
-				Handler: s.DepthHandler,
-			},
-			{
-				Name:    peersStreamName,
-				Version: peersStreamVersion,
-				Handler: s.PeersHandler,
-			},
-			{
-				Name:    peersStreamName,
-				Version: peersStreamVersion,
-				Handler: s.SubscribeHandler,
+				Name:    initStreamName,
+				Version: initStreamVersion,
+				Handler: s.InitHandler,
 			},
 		},
 	}
@@ -101,20 +97,13 @@ func (s *Service) Protocol() p2p.ProtocolSpec {
 // Hive is a skeleton of a hive function that should be called when the general hive service is started
 // this is an attempt to run these loops outside of the peer level, to avoid having them for every peer
 func (s *Service) Start() {
-	peerAddedEvents, unsubPS := s.peerTracker.Subscribe()
 	saturationChangeSignal, unsubSC := s.saturationTracker.Subscribe()
-	defer unsubPS()
 	defer unsubSC()
 
 	// todo: maybe refactor and listen separately on these events, this is just a skeleton for now
 	for {
 		select {
 		// todo: handle channel close and similar stuff, depending on the implementation of publishers
-		case peer := <-peerAddedEvents:
-			if err := s.notifyPeerAdded(peer); err != nil {
-				// todo: handle err
-				return
-			}
 		case <-saturationChangeSignal:
 			depth, err := s.saturationTracker.Depth()
 			if err != nil {
@@ -137,134 +126,92 @@ func (s *Service) Start() {
 // Init is called when the new peer is being initialized.
 // This should happen after overlay handshake is finished.
 func (s *Service) Init(ctx context.Context, peer p2p.Peer) error {
-	// todo: handle err
 	depth, err := s.saturationTracker.Depth()
 	if err != nil {
 		return err
 	}
-	stream, err := s.streamer.NewStream(ctx, peer.Address, protocolName, subscribeStreamName, subscribeStreamVersion)
+
+	stream, err := s.streamer.NewStream(ctx, peer.Address, protocolName, initStreamName, initStreamVersion)
 	if err != nil {
 		return fmt.Errorf("new stream: %w", err)
 	}
 
 	w, r := protobuf.NewWriterAndReader(stream)
 	if err := w.WriteMsg(&pb.Subscribe{
-		Depth: depth,
+		Depth:         uint32(depth),
+		PeersResponse: true,
 	}); err != nil {
 		return fmt.Errorf("write message: %w", err)
 	}
 
-	var resp pb.SubscribeResponse
-	if err := r.ReadMsg(&resp); err != nil {
-		return fmt.Errorf("read message: %w", err)
-	}
-
-	for _, address := range resp.Peers.BzzAddress {
-		if err := s.peerTracker.AddPeer(address.Overlay, address.Underlay); err != nil {
-			return err
-		}
-	}
-	//todo optional: return peers message here to avoid init on both sides, similar to handshake protocol
-	return nil
-}
-
-func (s *Service) SubscribeHandler(peer p2p.Peer, stream p2p.Stream) error {
-	w, r := protobuf.NewWriterAndReader(stream)
-	var subscribe pb.Subscribe
-	if err := r.ReadMsg(&subscribe); err != nil {
-		return fmt.Errorf("read message: %w", err)
-	}
-
-	connPeers := s.peerTracker.ConnectedPeers()
-	bzzAddresses := make([]*pb.BzzAddress, 1)
-	for _, peer := range connPeers {
-		if peer.po < subscribe.Depth {
-			// todo: should we also check seen peers here?
-			bzzAddresses = append(bzzAddresses, &pb.BzzAddress{
-				Overlay:  peer.overlay,
-				Underlay: peer.underlay, // todo: correct underlay
-			})
-		}
-	}
-
-	// todo: filter peers per depth criteria if needed
-	// todo: populate bzzAddresses
-	var peers pb.Peers
-	peers.BzzAddress = bzzAddresses[:maxPeersCount]
-	if err := w.WriteMsg(&pb.SubscribeResponse{
-		Peers: &peers,
-	}); err != nil {
-		return fmt.Errorf("write message: %w", err)
-	}
-
-	return nil
-}
-
-func (s *Service) DepthHandler(peer p2p.Peer, stream p2p.Stream) error {
-	// todo: update depth for peer
-	return nil
-}
-
-func (s *Service) PeersHandler(peer p2p.Peer, stream p2p.Stream) error {
-	_, r := protobuf.NewWriterAndReader(stream)
 	var peers pb.Peers
 	if err := r.ReadMsg(&peers); err != nil {
 		return fmt.Errorf("read message: %w", err)
 	}
 
 	for _, address := range peers.BzzAddress {
-		if err := s.peerTracker.AddPeer(address.Overlay, address.Underlay); err != nil {
+		// todo: maybe add and notify peers in a single batch
+		if err := s.addressBook.AddPeer(address.Overlay, address.Underlay); err != nil {
 			return err
 		}
+
+		if err := s.notifyPeerAdded(address.Overlay, address.Underlay); err != nil {
+			return err
+		}
+	}
+
+	//todo optional: do a 3-way style handshake here to make sure that both sides subscribed,
+	// similar to handshake protocol
+	return nil
+}
+
+func (s *Service) InitHandler(peer p2p.Peer, stream p2p.Stream) error {
+	w, r := protobuf.NewWriterAndReader(stream)
+	var subscribe pb.Subscribe
+	if err := r.ReadMsg(&subscribe); err != nil {
+		return fmt.Errorf("read message: %w", err)
+	}
+
+	var peers pb.Peers
+	// todo: populate peers response
+	if err := w.WriteMsg(&peers); err != nil {
+		return fmt.Errorf("write message: %w", err)
 	}
 
 	return nil
 }
 
 // notify new depth to all suggested peers
-func (s *Service) notifyDepthChanged(depth uint32) error {
-	peers, err := s.peerTracker.SuggestPeers()
-	if err != nil {
-		return err
-	}
+func (s *Service) notifyDepthChanged(depth uint8) error {
+	peers := s.addressBook.ConnectedPeers()
 
 	var eg errgroup.Group
 	for _, peer := range peers {
 		peer := peer
 		eg.Go(func() error {
-			return s.sendDepthMsg(peer, depth)
+			fmt.Println(peer)
+			//		return s.sendSubscribe(context.Background(), peer, depth)
+			return nil
 		})
 	}
 
 	// todo: add timeout
 	return eg.Wait()
-}
-
-func (s *Service) sendDepthMsg(peer p2p.Peer, depth uint32) error {
-	// todo:
-	return nil
 }
 
 // notify to suggested peers that the new peer has been added
-func (s *Service) notifyPeerAdded(peer p2p.Peer) error {
-	peers, err := s.peerTracker.SuggestPeers()
-	if err != nil {
-		return err
-	}
-
-	var eg errgroup.Group
-	for _, p := range peers {
-		peer := peer
-		eg.Go(func() error {
-			return s.sendPeer(p, peer)
-		})
-	}
+func (s *Service) notifyPeerAdded(overlay, underlay []byte) error {
+	//peers := s.addressBook.ConnectedPeers()
+	//
+	//var eg errgroup.Group
+	//for _, p := range peers {
+	//	peer := p
+	//	eg.Go(func() error {
+	//		return s.sendPeer(p, overlay, underlay)
+	//	})
+	//}
 
 	// todo: add timeout
-	return eg.Wait()
-}
-
-func (s *Service) sendPeer(peer p2p.Peer, peerUpdate p2p.Peer) error {
-	// todo: send peer request
+	//return eg.Wait()
 	return nil
 }
--- pkg/hive/pb/hive.pb.go
+++ pkg/hive/pb/hive.pb.go
@@ -23,7 +23,8 @@ var _ = math.Inf
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 type Subscribe struct {
-	Depth uint32 `protobuf:"varint,1,opt,name=Depth,proto3" json:"Depth,omitempty"`
+	Depth         uint32 `protobuf:"varint,1,opt,name=Depth,proto3" json:"Depth,omitempty"`
+	PeersResponse bool   `protobuf:"varint,2,opt,name=PeersResponse,proto3" json:"PeersResponse,omitempty"`
 }
 
 func (m *Subscribe) Reset()         { *m = Subscribe{} }
@@ -66,48 +67,11 @@ func (m *Subscribe) GetDepth() uint32 {
 	return 0
 }
 
-type SubscribeResponse struct {
-	Peers *Peers `protobuf:"bytes,1,opt,name=Peers,proto3" json:"Peers,omitempty"`
-}
-
-func (m *SubscribeResponse) Reset()         { *m = SubscribeResponse{} }
-func (m *SubscribeResponse) String() string { return proto.CompactTextString(m) }
-func (*SubscribeResponse) ProtoMessage()    {}
-func (*SubscribeResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_d635d1ead41ba02c, []int{1}
-}
-func (m *SubscribeResponse) XXX_Unmarshal(b []byte) error {
-	return m.Unmarshal(b)
-}
-func (m *SubscribeResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	if deterministic {
-		return xxx_messageInfo_SubscribeResponse.Marshal(b, m, deterministic)
-	} else {
-		b = b[:cap(b)]
-		n, err := m.MarshalToSizedBuffer(b)
-		if err != nil {
-			return nil, err
-		}
-		return b[:n], nil
-	}
-}
-func (m *SubscribeResponse) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_SubscribeResponse.Merge(m, src)
-}
-func (m *SubscribeResponse) XXX_Size() int {
-	return m.Size()
-}
-func (m *SubscribeResponse) XXX_DiscardUnknown() {
-	xxx_messageInfo_SubscribeResponse.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_SubscribeResponse proto.InternalMessageInfo
-
-func (m *SubscribeResponse) GetPeers() *Peers {
+func (m *Subscribe) GetPeersResponse() bool {
 	if m != nil {
-		return m.Peers
+		return m.PeersResponse
 	}
-	return nil
+	return false
 }
 
 type Peers struct {
@@ -118,7 +82,7 @@ func (m *Peers) Reset()         { *m = Peers{} }
 func (m *Peers) String() string { return proto.CompactTextString(m) }
 func (*Peers) ProtoMessage()    {}
 func (*Peers) Descriptor() ([]byte, []int) {
-	return fileDescriptor_d635d1ead41ba02c, []int{2}
+	return fileDescriptor_d635d1ead41ba02c, []int{1}
 }
 func (m *Peers) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -163,7 +127,7 @@ func (m *BzzAddress) Reset()         { *m = BzzAddress{} }
 func (m *BzzAddress) String() string { return proto.CompactTextString(m) }
 func (*BzzAddress) ProtoMessage()    {}
 func (*BzzAddress) Descriptor() ([]byte, []int) {
-	return fileDescriptor_d635d1ead41ba02c, []int{3}
+	return fileDescriptor_d635d1ead41ba02c, []int{2}
 }
 func (m *BzzAddress) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -206,76 +170,29 @@ func (m *BzzAddress) GetUnderlay() []byte {
 	return nil
 }
 
-type Depth struct {
-	Depth uint32 `protobuf:"varint,1,opt,name=Depth,proto3" json:"Depth,omitempty"`
-}
-
-func (m *Depth) Reset()         { *m = Depth{} }
-func (m *Depth) String() string { return proto.CompactTextString(m) }
-func (*Depth) ProtoMessage()    {}
-func (*Depth) Descriptor() ([]byte, []int) {
-	return fileDescriptor_d635d1ead41ba02c, []int{4}
-}
-func (m *Depth) XXX_Unmarshal(b []byte) error {
-	return m.Unmarshal(b)
-}
-func (m *Depth) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	if deterministic {
-		return xxx_messageInfo_Depth.Marshal(b, m, deterministic)
-	} else {
-		b = b[:cap(b)]
-		n, err := m.MarshalToSizedBuffer(b)
-		if err != nil {
-			return nil, err
-		}
-		return b[:n], nil
-	}
-}
-func (m *Depth) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Depth.Merge(m, src)
-}
-func (m *Depth) XXX_Size() int {
-	return m.Size()
-}
-func (m *Depth) XXX_DiscardUnknown() {
-	xxx_messageInfo_Depth.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_Depth proto.InternalMessageInfo
-
-func (m *Depth) GetDepth() uint32 {
-	if m != nil {
-		return m.Depth
-	}
-	return 0
-}
-
 func init() {
 	proto.RegisterType((*Subscribe)(nil), "pb.Subscribe")
-	proto.RegisterType((*SubscribeResponse)(nil), "pb.SubscribeResponse")
 	proto.RegisterType((*Peers)(nil), "pb.Peers")
 	proto.RegisterType((*BzzAddress)(nil), "pb.BzzAddress")
-	proto.RegisterType((*Depth)(nil), "pb.Depth")
 }
 
 func init() { proto.RegisterFile("hive.proto", fileDescriptor_d635d1ead41ba02c) }
 
 var fileDescriptor_d635d1ead41ba02c = []byte{
-	// 213 bytes of a gzipped FileDescriptorProto
+	// 201 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0xca, 0xc8, 0x2c, 0x4b,
-	0xd5, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x2a, 0x48, 0x52, 0x52, 0xe4, 0xe2, 0x0c, 0x2e,
+	0xd5, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x2a, 0x48, 0x52, 0x72, 0xe7, 0xe2, 0x0c, 0x2e,
 	0x4d, 0x2a, 0x4e, 0x2e, 0xca, 0x4c, 0x4a, 0x15, 0x12, 0xe1, 0x62, 0x75, 0x49, 0x2d, 0x28, 0xc9,
-	0x90, 0x60, 0x54, 0x60, 0xd4, 0xe0, 0x0d, 0x82, 0x70, 0x94, 0x4c, 0xb8, 0x04, 0xe1, 0x4a, 0x82,
-	0x52, 0x8b, 0x0b, 0xf2, 0xf3, 0x8a, 0x53, 0x85, 0xe4, 0xb9, 0x58, 0x03, 0x52, 0x53, 0x8b, 0x8a,
-	0xc1, 0x4a, 0xb9, 0x8d, 0x38, 0xf5, 0x0a, 0x92, 0xf4, 0xc0, 0x02, 0x41, 0x10, 0x71, 0x25, 0x73,
-	0xa8, 0x02, 0x21, 0x3d, 0x2e, 0x2e, 0xa7, 0xaa, 0x2a, 0xc7, 0x94, 0x94, 0xa2, 0xd4, 0x62, 0x90,
-	0x72, 0x66, 0x0d, 0x6e, 0x23, 0x3e, 0x90, 0x72, 0x84, 0x68, 0x10, 0x92, 0x0a, 0x25, 0x27, 0x64,
-	0xf5, 0x42, 0x12, 0x5c, 0xec, 0xfe, 0x65, 0xa9, 0x45, 0x39, 0x89, 0x95, 0x60, 0x9b, 0x78, 0x82,
-	0x60, 0x5c, 0x21, 0x29, 0x2e, 0x8e, 0xd0, 0xbc, 0x14, 0x88, 0x14, 0x13, 0x58, 0x0a, 0xce, 0x57,
-	0x92, 0x85, 0x7a, 0x04, 0xbb, 0x8f, 0x9c, 0x24, 0x4e, 0x3c, 0x92, 0x63, 0xbc, 0xf0, 0x48, 0x8e,
-	0xf1, 0xc1, 0x23, 0x39, 0xc6, 0x09, 0x8f, 0xe5, 0x18, 0x2e, 0x3c, 0x96, 0x63, 0xb8, 0xf1, 0x58,
-	0x8e, 0x21, 0x89, 0x0d, 0x1c, 0x32, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0x45, 0xd0, 0x07,
-	0x51, 0x27, 0x01, 0x00, 0x00,
+	0x90, 0x60, 0x54, 0x60, 0xd4, 0xe0, 0x0d, 0x82, 0x70, 0x84, 0x54, 0xb8, 0x78, 0x03, 0x52, 0x53,
+	0x8b, 0x8a, 0x83, 0x52, 0x8b, 0x0b, 0xf2, 0xf3, 0x8a, 0x53, 0x25, 0x98, 0x14, 0x18, 0x35, 0x38,
+	0x82, 0x50, 0x05, 0x95, 0xcc, 0xb9, 0x58, 0xc1, 0x02, 0x42, 0x7a, 0x5c, 0x5c, 0x4e, 0x55, 0x55,
+	0x8e, 0x29, 0x29, 0x45, 0xa9, 0xc5, 0xc5, 0x12, 0x8c, 0x0a, 0xcc, 0x1a, 0xdc, 0x46, 0x7c, 0x7a,
+	0x05, 0x49, 0x7a, 0x08, 0xd1, 0x20, 0x24, 0x15, 0x4a, 0x4e, 0xc8, 0xea, 0x85, 0x24, 0xb8, 0xd8,
+	0xfd, 0xcb, 0x52, 0x8b, 0x72, 0x12, 0x2b, 0xc1, 0x8e, 0xe0, 0x09, 0x82, 0x71, 0x85, 0xa4, 0xb8,
+	0x38, 0x42, 0xf3, 0x52, 0x20, 0x52, 0x4c, 0x60, 0x29, 0x38, 0xdf, 0x49, 0xe2, 0xc4, 0x23, 0x39,
+	0xc6, 0x0b, 0x8f, 0xe4, 0x18, 0x1f, 0x3c, 0x92, 0x63, 0x9c, 0xf0, 0x58, 0x8e, 0xe1, 0xc2, 0x63,
+	0x39, 0x86, 0x1b, 0x8f, 0xe5, 0x18, 0x92, 0xd8, 0xc0, 0x5e, 0x35, 0x06, 0x04, 0x00, 0x00, 0xff,
+	0xff, 0xa9, 0x10, 0xa0, 0x60, 0xf8, 0x00, 0x00, 0x00,
 }
 
 func (m *Subscribe) Marshal() (dAtA []byte, err error) {
@@ -298,45 +215,20 @@ func (m *Subscribe) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
+	if m.PeersResponse {
+		i--
+		if m.PeersResponse {
+			dAtA[i] = 1
+		} else {
+			dAtA[i] = 0
+		}
+		i--
+		dAtA[i] = 0x10
+	}
 	if m.Depth != 0 {
 		i = encodeVarintHive(dAtA, i, uint64(m.Depth))
 		i--
 		dAtA[i] = 0x8
-	}
-	return len(dAtA) - i, nil
-}
-
-func (m *SubscribeResponse) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalToSizedBuffer(dAtA[:size])
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *SubscribeResponse) MarshalTo(dAtA []byte) (int, error) {
-	size := m.Size()
-	return m.MarshalToSizedBuffer(dAtA[:size])
-}
-
-func (m *SubscribeResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
-	i := len(dAtA)
-	_ = i
-	var l int
-	_ = l
-	if m.Peers != nil {
-		{
-			size, err := m.Peers.MarshalToSizedBuffer(dAtA[:i])
-			if err != nil {
-				return 0, err
-			}
-			i -= size
-			i = encodeVarintHive(dAtA, i, uint64(size))
-		}
-		i--
-		dAtA[i] = 0xa
 	}
 	return len(dAtA) - i, nil
 }
@@ -415,34 +307,6 @@ func (m *BzzAddress) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
-func (m *Depth) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalToSizedBuffer(dAtA[:size])
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *Depth) MarshalTo(dAtA []byte) (int, error) {
-	size := m.Size()
-	return m.MarshalToSizedBuffer(dAtA[:size])
-}
-
-func (m *Depth) MarshalToSizedBuffer(dAtA []byte) (int, error) {
-	i := len(dAtA)
-	_ = i
-	var l int
-	_ = l
-	if m.Depth != 0 {
-		i = encodeVarintHive(dAtA, i, uint64(m.Depth))
-		i--
-		dAtA[i] = 0x8
-	}
-	return len(dAtA) - i, nil
-}
-
 func encodeVarintHive(dAtA []byte, offset int, v uint64) int {
 	offset -= sovHive(v)
 	base := offset
@@ -463,18 +327,8 @@ func (m *Subscribe) Size() (n int) {
 	if m.Depth != 0 {
 		n += 1 + sovHive(uint64(m.Depth))
 	}
-	return n
-}
-
-func (m *SubscribeResponse) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
-	if m.Peers != nil {
-		l = m.Peers.Size()
-		n += 1 + l + sovHive(uint64(l))
+	if m.PeersResponse {
+		n += 2
 	}
 	return n
 }
@@ -507,18 +361,6 @@ func (m *BzzAddress) Size() (n int) {
 	l = len(m.Underlay)
 	if l > 0 {
 		n += 1 + l + sovHive(uint64(l))
-	}
-	return n
-}
-
-func (m *Depth) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
-	if m.Depth != 0 {
-		n += 1 + sovHive(uint64(m.Depth))
 	}
 	return n
 }
@@ -577,64 +419,11 @@ func (m *Subscribe) Unmarshal(dAtA []byte) error {
 					break
 				}
 			}
-		default:
-			iNdEx = preIndex
-			skippy, err := skipHive(dAtA[iNdEx:])
-			if err != nil {
-				return err
+		case 2:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field PeersResponse", wireType)
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthHive
-			}
-			if (iNdEx + skippy) < 0 {
-				return ErrInvalidLengthHive
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
-func (m *SubscribeResponse) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowHive
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= uint64(b&0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: SubscribeResponse: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: SubscribeResponse: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Peers", wireType)
-			}
-			var msglen int
+			var v int
 			for shift := uint(0); ; shift += 7 {
 				if shift >= 64 {
 					return ErrIntOverflowHive
@@ -644,28 +433,12 @@ func (m *SubscribeResponse) Unmarshal(dAtA []byte) error {
 				}
 				b := dAtA[iNdEx]
 				iNdEx++
-				msglen |= int(b&0x7F) << shift
+				v |= int(b&0x7F) << shift
 				if b < 0x80 {
 					break
 				}
 			}
-			if msglen < 0 {
-				return ErrInvalidLengthHive
-			}
-			postIndex := iNdEx + msglen
-			if postIndex < 0 {
-				return ErrInvalidLengthHive
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			if m.Peers == nil {
-				m.Peers = &Peers{}
-			}
-			if err := m.Peers.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
-				return err
-			}
-			iNdEx = postIndex
+			m.PeersResponse = bool(v != 0)
 		default:
 			iNdEx = preIndex
 			skippy, err := skipHive(dAtA[iNdEx:])
@@ -874,78 +647,6 @@ func (m *BzzAddress) Unmarshal(dAtA []byte) error {
 				m.Underlay = []byte{}
 			}
 			iNdEx = postIndex
-		default:
-			iNdEx = preIndex
-			skippy, err := skipHive(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if skippy < 0 {
-				return ErrInvalidLengthHive
-			}
-			if (iNdEx + skippy) < 0 {
-				return ErrInvalidLengthHive
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
-func (m *Depth) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowHive
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= uint64(b&0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: Depth: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: Depth: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType != 0 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Depth", wireType)
-			}
-			m.Depth = 0
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowHive
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				m.Depth |= uint32(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
 		default:
 			iNdEx = preIndex
 			skippy, err := skipHive(dAtA[iNdEx:])
