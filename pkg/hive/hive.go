7fa0101415893145ba7f4f126349f7ae8e7c3065
Petar Radovic <petar.radovic@gmail.com>
2020-02-06 18:42:01 +0100

    hive peers and subscribe handlers prototype

--- pkg/hive/hive.go
+++ pkg/hive/hive.go
@@ -9,26 +9,28 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/ethersphere/bee/pkg/hive/pb"
-	"github.com/ethersphere/bee/pkg/p2p/protobuf"
-
 	"golang.org/x/sync/errgroup"
 
+	"github.com/ethersphere/bee/pkg/hive/pb"
 	"github.com/ethersphere/bee/pkg/logging"
 	"github.com/ethersphere/bee/pkg/p2p"
+	"github.com/ethersphere/bee/pkg/p2p/protobuf"
 )
 
 const (
-	protocolName      = "hive"
-	initStreamName    = "hive_init"
-	initStreamVersion = "1.0.0"
+	protocolName           = "hive"
+	initStreamName         = "hive_init"
+	initStreamVersion      = "1.0.0"
+	subscribeStreamName    = "hive_init"
+	subscribeStreamVersion = "1.0.0"
+	peersStreamName        = "hive_init"
+	peersStreamVersion     = "1.0.0"
 	//maxPeersCount     = 50
 )
 
 type AddressBook interface {
 	// Add peer to knows peers (address book)
 	AddPeer(overlay, underlay []byte) error // todo: introduce bzz address if needed
-	GetPeer(overlay []byte) ([]byte, []byte)
 	ConnectedPeers() (peers []p2p.Peer)
 }
 
@@ -37,22 +39,6 @@ type SaturationTracker interface {
 	Subscribe() (c <-chan struct{}, unsubscribe func())
 	Depth() (uint8, error)
 }
-
-//type Peer struct {
-//	seenPeers map[string]bool
-//	lock      sync.Mutex // guards seen
-//	po        uint32
-//	overlay   []byte
-//	underlay  []byte
-//}
-//
-//func (p *Peer) seen(peer *Peer) (seen bool) {
-//	p.lock.Lock()
-//	defer p.lock.Unlock()
-//	_, ok := p.seenPeers[string(peer.overlay)]
-//	p.seenPeers[string(peer.overlay)] = true
-//	return ok
-//}
 
 type Service struct {
 	streamer          p2p.Streamer
@@ -100,6 +86,8 @@ func (s *Service) Start() {
 	saturationChangeSignal, unsubSC := s.saturationTracker.Subscribe()
 	defer unsubSC()
 
+	ctx := context.Background()
+
 	// todo: maybe refactor and listen separately on these events, this is just a skeleton for now
 	for {
 		select {
@@ -110,7 +98,7 @@ func (s *Service) Start() {
 				// todo: handle err
 				return
 			}
-			if err := s.notifyDepthChanged(depth); err != nil {
+			if err := s.notifyDepthChanged(ctx, depth); err != nil {
 				// todo: handle err
 				return
 			}
@@ -118,7 +106,6 @@ func (s *Service) Start() {
 		// todo: cancel, quit, etc...
 		case <-s.quit:
 			return
-
 		}
 	}
 }
@@ -155,13 +142,12 @@ func (s *Service) Init(ctx context.Context, peer p2p.Peer) error {
 			return err
 		}
 
-		if err := s.notifyPeerAdded(address.Overlay, address.Underlay); err != nil {
+		if err := s.notifyPeerAdded(ctx, address.Overlay, address.Underlay); err != nil {
 			return err
 		}
 	}
 
-	//todo optional: do a 3-way style handshake here to make sure that both sides subscribed,
-	// similar to handshake protocol
+	// todo: maybe do a 3-way style handshake here to make sure that both sides subscribed, similar to handshake protocol.
 	return nil
 }
 
@@ -182,15 +168,25 @@ func (s *Service) InitHandler(peer p2p.Peer, stream p2p.Stream) error {
 }
 
 // notify new depth to all suggested peers
-func (s *Service) notifyDepthChanged(depth uint8) error {
+func (s *Service) notifyDepthChanged(ctx context.Context, depth uint8) error {
 	peers := s.addressBook.ConnectedPeers()
 
 	var eg errgroup.Group
 	for _, peer := range peers {
 		peer := peer
 		eg.Go(func() error {
-			fmt.Println(peer)
-			//		return s.sendSubscribe(context.Background(), peer, depth)
+			stream, err := s.streamer.NewStream(ctx, peer.Address, protocolName, subscribeStreamName, subscribeStreamVersion)
+			if err != nil {
+				return fmt.Errorf("new stream: %w", err)
+			}
+
+			w, _ := protobuf.NewWriterAndReader(stream)
+			if err := w.WriteMsg(&pb.Subscribe{
+				Depth:         uint32(depth),
+				PeersResponse: false,
+			}); err != nil {
+				return fmt.Errorf("write message: %w", err)
+			}
 			return nil
 		})
 	}
@@ -200,18 +196,31 @@ func (s *Service) notifyDepthChanged(depth uint8) error {
 }
 
 // notify to suggested peers that the new peer has been added
-func (s *Service) notifyPeerAdded(overlay, underlay []byte) error {
-	//peers := s.addressBook.ConnectedPeers()
-	//
-	//var eg errgroup.Group
-	//for _, p := range peers {
-	//	peer := p
-	//	eg.Go(func() error {
-	//		return s.sendPeer(p, overlay, underlay)
-	//	})
-	//}
+func (s *Service) notifyPeerAdded(ctx context.Context, overlay, underlay []byte) error {
+	peers := s.addressBook.ConnectedPeers()
+
+	var eg errgroup.Group
+	for _, p := range peers {
+		// todo: filter peers based on po & pot function
+		peer := p
+		eg.Go(func() error {
+			stream, err := s.streamer.NewStream(ctx, peer.Address, protocolName, peersStreamName, peersStreamVersion)
+			if err != nil {
+				return fmt.Errorf("new stream: %w", err)
+			}
+			w, _ := protobuf.NewWriterAndReader(stream)
+
+			if err := w.WriteMsg(&pb.Peers{BzzAddress: []*pb.BzzAddress{{
+				Overlay:  overlay,
+				Underlay: underlay,
+			}}}); err != nil {
+				return fmt.Errorf("write message: %w", err)
+			}
+
+			return nil
+		})
+	}
 
 	// todo: add timeout
-	//return eg.Wait()
-	return nil
+	return eg.Wait()
 }
