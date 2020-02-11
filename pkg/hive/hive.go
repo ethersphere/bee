318ecfc162072b537a8a69b875b4244662bc7547
Petar Radovic <petar.radovic@gmail.com>
2020-02-11 15:14:09 +0100

    init implementation

--- pkg/hive/hive.go
+++ pkg/hive/hive.go
@@ -5,34 +5,55 @@
 package hive
 
 import (
+	"context"
 	"errors"
+	"fmt"
+	"time"
 
+	"github.com/ethersphere/bee/pkg/hive/pb"
 	"github.com/ethersphere/bee/pkg/logging"
 	"github.com/ethersphere/bee/pkg/p2p"
+	"github.com/ethersphere/bee/pkg/p2p/protobuf"
+	"github.com/ethersphere/bee/pkg/swarm"
 )
 
 const (
 	protocolName             = "hive"
+	protocolVersion          = "1.0.0"
 	peersStreamName          = "peers"
 	peersBroadcastStreamName = "peers_broadcast"
+	maxPO                    = 7
+	messageTimeout           = 5 * time.Second // maximum allowed time for a message to be read or written.
 )
 
 type Service struct {
-	streamer p2p.Streamer
-	logger   logging.Logger
-	quit     chan struct{}
+	streamer          p2p.Streamer
+	connectionManager ConnectionManager
+	logger            logging.Logger
+	quit              chan struct{}
 }
 
 type Options struct {
-	Streamer p2p.Streamer
-	Logger   logging.Logger
+	Streamer          p2p.Streamer
+	ConnectionManager ConnectionManager
+	Logger            logging.Logger
+}
+
+type BzzAddress struct {
+	Overlay, Underlay swarm.Address
+}
+
+type ConnectionManager interface {
+	// todo: this can be the libp2p.Connect
+	Connect(underlay swarm.Address) error
 }
 
 func New(o Options) *Service {
 	return &Service{
-		streamer: o.Streamer,
-		logger:   o.Logger,
-		quit:     make(chan struct{}),
+		streamer:          o.Streamer,
+		logger:            o.Logger,
+		connectionManager: o.ConnectionManager,
+		quit:              make(chan struct{}),
 	}
 }
 
@@ -56,14 +77,58 @@ func (s *Service) Protocol() p2p.ProtocolSpec {
 // Init is called when the new peer is being initialized.
 // This should happen after overlay handshake is finished.
 func (s *Service) Init(peer p2p.Peer) error {
-	// todo: send get peers request for each bin and receive peers responses
-	return errors.New("not implemented")
+	for i := 0; i < maxPO; i++ {
+		// todo: figure out the limit
+		resp, err := s.getPeers(peer, i, 10)
+		if err != nil {
+			return err
+		}
+
+		for _, newPeer := range resp {
+			if err := s.connectionManager.Connect(newPeer.Underlay); err != nil {
+				continue
+			}
+		}
+	}
+
+	return nil
 }
 
 // BroadcastPeers broadcasts the provided list of peers to the provided peer.
 func (s *Service) BroadcastPeers(peer p2p.Peer, peers []p2p.Peer) error {
 	// todo: create Peers request and broadcast over peers_broadcast stream
 	return errors.New("not implemented")
+}
+
+func (s *Service) getPeers(peer p2p.Peer, bin, limit int) ([]BzzAddress, error) {
+	stream, err := s.streamer.NewStream(context.Background(), peer.Address, protocolName, protocolVersion, peersStreamName)
+	if err != nil {
+		return nil, fmt.Errorf("new stream: %w", err)
+	}
+	defer stream.Close()
+
+	w, r := protobuf.NewWriterAndReader(stream)
+	if err := w.WriteMsg(&pb.GetPeers{
+		Bin:   uint32(bin),
+		Limit: uint32(limit),
+	}); err != nil {
+		return nil, fmt.Errorf("write getPeers message: %w", err)
+	}
+
+	var peersResponse pb.Peers
+	if err := r.ReadMsgWithTimeout(messageTimeout, &peersResponse); err != nil {
+		return nil, fmt.Errorf("read getPeers message: %w", err)
+	}
+
+	var res []BzzAddress
+	for _, peer := range peersResponse.Peers {
+		res = append(res, BzzAddress{
+			Overlay:  swarm.NewAddress(peer.Overlay),
+			Underlay: swarm.NewAddress(peer.Underlay),
+		})
+	}
+
+	return res, nil
 }
 
 func (s *Service) peersHandler(peer p2p.Peer, stream p2p.Stream) error {
