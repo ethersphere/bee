92712df887a7c5f6fd9773b8bf5b2c7bff577b32
acud <theman@elad.im>
2020-02-11 22:02:08 +0800

    fix retrieval test

--- pkg/retrieval/retrieval.go
+++ pkg/retrieval/retrieval.go
@@ -14,6 +14,7 @@ import (
 	pb "github.com/ethersphere/bee/pkg/retrieval/pb"
 	"github.com/ethersphere/bee/pkg/storage"
 	"github.com/ethersphere/bee/pkg/swarm"
+	"github.com/ethersphere/bee/pkg/topology"
 )
 
 const (
