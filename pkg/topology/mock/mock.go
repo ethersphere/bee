898f06ea5847056d07ce97f04ced9144c5d0ecb8
Petar Radovic <petar.radovic@gmail.com>
2020-03-25 10:05:13 +0100

    toplogy mock err

--- pkg/topology/mock/mock.go
+++ pkg/topology/mock/mock.go
@@ -10,13 +10,22 @@ import (
 
 type TopologyDriver struct {
 	peers []swarm.Address
+	err   error
 }
 
 func NewTopologyDriver() *TopologyDriver {
 	return &TopologyDriver{}
 }
 
+func (d *TopologyDriver) SetErr(err error) {
+	d.err = err
+}
+
 func (d *TopologyDriver) AddPeer(addr swarm.Address) error {
+	if d.err != nil {
+		return d.err
+	}
+
 	d.peers = append(d.peers, addr)
 	return nil
 }
