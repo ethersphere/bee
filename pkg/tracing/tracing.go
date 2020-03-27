f2d50403112e6314eb1faec2d28f08d84126c98f
Janos Guljas <janos@resenje.org>
2020-03-27 12:04:56 +0100

    add comments to tracing package

--- pkg/tracing/tracing.go
+++ pkg/tracing/tracing.go
@@ -19,22 +19,31 @@ import (
 )
 
 var (
+	// ErrContextNotFound is returned when tracing context is not present
+	// in p2p Headers or context.
 	ErrContextNotFound = errors.New("tracing context not found")
 
+	// contextKey is used to reference a tracing context span as context value.
 	contextKey = struct{}{}
+	// noopTracer is the tracer that does nothing to handle a nil Tracer usage.
 	noopTracer = &Tracer{tracer: new(opentracing.NoopTracer)}
 )
 
+// Tracer connect to a tracing server and handles tracing spans and contexts
+// by using opentracing Tracer.
 type Tracer struct {
 	tracer opentracing.Tracer
 }
 
+// Options are optional parameters for Tracer constructor.
 type Options struct {
 	Enabled     bool
 	Endpoint    string
 	ServiceName string
 }
 
+// NewTracer creates a new Tracer and returns a closer which needs to be closed
+// when the Tracer is no longer used to flush remaining traces.
 func NewTracer(o *Options) (*Tracer, io.Closer, error) {
 	if o == nil {
 		o = new(Options)
@@ -61,6 +70,8 @@ func NewTracer(o *Options) (*Tracer, io.Closer, error) {
 	return &Tracer{tracer: t}, closer, nil
 }
 
+// StartSpanFromContext starts a new tracing span that is either a root one or a
+// child of existing one from the provided Context.
 func (t *Tracer) StartSpanFromContext(ctx context.Context, operationName string, opts ...opentracing.StartSpanOption) (opentracing.Span, context.Context) {
 	if t == nil {
 		t = noopTracer
@@ -76,6 +87,9 @@ func (t *Tracer) StartSpanFromContext(ctx context.Context, operationName string,
 	return span, WithContext(ctx, span.Context())
 }
 
+// AddContextHeader adds a tracing span context to provided p2p Headers from
+// the go context. If the tracing span context is not present in go context,
+// ErrContextNotFound is returned.
 func (t *Tracer) AddContextHeader(ctx context.Context, headers p2p.Headers) error {
 	if t == nil {
 		t = noopTracer
@@ -100,6 +114,8 @@ func (t *Tracer) AddContextHeader(ctx context.Context, headers p2p.Headers) erro
 	return nil
 }
 
+// FromHeaders returns tracing span context from p2p Headers. If the tracing
+// span context is not present in go context, ErrContextNotFound is returned.
 func (t *Tracer) FromHeaders(headers p2p.Headers) (opentracing.SpanContext, error) {
 	if t == nil {
 		t = noopTracer
@@ -120,6 +136,9 @@ func (t *Tracer) FromHeaders(headers p2p.Headers) (opentracing.SpanContext, erro
 	return c, nil
 }
 
+// WithContextFromHeaders returns a new context with injected tracing span
+// context if they are found in p2p Headers. If the tracing span context is not
+// present in go context, ErrContextNotFound is returned.
 func (t *Tracer) WithContextFromHeaders(ctx context.Context, headers p2p.Headers) (context.Context, error) {
 	if t == nil {
 		t = noopTracer
@@ -132,10 +151,13 @@ func (t *Tracer) WithContextFromHeaders(ctx context.Context, headers p2p.Headers
 	return WithContext(ctx, c), nil
 }
 
+// WithContext adds tracing span context to go context.
 func WithContext(ctx context.Context, c opentracing.SpanContext) context.Context {
 	return context.WithValue(ctx, contextKey, c)
 }
 
+// FromContext return tracing span context from go context. If the tracing span
+// context is not present in go context, nil is returned.
 func FromContext(ctx context.Context) opentracing.SpanContext {
 	c, ok := ctx.Value(contextKey).(opentracing.SpanContext)
 	if !ok {
