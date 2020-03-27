a9201b91a9a45c30a6d1300d5d6fcc7084d805db
Janos Guljas <janos@resenje.org>
2020-03-27 12:15:49 +0100

    add missing comments in p2p package

--- pkg/p2p/p2p.go
+++ pkg/p2p/p2p.go
@@ -12,6 +12,7 @@ import (
 	ma "github.com/multiformats/go-multiaddr"
 )
 
+// Service provides methods to handle p2p Peers and Protocols.
 type Service interface {
 	AddProtocol(ProtocolSpec) error
 	Connect(ctx context.Context, addr ma.Multiaddr) (overlay swarm.Address, err error)
@@ -19,10 +20,12 @@ type Service interface {
 	Peers() []Peer
 }
 
+// Streamer is able to create a new Stream.
 type Streamer interface {
 	NewStream(ctx context.Context, address swarm.Address, h Headers, protocol, version, stream string) (Stream, error)
 }
 
+// Stream represent a bidirectional data Stream.
 type Stream interface {
 	io.ReadWriter
 	io.Closer
@@ -30,34 +33,45 @@ type Stream interface {
 	FullClose() error
 }
 
+// ProtocolSpec defines a collection of Stream specifications with handlers.
 type ProtocolSpec struct {
 	Name        string
 	Version     string
 	StreamSpecs []StreamSpec
 }
 
+// StreamSpec defines a Stream handling within the protocol.
 type StreamSpec struct {
 	Name    string
 	Handler HandlerFunc
 	Headler HeadlerFunc
 }
 
+// Peer holds information about a Peer.
 type Peer struct {
 	Address swarm.Address
 }
 
+// HandlerFunc handles a received Stream from a Peer.
 type HandlerFunc func(context.Context, Peer, Stream) error
 
+// HandlerMiddleware decorates a HandlerFunc by returning a new one.
 type HandlerMiddleware func(HandlerFunc) HandlerFunc
 
+// HeadlerFunc is returning response headers based on the received request
+// headers.
 type HeadlerFunc func(Headers) Headers
 
+// Headers represents a collection of p2p header key value pairs.
 type Headers map[string][]byte
 
+// Common header names.
 const (
 	HeaderNameTracingSpanContext = "tracing-span-context"
 )
 
+// NewSwarmStreamName constructs a libp2p compatible stream name out of
+// protocol name and version and stream name.
 func NewSwarmStreamName(protocol, version, stream string) string {
 	return "/swarm/" + protocol + "/" + version + "/" + stream
 }
