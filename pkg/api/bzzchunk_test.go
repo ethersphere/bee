87e907d9112904fba37ee449c8710ce52dd21e01
acud <12988138+acud@users.noreply.github.com>
2020-03-31 12:54:05 +0200

    api: add bzzchunk tests

--- pkg/api/bzzchunk_test.go
+++ pkg/api/bzzchunk_test.go
@@ -5,9 +5,14 @@
 package api_test
 
 import (
+	"bytes"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"testing"
+
+	"github.com/ethersphere/bee/pkg/storage/mock"
+	"github.com/ethersphere/bee/pkg/swarm"
 )
 
 func TestChunkUpload(t *testing.T) {
@@ -20,21 +25,83 @@ func TestChunkUpload(t *testing.T) {
 
 	// download the chunk in both cases, in the first one - make sure it is retrievable
 	// in the second - that it isnt
+
+	const requestPath = "/bzz-chunk"
+
+	validHash, err := swarm.ParseHexAddress("aabbcc")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	invalidHash, err := swarm.ParseHexAddress("bbccdd")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	validContent := []byte("bbaatt")
+	invalidContent := []byte("bbaattss")
+
+	validContentBuffer := bytes.NewBuffer(validContent)
+	invalidContentBuffer := bytes.NewBuffer(invalidContent)
+
+	validatorF := func(addr swarm.Address, data []byte) bool {
+		if !addr.Equal(validHash) {
+			return false
+
+		}
+		if !bytes.Equal(data, validContent) {
+			return false
+		}
+		return true
+	}
+
+	mockValidatingStorer := mock.NewValidatingStorer(validatorF)
+	client, cleanup := newTestServer(t, testServerOptions{
+		Storer: mockValidatingStorer,
+	})
+	defer cleanup()
+
+	t.Run("ok", func(t *testing.T) {
+		res := request(t, client, http.MethodPost, requestPath+"/"+validHash.String(), validContentBuffer, 200)
+		if res == nil {
+			t.Fatal("err nil resposen")
+		}
+	})
+
+	t.Run("invalid hash", func(t *testing.T) {
+		res := request(t, client, http.MethodPost, requestPath+"/"+invalidHash.String(), validContentBuffer, 200)
+		if res == nil {
+			t.Fatal("err nil resposen")
+		}
+	})
+
+	t.Run("invalid content", func(t *testing.T) {
+		res := request(t, client, http.MethodPost, requestPath+"/"+invalidHash.String(), invalidContentBuffer, 200)
+		if res == nil {
+			t.Fatal("err nil resposen")
+		}
+	})
 }
 
-func request(t *testing.T, method, url string, body io.Reader, responseCode int) *http.Response {
+func request(t *testing.T, client *http.Client, method, url string, body io.Reader, responseCode int) *http.Response {
 	t.Helper()
 
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
 		t.Fatal(err)
 	}
-	resp, err := http.DefaultClient.Do(req)
+	resp, err := client.Do(req)
 	if err != nil {
 		t.Fatal(err)
 	}
 	if resp.StatusCode != responseCode {
 		t.Errorf("got response status %s, want %v %s", resp.Status, responseCode, http.StatusText(responseCode))
 	}
+
+	_, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	return resp
 }
