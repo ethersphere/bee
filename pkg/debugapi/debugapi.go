4c75c749e61b65339f7a34d49eba882a78de18b4
Janoš Guljaš <janos@users.noreply.github.com>
2020-04-08 20:46:42 +0200

    add debug api overlay address endpoint (#78)

--- pkg/debugapi/debugapi.go
+++ pkg/debugapi/debugapi.go
@@ -10,6 +10,7 @@ import (
 	"github.com/ethersphere/bee/pkg/addressbook"
 	"github.com/ethersphere/bee/pkg/logging"
 	"github.com/ethersphere/bee/pkg/p2p"
+	"github.com/ethersphere/bee/pkg/swarm"
 	"github.com/ethersphere/bee/pkg/topology"
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -27,6 +28,7 @@ type server struct {
 }
 
 type Options struct {
+	Overlay        swarm.Address
 	P2P            p2p.Service
 	Addressbook    addressbook.GetPutter
 	TopologyDriver topology.PeerAdder
