47aa01751ed734e2e09b38452814097831b211ac
nolash <dev@holbrook.no>
2020-04-20 20:44:45 +0200

    Add process loop for levels in joiner

--- pkg/file/joiner/internal/job.go
+++ pkg/file/joiner/internal/job.go
@@ -3,8 +3,11 @@ package internal
 import (
 	"context"
 	"encoding/binary"
+	"io"
 	"math"
+	"os"
 
+	"github.com/ethersphere/bee/pkg/logging"
 	"github.com/ethersphere/bee/pkg/storage"
 	"github.com/ethersphere/bee/pkg/swarm"
 )
@@ -17,7 +20,8 @@ type SimpleJoinerJob struct {
 	readCount int64
 	cursors [9]int
 	data [9][]byte
-	chunkC chan []byte
+	dataC chan []byte
+	logger logging.Logger
 }
 
 func NewSimpleJoinerJob(ctx context.Context, store storage.Storer, rootChunk swarm.Chunk) *SimpleJoinerJob {
@@ -28,17 +32,68 @@ func NewSimpleJoinerJob(ctx context.Context, store storage.Storer, rootChunk swa
 		store: store,
 		spanLength: int64(spanLength),
 		levelCount: levelCount,
-		chunkC: make(chan []byte),
+		dataC: make(chan []byte),
+		logger: logging.New(os.Stderr, 5),
 	}
 
 	// keeping the data level as 0 index matches the file hasher solution
 	j.data[levelCount-1] = rootChunk.Data()[8:]
+
+	go func() {
+		err := j.process(levelCount-1, rootChunk.Address())
+		if err != nil {
+			j.logger.Errorf("error in process: %v", err)
+			close(j.dataC)
+		}
+	}()
+
 	return j
+}
+
+func (j *SimpleJoinerJob) process(level int, reference swarm.Address) error {
+	for ;j.readCount < j.spanLength; {
+		err := j.next(level, reference)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (j *SimpleJoinerJob) next(level int, reference swarm.Address) error {
+	j.logger.Debugf("next get: %v", reference)
+	ch, err := j.store.Get(j.ctx, storage.ModeGetRequest, reference)
+	if err != nil {
+		return err
+	}
+
+	if level > 0 {
+		if len(j.data[level]) == j.cursors[level]  {
+			j.data[level] = ch.Data()[8:]
+			j.cursors[level] = 0
+		}
+		cursor := j.cursors[level]
+		nextAddress := swarm.NewAddress(j.data[level][cursor:cursor+swarm.SectionSize])
+		err := j.next(level - 1, nextAddress)
+		if err != nil {
+			return err
+		}
+		j.cursors[level] += swarm.SectionSize
+	} else {
+		data := ch.Data()[8:]
+		j.dataC <- data
+		j.readCount += int64(len(data))
+	}
+	return nil
 }
 
 func (j *SimpleJoinerJob) Read(b []byte) (n int, err error) {
 	select {
-	case b := <-j.chunkC:
+	case b, ok := <-j.dataC:
+		if !ok {
+			j.logger.Debug("eof")
+			return 0, io.EOF
+		}
 		return len(b), nil
 	case <-j.ctx.Done():
 		return 0, j.ctx.Err()
--- pkg/file/joiner/internal/job_test.go
+++ pkg/file/joiner/internal/job_test.go
@@ -2,32 +2,44 @@ package internal_test
 
 import (
 	"context"
+	"os"
 	"testing"
 	"time"
 
 	"github.com/ethersphere/bee/pkg/file/joiner/internal"
 	filetest "github.com/ethersphere/bee/pkg/file/testing"
+	"github.com/ethersphere/bee/pkg/logging"
 	"github.com/ethersphere/bee/pkg/storage"
 	"github.com/ethersphere/bee/pkg/storage/mock"
 	"github.com/ethersphere/bee/pkg/swarm"
 )
 
-
-func TestSimpleJoinerJob(t *testing.T) {
+func TestSimpleJoinerJobRead(t *testing.T) {
 	store := mock.NewStorer()
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
+	logger := logging.New(os.Stderr, 5)
 	rootChunk := filetest.GenerateTestRandomFileChunk(swarm.ZeroAddress, swarm.ChunkSize*2, swarm.SectionSize*2)
+	store.Put(ctx, storage.ModePutUpload, rootChunk)
+	logger.Debugf("put rootchunk %v", rootChunk)
 
-	firstAddress := swarm.NewAddress(rootChunk.Data()[:swarm.SectionSize])
+	firstAddress := swarm.NewAddress(rootChunk.Data()[8:swarm.SectionSize+8])
 	firstChunk := filetest.GenerateTestRandomFileChunk(firstAddress, swarm.ChunkSize, swarm.ChunkSize)
-	store.Put(ctx, storage.ModePutUpload, firstChunk)
+	_, err := store.Put(ctx, storage.ModePutUpload, firstChunk)
+	if err != nil {
+		t.Fatal(err)
+	}
+	logger.Debugf("put firstchunk %v", firstChunk)
 
-	secondAddress := swarm.NewAddress(rootChunk.Data()[swarm.SectionSize:])
+	secondAddress := swarm.NewAddress(rootChunk.Data()[swarm.SectionSize+8:])
 	secondChunk := filetest.GenerateTestRandomFileChunk(secondAddress, swarm.ChunkSize, swarm.ChunkSize)
-	store.Put(ctx, storage.ModePutUpload, secondChunk)
+	_, err = store.Put(ctx, storage.ModePutUpload, secondChunk)
+	if err != nil {
+		t.Fatal(err)
+	}
+	logger.Debugf("put secondtchunk %v", secondChunk)
 
 	j := internal.NewSimpleJoinerJob(ctx, store, rootChunk)
 
