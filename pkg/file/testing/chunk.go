1db9d3b75186786187b427801b3a5415121ffa74
nolash <dev@holbrook.no>
2020-04-22 14:36:42 +0200

    Skip math/rand security check in 'deepsource'

--- pkg/file/testing/chunk.go
+++ pkg/file/testing/chunk.go
@@ -10,10 +10,10 @@ import (
 func GenerateTestRandomFileChunk(address swarm.Address, spanLength int, dataSize int) swarm.Chunk {
 	data := make([]byte, dataSize+8)
 	binary.LittleEndian.PutUint64(data, uint64(spanLength))
-	_, _ = rand.Read(data[8:])
+	_, _ = rand.Read(data[8:]) // # skipcq: GSC-G404
 	key := make([]byte, swarm.SectionSize)
 	if address.IsZero() {
-		_, _ = rand.Read(key)
+		_, _ = rand.Read(key) // # skipcq: GSC-G404
 	} else {
 		copy(key, address.Bytes())
 	}
