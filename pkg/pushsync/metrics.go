918c2371436867736aac5082b767b5e9421e4aa8
Zahoor Mohamed <zahoor@ethswarm.org>
2020-05-07 21:23:28 +0530

    [bee #118] Receipts for pushsync (#122)
    * pushsync: add receipt forwarding logic
    
    Co-authored-by: acud <12988138+acud@users.noreply.github.com>

--- pkg/pushsync/metrics.go
+++ pkg/pushsync/metrics.go
@@ -14,29 +14,58 @@ type metrics struct {
 	// to be able to return them by Metrics()
 	// using reflection
 
-	SendChunkCounter      prometheus.Counter
-	SendChunkTimer        prometheus.Counter
-	SendChunkErrorCounter prometheus.Counter
-	MarkAndSweepTimer     prometheus.Counter
-
-	ChunksInBatch prometheus.Gauge
+	TotalChunksToBeSentCounter prometheus.Counter
+	TotalChunksSynced          prometheus.Counter
+	TotalChunksStoredInDB      prometheus.Counter
+	ChunksSentCounter          prometheus.Counter
+	ChunksReceivedCounter      prometheus.Counter
+	SendChunkErrorCounter      prometheus.Counter
+	ReceivedChunkErrorCounter  prometheus.Counter
+	ReceiptsReceivedCounter    prometheus.Counter
+	ReceiptsSentCounter        prometheus.Counter
+	SendReceiptErrorCounter    prometheus.Counter
+	ReceiveReceiptErrorCounter prometheus.Counter
+	ErrorSettingChunkToSynced  prometheus.Counter
+	RetriesExhaustedCounter    prometheus.Counter
+	InvalidReceiptReceived     prometheus.Counter
+	SendChunkTimer             prometheus.Histogram
+	ReceiptRTT                 prometheus.Histogram
+	MarkAndSweepTimer          prometheus.Histogram
 }
 
 func newMetrics() metrics {
 	subsystem := "pushsync"
 
 	return metrics{
-		SendChunkCounter: prometheus.NewCounter(prometheus.CounterOpts{
+		TotalChunksToBeSentCounter: prometheus.NewCounter(prometheus.CounterOpts{
 			Namespace: m.Namespace,
 			Subsystem: subsystem,
-			Name:      "send_chunk",
+			Name:      "total_chunk_to_be_sent",
 			Help:      "Total chunks to be sent.",
 		}),
-		SendChunkTimer: prometheus.NewCounter(prometheus.CounterOpts{
+		TotalChunksSynced: prometheus.NewCounter(prometheus.CounterOpts{
 			Namespace: m.Namespace,
 			Subsystem: subsystem,
-			Name:      "send_chunk_time_taken",
-			Help:      "Total time taken to send a chunk.",
+			Name:      "total_chunk_synced",
+			Help:      "Total chunks synced succesfully with valid receipts.",
+		}),
+		TotalChunksStoredInDB: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: m.Namespace,
+			Subsystem: subsystem,
+			Name:      "total_chunk_stored_in_DB",
+			Help:      "Total chunks stored succesfully in local store.",
+		}),
+		ChunksSentCounter: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: m.Namespace,
+			Subsystem: subsystem,
+			Name:      "sent_chunk",
+			Help:      "Total chunks sent.",
+		}),
+		ChunksReceivedCounter: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: m.Namespace,
+			Subsystem: subsystem,
+			Name:      "received_chunk",
+			Help:      "Total chunks received.",
 		}),
 		SendChunkErrorCounter: prometheus.NewCounter(prometheus.CounterOpts{
 			Namespace: m.Namespace,
@@ -44,18 +73,75 @@ func newMetrics() metrics {
 			Name:      "send_chunk_error",
 			Help:      "Total no of time error received while sending chunk.",
 		}),
-		MarkAndSweepTimer: prometheus.NewCounter(prometheus.CounterOpts{
+		ReceivedChunkErrorCounter: prometheus.NewCounter(prometheus.CounterOpts{
 			Namespace: m.Namespace,
 			Subsystem: subsystem,
-			Name:      "mark_and_sweep_time",
-			Help:      "Total time spent in mark and sweep.",
+			Name:      "received_chunk_error",
+			Help:      "Total no of time error received while receiving chunk.",
+		}),
+		ReceiptsReceivedCounter: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: m.Namespace,
+			Subsystem: subsystem,
+			Name:      "received_receipts",
+			Help:      "Total no of times receipts received.",
+		}),
+		ReceiptsSentCounter: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: m.Namespace,
+			Subsystem: subsystem,
+			Name:      "sent_receipts",
+			Help:      "Total no of times receipts are sent.",
+		}),
+		SendReceiptErrorCounter: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: m.Namespace,
+			Subsystem: subsystem,
+			Name:      "sent_receipts_error",
+			Help:      "Total no of times receipts were sent and error was encountered.",
+		}),
+		ReceiveReceiptErrorCounter: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: m.Namespace,
+			Subsystem: subsystem,
+			Name:      "receive_receipt_error",
+			Help:      "Total no of time error received while receiving receipt.",
 		}),
 
-		ChunksInBatch: prometheus.NewGauge(prometheus.GaugeOpts{
+		ErrorSettingChunkToSynced: prometheus.NewCounter(prometheus.CounterOpts{
 			Namespace: m.Namespace,
 			Subsystem: subsystem,
-			Name:      "chunks_in_batch",
-			Help:      "Chunks in batch at a given time.",
+			Name:      "cannot_set_chunk_sync_in_DB",
+			Help:      "Total no of times the chunk cannot be synced in DB.",
+		}),
+		RetriesExhaustedCounter: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: m.Namespace,
+			Subsystem: subsystem,
+			Name:      "chunk_retries_exhausted",
+			Help:      "CHunk retries exhausted.",
+		}),
+		InvalidReceiptReceived: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: m.Namespace,
+			Subsystem: subsystem,
+			Name:      "invalid_receipt_receipt",
+			Help:      "Invalid receipt received from peer.",
+		}),
+		SendChunkTimer: prometheus.NewHistogram(prometheus.HistogramOpts{
+			Namespace: m.Namespace,
+			Subsystem: subsystem,
+			Name:      "send_chunk_time_histogram",
+			Help:      "Histogram for Time taken to send a chunk.",
+			Buckets:   []float64{0.1, 0.25, 0.5, 1, 2.5, 5, 10, 60},
+		}),
+		MarkAndSweepTimer: prometheus.NewHistogram(prometheus.HistogramOpts{
+			Namespace: m.Namespace,
+			Subsystem: subsystem,
+			Name:      "mark_and_sweep_time_histogram",
+			Help:      "Histogram of time spent in mark and sweep.",
+			Buckets:   []float64{0.1, 0.25, 0.5, 1, 2.5, 5, 10, 60},
+		}),
+		ReceiptRTT: prometheus.NewHistogram(prometheus.HistogramOpts{
+			Namespace: m.Namespace,
+			Subsystem: subsystem,
+			Name:      "receipt_rtt_histogram",
+			Help:      "Histogram of RTT for receiving receipt for a pushed chunk.",
+			Buckets:   []float64{0.1, 0.25, 0.5, 1, 2.5, 5, 10, 60},
 		}),
 	}
 }
--- pkg/pushsync/pushsync.go
+++ pkg/pushsync/pushsync.go
@@ -8,7 +8,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io"
 	"time"
 
 	"github.com/ethersphere/bee/pkg/logging"
@@ -27,12 +26,13 @@ const (
 )
 
 type PushSync struct {
-	streamer      p2p.Streamer
-	storer        storage.Storer
-	peerSuggester topology.ClosestPeerer
-	quit          chan struct{}
-	logger        logging.Logger
-	metrics       metrics
+	streamer          p2p.Streamer
+	storer            storage.Storer
+	peerSuggester     topology.ClosestPeerer
+	logger            logging.Logger
+	metrics           metrics
+	quit              chan struct{}
+	chunksWorkerQuitC chan struct{}
 }
 
 type Options struct {
@@ -42,20 +42,23 @@ type Options struct {
 	Logger        logging.Logger
 }
 
-var retryInterval = 10 * time.Second // time interval between retries
+var (
+	retryInterval        = 10 * time.Second // time interval between retries
+	timeToWaitForReceipt = 3 * time.Second  // time to wait to get a receipt for a chunk
+)
 
 func New(o Options) *PushSync {
 	ps := &PushSync{
-		streamer:      o.Streamer,
-		storer:        o.Storer,
-		peerSuggester: o.ClosestPeerer,
-		logger:        o.Logger,
-		metrics:       newMetrics(),
-		quit:          make(chan struct{}),
+		streamer:          o.Streamer,
+		storer:            o.Storer,
+		peerSuggester:     o.ClosestPeerer,
+		logger:            o.Logger,
+		metrics:           newMetrics(),
+		quit:              make(chan struct{}),
+		chunksWorkerQuitC: make(chan struct{}),
 	}
 
 	go ps.chunksWorker()
-
 	return ps
 }
 
@@ -74,62 +77,164 @@ func (s *PushSync) Protocol() p2p.ProtocolSpec {
 
 func (ps *PushSync) Close() error {
 	close(ps.quit)
+
+	// Wait for chunks worker to finish
+	select {
+	case <-ps.chunksWorkerQuitC:
+	case <-time.After(3 * time.Second):
+	}
 	return nil
 }
 
-// handler handles chunk delivery from other node and inserts it to localstore.
-// it also sends this chunk to the closest peer if one exists.
+// handler handles chunk delivery from other node and forwards to its destination node.
+// If the current node is the destination, it stores in the local store and sends a receipt.
 func (ps *PushSync) handler(ctx context.Context, p p2p.Peer, stream p2p.Stream) error {
-	_, r := protobuf.NewWriterAndReader(stream)
+	w, r := protobuf.NewWriterAndReader(stream)
 	defer stream.Close()
 
-	var ch pb.Delivery
-
-	if err := r.ReadMsg(&ch); err != nil {
-		if err == io.EOF {
-			return nil
-		}
-		return err
+	// Get the delivery
+	chunk, err := ps.getChunkDelivery(r)
+	if err != nil {
+		return fmt.Errorf("chunk delivery: %w", err)
 	}
 
-	// create chunk and store it in the local store
-	addr := swarm.NewAddress(ch.Address)
-	chunk := swarm.NewChunk(addr, ch.Data)
-	_, err := ps.storer.Put(ctx, storage.ModePutSync, chunk)
+	// Select the closest peer to forward the chunk
+	peer, err := ps.peerSuggester.ClosestPeer(chunk.Address())
 	if err != nil {
-		return err
-	}
-
-	// push this to the closest node too
-	peer, err := ps.peerSuggester.ClosestPeer(addr)
-	if err != nil {
+		// If i am the closest peer then store the chunk and send receipt
 		if errors.Is(err, topology.ErrWantSelf) {
-			// i'm the closest - nothing to do
+
+			// Store the chunk in the local store
+			_, err := ps.storer.Put(ctx, storage.ModePutSync, chunk)
+			if err != nil {
+				return fmt.Errorf("chunk store: %w", err)
+			}
+			ps.metrics.TotalChunksStoredInDB.Inc()
+
+			// Send a receipt immediately once the storage of the chunk is successfull
+			receipt := &pb.Receipt{Address: chunk.Address().Bytes()}
+			err = ps.sendReceipt(w, receipt)
+			if err != nil {
+				return fmt.Errorf("send receipt: %w", err)
+			}
 			return nil
 		}
 		return err
 	}
-	if err := ps.sendChunkMsg(ctx, peer, chunk); err != nil {
+
+	// This is a special situation in that the other peer thinks thats we are the closest node
+	// and we think that the sending peer
+	if p.Address.Equal(peer) {
+
+		// Store the chunk in the local store
+		_, err := ps.storer.Put(ctx, storage.ModePutSync, chunk)
+		if err != nil {
+			return fmt.Errorf("chunk store: %w", err)
+		}
+		ps.metrics.TotalChunksStoredInDB.Inc()
+
+		// Send a receipt immediately once the storage of the chunk is successfull
+		receipt := &pb.Receipt{Address: chunk.Address().Bytes()}
+		return ps.sendReceipt(w, receipt)
+	}
+
+	// Forward chunk to closest peer
+	streamer, err := ps.streamer.NewStream(ctx, peer, nil, protocolName, protocolVersion, streamName)
+	if err != nil {
+		return fmt.Errorf("new stream: %w", err)
+	}
+	defer streamer.Close()
+
+	wc, rc := protobuf.NewWriterAndReader(streamer)
+
+	if err := ps.sendChunkDelivery(wc, chunk); err != nil {
+		return fmt.Errorf("forward chunk: %w", err)
+	}
+	receiptRTTTimer := time.Now()
+
+	receipt, err := ps.receiveReceipt(rc)
+	if err != nil {
+		return fmt.Errorf("receive receipt: %w", err)
+	}
+	ps.metrics.ReceiptRTT.Observe(time.Since(receiptRTTTimer).Seconds())
+
+	// Check if the receipt is valid
+	if !chunk.Address().Equal(swarm.NewAddress(receipt.Address)) {
+		ps.metrics.InvalidReceiptReceived.Inc()
+		return errors.New("invalid receipt")
+	}
+
+	// pass back the received receipt in the previously received stream
+	err = ps.sendReceipt(w, &receipt)
+	if err != nil {
+		return fmt.Errorf("send receipt: %w", err)
+	}
+	ps.metrics.ReceiptsSentCounter.Inc()
+
+	return nil
+}
+
+func (ps *PushSync) getChunkDelivery(r protobuf.Reader) (chunk swarm.Chunk, err error) {
+	var ch pb.Delivery
+	if err = r.ReadMsg(&ch); err != nil {
+		ps.metrics.ReceivedChunkErrorCounter.Inc()
+		return nil, err
+	}
+	ps.metrics.ChunksSentCounter.Inc()
+
+	// create chunk
+	addr := swarm.NewAddress(ch.Address)
+	chunk = swarm.NewChunk(addr, ch.Data)
+	return chunk, nil
+}
+
+func (ps *PushSync) sendChunkDelivery(w protobuf.Writer, chunk swarm.Chunk) (err error) {
+	startTimer := time.Now()
+	if err = w.WriteMsgWithTimeout(timeToWaitForReceipt, &pb.Delivery{
+		Address: chunk.Address().Bytes(),
+		Data:    chunk.Data(),
+	}); err != nil {
 		ps.metrics.SendChunkErrorCounter.Inc()
 		return err
 	}
-
-	return ps.storer.Set(ctx, storage.ModeSetSyncPush, chunk.Address())
+	ps.metrics.SendChunkTimer.Observe(time.Since(startTimer).Seconds())
+	ps.metrics.ChunksSentCounter.Inc()
+	return nil
 }
 
-// chunksWorker polls localstore sends chunks to peers.
-func (ps *PushSync) chunksWorker() {
-	var (
-		chunks      <-chan swarm.Chunk
-		unsubscribe func()
-		ctx         = context.Background()
-	)
+func (ps *PushSync) sendReceipt(w protobuf.Writer, receipt *pb.Receipt) (err error) {
+	if err := w.WriteMsg(receipt); err != nil {
+		ps.metrics.SendReceiptErrorCounter.Inc()
+		return err
+	}
+	ps.metrics.ReceiptsSentCounter.Inc()
+	return nil
+}
 
+func (ps *PushSync) receiveReceipt(r protobuf.Reader) (receipt pb.Receipt, err error) {
+	if err := r.ReadMsg(&receipt); err != nil {
+		ps.metrics.ReceiveReceiptErrorCounter.Inc()
+		return receipt, err
+	}
+	ps.metrics.ReceiptsReceivedCounter.Inc()
+	return receipt, nil
+}
+
+// chunksWorker is a loop that keeps looking for chunks that are locally uploaded ( by monitoring pushIndex )
+// and pushes them to the closest peer and get a receipt.
+func (ps *PushSync) chunksWorker() {
+	var chunks <-chan swarm.Chunk
+	var unsubscribe func()
 	// timer, initially set to 0 to fall through select case on timer.C for initialisation
 	timer := time.NewTimer(0)
 	defer timer.Stop()
+	defer close(ps.chunksWorkerQuitC)
 	chunksInBatch := -1
-
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		<-ps.quit
+		cancel()
+	}()
 	for {
 		select {
 		// handle incoming chunks
@@ -146,25 +251,21 @@ func (ps *PushSync) chunksWorker() {
 			}
 
 			chunksInBatch++
-			ps.metrics.SendChunkCounter.Inc()
+			ps.metrics.TotalChunksToBeSentCounter.Inc()
 			peer, err := ps.peerSuggester.ClosestPeer(ch.Address())
 			if err != nil {
 				if errors.Is(err, topology.ErrWantSelf) {
 					if err := ps.storer.Set(ctx, storage.ModeSetSyncPush, ch.Address()); err != nil {
-						ps.logger.Error("pushsync: error setting chunks to synced", "err", err)
+						ps.logger.Errorf("pushsync: error setting chunks to synced: %v", err)
 					}
 					continue
 				}
 			}
 
-			if err := ps.sendChunkMsg(ctx, peer, ch); err != nil {
-				ps.metrics.SendChunkErrorCounter.Inc()
-				ps.logger.Errorf("error sending chunk", "addr", ch.Address().String(), "err", err)
-			}
-
-			// set chunk status to synced, insert to db GC index
-			if err := ps.storer.Set(ctx, storage.ModeSetSyncPush, ch.Address()); err != nil {
-				ps.logger.Error("pushsync: error setting chunks to synced", "err", err)
+			// TODO: make this function as a go routine and process several chunks in parallel
+			if err := ps.SendChunkAndReceiveReceipt(ctx, peer, ch); err != nil {
+				ps.logger.Errorf("pushsync: error while sending chunk or receiving receipt: %v", err)
+				continue
 			}
 
 			// retry interval timer triggers starting from new
@@ -182,9 +283,7 @@ func (ps *PushSync) chunksWorker() {
 
 			// reset timer to go off after retryInterval
 			timer.Reset(retryInterval)
-
-			timeSpent := float64(time.Since(startTime))
-			ps.metrics.MarkAndSweepTimer.Add(timeSpent)
+			ps.metrics.MarkAndSweepTimer.Observe(time.Since(startTime).Seconds())
 
 		case <-ps.quit:
 			if unsubscribe != nil {
@@ -195,25 +294,41 @@ func (ps *PushSync) chunksWorker() {
 	}
 }
 
-// sendChunkMsg sends a chunk to a given peer.
-func (ps *PushSync) sendChunkMsg(ctx context.Context, peer swarm.Address, ch swarm.Chunk) error {
-	startTimer := time.Now()
+// sendChunkAndReceiveReceipt sends chunk to a given peer
+// by opening a stream. It then waits for a receipt from that peer.
+// Once the receipt is received within a given time frame it marks that this chunk
+// as synced in the localstore.
+func (ps *PushSync) SendChunkAndReceiveReceipt(ctx context.Context, peer swarm.Address, ch swarm.Chunk) error {
 	streamer, err := ps.streamer.NewStream(ctx, peer, nil, protocolName, protocolVersion, streamName)
 	if err != nil {
 		return fmt.Errorf("new stream: %w", err)
 	}
 	defer streamer.Close()
 
-	w, _ := protobuf.NewWriterAndReader(streamer)
+	w, r := protobuf.NewWriterAndReader(streamer)
+	if err := ps.sendChunkDelivery(w, ch); err != nil {
+		return fmt.Errorf("chunk deliver: %w", err)
+	}
+	receiptRTTTimer := time.Now()
 
-	if err := w.WriteMsg(&pb.Delivery{
-		Address: ch.Address().Bytes(),
-		Data:    ch.Data(),
-	}); err != nil {
-		return err
+	receipt, err := ps.receiveReceipt(r)
+	if err != nil {
+		return fmt.Errorf("receive receipt: %w", err)
+	}
+	ps.metrics.ReceiptRTT.Observe(time.Since(receiptRTTTimer).Seconds())
+
+	// Check if the receipt is valid
+	if !ch.Address().Equal(swarm.NewAddress(receipt.Address)) {
+		ps.metrics.InvalidReceiptReceived.Inc()
+		return errors.New("invalid receipt")
 	}
 
-	timeSpent := float64(time.Since(startTimer))
-	ps.metrics.SendChunkTimer.Add(timeSpent)
-	return err
+	// set chunk status to synced, insert to db GC index
+	if err := ps.storer.Set(ctx, storage.ModeSetSyncPush, ch.Address()); err != nil {
+		ps.metrics.ErrorSettingChunkToSynced.Inc()
+		return fmt.Errorf("chunk store: %w", err)
+	}
+
+	ps.metrics.TotalChunksSynced.Inc()
+	return nil
 }
