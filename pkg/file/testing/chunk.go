44ace3b47c175c2173583f99780b8631b8d69779
lash <nolash@users.noreply.github.com>
2020-05-08 11:47:59 +0200

    Add splitter interface and simple splitter implementation stub (#124)

--- pkg/file/testing/chunk.go
+++ pkg/file/testing/chunk.go
@@ -1,3 +1,7 @@
+// Copyright 2020 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package testing
 
 import (
@@ -7,7 +11,8 @@ import (
 	"github.com/ethersphere/bee/pkg/swarm"
 )
 
-func GenerateTestRandomFileChunk(address swarm.Address, spanLength int, dataSize int) swarm.Chunk {
+// GenerateTestRandomFileChunk generates one single chunk with arbitrary content and address
+func GenerateTestRandomFileChunk(address swarm.Address, spanLength, dataSize int) swarm.Chunk {
 	data := make([]byte, dataSize+8)
 	binary.LittleEndian.PutUint64(data, uint64(spanLength))
 	_, _ = rand.Read(data[8:]) // # skipcq: GSC-G404
