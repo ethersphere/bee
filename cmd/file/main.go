f5e52eca5069e86468c4ef455d46726167f04607
nolash <dev@holbrook.no>
2020-05-14 19:09:45 +0200

    Add flags and stdin processing

--- cmd/file/main.go
+++ cmd/file/main.go
@@ -17,6 +17,8 @@ import (
 
 var (
 	notImplementedError = errors.New("method not implemented")
+	outdir string
+	inputLength int64
 )
 
 
@@ -43,30 +45,34 @@ func (f *fsStore) Put(ctx context.Context, mode storage.ModePut, chs ...swarm.Ch
 
 func Split(cmd *cobra.Command, args []string) (err error) {
 	var infile *os.File
-	var infileLength int64
-	var outdir string
 
 	if len(args) > 0 {
 		info, err := os.Stat(args[0])
 		if err != nil {
-			fmt.Fprint(os.Stderr, err.Error())
 			return err
 		}
-		infileLength = info.Size()
+		fileLength := info.Size()
+		if inputLength > 0 {
+			if inputLength > fileLength {
+				return fmt.Errorf("input data length set to %d on file with length %d", inputLength, fileLength)
+			}
+		} else {
+			inputLength = fileLength
+		}
 		infile, err = os.Open(args[0])
+		defer infile.Close()
 		if err != nil {
-			fmt.Fprint(os.Stderr, err.Error())
 			return err
 		}
 	} else {
-		panic("cant before count is set")
+		if inputLength == 0 {
+			return errors.New("must specify length of input on stdin")
+		}
 		infile = os.Stdin
 	}
 
-	outdir = "chunk"
 	err = os.MkdirAll(outdir, 0o777)
 	if err != nil {
-		fmt.Fprint(os.Stderr, err.Error())
 		return err
 	}
 
@@ -74,9 +80,8 @@ func Split(cmd *cobra.Command, args []string) (err error) {
 	s := splitter.NewSimpleSplitter(store)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second * 60)
 	defer cancel()
-	addr, err := s.Split(ctx, infile, infileLength)
+	addr, err := s.Split(ctx, infile, inputLength)
 	if err != nil {
-		fmt.Fprint(os.Stderr, err.Error())
 		return err
 	}
 	fmt.Println(addr)
@@ -89,5 +94,13 @@ func main() {
 		Short: "split data into swarm chunks",
 		RunE: Split,
 	}
+
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Fprint(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+	c.Flags().StringVar(&outdir, "output-dir", filepath.Join(dir, "chunks"), "output directory")
+	c.Flags().Int64Var(&inputLength, "count", 0, "input data length")
 	c.Execute()
 }
