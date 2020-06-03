baecc9ee3dcf984f217e8a920b6e9cc8ebde6aa1
Petar Radovic <petar.radovic@gmail.com>
2020-06-03 11:49:43 +0200

    fix debug logs

--- pkg/kademlia/kademlia.go
+++ pkg/kademlia/kademlia.go
@@ -139,7 +139,7 @@ func (k *Kad) manage() {
 				err = k.connect(ctx, peer, bzzAddr.Underlay, po)
 				if err != nil {
 					k.logger.Debugf("error connecting to peer from kademlia %s %+v: %v", peer, bzzAddr, err)
-					k.logger.Errorf("error connecting to peer %s: %v", peer, err)
+					k.logger.Errorf("connecting to peer %s: %v", peer, err)
 					// continue to next
 					return false, false, nil
 				}
