beb3e0ce8263c17638a1a55119637d567b001680
nolash <dev@holbrook.no>
2020-06-03 16:53:59 +0200

    Add component tests for cli file io helper

--- cmd/file/io_test.go
+++ cmd/file/io_test.go
@@ -5,11 +5,14 @@
 package file_test
 
 import (
+	"bytes"
 	"context"
+	"io/ioutil"
 	"os"
 	"net"
 	"net/http"
 	"net/url"
+	"path/filepath"
 	"strconv"
 	"testing"
 
@@ -21,9 +24,14 @@ import (
 	cmdfile "github.com/ethersphere/bee/cmd/file"
 )
 
-func TestApiServer(t *testing.T) {
+const (
+	hashOfFoo = "2387e8e7d8a48c2a9339c97c1dc3461a9a7aa07e994c5cb8b38fd7c1b3e6ea48"
+)
+
+func TestApiStore(t *testing.T) {
+	storer := mock.NewStorer()
 	ctx := context.Background()
-	srv, addr := newTestServer(t)
+	srv, addr := newTestServer(t, storer)
 	defer srv.Shutdown(ctx)
 
 	srvUrl, err := url.Parse("http://" + addr.String())
@@ -38,15 +46,113 @@ func TestApiServer(t *testing.T) {
 	a := cmdfile.NewApiStore(srvHost, srvPort, false)
 	t.Log(a)
 
-	chunkAddr := swarm.MustParseHexAddress("2387e8e7d8a48c2a9339c97c1dc3461a9a7aa07e994c5cb8b38fd7c1b3e6ea48")
+	chunkAddr := swarm.MustParseHexAddress(hashOfFoo)
 	chunkData := []byte("foo")
 	ch := swarm.NewChunk(chunkAddr, chunkData)
-	a.Put(ctx, storage.ModePutUpload, ch)
+	_, err = a.Put(ctx, storage.ModePutUpload, ch)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = storer.Get(ctx, storage.ModeGetRequest, chunkAddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	chResult, err := a.Get(ctx, storage.ModeGetRequest, chunkAddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !ch.Equal(chResult) {
+		t.Fatal("chunk mismatch")
+	}
 
 }
 
-func newTestServer(t *testing.T) (*http.Server, net.Addr) {
-	storer := mock.NewStorer()
+func TestFsStore(t *testing.T) {
+	tmpPath, err := ioutil.TempDir("", "cli-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tmpPath)
+	storer := cmdfile.NewFsStore(tmpPath)
+	chunkAddr := swarm.MustParseHexAddress(hashOfFoo)
+	chunkData := []byte("foo")
+	ch := swarm.NewChunk(chunkAddr, chunkData)
+
+	ctx := context.Background()
+	_, err = storer.Put(ctx, storage.ModePutUpload, ch)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	chunkFilename := filepath.Join(tmpPath, hashOfFoo)
+	chunkDataResult, err := ioutil.ReadFile(chunkFilename)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	chResult := swarm.NewChunk(chunkAddr, chunkDataResult)
+	if !ch.Equal(chResult) {
+		t.Fatal("chunk mismatch")
+	}
+}
+
+
+func TestTeeStore(t *testing.T) {
+	storeFee := mock.NewStorer()
+	storeFi := mock.NewStorer()
+	storeFo := mock.NewStorer()
+	storeFum := cmdfile.NewTeeStore()
+	storeFum.Add(storeFee)
+	storeFum.Add(storeFi)
+	storeFum.Add(storeFo)
+
+	chunkAddr := swarm.MustParseHexAddress(hashOfFoo)
+	chunkData := []byte("foo")
+	ch := swarm.NewChunk(chunkAddr, chunkData)
+
+	ctx := context.Background()
+	var err error
+	_, err = storeFum.Put(ctx, storage.ModePutUpload, ch)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = storeFee.Get(ctx, storage.ModeGetRequest, chunkAddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = storeFi.Get(ctx, storage.ModeGetRequest, chunkAddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = storeFo.Get(ctx, storage.ModeGetRequest, chunkAddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestLimitWriter(t *testing.T) {
+	buf := bytes.NewBuffer(nil)
+	data := []byte("foo")
+	closer := ioutil.NopCloser(buf).Close
+	w := cmdfile.NewLimitWriteCloser(buf, closer, int64(len(data)))
+	c, err := w.Write(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if c < 3 {
+		t.Fatal("short write")
+	}
+	if !bytes.Equal(buf.Bytes(), data) {
+		t.Fatalf("expected written data %x, got %x", data, buf.Bytes())
+	}
+	_, err = w.Write(data[:1])
+	if err == nil {
+		t.Fatal("expected overflow error")
+	}
+}
+
+func newTestServer(t *testing.T, storer storage.Storer) (*http.Server, net.Addr) {
 	s := api.New(api.Options{
 		Storer:   storer,
 		Logger:   logging.New(os.Stdout, 6),
@@ -59,10 +165,10 @@ func newTestServer(t *testing.T) (*http.Server, net.Addr) {
 		t.Fatal(err)
 	}
 	go func() {
-	err := srv.Serve(l)
-	if err != nil {
-		t.Fatal(err)
-	}
-}()
+		err := srv.Serve(l)
+		if err != nil {
+			t.Log(err)
+		}
+	}()
 	return srv, l.Addr()
 }
