00590d2de382b3f69fa6ae95d08bf5539eb6658d
Petar Radovic <petar.radovic@gmail.com>
2020-06-12 13:18:44 +0200

    move magic sleep

--- pkg/node/node.go
+++ pkg/node/node.go
@@ -155,14 +155,13 @@ func NewBee(o Options) (*Bee, error) {
 	logger.Debug("initializing NAT manager")
 	select {
 	case <-p2ps.NATManager().Ready():
+		// this is magic sleep to give NAT time to sync the mappings
+		// this is a hack, kind of alchemy and should be improved
+		time.Sleep(3 * time.Second)
 		logger.Debug("NAT manager initialized")
 	case <-time.After(10 * time.Second):
 		logger.Warning("NAT manager init timeout")
 	}
-
-	// this is magic sleep to give NAT time to sync the mappings
-	// this is a hack, kind of alchemy and should be improved
-	time.Sleep(3 * time.Second)
 
 	// Construct protocols.
 	pingPong := pingpong.New(pingpong.Options{
