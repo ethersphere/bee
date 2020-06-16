4d74062cb358ecba2f0db766d01eb4ec6cedee98
Janoš Guljaš <janos@users.noreply.github.com>
2020-06-16 12:10:58 +0200

    support binary upload to api files endpoint (#306)

--- pkg/api/router.go
+++ pkg/api/router.go
@@ -35,18 +35,16 @@ func (s *server) setupRouting() {
 		fmt.Fprintln(w, "User-agent: *\nDisallow: /")
 	})
 
+	handle(router, "/files", jsonhttp.MethodHandler{
+		"POST": http.HandlerFunc(s.fileUploadHandler),
+	})
+	handle(router, "/files/{addr}", jsonhttp.MethodHandler{
+		"GET": http.HandlerFunc(s.fileDownloadHandler),
+	})
+
 	handle(router, "/bytes", jsonhttp.MethodHandler{
 		"POST": http.HandlerFunc(s.bytesUploadHandler),
 	})
-
-	handle(router, "/files", jsonhttp.MethodHandler{
-		"POST": http.HandlerFunc(s.bzzFileUploadHandler),
-	})
-
-	handle(router, "/files/{addr}", jsonhttp.MethodHandler{
-		"GET": http.HandlerFunc(s.bzzFileDownloadHandler),
-	})
-
 	handle(router, "/bytes/{address}", jsonhttp.MethodHandler{
 		"GET": http.HandlerFunc(s.bytesGetHandler),
 	})
