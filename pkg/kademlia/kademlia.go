5587d4666c93980bb5fe05f7f1f6e2e3069942d5
Janoš Guljaš <janos@users.noreply.github.com>
2020-06-17 17:56:11 +0200

    suppress p2p.ErrAlreadyConnected log when connecting to bootnodes (#318)
    * suppress p2p.ErrAlreadyConnected log when connecting to bootnodes, adjust debug log in kademlia

--- pkg/kademlia/kademlia.go
+++ pkg/kademlia/kademlia.go
@@ -149,7 +149,7 @@ func (k *Kad) manage() {
 
 				err = k.connect(ctx, peer, bzzAddr.Underlay, po)
 				if err != nil {
-					k.logger.Debugf("error connecting to peer from kademlia %s %s: %v", peer, bzzAddr, err)
+					k.logger.Debugf("error connecting to peer from kademlia %s: %v", bzzAddr.String(), err)
 					k.logger.Errorf("connecting to peer %s: %v", bzzAddr.ShortString(), err)
 					// continue to next
 					return false, false, nil
