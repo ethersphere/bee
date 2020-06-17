69ccac345bcc4ed6cee57cece2be644ade3d5553
Janoš Guljaš <janos@users.noreply.github.com>
2020-06-17 16:18:44 +0200

    improve discover log messages and errors (#317)

--- pkg/p2p/discover.go
+++ pkg/p2p/discover.go
@@ -7,6 +7,7 @@ package p2p
 import (
 	"context"
 	"errors"
+	"fmt"
 	"math/rand"
 
 	ma "github.com/multiformats/go-multiaddr"
@@ -21,7 +22,7 @@ func Discover(ctx context.Context, addr ma.Multiaddr, f func(ma.Multiaddr) (stop
 	dnsResolver := madns.DefaultResolver
 	addrs, err := dnsResolver.Resolve(ctx, addr)
 	if err != nil {
-		return false, err
+		return false, fmt.Errorf("dns resolve address %s: %w", addr, err)
 	}
 	if len(addrs) == 0 {
 		return false, errors.New("non-resolvable API endpoint")
@@ -33,7 +34,7 @@ func Discover(ctx context.Context, addr ma.Multiaddr, f func(ma.Multiaddr) (stop
 	for _, addr := range addrs {
 		stopped, err = Discover(ctx, addr, f)
 		if err != nil {
-			return false, err
+			return false, fmt.Errorf("discover %s: %w", addr, err)
 		}
 		if stopped {
 			break
