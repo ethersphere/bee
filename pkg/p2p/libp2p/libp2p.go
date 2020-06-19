5efa9ca9f9f1530a55214386c44727abad39a233
Janoš Guljaš <janos@users.noreply.github.com>
2020-06-19 12:51:07 +0200

    Revert "Disconnect on headers error (#333)" (#335)
    This reverts commit d70b054041ec7fa0fd8c8e1c3a46fcf81bbbf645.

--- pkg/p2p/libp2p/libp2p.go
+++ pkg/p2p/libp2p/libp2p.go
@@ -285,8 +285,6 @@ func (s *Service) AddProtocol(p p2p.ProtocolSpec) (err error) {
 			// exchange headers
 			if err := handleHeaders(ss.Headler, stream); err != nil {
 				s.logger.Debugf("handle protocol %s/%s: stream %s: peer %s: handle headers: %v", p.Name, p.Version, ss.Name, overlay, err)
-				s.logger.Errorf("handle protocol %s/%s: peer %s", p.Name, p.Version, overlay)
-				_ = s.disconnect(peerID)
 				return
 			}
 
@@ -442,8 +440,6 @@ func (s *Service) NewStream(ctx context.Context, overlay swarm.Address, headers 
 
 	// exchange headers
 	if err := sendHeaders(ctx, headers, stream); err != nil {
-		s.logger.Debugf("send headers: returned error %s: %v", peerID, err)
-		_ = s.disconnect(peerID)
 		return nil, fmt.Errorf("send headers: %w", err)
 	}
 
