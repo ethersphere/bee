a9b5e9c5f10f1b2115982110335a4a1e417344e5
acud <12988138+acud@users.noreply.github.com>
2020-06-19 14:36:11 +0200

    kademlia: add timeout to connect (#332)
    * add timeout to connect

--- pkg/kademlia/kademlia.go
+++ pkg/kademlia/kademlia.go
@@ -251,6 +251,8 @@ func (k *Kad) recalcDepth() uint8 {
 // connect connects to a peer and gossips its address to our connected peers,
 // as well as sends the peers we are connected to to the newly connected peer
 func (k *Kad) connect(ctx context.Context, peer swarm.Address, ma ma.Multiaddr, po uint8) error {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
 	_, err := k.p2p.Connect(ctx, ma)
 	if err != nil {
 		if errors.Is(err, p2p.ErrAlreadyConnected) {
