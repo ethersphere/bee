a230499190771ca7f880fd5b9bf5967cc2123874
Ralph Pichler <pichler.ralph@gmail.com>
2020-07-11 10:17:38 +0200

    use statestore for persistent accounting

--- pkg/accounting/accounting.go
+++ pkg/accounting/accounting.go
@@ -1,10 +1,12 @@
 package accounting
 
 import (
-	"errors"
+	"fmt"
 	"sync"
 
 	"github.com/ethersphere/bee/pkg/logging"
+	"github.com/ethersphere/bee/pkg/p2p"
+	"github.com/ethersphere/bee/pkg/storage"
 	"github.com/ethersphere/bee/pkg/swarm"
 )
 
@@ -16,35 +18,32 @@ type Interface interface {
 	Reserve(peer swarm.Address, price uint64) error
 	// Release releases reserved funds
 	Release(peer swarm.Address, price uint64)
-	// Apply a balance change to the peers balance
+	// Add applies a balance change to the peers balance
 	Add(peer swarm.Address, price int64) error
 }
 
 type PeerBalance struct {
 	lock     sync.Mutex
-	balance  int64 // positive amount is the nodes debt with us
-	reserved uint64
+	balance  int64  // amount that the peer owes us if positive, our debt if negative
+	reserved uint64 // amount currently reserved for active peer interaction
 }
 
 type Options struct {
 	DisconnectThreshold uint64
 	PaymentThreshold    uint64
 	Logger              logging.Logger
+	Store               storage.StateStorer
 }
 
 type Accounting struct {
 	balancesMu sync.Mutex
 	balances   map[string]*PeerBalance
 	logger     logging.Logger
+	store      storage.StateStorer
 
 	disconnectThreshold uint64
 	paymentThreshold    uint64
 }
-
-var (
-	DisconnectError   = errors.New("DisconnectError")
-	CannotAffordError = errors.New("CannotAffordError")
-)
 
 func NewAccounting(o Options) *Accounting {
 	return &Accounting{
@@ -53,16 +52,22 @@ func NewAccounting(o Options) *Accounting {
 		paymentThreshold:    o.PaymentThreshold,
 		disconnectThreshold: o.DisconnectThreshold,
 		logger:              o.Logger,
+		store:               o.Store,
 	}
 }
 
+// Reserve reserves a portion of the balance for peer
 func (a *Accounting) Reserve(peer swarm.Address, price uint64) error {
-	balance := a.getPeerBalance(peer)
+	balance, err := a.getPeerBalance(peer)
+	if err != nil {
+		return err
+	}
+
 	balance.lock.Lock()
 	defer balance.lock.Unlock()
 
 	if balance.balance-int64(balance.reserved+price) < -int64(a.disconnectThreshold) {
-		return CannotAffordError
+		return fmt.Errorf("cannot afford operation with peer %v", peer.String())
 	}
 
 	balance.reserved += price
@@ -70,8 +75,14 @@ func (a *Accounting) Reserve(peer swarm.Address, price uint64) error {
 	return nil
 }
 
+// Release releases reserved funds
 func (a *Accounting) Release(peer swarm.Address, price uint64) {
-	balance := a.getPeerBalance(peer)
+	balance, err := a.getPeerBalance(peer)
+	if err != nil {
+		a.logger.Error("Releasing balance for peer that is not loaded")
+		return
+	}
+
 	balance.lock.Lock()
 	defer balance.lock.Unlock()
 
@@ -83,8 +94,13 @@ func (a *Accounting) Release(peer swarm.Address, price uint64) {
 	}
 }
 
+// Add applies a balance change to the peers balance
 func (a *Accounting) Add(peer swarm.Address, price int64) error {
-	balance := a.getPeerBalance(peer)
+	balance, err := a.getPeerBalance(peer)
+	if err != nil {
+		return err
+	}
+
 	balance.lock.Lock()
 	defer balance.lock.Unlock()
 
@@ -92,12 +108,16 @@ func (a *Accounting) Add(peer swarm.Address, price int64) error {
 		// we gain balannce ith the peer
 		if balance.balance+price > int64(a.disconnectThreshold) {
 			// peer to much in debt
-			return DisconnectError
+			return p2p.NewDisconnectError(fmt.Errorf("disconnect threshold exceeded for peer %s", peer.String()))
 		}
 	}
 
+	err = a.store.Put(a.balanceKey(peer), balance.balance+price)
+	if err != nil {
+		return err
+	}
+
 	balance.balance += price
-	// TODO: save to state store
 
 	// TODO: try to initiate payment if payment threshold is reached
 	// if balance.balance < -int64(a.paymentThreshold) { }
@@ -105,24 +125,44 @@ func (a *Accounting) Add(peer swarm.Address, price int64) error {
 	return nil
 }
 
-func (a *Accounting) GetPeerBalance(peer swarm.Address) int64 {
-	return a.getPeerBalance(peer).balance
+// Balance returns the current balance for the given peer
+func (a *Accounting) Balance(peer swarm.Address) (int64, error) {
+	peerBalance, err := a.getPeerBalance(peer)
+	if err != nil {
+		return 0, err
+	}
+	return peerBalance.balance, nil
 }
 
-func (a *Accounting) getPeerBalance(peer swarm.Address) *PeerBalance {
+func (a *Accounting) balanceKey(peer swarm.Address) string {
+	return fmt.Sprintf("balance_%s", peer.String())
+}
+
+func (a *Accounting) getPeerBalance(peer swarm.Address) (*PeerBalance, error) {
 	a.balancesMu.Lock()
 	defer a.balancesMu.Unlock()
 
 	peerInfo, ok := a.balances[peer.String()]
 	if !ok {
-		// TODO: try loading from state store
-		peerInfo = &PeerBalance{
-			balance:  0,
-			reserved: 0,
+		var balance int64
+		err := a.store.Get(a.balanceKey(peer), &balance)
+		if err == nil {
+			peerInfo = &PeerBalance{
+				balance:  balance,
+				reserved: 0,
+			}
+		} else if err == storage.ErrNotFound {
+			peerInfo = &PeerBalance{
+				balance:  0,
+				reserved: 0,
+			}
+		} else {
+			return nil, err
 		}
+
 		a.balances[peer.String()] = peerInfo
-		return peerInfo
+		return peerInfo, nil
 	}
 
-	return peerInfo
+	return peerInfo, nil
 }
--- pkg/accounting/accounting_test.go
+++ pkg/accounting/accounting_test.go
@@ -3,22 +3,29 @@ package accounting_test
 import (
 	"github.com/ethersphere/bee/pkg/accounting"
 	"github.com/ethersphere/bee/pkg/logging"
+	"github.com/ethersphere/bee/pkg/statestore/mock"
 	"github.com/ethersphere/bee/pkg/swarm"
 	"io/ioutil"
 	"testing"
 )
 
 const (
-	testPrice = 10
+	testDisconnectThreshold = 10000
+	testPaymentThreshold    = 1000
+	testPrice               = 10
 )
 
 func TestAccountingAddBalance(t *testing.T) {
 	logger := logging.New(ioutil.Discard, 0)
 
-	accounting := accounting.NewAccounting(accounting.Options{
-		DisconnectThreshold: 10000,
-		PaymentThreshold:    1000,
+	store := mock.NewStateStore()
+	defer store.Close()
+
+	acc := accounting.NewAccounting(accounting.Options{
+		DisconnectThreshold: testDisconnectThreshold,
+		PaymentThreshold:    testPaymentThreshold,
 		Logger:              logger,
+		Store:               store,
 	})
 
 	peer1Addr, err := swarm.ParseHexAddress("00112233")
@@ -31,33 +38,102 @@ func TestAccountingAddBalance(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	err = accounting.Add(peer1Addr, testPrice)
+	err = acc.Add(peer1Addr, testPrice)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	peer1Balance := accounting.GetPeerBalance(peer1Addr)
+	peer1Balance, err := acc.Balance(peer1Addr)
+	if err != nil {
+		t.Fatal(err)
+	}
 	if peer1Balance != testPrice {
 		t.Fatalf("peer1Balance not equal to price. got %d, wanted %d", peer1Balance, testPrice)
 	}
 
-	err = accounting.Add(peer2Addr, testPrice)
+	err = acc.Add(peer2Addr, testPrice)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	peer2Balance := accounting.GetPeerBalance(peer2Addr)
+	peer2Balance, err := acc.Balance(peer2Addr)
+	if err != nil {
+		t.Fatal(err)
+	}
 	if peer2Balance != testPrice {
 		t.Fatalf("peer1Balance not equal to price. got %d, wanted %d", peer1Balance, testPrice)
 	}
 
-	err = accounting.Add(peer1Addr, testPrice)
+	err = acc.Add(peer1Addr, testPrice)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	peer1Balance = accounting.GetPeerBalance(peer1Addr)
+	peer1Balance, err = acc.Balance(peer1Addr)
+	if err != nil {
+		t.Fatal(err)
+	}
 	if peer1Balance != 2*testPrice {
 		t.Fatalf("peer1Balance not equal to price. got %d, wanted %d", peer1Balance, 2*testPrice)
 	}
+}
+
+func TestAccountingAdd_persistentBalances(t *testing.T) {
+	logger := logging.New(ioutil.Discard, 0)
+
+	store := mock.NewStateStore()
+	defer store.Close()
+
+	acc := accounting.NewAccounting(accounting.Options{
+		DisconnectThreshold: testDisconnectThreshold,
+		PaymentThreshold:    testPaymentThreshold,
+		Logger:              logger,
+		Store:               store,
+	})
+
+	peer1Addr, err := swarm.ParseHexAddress("00112233")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	peer2Addr, err := swarm.ParseHexAddress("00112244")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = acc.Add(peer1Addr, testPrice)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = acc.Add(peer2Addr, 2*testPrice)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	acc = accounting.NewAccounting(accounting.Options{
+		DisconnectThreshold: testDisconnectThreshold,
+		PaymentThreshold:    testPaymentThreshold,
+		Logger:              logger,
+		Store:               store,
+	})
+
+	peer1Balance, err := acc.Balance(peer1Addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if peer1Balance != testPrice {
+		t.Fatalf("peer1Balance not loaded correctly. got %d, wanted %d", peer1Balance, testPrice)
+	}
+
+	peer2Balance, err := acc.Balance(peer2Addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if peer2Balance != 2*testPrice {
+		t.Fatalf("peer2Balance not loaded correctly. got %d, wanted %d", peer2Balance, 2*testPrice)
+	}
+
 }
