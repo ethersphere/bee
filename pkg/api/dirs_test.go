d35c231fe44ad893a05d0854801c9f9e05f72d35
mortelli <ortellimarcelo@gmail.com>
2020-07-21 15:53:00 -0300

    api: add first version for TestDirs func

--- pkg/api/dirs_test.go
+++ pkg/api/dirs_test.go
@@ -4,4 +4,35 @@
 
 package api_test
 
-// TODO: add tests
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/ethersphere/bee/pkg/jsonhttp"
+	"github.com/ethersphere/bee/pkg/jsonhttp/jsonhttptest"
+	"github.com/ethersphere/bee/pkg/logging"
+	"github.com/ethersphere/bee/pkg/storage/mock"
+	"github.com/ethersphere/bee/pkg/tags"
+)
+
+func TestDirs(t *testing.T) {
+	var (
+		dirUploadResource = "/dirs"
+		//dirDownloadResource = func(addr string) string { return "/dirs/" + addr }
+		client = newTestServer(t, testServerOptions{
+			Storer: mock.NewStorer(),
+			Tags:   tags.NewTags(),
+			Logger: logging.New(ioutil.Discard, 5),
+		})
+	)
+
+	t.Run("non-tar", func(t *testing.T) {
+		d := []byte("text")
+		jsonhttptest.ResponseDirectSendHeadersAndReceiveHeaders(t, client, http.MethodPost, dirUploadResource, bytes.NewReader(d), http.StatusInternalServerError, jsonhttp.StatusResponse{
+			Message: "could not store dir",
+			Code:    http.StatusInternalServerError,
+		}, nil)
+	})
+}
