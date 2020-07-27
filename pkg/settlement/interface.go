7569d24d280171649ccccc1811ebe8fe7df3653f
Ralph Pichler <pichler.ralph@gmail.com>
2020-07-27 12:52:14 +0200

    add docs

--- pkg/settlement/interface.go
+++ pkg/settlement/interface.go
@@ -6,10 +6,15 @@ import (
 	"github.com/ethersphere/bee/pkg/swarm"
 )
 
+// Interface is the interface used by Accounting to trigger settlement
 type Interface interface {
+	// Pay initiates a payment to the given peer
+	// It should return without error it is likely that the payment worked
 	Pay(ctx context.Context, peer swarm.Address, amount uint64) error
 }
 
+// PaymentObserver is the interface Settlement uses to notify other components of an incoming payment
 type PaymentObserver interface {
+	// NotifyPayment is called when a payment from peer was successfully received
 	NotifyPayment(peer swarm.Address, amount uint64) error
 }
