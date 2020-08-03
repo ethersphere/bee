701f4766b15498775e91f5f9c1af6b74994722f4
mortelli <ortellimarcelo@gmail.com>
2020-08-03 10:12:45 -0300

    jsonmanifest: iterate TestMarshal func

--- pkg/manifest/jsonmanifest/jsonmanifest_test.go
+++ pkg/manifest/jsonmanifest/jsonmanifest_test.go
@@ -29,7 +29,24 @@ func TestMarshal(t *testing.T) {
 				{
 					reference: test.RandomAddress(),
 					name:      "entry-1",
+					headers:   http.Header{},
+					path:      "",
+				},
+			},
+		},
+		{
+			name: "two-entries",
+			entries: []e{
+				{
+					reference: test.RandomAddress(),
+					name:      "entry-1.txt",
 					headers:   http.Header{"Content-Type": {"text/plain; charset=utf-8"}},
+					path:      "",
+				},
+				{
+					reference: test.RandomAddress(),
+					name:      "entry-2,png",
+					headers:   http.Header{"Content-Type": {"image/png"}},
 					path:      "",
 				},
 			},
@@ -61,38 +78,6 @@ func TestMarshal(t *testing.T) {
 				t.Fatalf("marshalled and unmarshalled manifests are not equal: %v, %v", m, um)
 			}
 		})
-	}
-	m1 := jsonmanifest.NewManifest()
-
-	e1 := jsonmanifest.NewEntry(
-		test.RandomAddress(),
-		"entry-1",
-		http.Header{"Content-Type": {"text/plain; charset=utf-8"}},
-	)
-
-	m1.Add("entry-1", e1)
-
-	e2 := jsonmanifest.NewEntry(
-		test.RandomAddress(),
-		"entry-2",
-		http.Header{"Content-Type": {"image/png"}},
-	)
-
-	m1.Add("entry-2", e2)
-
-	b, err := m1.MarshalBinary()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	m2 := jsonmanifest.NewManifest()
-
-	if err := m2.UnmarshalBinary(b); err != nil {
-		t.Fatal(err)
-	}
-
-	if !reflect.DeepEqual(m1, m2) {
-		t.Fatalf("marshalled and unmarshalled manifests are not equal: %v, %v", m1, m2)
 	}
 }
 
