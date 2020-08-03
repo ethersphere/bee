65b65f5f7314dc6d3b44a55cc808b8c9f68ee90c
Pavle Batuta <pavle@batuta.xyz>
2020-08-03 09:33:02 +0200

    Dynamic welcome message (#439)
    * Add dynamic welcome message to p2p handshake
    
    * Add '/welcome-msg' to debug api
    
    * Add missing license headers
    
    * Rename to WelcomeMessageSynced to Get
    
    * Add atomic.value instead of struct
    
    * Simplify mocks, refactor tests
    
    * Add request struct and length check
    
    * Extract DebugService p2p interface
    
    * Add check
    
    * Remove MaxBytesReader
    
    * Remove unused constant
    
    * Remove out of scope test
    
    * Set max request size
    
    * Refactor test

--- pkg/debugapi/router.go
+++ pkg/debugapi/router.go
@@ -86,6 +86,13 @@ func (s *server) setupRouting() {
 	router.Handle("/topology", jsonhttp.MethodHandler{
 		"GET": http.HandlerFunc(s.topologyHandler),
 	})
+	router.Handle("/welcome-message", jsonhttp.MethodHandler{
+		"GET": http.HandlerFunc(s.getWelcomeMessageHandler),
+		"POST": web.ChainHandlers(
+			jsonhttp.NewMaxBodyBytesHandler(welcomeMessageMaxRequestSize),
+			web.FinalHandlerFunc(s.setWelcomeMessageHandler),
+		),
+	})
 
 	baseRouter.Handle("/", web.ChainHandlers(
 		logging.NewHTTPAccessLogHandler(s.Logger, logrus.InfoLevel, "debug api access"),
---
+++ pkg/debugapi/welcome_message_test.go
@@ -0,0 +1,119 @@
+// Copyright 2020 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package debugapi_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/ethersphere/bee/pkg/debugapi"
+	"github.com/ethersphere/bee/pkg/jsonhttp"
+	"github.com/ethersphere/bee/pkg/jsonhttp/jsonhttptest"
+	"github.com/ethersphere/bee/pkg/p2p/mock"
+)
+
+func TestGetWelcomeMessage(t *testing.T) {
+	const DefaultTestWelcomeMessage = "Hello World!"
+
+	srv := newTestServer(t, testServerOptions{
+		P2P: mock.New(mock.WithGetWelcomeMessageFunc(func() string {
+			return DefaultTestWelcomeMessage
+		}))})
+
+	jsonhttptest.ResponseDirect(t, srv.Client, http.MethodGet, "/welcome-message", nil, http.StatusOK, debugapi.WelcomeMessageResponse{
+		WelcomeMesssage: DefaultTestWelcomeMessage,
+	})
+}
+
+func TestSetWelcomeMessage(t *testing.T) {
+	testCases := []struct {
+		desc        string
+		message     string
+		wantFail    bool
+		wantStatus  int
+		wantMessage string
+	}{
+		{
+			desc:       "OK",
+			message:    "Changed value",
+			wantStatus: http.StatusOK,
+		},
+		{
+			desc:       "OK - welcome message empty",
+			message:    "",
+			wantStatus: http.StatusOK,
+		},
+		{
+			desc:     "fails - request entity too large",
+			wantFail: true,
+			message: `zZZbzbzbzbBzBBZbBbZbbbBzzzZBZBbzzBBBbBzBzzZbbBzBBzBBbZz
+			bZZZBBbbZbbZzBbzBbzbZBZzBZZbZzZzZzbbZZBZzzbBZBzZzzBBzZZzzZbZZZzbbbzz
+			bBzZZBbBZBzZzBZBzbzBBbzBBzbzzzBbBbZzZBZBZzBZZbbZZBZZBzZzBZbzZBzZbBzZ
+			bbbBbbZzZbzbZzZzbzzzbzzbzZZzbbzbBZZbBbBZBBZzZzzbBBBBBZbZzBzzBbzBbbbz
+			BBzbbZBbzbzBZbzzBzbZBzzbzbbbBZBzBZzBZbzBzZzBZZZBzZZBzBZZzbzZbzzZzBBz
+			ZZzbZzzZZZBZBBbZZbZzBBBzbzZZbbZZBZZBBBbBZzZbZBZBBBzzZBbbbbzBzbbzBBBz
+			bZBBbZzBbZZBzbBbZZBzBzBzBBbzzzZBbzbZBbzBbZzbbBZBBbbZbBBbbBZbzbZzbBzB
+			bBbbZZbzZzbbBbzZbZZZZbzzZZbBzZZbZzZzzBzbZZ`, // 513 characters
+			wantStatus: http.StatusRequestEntityTooLarge,
+		},
+	}
+	testURL := "/welcome-message"
+
+	srv := newTestServer(t, testServerOptions{
+		P2P: mock.New(),
+	})
+
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			if tC.wantMessage == "" {
+				tC.wantMessage = http.StatusText(tC.wantStatus)
+			}
+			data, _ := json.Marshal(debugapi.WelcomeMessageRequest{
+				WelcomeMesssage: tC.message,
+			})
+			body := bytes.NewReader(data)
+			wantResponse := jsonhttp.StatusResponse{
+				Message: tC.wantMessage,
+				Code:    tC.wantStatus,
+			}
+			jsonhttptest.ResponseDirect(t, srv.Client, http.MethodPost, testURL, body, tC.wantStatus, wantResponse)
+			if !tC.wantFail {
+				got := srv.P2PMock.GetWelcomeMessage()
+				if got != tC.message {
+					t.Fatalf("could not set dynamic welcome message: want %s, got %s", tC.message, got)
+				}
+			}
+		})
+	}
+}
+
+func TestSetWelcomeMessageInternalServerError(t *testing.T) {
+	testMessage := "NO CHANCE BYE"
+	testError := errors.New("Could not set value")
+	testURL := "/welcome-message"
+
+	srv := newTestServer(t, testServerOptions{
+		P2P: mock.New(mock.WithSetWelcomeMessageFunc(func(string) error {
+			return testError
+		})),
+	})
+
+	data, _ := json.Marshal(debugapi.WelcomeMessageRequest{
+		WelcomeMesssage: testMessage,
+	})
+	body := bytes.NewReader(data)
+	t.Run("internal server error - failed to store", func(t *testing.T) {
+		wantCode := http.StatusInternalServerError
+		wantResp := jsonhttp.StatusResponse{
+			Message: testError.Error(),
+			Code:    wantCode,
+		}
+		jsonhttptest.ResponseDirect(t, srv.Client, http.MethodPost, testURL, body, wantCode, wantResp)
+	})
+
+}
