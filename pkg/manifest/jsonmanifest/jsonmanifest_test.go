9e2c071a30207a56df5a36c090146c89984a170c
mortelli <ortellimarcelo@gmail.com>
2020-08-03 18:24:56 -0300

    jsonmanifest: iterate tests

--- pkg/manifest/jsonmanifest/jsonmanifest_test.go
+++ pkg/manifest/jsonmanifest/jsonmanifest_test.go
@@ -6,6 +6,7 @@ package jsonmanifest_test
 
 import (
 	"net/http"
+	"path/filepath"
 	"reflect"
 	"testing"
 
@@ -14,129 +15,114 @@ import (
 	"github.com/ethersphere/bee/pkg/swarm/test"
 )
 
-// simple test entries
-var testEntry1 = jsonmanifest.NewEntry(
-	test.RandomAddress(),
-	"entry_1.txt",
-	http.Header{"Content-Type": {"text/plain; charset=utf-8"}},
-)
-var testEntry2 = jsonmanifest.NewEntry(
-	test.RandomAddress(),
-	"entry_2.png",
-	http.Header{"Content-Type": {"image/png"}},
-)
+var testCases = []testCase{
+	{
+		name:    "empty-manifest",
+		entries: nil,
+	},
+	{
+		name: "one-entry",
+		entries: []e{
+			{
+				reference: test.RandomAddress(),
+				name:      "entry-1",
+				headers:   http.Header{},
+				path:      "",
+			},
+		},
+	},
+	{
+		name: "two-entries",
+		entries: []e{
+			{
+				reference: test.RandomAddress(),
+				name:      "entry-1.txt",
+				headers:   http.Header{"Content-Type": {"text/plain; charset=utf-8"}},
+				path:      "",
+			},
+			{
+				reference: test.RandomAddress(),
+				name:      "entry-2.png",
+				headers:   http.Header{"Content-Type": {"image/png"}},
+				path:      "",
+			},
+		},
+	},
+	{
+		name: "nested-entries",
+		entries: []e{
+			{
+				reference: test.RandomAddress(),
+				name:      "robots.txt",
+				headers:   http.Header{"Content-Type": {"text/plain; charset=utf-8"}},
+				path:      "text",
+			},
+			{
+				reference: test.RandomAddress(),
+				name:      "1.png",
+				headers:   http.Header{"Content-Type": {"image/png"}},
+				path:      "img",
+			},
+			{
+				reference: test.RandomAddress(),
+				name:      "2.jpg",
+				headers:   http.Header{"Content-Type": {"image/jpg"}},
+				path:      "img",
+			},
+			{
+				reference: test.RandomAddress(),
+				name:      "readme.md",
+				headers:   http.Header{"Content-Type": {"text/markdown; charset=UTF-8"}},
+				path:      "",
+			},
+		},
+	},
+}
 
-// TestAdd verifies that manifests behave as expected when adding entries
-func TestAdd(t *testing.T) {
+// TestAddRemove verifies that manifests behave as expected when adding and removing entries.
+// It also verifies the Length function.
+func TestAddRemove(t *testing.T) {
+	// get non-trivial test case
+	tc := testCases[len(testCases)-1]
+
 	m := jsonmanifest.NewManifest()
 	if m.Length() != 0 {
 		t.Fatalf("expected length to be %d, but is %d instead", 0, m.Length())
 	}
 
-	m.Add("entry_1", testEntry1)
-	if m.Length() != 1 {
-		t.Fatalf("expected length to be %d, but is %d instead", 1, m.Length())
-	}
+	// add and check all entries
+	for i, e := range tc.entries {
+		entry := jsonmanifest.NewEntry(
+			e.reference,
+			e.name,
+			e.headers,
+		)
+		path := filepath.Join(e.path, e.name)
+		m.Add(path, entry)
 
-	// check if retrieved entry matches original entry
-	e, err := m.Entry("entry_1")
-	if err != nil {
-		t.Fatal(err)
-	}
+		if m.Length() != i+1 {
+			t.Fatalf("expected length to be %d, but is %d instead", i+1, m.Length())
+		}
 
-	if !reflect.DeepEqual(testEntry1, e) {
-		t.Fatalf("original and retrieved entry are not equal: %v, %v", testEntry1, e)
-	}
+		re, err := m.Entry(path)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	m.Add("entry_2", testEntry2)
-	if m.Length() != 2 {
-		t.Fatalf("expected length to be %d, but is %d instead", 2, m.Length())
-	}
-
-	// check if retrieved entry matches original entry
-	e, err = m.Entry("entry_2")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if !reflect.DeepEqual(testEntry2, e) {
-		t.Fatalf("original and retrieved entry are not equal: %v, %v", testEntry1, e)
+		if !reflect.DeepEqual(entry, re) {
+			t.Fatalf("original and retrieved entry are not equal: %v, %v", entry, re)
+		}
 	}
 }
 
-// TestEntries verifies that the manifest entries are read-only
+// TestEntries verifies that manifest entries are read-only.
 func TestEntries(t *testing.T) {
 	_ = jsonmanifest.NewManifest()
 }
 
-// TestMarshal verifies that created manifests are successfully marshalled and unmarshalled
+// TestMarshal verifies that created manifests are successfully marshalled and unmarshalled.
 func TestMarshal(t *testing.T) {
-	for _, tc := range []struct {
-		name    string
-		entries []e // entries to add to manifest
-	}{
-		{
-			name:    "empty-manifest",
-			entries: nil,
-		},
-		{
-			name: "one-entry",
-			entries: []e{
-				{
-					reference: test.RandomAddress(),
-					name:      "entry-1",
-					headers:   http.Header{},
-					path:      "",
-				},
-			},
-		},
-		{
-			name: "two-entries",
-			entries: []e{
-				{
-					reference: test.RandomAddress(),
-					name:      "entry-1.txt",
-					headers:   http.Header{"Content-Type": {"text/plain; charset=utf-8"}},
-					path:      "",
-				},
-				{
-					reference: test.RandomAddress(),
-					name:      "entry-2.png",
-					headers:   http.Header{"Content-Type": {"image/png"}},
-					path:      "",
-				},
-			},
-		},
-		{
-			name: "nested-entries",
-			entries: []e{
-				{
-					reference: test.RandomAddress(),
-					name:      "robots.txt",
-					headers:   http.Header{"Content-Type": {"text/plain; charset=utf-8"}},
-					path:      "text",
-				},
-				{
-					reference: test.RandomAddress(),
-					name:      "1.png",
-					headers:   http.Header{"Content-Type": {"image/png"}},
-					path:      "img",
-				},
-				{
-					reference: test.RandomAddress(),
-					name:      "2.jpg",
-					headers:   http.Header{"Content-Type": {"image/jpg"}},
-					path:      "img",
-				},
-				{
-					reference: test.RandomAddress(),
-					name:      "readme.md",
-					headers:   http.Header{"Content-Type": {"text/markdown; charset=UTF-8"}},
-					path:      "",
-				},
-			},
-		},
-	} {
+	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			m := jsonmanifest.NewManifest()
 
@@ -146,7 +132,7 @@ func TestMarshal(t *testing.T) {
 					e.name,
 					e.headers,
 				)
-				m.Add(e.path, entry)
+				m.Add(filepath.Join(e.path, e.name), entry)
 			}
 
 			b, err := m.MarshalBinary()
@@ -166,7 +152,13 @@ func TestMarshal(t *testing.T) {
 	}
 }
 
-// struct for manifes entries for test cases
+// Struct for manifest test cases.
+type testCase struct {
+	name    string
+	entries []e // entries to add to manifest
+}
+
+// Struct for manifest entries for test cases.
 type e struct {
 	reference swarm.Address
 	name      string
