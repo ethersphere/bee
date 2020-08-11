f33bc4cfffd72cbb8eca25ab285cb8d6d6b1fb0c
Petar Radovic <petar.radovic@gmail.com>
2020-08-11 18:58:07 +0200

    peers handler rename

--- pkg/node/node.go
+++ pkg/node/node.go
@@ -213,7 +213,7 @@ func NewBee(addr string, logger logging.Logger, o Options) (*Bee, error) {
 
 	kad := kademlia.New(kademlia.Options{Base: address, Discovery: hive, AddressBook: addressbook, P2P: p2ps, Bootnodes: bootnodes, Logger: logger})
 	b.topologyCloser = kad
-	hive.SetPeerAddedHandler(kad.AddPeers)
+	hive.SetPeersAddedHandler(kad.AddPeers)
 	p2ps.AddNotifier(kad)
 	addrs, err := p2ps.Addresses()
 	if err != nil {
