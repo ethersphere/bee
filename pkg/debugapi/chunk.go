53fb51fc3e2eee54544abce40987a5a418c614cd
Zahoor Mohamed <zahoor@ethswarm.org>
2020-08-12 12:41:07 +0530

    expose remove chunk debug API (#556)

--- pkg/debugapi/chunk.go
+++ pkg/debugapi/chunk.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 
 	"github.com/ethersphere/bee/pkg/jsonhttp"
+	"github.com/ethersphere/bee/pkg/storage"
 	"github.com/ethersphere/bee/pkg/swarm"
 	"github.com/gorilla/mux"
 )
@@ -32,5 +33,33 @@ func (s *server) hasChunkHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	jsonhttp.OK(w, nil)
+}
 
+func (s *server) removeChunk(w http.ResponseWriter, r *http.Request) {
+	addr, err := swarm.ParseHexAddress(mux.Vars(r)["address"])
+	if err != nil {
+		s.Logger.Debugf("debug api: parse chunk address: %v", err)
+		jsonhttp.BadRequest(w, "bad address")
+		return
+	}
+
+	has, err := s.Storer.Has(r.Context(), addr)
+	if err != nil {
+		s.Logger.Debugf("debug api: localstore remove: %v", err)
+		jsonhttp.BadRequest(w, err)
+		return
+	}
+
+	if !has {
+		jsonhttp.OK(w, nil)
+		return
+	}
+
+	err = s.Storer.Set(r.Context(), storage.ModeSetRemove, addr)
+	if err != nil {
+		s.Logger.Debugf("debug api: localstore remove: %v", err)
+		jsonhttp.InternalServerError(w, err)
+		return
+	}
+	jsonhttp.OK(w, nil)
 }
