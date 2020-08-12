7ba06bd92bc4e9f4ec2cd8f6c02c7d026e40fc7c
santicomp2014 <santicomp2@gmail.com>
2020-08-12 12:03:19 -0300

    trojan, pss, pss_test: pss port from Swarm (#466)
    * trojan,pss,pss_test: added pss base, still have to fix logging metrics and tests
    
    * pss,pss_test,mock,trojan: added tests for pss
    
    * mock,pss_test: added LastPullSubscription in mock store with @acud, still needs another pass to finish it
    
    * mock: add init for bins
    
    * pss: changed localstore to storer
    
    * pss_test: changed recovery topic to generic pss topic
    
    * pss_test: deadline added to TestPssMonitor
    
    * pss_test: deadline added to TestPssMonitor
    
    * pss_test: deadline added to TestPssMonitor
    
    * pss_test: deadline added to TestPssMonitor
    
    * pss_test: skip TestTrojanChunkRetrieval refactor in progress
    
    * pss_test: removed TestTrojanChunkRetrieval until refactor complete
    
    * pss,pss_test: pss refactored removed Monitor struct replaced with tags, also replaced storer.Put with pushsync
    
    * pss_test: disabled checks for tag in TestPssMonitor
    
    * pss,pss_test: fixed monitor test and removed storer from pss
    
    * pss,pss_test: added metrics and logger to pss
    
    * pss,pss_test: refactored NewPss with Options Struct
    
    * pss,pss_test: changed signature of pss to include ctx and return error
    
    * metrics,pss,pss_test,pusher_test: refactored to interfaces and cleanup of tests
    
    * pss,pss_test: changed Deliver to TryUnwrap
    
    * pss: refactored metrics
    
    * pss,pss_test: refactored Interface again for testing
    
    * pss: fixed error not being used
    
    * pss_test: fix in test and rename TestPssMonitor to TestPssTags
    
    * pss_test: fixed deepsource complaint
    
    * pss_test: fixed deepsource complaint
    
    * pss_test: remove unsed Store struct
    
    * pusher_test: rollback changes done in pusher test
    
    * mock: revert storer mock to master, these changes are not necesarry any more
    
    * pss_test: remove state stored from tags, not needed in tc
    
    * pss_test: removed tag.Total in Send, not needed
    
    * pss_test: removed comment on Store state, since this was removed, cleanup
    
    * pss: removedlong in TryUnwrap comment
    
    * pss,pss_test: removed tags and deferenced handler
    
    * fix test case
    
    * pss_test: added more info in debug comment
    
    * pss_test: added err check in Unwrap
    
    Co-authored-by: acud <12988138+acud@users.noreply.github.com>

---
+++ pkg/pss/pss.go
@@ -0,0 +1,107 @@
+// Copyright 2020 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package pss
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+
+	"github.com/ethersphere/bee/pkg/logging"
+	"github.com/ethersphere/bee/pkg/pushsync"
+	"github.com/ethersphere/bee/pkg/swarm"
+	"github.com/ethersphere/bee/pkg/trojan"
+)
+
+var (
+	_            Interface = (*pss)(nil)
+	ErrNoHandler           = errors.New("no handler found")
+)
+
+type Interface interface {
+	Send(ctx context.Context, targets trojan.Targets, topic trojan.Topic, payload []byte) error
+	Register(topic trojan.Topic, hndlr Handler)
+	GetHandler(topic trojan.Topic) Handler
+	TryUnwrap(ctx context.Context, c swarm.Chunk) error
+}
+
+// pss is the top-level struct, which takes care of message sending
+type pss struct {
+	pusher     pushsync.PushSyncer
+	handlers   map[trojan.Topic]Handler
+	handlersMu sync.RWMutex
+	metrics    metrics
+	logger     logging.Logger
+}
+
+// New inits the pss struct with the storer
+func New(logger logging.Logger, pusher pushsync.PushSyncer) Interface {
+	return &pss{
+		logger:   logger,
+		pusher:   pusher,
+		handlers: make(map[trojan.Topic]Handler),
+		metrics:  newMetrics(),
+	}
+}
+
+// Handler defines code to be executed upon reception of a trojan message
+type Handler func(*trojan.Message)
+
+// Send constructs a padded message with topic and payload,
+// wraps it in a trojan chunk such that one of the targets is a prefix of the chunk address
+// uses push-sync to deliver message
+func (p *pss) Send(ctx context.Context, targets trojan.Targets, topic trojan.Topic, payload []byte) error {
+	p.metrics.TotalMessagesSentCounter.Inc()
+
+	//construct Trojan Chunk
+	m, err := trojan.NewMessage(topic, payload)
+	if err != nil {
+		return err
+	}
+	var tc swarm.Chunk
+	tc, err = m.Wrap(targets)
+	if err != nil {
+		return err
+	}
+
+	// push the chunk using push sync so that it reaches it destination in network
+	if _, err = p.pusher.PushChunkToClosest(ctx, tc); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Register allows the definition of a Handler func for a specific topic on the pss struct
+func (p *pss) Register(topic trojan.Topic, hndlr Handler) {
+	p.handlersMu.Lock()
+	defer p.handlersMu.Unlock()
+	p.handlers[topic] = hndlr
+}
+
+// TryUnwrap allows unwrapping a chunk as a trojan message and calling its handler func based on its topic
+func (p *pss) TryUnwrap(ctx context.Context, c swarm.Chunk) error {
+	if !trojan.IsPotential(c) {
+		return nil
+	}
+	m, err := trojan.Unwrap(c) // if err occurs unwrapping, there will be no handler
+	if err != nil {
+		return err
+	}
+	h := p.GetHandler(m.Topic)
+	if h == nil {
+		return fmt.Errorf("topic %v, %w", m.Topic, ErrNoHandler)
+	}
+	h(m)
+	return nil
+}
+
+// GetHandler returns the Handler func registered in pss for the given topic
+func (p *pss) GetHandler(topic trojan.Topic) Handler {
+	p.handlersMu.RLock()
+	defer p.handlersMu.RUnlock()
+	return p.handlers[topic]
+}
