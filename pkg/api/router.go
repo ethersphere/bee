d41b8d6702e67b7b1e00ca71daa0a2c60eed8859
Janoš Guljaš <janos@users.noreply.github.com>
2020-08-18 12:37:10 +0200

    support api version path for tags api endpoints (#586)

--- pkg/api/router.go
+++ pkg/api/router.go
@@ -66,14 +66,13 @@ func (s *server) setupRouting() {
 		"GET": http.HandlerFunc(s.bzzDownloadHandler),
 	})
 
-	router.Handle("/tags", jsonhttp.MethodHandler{
+	handle(router, "/tags", jsonhttp.MethodHandler{
 		"POST": web.ChainHandlers(
 			jsonhttp.NewMaxBodyBytesHandler(1024),
 			web.FinalHandlerFunc(s.createTag),
 		),
 	})
-
-	router.Handle("/tags/{id}", jsonhttp.MethodHandler{
+	handle(router, "/tags/{id}", jsonhttp.MethodHandler{
 		"GET":    http.HandlerFunc(s.getTag),
 		"DELETE": http.HandlerFunc(s.deleteTag),
 		"PATCH": web.ChainHandlers(
