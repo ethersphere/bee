1a8d4384e28ad5371d3e0488c345a89f5dd05c07
Pavle Batuta <pavle@batuta.xyz>
2020-08-24 19:05:45 +0200

    Remove Address alias from mock

--- pkg/resolver/mock/resolver.go
+++ pkg/resolver/mock/resolver.go
@@ -8,18 +8,14 @@ import (
 	"fmt"
 
 	"github.com/ethersphere/bee/pkg/resolver"
-	"github.com/ethersphere/bee/pkg/swarm"
 )
-
-// Address is the swarm bzz address.
-type Address = swarm.Address
 
 // Assure mock Resolver implements the Resolver interface.
 var _ resolver.Interface = (*Resolver)(nil)
 
 // Resolver is the mock Resolver implementation.
 type Resolver struct {
-	resolveFunc func(string) (Address, error)
+	resolveFunc func(string) (resolver.Address, error)
 }
 
 // Option function sets the option on the mock Resolver.
@@ -38,16 +34,16 @@ func NewResolver(opts ...Option) resolver.Interface {
 }
 
 // WithResolveFunc will override the Resolve function implementation.
-func WithResolveFunc(f func(string) (Address, error)) Option {
+func WithResolveFunc(f func(string) (resolver.Address, error)) Option {
 	return func(r *Resolver) {
 		r.resolveFunc = f
 	}
 }
 
 // Resolve implements the Resolver interface.
-func (r *Resolver) Resolve(name string) (Address, error) {
+func (r *Resolver) Resolve(name string) (resolver.Address, error) {
 	if r.resolveFunc != nil {
 		return r.resolveFunc(name)
 	}
-	return Address{}, fmt.Errorf("not implemented")
+	return resolver.Address{}, fmt.Errorf("not implemented")
 }
