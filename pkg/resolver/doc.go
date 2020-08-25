bfaa93beb8f4b6dcd3c2ba9c4d339d3865e853b2
Pavle Batuta <pavle@batuta.xyz>
2020-08-25 14:46:39 +0200

    Add multiresolver (#605)
    Multiresolver allows to resolve a name using multiple resolver chains.
    Chains are selected using the TLD of the name (if no TLD is present, we use the default).
    A chain resolves a name by consecutively calling Resolve on each element.
    The name is resolved when the first resolver succeeds. It fails if all resolvers fail.

---
+++ pkg/resolver/doc.go
@@ -0,0 +1,8 @@
+// Copyright 2020 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package resolver handles name resolution for the Swarm bee. It implements
+// the Resolver interface as well as clients to connect to external name
+// resolution services.
+package resolver
