b4dd1f3d8b21f8aa0cdc78076e5f16e4d437a645
acud <12988138+acud@users.noreply.github.com>
2020-09-02 18:22:59 +0200

    bump bee version (#666)
    * bump bee version to 0.3.0

--- version.go
+++ version.go
@@ -5,7 +5,7 @@
 package bee
 
 var (
-	version = "0.1.0" // manually set semantic version number
+	version = "0.3.0" // manually set semantic version number
 	commit  string    // automatically set git commit hash
 
 	Version = func() string {
