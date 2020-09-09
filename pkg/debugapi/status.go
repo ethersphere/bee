dbd3b7859df5573441f414f531762c4d0a527bd3
Janoš Guljaš <janos@users.noreply.github.com>
2020-09-09 11:41:44 +0200

    add git commit to version and expose version in debugapi (#684)

--- pkg/debugapi/status.go
+++ pkg/debugapi/status.go
@@ -7,15 +7,18 @@ package debugapi
 import (
 	"net/http"
 
+	"github.com/ethersphere/bee"
 	"github.com/ethersphere/bee/pkg/jsonhttp"
 )
 
 type statusResponse struct {
-	Status string `json:"status"`
+	Status  string `json:"status"`
+	Version string `json:"version"`
 }
 
 func (s *server) statusHandler(w http.ResponseWriter, r *http.Request) {
 	jsonhttp.OK(w, statusResponse{
-		Status: "ok",
+		Status:  "ok",
+		Version: bee.Version,
 	})
 }
