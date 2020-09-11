25d9b0094db8849e074a8daad7c04f989a2faac2
Janoš Guljaš <janos@users.noreply.github.com>
2020-09-11 15:20:00 +0200

    Revert "blocklist peer on retrieval timeout (#685)" (#692)
    This reverts commit c9bb655609bb81723b85192231119e08609a6e34.

--- pkg/recovery/repair_test.go
+++ pkg/recovery/repair_test.go
@@ -269,11 +269,13 @@ func newTestNetStore(t *testing.T, recoveryFunc recovery.RecoveryHook) storage.S
 		_, _, _ = f(peerID, 0)
 		return nil
 	}}
-	server := retrieval.New(nil, mockStorer, ps, logger, serverMockAccounting, nil, nil)
-	recorder := streamtest.NewRecorderDisconnecter(streamtest.New(
+	server := retrieval.New(nil, nil, logger, serverMockAccounting, nil, nil)
+	server.SetStorer(mockStorer)
+	recorder := streamtest.New(
 		streamtest.WithProtocols(server.Protocol()),
-	))
-	retrieve := retrieval.New(recorder, mockStorer, ps, logger, serverMockAccounting, pricerMock, nil)
+	)
+	retrieve := retrieval.New(recorder, ps, logger, serverMockAccounting, pricerMock, nil)
+	retrieve.SetStorer(mockStorer)
 	ns := netstore.New(storer, recoveryFunc, retrieve, logger, nil)
 	return ns
 }
