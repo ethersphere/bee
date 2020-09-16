75838eaf64c2c6b32588c24d62c783caaad2ddaa
Viktor Trón <viktor.tron@gmail.com>
2020-09-17 00:39:12 +0200

    trojan,pss: optimise trojan mining (#695)
    * trojan,pss: optimise trojan mining
     - introduce context to Wrap and eliminate MinerTimeout
     - simplify Wrap
     - add mine function that finds a nonce that makes a function true
     - introduce mining benchmarks
    
    * trojan: redo mine function to work with chunk not interface

--- pkg/trojan/error.go
+++ pkg/trojan/error.go
@@ -15,9 +15,6 @@ var (
 	// ErrVarLenTargets is returned when the given target list for a trojan chunk has addresses of different lengths
 	ErrVarLenTargets = errors.New("target list cannot have targets of different length")
 
-	// ErrUnMarshallingTrojanMessage is returned when a trojan message could not be de-serialized
+	// ErrUnmarshal is returned when a trojan message could not be de-serialized
 	ErrUnmarshal = errors.New("trojan message unmarshall error")
-
-	// ErrMinerTimeout is returned when mining a new nonce takes more time than swarm.TrojanMinerTimeout seconds
-	ErrMinerTimeout = errors.New("miner timeout error")
 )
