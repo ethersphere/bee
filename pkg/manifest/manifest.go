393fe0e466f6b9edbcbcc143cf4fe5089a8c1de3
acud <12988138+acud@users.noreply.github.com>
2020-09-24 14:10:00 +0200

    revert manifest

--- pkg/manifest/manifest.go
+++ pkg/manifest/manifest.go
@@ -52,9 +52,6 @@ func NewDefaultManifest(
 	encrypted bool,
 	storer storage.Storer,
 ) (Interface, error) {
-	if encrypted {
-		panic(0)
-	}
 	return NewManifest(DefaultManifestType, encrypted, storer)
 }
 
