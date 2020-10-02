dc120be3758b4d8b38381d1dc22287c6af4e79a2
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2020-10-02 19:32:48 +0200

    Redirect '/debug/pprof' to endpoint with slash (#789)

--- pkg/debugapi/router.go
+++ pkg/debugapi/router.go
@@ -32,7 +32,11 @@ func (s *server) setupRouting() {
 	router := mux.NewRouter()
 	router.NotFoundHandler = http.HandlerFunc(jsonhttp.NotFoundHandler)
 
-	router.Handle("/debug/pprof", http.HandlerFunc(pprof.Index))
+	router.Handle("/debug/pprof", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		u := r.URL
+		u.Path += "/"
+		http.Redirect(w, r, u.String(), http.StatusPermanentRedirect)
+	}))
 	router.Handle("/debug/pprof/cmdline", http.HandlerFunc(pprof.Cmdline))
 	router.Handle("/debug/pprof/profile", http.HandlerFunc(pprof.Profile))
 	router.Handle("/debug/pprof/symbol", http.HandlerFunc(pprof.Symbol))
