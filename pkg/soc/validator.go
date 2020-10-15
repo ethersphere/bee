887cef78af5d966b6d9eee8415327aff214ce1d0
Janos Guljas <janos@resenje.org>
2020-10-15 10:45:36 +0200

    fix comments in soc package

--- pkg/soc/validator.go
+++ pkg/soc/validator.go
@@ -1,6 +1,7 @@
 // Copyright 2020 The Swarm Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
 package soc
 
 import (
@@ -9,7 +10,7 @@ import (
 
 var _ swarm.Validator = (*Validator)(nil)
 
-// SocVaildator validates that the address of a given chunk
+// Validator validates that the address of a given chunk
 // is a single-owner chunk.
 type Validator struct {
 }
