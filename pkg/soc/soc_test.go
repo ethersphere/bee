96e346e7ad0096f15a213a150747702edb5ef23c
Janoš Guljaš <janos@users.noreply.github.com>
2020-10-15 11:03:47 +0200

    fix comments in soc package (#838)

--- pkg/soc/soc_test.go
+++ pkg/soc/soc_test.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package swarm contains most basic and general Swarm concepts.
 package soc_test
 
 import (
