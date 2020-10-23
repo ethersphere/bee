5c235062ba3649a0cc34be5923358664b5a459eb
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2020-10-23 13:24:48 +0200

    Document 'jsonhttp' package(s)

---
+++ pkg/jsonhttp/doc.go
@@ -0,0 +1,9 @@
+// Copyright 2020 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package jsonhttp contains utility functions that make it easier to create
+JSON-based HTTP APIs.
+*/
+package jsonhttp
