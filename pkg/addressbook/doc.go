41ed89d5f07d1189f85b59eacb1304c4b4c8f9c5
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2020-10-23 19:56:04 +0200

    Fix 'addressbook' doc

--- pkg/addressbook/doc.go
+++ pkg/addressbook/doc.go
@@ -5,6 +5,7 @@
 /*
 Package addressbook provides persisted mapping between overlay (topology) address
 and bzz.Address address, which contains underlay (physical) address.
+The underlay address contains both physical and p2p addresses.
 
 It is single point of truth about known peers and relations of their
 overlay and underlay addresses.
