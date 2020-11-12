d6b4ef80b3ef843b61e097959830df5ecff956e1
Metacertain <metacertain@gmail.com>
2020-11-12 16:39:03 +0100

    Include Kardianos as hack for running as windows service

--- cmd/bee/main.go
+++ cmd/bee/main.go
@@ -6,14 +6,53 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/ethersphere/bee/cmd/bee/cmd"
+	"github.com/kardianos/service"
 )
 
-func main() {
+var logger service.Logger
+
+type program struct{}
+
+func (p *program) run() {
 	if err := cmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, "Error:", err)
 		os.Exit(1)
+	}
+}
+
+func (p *program) Start(s service.Service) error {
+	// Start should not block. Do the actual work async.
+	go p.run()
+	return nil
+}
+
+func (p *program) Stop(s service.Service) error {
+	// Stop should not block. Return with a few seconds.
+	return nil
+}
+
+func main() {
+	svcConfig := &service.Config{
+		Name:        "bee",
+		DisplayName: "Bee",
+		Description: "This is an example Go service.",
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger, err = s.Logger(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = s.Run()
+	if err != nil {
+		logger.Error(err)
 	}
 }
