0000fb95b0956ed1d14716e015d0004074b4abe8
Ivan Vandot <ivan@vandot.rs>
2020-11-13 13:49:36 +0100

    run as a service only with start

--- cmd/bee/main.go
+++ cmd/bee/main.go
@@ -6,47 +6,14 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/ethersphere/bee/cmd/bee/cmd"
-	"github.com/kardianos/service"
 )
 
-type program struct{}
-
-func (p *program) run() {
+func main() {
 	if err := cmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, "Error:", err)
 		os.Exit(1)
-	}
-}
-
-func (p *program) Start(s service.Service) error {
-	// Start should not block. Do the actual work async.
-	go p.run()
-	return nil
-}
-
-func (p *program) Stop(s service.Service) error {
-	// Stop should not block. Return with a few seconds.
-	return nil
-}
-
-func main() {
-	svcConfig := &service.Config{
-		Name:        "BeeSvc",
-		DisplayName: "Bee",
-		Description: "Bee, Swarm client.",
-	}
-
-	prg := &program{}
-	s, err := service.New(prg, svcConfig)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = s.Run()
-	if err != nil {
-		log.Fatal(err)
 	}
 }
