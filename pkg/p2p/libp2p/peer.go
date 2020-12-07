bf1d9c2e37fe0478f5dc952e961655bbc8693d5d
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2020-12-07 22:58:49 +0100

    Resolve peer topology inconsistencies (#1032)

--- pkg/p2p/libp2p/peer.go
+++ pkg/p2p/libp2p/peer.go
@@ -14,6 +14,7 @@ import (
 	"github.com/ethersphere/bee/pkg/swarm"
 	"github.com/libp2p/go-libp2p-core/network"
 	libp2ppeer "github.com/libp2p/go-libp2p-core/peer"
+	ma "github.com/multiformats/go-multiaddr"
 )
 
 type peerRegistry struct {
@@ -161,6 +162,35 @@ func (r *peerRegistry) overlay(peerID libp2ppeer.ID) (swarm.Address, bool) {
 	overlay, found := r.overlays[peerID]
 	r.mu.RUnlock()
 	return overlay, found
+}
+
+func (r *peerRegistry) isConnected(peerID libp2ppeer.ID, remoteAddr ma.Multiaddr) (swarm.Address, bool) {
+	if remoteAddr == nil {
+		return swarm.ZeroAddress, false
+	}
+
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	overlay, found := r.overlays[peerID]
+	if !found {
+		return swarm.ZeroAddress, false
+	}
+
+	// check connection remote address
+	conns, ok := r.connections[peerID]
+	if !ok {
+		return swarm.ZeroAddress, false
+	}
+
+	for c := range conns {
+		if c.RemoteMultiaddr().Equal(remoteAddr) {
+			// we ARE connected to the peer on expected address
+			return overlay, true
+		}
+	}
+
+	return swarm.ZeroAddress, false
 }
 
 func (r *peerRegistry) remove(overlay swarm.Address) (bool, libp2ppeer.ID) {
