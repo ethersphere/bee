b7c63c8d33b0785aee45add1620ccea0eed881b2
acud <12988138+acud@users.noreply.github.com>
2020-12-15 17:07:51 +0100

    Storage incentives: add stamper putter to api

--- pkg/postage/stamper.go
+++ pkg/postage/stamper.go
@@ -16,21 +16,25 @@ var (
 	ErrBucketFull = errors.New("bucket full")
 )
 
-// Stamper connects a stampissuer with a signer.
-// A Stamper is created for each upload session.
-type Stamper struct {
+type Stamper interface {
+	Stamp(swarm.Address) (*Stamp, error)
+}
+
+// stamper connects a stampissuer with a signer.
+// A stamper is created for each upload session.
+type stamper struct {
 	issuer *StampIssuer
 	signer crypto.Signer
 }
 
 // NewStamper constructs a Stamper.
-func NewStamper(st *StampIssuer, signer crypto.Signer) *Stamper {
-	return &Stamper{st, signer}
+func NewStamper(st *StampIssuer, signer crypto.Signer) Stamper {
+	return &stamper{st, signer}
 }
 
 // Stamp takes chunk, see if the chunk can included in the batch and
 // signs it with the owner of the batch of this Stamp issuer.
-func (st *Stamper) Stamp(addr swarm.Address) (*Stamp, error) {
+func (st *stamper) Stamp(addr swarm.Address) (*Stamp, error) {
 	toSign, err := toSignDigest(addr, st.issuer.batchID)
 	if err != nil {
 		return nil, err
