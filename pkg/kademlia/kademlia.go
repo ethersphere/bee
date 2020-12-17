bc3ef51dd4e7571e21a91fcfc7a48fd0a9487c2b
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2020-12-17 11:20:11 +0100

    Remove no longer needed check

--- pkg/kademlia/kademlia.go
+++ pkg/kademlia/kademlia.go
@@ -336,13 +336,11 @@ func (k *Kad) connect(ctx context.Context, peer swarm.Address, ma ma.Multiaddr, 
 	i, err := k.p2p.Connect(ctx, ma)
 	if err != nil {
 		if errors.Is(err, p2p.ErrAlreadyConnected) {
-			if i == nil || swarm.ZeroAddress.Equal(i.Overlay) || i.Overlay.Equal(peer) {
-				return nil
-			}
-
 			if !i.Overlay.Equal(peer) {
 				return errOverlayMismatch
 			}
+
+			return nil
 		}
 
 		k.logger.Debugf("could not connect to peer %s: %v", peer, err)
