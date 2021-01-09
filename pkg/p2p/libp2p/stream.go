b280b8b208f6eb486d5c1a82e8a2f4d4bbc3f8f3
acud <12988138+acud@users.noreply.github.com>
2021-01-09 11:10:11 +0100

    works

--- pkg/p2p/libp2p/stream.go
+++ pkg/p2p/libp2p/stream.go
@@ -5,6 +5,10 @@
 package libp2p
 
 import (
+	"errors"
+	"io"
+	"time"
+
 	"github.com/ethersphere/bee/pkg/p2p"
 	"github.com/libp2p/go-libp2p-core/network"
 )
@@ -28,6 +32,28 @@ func (s *stream) Headers() p2p.Headers {
 }
 
 func (s *stream) FullClose() error {
-	_ = s.CloseWrite() // this needs to change. since FullClose no longer exists
-	s.Read()
+	if err := s.CloseWrite(); err != nil {
+		s.Reset()
+		return err
+	}
+	// So we don't wait forever
+	s.SetDeadline(time.Now().Add(5 * time.Second))
+
+	// We *have* to observe the EOF. Otherwise, we leak the stream.
+	// Now, technically, we should do this *before*
+	// returning from SendMessage as the message
+	// hasn't really been sent yet until we see the
+	// EOF but we don't actually *know* what
+	// protocol the other side is speaking.
+	n, err := s.Read([]byte{0})
+	if n > 0 || err == nil {
+		s.Reset()
+		return errors.New("expected eof") //ErrExpectedEOF
+	}
+	if err != io.EOF {
+		s.Reset()
+		return err
+	}
+	return nil
+
 }
