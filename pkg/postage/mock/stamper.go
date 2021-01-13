8ebfc51611bf5ded608123c6c9f701d48dbb4429
Pavle Batuta <pavle@batuta.xyz>
2021-01-13 13:02:13 +0100

    Postage BatchStore and BatchService (#1070)
    Co-authored-by: zelig <viktor.tron@gmail.com>

--- pkg/postage/mock/stamper.go
+++ pkg/postage/mock/stamper.go
@@ -1,3 +1,7 @@
+// Copyright 2020 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package mock
 
 import (
@@ -7,10 +11,12 @@ import (
 
 type mockStamper struct{}
 
+// NewStamper returns anew new mock stamper.
 func NewStamper() postage.Stamper {
 	return &mockStamper{}
 }
 
+// Stamp implements the Stamper interface. It returns an empty postage stamp.
 func (mockStamper) Stamp(_ swarm.Address) (*postage.Stamp, error) {
 	return &postage.Stamp{}, nil
 }
--- pkg/postage/stamp.go
+++ pkg/postage/stamp.go
@@ -6,9 +6,7 @@ package postage
 
 import (
 	"bytes"
-	"encoding/binary"
 	"errors"
-	"math/big"
 
 	"github.com/ethersphere/bee/pkg/crypto"
 	"github.com/ethersphere/bee/pkg/swarm"
@@ -23,38 +21,6 @@ var (
 	// ErrStampInvalid is the error given if stamp cannot deserialise.
 	ErrStampInvalid = errors.New("invalid stamp")
 )
-
-// Batch represents a postage batch, a payment on the blockchain.
-type Batch struct {
-	ID    []byte   // batch ID
-	Value *big.Int // overall balance of the batch
-	Start uint64   // blocknumber the batch was created
-	Owner []byte   // owner's ethereum address
-	Depth uint8    // batch depth, i.e., size = 2^{depth}
-}
-
-// MarshalBinary serialises a postage batch to a byte slice len 117.
-func (b *Batch) MarshalBinary() ([]byte, error) {
-	out := make([]byte, 93)
-	copy(out, b.ID)
-	value := b.Value.Bytes()
-	copy(out[64-len(value):], value)
-	binary.BigEndian.PutUint64(out[64:72], b.Start)
-	copy(out[72:], b.Owner)
-	out[92] = b.Depth
-	return out, nil
-}
-
-// UnmarshalBinary deserialises the batch.
-// Unsafe on slice index (len(buf) = 117) as only internally used in db.
-func (b *Batch) UnmarshalBinary(buf []byte) error {
-	b.ID = buf[:32]
-	b.Value = big.NewInt(0).SetBytes(buf[32:64])
-	b.Start = binary.BigEndian.Uint64(buf[64:72])
-	b.Owner = buf[72:92]
-	b.Depth = buf[92]
-	return nil
-}
 
 // Valid checks the validity of the postage stamp; in particular:
 // - authenticity - check batch is valid on the blockchain
@@ -88,22 +54,23 @@ type Stamp struct {
 	sig     []byte // common r[32]s[32]v[1]-style 65 byte ECDSA signature
 }
 
-// MewStamp constructs a stamp
+// NewStamp constructs a new stamp from a given batch ID and signature.
 func NewStamp(batchID, sig []byte) *Stamp {
 	return &Stamp{batchID, sig}
 }
 
-// BatchID
+// BatchID returns the batch ID of the stamp.
 func (s *Stamp) BatchID() []byte {
 	return s.batchID
 }
 
-// Sig
+// Sig returns the signature of the stamp.
 func (s *Stamp) Sig() []byte {
 	return s.sig
 }
 
-// MarshalBinary gives the byte slice serialisation of a stamp: batchID[32]|Signature[65].
+// MarshalBinary gives the byte slice serialisation of a stamp:
+// batchID[32]|Signature[65].
 func (s *Stamp) MarshalBinary() ([]byte, error) {
 	buf := make([]byte, StampSize)
 	copy(buf, s.batchID)
@@ -121,7 +88,8 @@ func (s *Stamp) UnmarshalBinary(buf []byte) error {
 	return nil
 }
 
-// toSignDigest creates a digest to represent the stamp which is to be signed by the owner.
+// toSignDigest creates a digest to represent the stamp which is to be signed by
+// the owner.
 func toSignDigest(addr swarm.Address, id []byte) ([]byte, error) {
 	h := swarm.NewHasher()
 	_, err := h.Write(addr.Bytes())
