bfdc4ad6fe636534c1f0288604a3c3a81d373a0e
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2021-01-14 11:51:10 +0100

    Remove spaces

--- pkg/retrieval/retrieval.go
+++ pkg/retrieval/retrieval.go
@@ -134,11 +134,9 @@ func (s *Service) RetrieveChunk(ctx context.Context, addr swarm.Address) (swarm.
 				// break
 			case chunk := <-resultC:
 				peersResults++
-
 				return chunk, nil
 			case <-errC:
 				peersResults++
-
 				break
 			case <-ctx.Done():
 				logger.Tracef("retrieval: failed to get chunk %s: %v", addr, ctx.Err())
