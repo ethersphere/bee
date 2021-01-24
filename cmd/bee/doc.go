74ab79b0668ad0d04af3e8b53203315e1592c6c4
eknir <rinkehendriksen@gmail.com>
2021-01-24 14:50:00 +0100

    add godoc for bee package

---
+++ cmd/bee/doc.go
@@ -0,0 +1,8 @@
+// Copyright 2021 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package bee provides the the initial entrypoint into the bee client
+*/
+package bee
