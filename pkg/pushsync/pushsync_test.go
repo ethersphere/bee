6f13dbf4aa8cf89c77368be943e2fc6144d5fcb9
Metacertain <metacertain@gmail.com>
2021-01-24 19:27:33 +0100

    Push sync test make address returned by mock match address

--- pkg/pushsync/pushsync_test.go
+++ pkg/pushsync/pushsync_test.go
@@ -403,7 +403,7 @@ func createPushSyncNode(t *testing.T, addr swarm.Address, recorder *streamtest.R
 	mockAccounting := accountingmock.NewAccounting()
 
 	readPricingResponseFunc := func(receivedHeaders p2p.Headers) (swarm.Address, uint64, uint8, error) {
-		return swarm.MustParseHexAddress("0033153ac8cfb0c343db1795f578c15ed8ef827f3e68ed3c58329900bf0d7276"), 10, 0, nil
+		return swarm.MustParseHexAddress("70002115a015d40a1f5ef68c29d072f06fae58854934c1cb399fcb63cf336127"), 10, 0, nil
 	}
 
 	readPriceFunc := func(receivedHeaders p2p.Headers) (uint64, error) {
