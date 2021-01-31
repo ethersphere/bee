84a95f97bc27714c4cd56c03618a8bf16bf17528
Metacertain <metacertain@gmail.com>
2021-01-31 01:47:50 +0100

    Retrieval test for update

--- pkg/pricer/mock/pricer.go
+++ pkg/pricer/mock/pricer.go
@@ -10,24 +10,19 @@ import (
 )
 
 type Service struct {
-	peerPrice                      uint64
-	price                          uint64
-	peerPriceFunc                  func(peer, chunk swarm.Address) uint64
-	priceForPeerFunc               func(peer, chunk swarm.Address) uint64
-	priceTableFunc                 func() (priceTable []uint64)
-	peerPriceTableFunc             func(peer, chunk swarm.Address) (priceTable []uint64, err error)
-	pricePOFunc                    func(PO uint8) (uint64, error)
-	peerPricePOFunc                func(peer swarm.Address, PO uint8) (uint64, error)
-	notifyPriceTableFunc           func(peer swarm.Address, priceTable []uint64) error
-	defaultPriceTableFunc          func() (priceTable []uint64)
-	defaultPriceFunc               func(PO uint8) uint64
-	priceHeadlerFunc               func(p2p.Headers, swarm.Address) p2p.Headers
-	makePricingHeadersFunc         func(uint64, swarm.Address) (p2p.Headers, error)
-	readPricingHeadersFunc         func(p2p.Headers) (swarm.Address, uint64, error)
-	readPriceHeaderFunc            func(p2p.Headers) (uint64, error)
-	notifyPeerPriceFunc            func(peer swarm.Address, price uint64, index uint8) error
-	makePricingResponseHeadersFunc func(chunkPrice uint64, addr swarm.Address, index uint8) (p2p.Headers, error)
-	readPricingResponseHeadersFunc func(receivedHeaders p2p.Headers) (swarm.Address, uint64, uint8, error)
+	peerPrice             uint64
+	price                 uint64
+	peerPriceFunc         func(peer, chunk swarm.Address) uint64
+	priceForPeerFunc      func(peer, chunk swarm.Address) uint64
+	priceTableFunc        func() (priceTable []uint64)
+	peerPriceTableFunc    func(peer swarm.Address) (priceTable []uint64, err error)
+	pricePOFunc           func(PO uint8) (uint64, error)
+	peerPricePOFunc       func(peer swarm.Address, PO uint8) (uint64, error)
+	notifyPriceTableFunc  func(peer swarm.Address, priceTable []uint64) error
+	defaultPriceTableFunc func() (priceTable []uint64)
+	defaultPriceFunc      func(PO uint8) uint64
+	priceHeadlerFunc      func(p2p.Headers, swarm.Address) p2p.Headers
+	notifyPeerPriceFunc   func(peer swarm.Address, price uint64, index uint8) error
 }
 
 // WithReserveFunc sets the mock Reserve function
@@ -44,34 +39,22 @@ func WithPriceForPeerFunc(f func(peer, chunk swarm.Address) uint64) Option {
 	})
 }
 
+func WithPrice(p uint64) Option {
+	return optionFunc(func(s *Service) {
+		s.price = p
+	})
+}
+
+func WithPeerPrice(p uint64) Option {
+	return optionFunc(func(s *Service) {
+		s.price = p
+	})
+}
+
 // WithPriceTableFunc sets the mock Release function
 func WithPriceTableFunc(f func() (priceTable []uint64)) Option {
 	return optionFunc(func(s *Service) {
 		s.priceTableFunc = f
-	})
-}
-
-func WithMakePricingHeadersFunc(f func(uint64, swarm.Address) (p2p.Headers, error)) Option {
-	return optionFunc(func(s *Service) {
-		s.makePricingHeadersFunc = f
-	})
-}
-
-func WithReadPricingHeadersFunc(f func(receivedHeaders p2p.Headers) (swarm.Address, uint64, error)) Option {
-	return optionFunc(func(s *Service) {
-		s.readPricingHeadersFunc = f
-	})
-}
-
-func WithReadPricingResponseHeadersFunc(f func(receivedHeaders p2p.Headers) (swarm.Address, uint64, uint8, error)) Option {
-	return optionFunc(func(s *Service) {
-		s.readPricingResponseHeadersFunc = f
-	})
-}
-
-func WithReadPriceHeaderFunc(f func(receivedHeaders p2p.Headers) (uint64, error)) Option {
-	return optionFunc(func(s *Service) {
-		s.readPriceHeaderFunc = f
 	})
 }
 
@@ -111,9 +94,9 @@ func (pricer *Service) PriceTable() (priceTable []uint64) {
 	}
 	return nil
 }
-func (pricer *Service) PeerPriceTable(peer, chunk swarm.Address) (priceTable []uint64, err error) {
+func (pricer *Service) PeerPriceTable(peer swarm.Address) (priceTable []uint64, err error) {
 	if pricer.peerPriceTableFunc != nil {
-		return pricer.peerPriceTableFunc(peer, chunk)
+		return pricer.peerPriceTableFunc(peer)
 	}
 	return nil, nil
 }
@@ -155,46 +138,11 @@ func (pricer *Service) PriceHeadler(headers p2p.Headers, addr swarm.Address) p2p
 	return p2p.Headers{}
 }
 
-func (pricer *Service) MakePricingHeaders(chunkPrice uint64, addr swarm.Address) (p2p.Headers, error) {
-	if pricer.makePricingHeadersFunc != nil {
-		return pricer.makePricingHeadersFunc(chunkPrice, addr)
-	}
-	return p2p.Headers{}, nil
-}
-
-func (pricer *Service) ReadPricingHeaders(receivedHeaders p2p.Headers) (swarm.Address, uint64, error) {
-	if pricer.readPricingHeadersFunc != nil {
-		return pricer.readPricingHeadersFunc(receivedHeaders)
-	}
-	return swarm.Address{}, 0, nil
-}
-
-func (pricer *Service) ReadPriceHeader(receivedHeaders p2p.Headers) (uint64, error) {
-	if pricer.readPriceHeaderFunc != nil {
-		return pricer.readPriceHeaderFunc(receivedHeaders)
-	}
-	return 0, nil
-}
-
 func (pricer *Service) NotifyPeerPrice(peer swarm.Address, price uint64, index uint8) error {
 	if pricer.notifyPeerPriceFunc != nil {
 		return pricer.notifyPeerPriceFunc(peer, price, index)
 	}
 	return nil
-}
-
-func (pricer *Service) MakePricingResponseHeaders(chunkPrice uint64, addr swarm.Address, index uint8) (p2p.Headers, error) {
-	if pricer.makePricingResponseHeadersFunc != nil {
-		return pricer.makePricingResponseHeadersFunc(chunkPrice, addr, index)
-	}
-	return p2p.Headers{}, nil
-}
-
-func (pricer *Service) ReadPricingResponseHeaders(receivedHeaders p2p.Headers) (swarm.Address, uint64, uint8, error) {
-	if pricer.readPricingResponseHeadersFunc != nil {
-		return pricer.readPricingResponseHeadersFunc(receivedHeaders)
-	}
-	return swarm.ZeroAddress, 0, 0, nil
 }
 
 // Option is the option passed to the mock accounting service
