99d11f18767b2a83285bfd2c1e7649aa391e7567
Metacertain <metacertain@gmail.com>
2021-01-31 01:47:50 +0100

    Retrieval test for update

--- pkg/retrieval/retrieval_test.go
+++ pkg/retrieval/retrieval_test.go
@@ -138,6 +138,113 @@ func TestDelivery(t *testing.T) {
 	}
 }
 
+// TestDelivery tests that a naive request -> delivery flow works.
+func TestDeliveryWithPriceUpdate(t *testing.T) {
+	logger := logging.New(os.Stdout, 5)
+	mockStorer := storemock.NewStorer()
+	chunk := testingc.FixtureChunk("0033")
+
+	// put testdata in the mock store of the server
+	_, err := mockStorer.Put(context.Background(), storage.ModePutUpload, chunk)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	serverMockAccounting := accountingmock.NewAccounting()
+
+	price := uint64(10)
+	serverPrice := uint64(17)
+
+	headlerFunc := func(h p2p.Headers, a swarm.Address) p2p.Headers {
+		headers, _ := headerutils.MakePricingResponseHeaders(serverPrice, chunk.Address(), 5)
+		return headers
+	}
+
+	clientPricerMock := pricermock.NewMockService(pricermock.WithPeerPrice(price))
+	serverPricerMock := pricermock.NewMockService(pricermock.WithPriceHeadlerFunc(headlerFunc), pricermock.WithPrice(serverPrice))
+
+	// create the server that will handle the request and will serve the response
+	server := retrieval.New(swarm.MustParseHexAddress("0034"), mockStorer, nil, nil, logger, serverMockAccounting, serverPricerMock, nil)
+	recorder := streamtest.New(
+		streamtest.WithProtocols(server.Protocol()),
+	)
+
+	clientMockAccounting := accountingmock.NewAccounting()
+
+	// client mock storer does not store any data at this point
+	// but should be checked at at the end of the test for the
+	// presence of the chunk address key and value to ensure delivery
+	// was successful
+	clientMockStorer := storemock.NewStorer()
+
+	peerID := swarm.MustParseHexAddress("9ee7add7")
+	ps := mockPeerSuggester{eachPeerRevFunc: func(f topology.EachPeerFunc) error {
+		_, _, _ = f(peerID, 0)
+		return nil
+	}}
+	client := retrieval.New(swarm.MustParseHexAddress("9ee7add8"), clientMockStorer, recorder, ps, logger, clientMockAccounting, clientPricerMock, nil)
+	ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+	defer cancel()
+	v, err := client.RetrieveChunk(ctx, chunk.Address())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(v.Data(), chunk.Data()) {
+		t.Fatalf("request and response data not equal. got %s want %s", v, chunk.Data())
+	}
+	records, err := recorder.Records(peerID, "retrieval", "1.0.0", "retrieval")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if l := len(records); l != 1 {
+		t.Fatalf("got %v records, want %v", l, 1)
+	}
+
+	record := records[0]
+
+	messages, err := protobuf.ReadMessages(
+		bytes.NewReader(record.In()),
+		func() protobuf.Message { return new(pb.Request) },
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var reqs []string
+	for _, m := range messages {
+		reqs = append(reqs, hex.EncodeToString(m.(*pb.Request).Addr))
+	}
+
+	if len(reqs) != 1 {
+		t.Fatalf("got too many requests. want 1 got %d", len(reqs))
+	}
+
+	messages, err = protobuf.ReadMessages(
+		bytes.NewReader(record.Out()),
+		func() protobuf.Message { return new(pb.Delivery) },
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var gotDeliveries []string
+	for _, m := range messages {
+		gotDeliveries = append(gotDeliveries, string(m.(*pb.Delivery).Data))
+	}
+
+	if len(gotDeliveries) != 1 {
+		t.Fatalf("got too many deliveries. want 1 got %d", len(gotDeliveries))
+	}
+
+	clientBalance, _ := clientMockAccounting.Balance(peerID)
+	if clientBalance != -int64(serverPrice) {
+		t.Fatalf("unexpected balance on client. want %d got %d", -serverPrice, clientBalance)
+	}
+
+	serverBalance, _ := serverMockAccounting.Balance(peerID)
+	if serverBalance != int64(serverPrice) {
+		t.Fatalf("unexpected balance on server. want %d got %d", serverPrice, serverBalance)
+	}
+}
+
 func TestRetrieveChunk(t *testing.T) {
 	logger := logging.New(os.Stdout, 5)
 
