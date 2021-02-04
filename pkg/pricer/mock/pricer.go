e3f38b54862ca589e4c00d5be4ef41a695338124
Metacertain <metacertain@gmail.com>
2021-02-04 12:40:28 +0100

    Unexport / export for tests non interface pricer functions

--- pkg/pricer/mock/pricer.go
+++ pkg/pricer/mock/pricer.go
@@ -10,19 +10,14 @@ import (
 )
 
 type Service struct {
-	peerPrice             uint64
-	price                 uint64
-	peerPriceFunc         func(peer, chunk swarm.Address) uint64
-	priceForPeerFunc      func(peer, chunk swarm.Address) uint64
-	priceTableFunc        func() (priceTable []uint64)
-	peerPriceTableFunc    func(peer swarm.Address) (priceTable []uint64, err error)
-	pricePOFunc           func(PO uint8) (uint64, error)
-	peerPricePOFunc       func(peer swarm.Address, PO uint8) (uint64, error)
-	notifyPriceTableFunc  func(peer swarm.Address, priceTable []uint64) error
-	defaultPriceTableFunc func() (priceTable []uint64)
-	defaultPriceFunc      func(PO uint8) uint64
-	priceHeadlerFunc      func(p2p.Headers, swarm.Address) p2p.Headers
-	notifyPeerPriceFunc   func(peer swarm.Address, price uint64, index uint8) error
+	peerPrice            uint64
+	price                uint64
+	peerPriceFunc        func(peer, chunk swarm.Address) uint64
+	priceForPeerFunc     func(peer, chunk swarm.Address) uint64
+	priceTableFunc       func() (priceTable []uint64)
+	notifyPriceTableFunc func(peer swarm.Address, priceTable []uint64) error
+	priceHeadlerFunc     func(p2p.Headers, swarm.Address) p2p.Headers
+	notifyPeerPriceFunc  func(peer swarm.Address, price uint64, index uint8) error
 }
 
 // WithReserveFunc sets the mock Reserve function
@@ -94,41 +89,11 @@ func (pricer *Service) PriceTable() (priceTable []uint64) {
 	}
 	return nil
 }
-func (pricer *Service) PeerPriceTable(peer swarm.Address) (priceTable []uint64, err error) {
-	if pricer.peerPriceTableFunc != nil {
-		return pricer.peerPriceTableFunc(peer)
-	}
-	return nil, nil
-}
-func (pricer *Service) PricePO(PO uint8) (uint64, error) {
-	if pricer.pricePOFunc != nil {
-		return pricer.pricePOFunc(PO)
-	}
-	return 0, nil
-}
-func (pricer *Service) PeerPricePO(peer swarm.Address, PO uint8) (uint64, error) {
-	if pricer.peerPricePOFunc != nil {
-		return pricer.peerPricePOFunc(peer, PO)
-	}
-	return 0, nil
-}
 func (pricer *Service) NotifyPriceTable(peer swarm.Address, priceTable []uint64) error {
 	if pricer.notifyPriceTableFunc != nil {
 		return pricer.notifyPriceTableFunc(peer, priceTable)
 	}
 	return nil
-}
-func (pricer *Service) DefaultPriceTable() (priceTable []uint64) {
-	if pricer.defaultPriceTableFunc != nil {
-		return pricer.defaultPriceTableFunc()
-	}
-	return nil
-}
-func (pricer *Service) DefaultPrice(PO uint8) uint64 {
-	if pricer.defaultPriceFunc != nil {
-		return pricer.defaultPriceFunc(PO)
-	}
-	return 0
 }
 
 func (pricer *Service) PriceHeadler(headers p2p.Headers, addr swarm.Address) p2p.Headers {
