b624cc649f3cc6fd8211f3ca200ff5f03dc8386c
Metacertain <metacertain@gmail.com>
2021-02-11 16:57:27 +0100

    Review comments pt V

---
+++ pkg/pricer/headerutils/export_test.go
@@ -0,0 +1,11 @@
+// Copyright 2021 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package headerutils
+
+const (
+	PriceFieldName  = priceFieldName
+	TargetFieldName = targetFieldName
+	IndexFieldName  = indexFieldName
+)
