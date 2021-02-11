92f26615f76518bc7a2e71d9ba8fac46b4f4aadd
acud <12988138+acud@users.noreply.github.com>
2021-02-11 14:55:26 +0100

    Update pkg/p2p/p2p.go
    Co-authored-by: Rinke Hendriksen <33000441+Eknir@users.noreply.github.com>

--- pkg/p2p/p2p.go
+++ pkg/p2p/p2p.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package p2p provides the p2p abstractions used
+// Package p2p provides the peer-to-peer abstractions used
 // across different protocols in Bee.
 package p2p
 
