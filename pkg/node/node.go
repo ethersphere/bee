5ddfd08bbe0446a36d1717d43f6537227ce60acd
Metacertain <metacertain@gmail.com>
2021-02-15 13:26:57 +0100

    Readd SetPriceTableObserver

--- pkg/node/node.go
+++ pkg/node/node.go
@@ -329,6 +329,7 @@ func NewBee(addr string, swarmAddress swarm.Address, publicKey ecdsa.PublicKey, 
 	}
 	pricer := pricer.New(logger, stateStore, kad, swarmAddress, 1000000000)
 	pricing := pricing.New(p2ps, logger, paymentThreshold, pricer)
+	pricing.SetPriceTableObserver(pricer)
 
 	if err = p2ps.AddProtocol(pricing.Protocol()); err != nil {
 		return nil, fmt.Errorf("pricing service: %w", err)
