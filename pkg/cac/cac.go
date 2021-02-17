a4cfcbbceba7085e57dbdc201279978b77dfef4a
Rodrigo Q. Saramago <rodrigoqsaramago@gmail.com>
2021-02-17 21:51:33 +0100

    cac refactor

--- pkg/cac/cac.go
+++ pkg/cac/cac.go
@@ -2,25 +2,59 @@ package cac
 
 import (
 	"encoding/binary"
+	"errors"
 
 	"github.com/ethersphere/bee/pkg/bmtpool"
 	"github.com/ethersphere/bee/pkg/swarm"
 )
 
+var (
+	errTooShortChunkData = errors.New("short chunk data")
+	errTooLargeChunkData = errors.New("data too large")
+)
+
+// New creates a new content address chunk by initializing a span and appending the data to it.
 func New(data []byte) (swarm.Chunk, error) {
-	hasher := bmtpool.Get()
-	defer bmtpool.Put(hasher)
-
-	_, err := hasher.Write(data)
-	if err != nil {
-		return nil, err
-	}
-	span := make([]byte, 8)
+	span := make([]byte, swarm.SpanSize)
 	binary.LittleEndian.PutUint64(span, uint64(len(data)))
-	err = hasher.SetSpanBytes(span)
+	return newWithSpan(data, span)
+}
+
+// NewWithDataSpan creates a new chunk assuming that the span precedes the actual data.
+func NewWithDataSpan(data []byte) (swarm.Chunk, error) {
+	if len(data) > swarm.ChunkSize+swarm.SpanSize {
+		return nil, errTooLargeChunkData
+	}
+
+	if len(data) < swarm.SpanSize {
+		return nil, errTooShortChunkData
+	}
+
+	return newWithSpan(data[swarm.SpanSize:], data[:swarm.SpanSize])
+}
+
+// newWithSpan creates a new chunk prepending the given span to the data.
+func newWithSpan(data []byte, span []byte) (swarm.Chunk, error) {
+	h := hasher(data)
+	hash, err := h(span)
 	if err != nil {
 		return nil, err
 	}
+	return swarm.NewChunk(swarm.NewAddress(hash), append(span, data...)), nil
+}
 
-	return swarm.NewChunk(swarm.NewAddress(hasher.Sum(nil)), append(span, data...)), nil
+// hasher is a helper function to hash a given data based on the given span.
+func hasher(data []byte) func([]byte) ([]byte, error) {
+	return func(span []byte) ([]byte, error) {
+		hasher := bmtpool.Get()
+		defer bmtpool.Put(hasher)
+
+		if err := hasher.SetSpanBytes(span); err != nil {
+			return nil, err
+		}
+		if _, err := hasher.Write(data); err != nil {
+			return nil, err
+		}
+		return hasher.Sum(nil), nil
+	}
 }
--- pkg/cac/cac_test.go
+++ pkg/cac/cac_test.go
@@ -1,16 +1,17 @@
 package cac_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/ethersphere/bee/pkg/cac"
 	"github.com/ethersphere/bee/pkg/swarm"
 )
 
-func TestCac(t *testing.T) {
-	bmtHashOfFoo := "2387e8e7d8a48c2a9339c97c1dc3461a9a7aa07e994c5cb8b38fd7c1b3e6ea48"
+func TestNewCAC(t *testing.T) {
+	foo := "greaterthanspan"
+	bmtHashOfFoo := "27913f1bdb6e8e52cbd5a5fd4ab577c857287edf6969b41efe926b51de0f4f23"
 	address := swarm.MustParseHexAddress(bmtHashOfFoo)
-	foo := "foo"
 
 	c, err := cac.New([]byte(foo))
 	if err != nil {
@@ -19,5 +20,52 @@ func TestCac(t *testing.T) {
 
 	if !c.Address().Equal(address) {
 		t.Fatalf("address mismatch. got %s want %s", c.Address().String(), address.String())
+	}
+}
+
+func TestNewWithDataSpan(t *testing.T) {
+	foo := "greaterthanspan"
+	bmtHashOfFoo := "95022e6af5c6d6a564ee55a67f8455a3e18c511b5697c932d9e44f07f2fb8c53"
+	address := swarm.MustParseHexAddress(bmtHashOfFoo)
+
+	c, err := cac.NewWithDataSpan([]byte(foo))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !c.Address().Equal(address) {
+		t.Fatalf("address mismatch. got %s want %s", c.Address().String(), address.String())
+	}
+}
+
+var testChunkData = []struct {
+	name string
+	data []byte
+	err  error
+}{
+	{
+		name: "too short data chunk",
+		data: []byte(strings.Repeat("a", swarm.SpanSize-1)),
+		err:  cac.ErrTooShortChunkData,
+	},
+	{
+		name: "too large data chunk",
+		data: []byte(strings.Repeat("a", swarm.ChunkSize+swarm.SpanSize+1)),
+		err:  cac.ErrTooLargeChunkData,
+	},
+}
+
+func TestChunkInvariants(t *testing.T) {
+	for _, cc := range testChunkData {
+		t.Run(cc.name, func(t *testing.T) {
+			_, err := cac.NewWithDataSpan(cc.data)
+			if err != nil {
+				if err != cc.err {
+					t.Fatalf("got %v want %v", err, cc.err)
+				}
+			} else {
+				t.Fatalf("got %v want %v", err, cc.err)
+			}
+		})
 	}
 }
