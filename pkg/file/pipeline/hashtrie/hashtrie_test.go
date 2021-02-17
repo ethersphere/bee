c228ab889c6301572b4537c46ed705132ac4894c
acud <12988138+acud@users.noreply.github.com>
2021-02-17 18:30:21 +0100

    clean

--- pkg/file/pipeline/hashtrie/hashtrie_test.go
+++ pkg/file/pipeline/hashtrie/hashtrie_test.go
@@ -7,7 +7,6 @@ package hashtrie_test
 import (
 	"context"
 	"encoding/binary"
-	"encoding/hex"
 	"errors"
 	"testing"
 
@@ -161,34 +160,6 @@ func TestLevels_TrieFull(t *testing.T) {
 	_, err = ht.Sum()
 	if err != nil {
 		t.Fatal(err)
-	}
-}
-
-func TestOneAddr(t *testing.T) {
-	s := mock.NewStorer()
-	pf := func() pipeline.ChainWriter {
-		lsw := store.NewStoreWriter(ctx, s, mode, nil)
-		return bmt.NewBmtWriter(lsw)
-	}
-
-	ht := hashtrie.NewHashTrieWriter(64, 2, 32, pf)
-	spb := make([]byte, 8)
-	binary.LittleEndian.PutUint64(spb, 1)
-
-	for i := 0; i < 2; i++ {
-		a := &pipeline.PipeWriteArgs{Ref: addr.Bytes(), Span: spb}
-		if err := ht.ChainWrite(a); err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	ref, err := ht.Sum()
-	if err != nil {
-		t.Fatal(err)
-	}
-	exphash := "281907c4199cd2b05b80469d2af5be103cc1317dcf78d5d9b95694aadb2d4994"
-	if a := hex.EncodeToString(ref); a != exphash {
-		t.Fatalf("expected hash %s but got %s", exphash, a)
 	}
 }
 
