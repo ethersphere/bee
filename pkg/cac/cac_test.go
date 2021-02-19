2ec7ad2a5cb1a906e1328a6ff1789ff9a75299d7
Rodrigo Q. Saramago <rodrigoqsaramago@gmail.com>
2021-02-19 12:10:43 +0100

    Add sanity check for chunk content

--- pkg/cac/cac_test.go
+++ pkg/cac/cac_test.go
@@ -1,6 +1,7 @@
 package cac_test
 
 import (
+	"bytes"
 	"encoding/binary"
 	"strings"
 	"testing"
@@ -10,50 +11,68 @@ import (
 )
 
 func TestNewCAC(t *testing.T) {
-	foo := "greaterthanspan"
-	bmtHashOfFoo := "27913f1bdb6e8e52cbd5a5fd4ab577c857287edf6969b41efe926b51de0f4f23"
-	address := swarm.MustParseHexAddress(bmtHashOfFoo)
+	data := []byte("greaterthanspan")
+	bmtHashOfData := "27913f1bdb6e8e52cbd5a5fd4ab577c857287edf6969b41efe926b51de0f4f23"
+	address := swarm.MustParseHexAddress(bmtHashOfData)
 
-	c, err := cac.New([]byte(foo))
+	expectedSpan := make([]byte, swarm.SpanSize)
+	binary.LittleEndian.PutUint64(expectedSpan, uint64(len(data)))
+	expectedContent := append(expectedSpan, data...)
+
+	c, err := cac.New(data)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	if !c.Address().Equal(address) {
 		t.Fatalf("address mismatch. got %s want %s", c.Address().String(), address.String())
+	}
+
+	if !bytes.Equal(c.Data(), expectedContent) {
+		t.Fatalf("chunk data mismatch. got %x want %x", c.Data(), expectedContent)
 	}
 }
 
 func TestNewWithDataSpan(t *testing.T) {
-	foo := "greaterthanspan"
-	bmtHashOfFoo := "95022e6af5c6d6a564ee55a67f8455a3e18c511b5697c932d9e44f07f2fb8c53"
-	address := swarm.MustParseHexAddress(bmtHashOfFoo)
+	data := []byte("greaterthanspan")
+	bmtHashOfData := "95022e6af5c6d6a564ee55a67f8455a3e18c511b5697c932d9e44f07f2fb8c53"
+	address := swarm.MustParseHexAddress(bmtHashOfData)
 
-	c, err := cac.NewWithDataSpan([]byte(foo))
+	c, err := cac.NewWithDataSpan(data)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	if !c.Address().Equal(address) {
 		t.Fatalf("address mismatch. got %s want %s", c.Address().String(), address.String())
+	}
+
+	if !bytes.Equal(c.Data(), data) {
+		t.Fatalf("chunk data mismatch. got %x want %x", c.Data(), data)
 	}
 }
 
 func TestNewWithSpan(t *testing.T) {
-	foo := "greaterthanspan"
-	bmtHashOfFoo := "27913f1bdb6e8e52cbd5a5fd4ab577c857287edf6969b41efe926b51de0f4f23"
-	address := swarm.MustParseHexAddress(bmtHashOfFoo)
+	data := []byte("greaterthanspan")
+	bmtHashOfData := "27913f1bdb6e8e52cbd5a5fd4ab577c857287edf6969b41efe926b51de0f4f23"
+	address := swarm.MustParseHexAddress(bmtHashOfData)
 
 	span := make([]byte, swarm.SpanSize)
-	binary.LittleEndian.PutUint64(span, uint64(len(foo)))
+	binary.LittleEndian.PutUint64(span, uint64(len(data)))
 
-	c, err := cac.NewWithSpan([]byte(foo), span)
+	expectedContent := append(span, data...)
+
+	c, err := cac.NewWithSpan(data, span)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	if !c.Address().Equal(address) {
 		t.Fatalf("address mismatch. got %s want %s", c.Address().String(), address.String())
+	}
+
+	if !bytes.Equal(c.Data(), expectedContent) {
+		t.Fatalf("chunk data mismatch. got %x want %x", c.Data(), expectedContent)
 	}
 }
 
