a6e123467ed0a3ced6587d1584b78233dc046eac
Rodrigo Q. Saramago <rodrigoqsaramago@gmail.com>
2021-03-13 12:23:28 +0100

    fix typo

--- pkg/soc/soc.go
+++ pkg/soc/soc.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package soc provides the single-owner chunk implemention
+// Package soc provides the single-owner chunk implementation
 // and validator.
 package soc
 
