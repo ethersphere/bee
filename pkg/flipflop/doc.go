452443ff8e045ba2ae0ffe7e8055fc99112d233f
acud <12988138+acud@users.noreply.github.com>
2021-03-19 12:26:53 +0100

    wip

--- pkg/flipflop/doc.go
+++ pkg/flipflop/doc.go
@@ -1,4 +1,6 @@
-// Copyright (c) 2021 acud
+// Copyright 2021 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 // Package flipflop exposes a buffered input functionality
 // that mimicks the behavior of falling edge detection
