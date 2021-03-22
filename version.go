b46f0c8a7cb03455f1dfe6fc90030b716820b5ea
acud <12988138+acud@users.noreply.github.com>
2021-03-22 16:57:43 +0100

    Update version.go

--- version.go
+++ version.go
@@ -5,7 +5,7 @@
 package bee
 
 var (
-	version = "0.5.3" // manually set semantic version number
+	version = "0.5.4" // manually set semantic version number
 	commit  string    // automatically set git commit hash
 
 	Version = func() string {
