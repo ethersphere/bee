e1c0f64436e4023513bcd7fe3121faccc6aa095f
Alok Nerurkar <alok@stream.space>
2021-04-26 18:49:12 +0530

    Adding error page view metrics

--- pkg/api/metrics.go
+++ pkg/api/metrics.go
@@ -5,6 +5,7 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
@@ -19,6 +20,7 @@ type metrics struct {
 	RequestCount     prometheus.Counter
 	ResponseDuration prometheus.Histogram
 	PingRequestCount prometheus.Counter
+	ErrorPageViews   *prometheus.CounterVec
 }
 
 func newMetrics() metrics {
@@ -38,6 +40,15 @@ func newMetrics() metrics {
 			Help:      "Histogram of API response durations.",
 			Buckets:   []float64{0.01, 0.1, 0.25, 0.5, 1, 2.5, 5, 10},
 		}),
+		ErrorPageViews: prometheus.NewCounterVec(
+			prometheus.CounterOpts{
+				Namespace: m.Namespace,
+				Subsystem: subsystem,
+				Name:      "http_error_responses",
+				Help:      "How many 5XX HTTP errors were returned",
+			},
+			[]string{"code", "method"},
+		),
 	}
 }
 
@@ -52,4 +63,32 @@ func (s *server) pageviewMetricsHandler(h http.Handler) http.Handler {
 		h.ServeHTTP(w, r)
 		s.metrics.ResponseDuration.Observe(time.Since(start).Seconds())
 	})
+}
+
+func (s *server) errorPageviewMetricsHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		wrapper := NewErrorResponseWriter(w)
+		h.ServeHTTP(w, r)
+		if wrapper.statusCode >= 500 {
+			s.metrics.ErrorPageViews.WithLabelValues(
+				fmt.Sprintf("%d", wrapper.statusCode),
+				r.Method,
+			).Inc()
+		}
+	})
+}
+
+type errResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewErrorResponseWriter(w http.ResponseWriter) *errResponseWriter {
+	// StatusOK is called by default if nothing else is called
+	return &errResponseWriter{w, http.StatusOK}
+}
+
+func (erw *errResponseWriter) WriteHeader(code int) {
+	erw.statusCode = code
+	erw.ResponseWriter.WriteHeader(code)
 }
