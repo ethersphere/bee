300f0b75572b4c0939b2eaf57e29ac3b81b556ed
Adam Uhlíř <adam@uhlir.dev>
2021-05-07 11:23:12 +0200

    fix reservestate test

--- pkg/debugapi/postage_test.go
+++ pkg/debugapi/postage_test.go
@@ -5,9 +5,12 @@
 package debugapi_test
 
 import (
+	"math/big"
 	"net/http"
 	"testing"
 
+	"github.com/ethersphere/bee/pkg/bigint"
+	"github.com/ethersphere/bee/pkg/debugapi"
 	"github.com/ethersphere/bee/pkg/jsonhttp/jsonhttptest"
 	"github.com/ethersphere/bee/pkg/postage"
 	"github.com/ethersphere/bee/pkg/postage/batchstore/mock"
@@ -18,13 +21,17 @@ func TestReservestate(t *testing.T) {
 	ts := newTestServer(t, testServerOptions{
 		BatchStore: mock.New(mock.WithReserveState(&postage.Reservestate{
 			Radius: 5,
+			Outer: big.NewInt(5),
+			Inner: big.NewInt(5),
 		})),
 	})
 
 	t.Run("ok", func(t *testing.T) {
 		jsonhttptest.Request(t, ts.Client, http.MethodGet, "/reservestate", http.StatusOK,
-			jsonhttptest.WithExpectedJSONResponse(&postage.Reservestate{
+			jsonhttptest.WithExpectedJSONResponse(&debugapi.ReserveStateResponse{
 				Radius: 5,
+				Outer: bigint.NewBigInt(5),
+				Inner: bigint.NewBigInt(5),
 			}),
 		)
 	})
