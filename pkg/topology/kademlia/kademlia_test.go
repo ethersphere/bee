192fe5d47fce3d9c26d08dbe98dfb95d81b52f3b
Peter Mrekaj <peter@ethswarm.org>
2021-05-10 08:24:29 +0200

    kademlia: code cleanup

--- pkg/topology/kademlia/kademlia_test.go
+++ pkg/topology/kademlia/kademlia_test.go
@@ -282,7 +282,6 @@ func TestManage(t *testing.T) {
 		addOne(t, signer, kad, ab, addr)
 	}
 
-	time.Sleep(500 * time.Millisecond) // Give the goroutines enough time to finish the connection.
 	waitCounter(t, &conns, 50)
 	saturationVal = true
 
@@ -656,7 +655,7 @@ func TestAddressBookPrune(t *testing.T) {
 		*kademlia.TimeToRetry = t
 	}(*kademlia.TimeToRetry)
 
-	*kademlia.TimeToRetry = 100 * time.Millisecond
+	*kademlia.TimeToRetry = 50 * time.Millisecond
 
 	var (
 		conns, failedConns       int32 // how many connect calls were made to the p2p mock
