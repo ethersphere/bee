bc069020cd74f87f0570b98258fd8b152dc1af76
acud <12988138+acud@users.noreply.github.com>
2021-05-12 11:32:12 +0200

    ci: enforce conventional commits (#1684)

---
+++ commitlint.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+	parserPreset: 'conventional-changelog-conventionalcommits',
+	rules: {
+		'body-leading-blank': [2, 'always'],
+		'body-max-line-length': [2, 'always', 72],
+    'body-case': [
+			2,
+			'never',
+			['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+		],
+		'footer-leading-blank': [1, 'always'],
+		'footer-max-line-length': [2, 'always', 72],
+		'header-max-length': [2, 'always', 72],
+		'subject-case': [
+			2,
+			'never',
+			['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+		],
+		'subject-empty': [2, 'never'],
+		'subject-full-stop': [2, 'never', '.'],
+		'type-case': [2, 'always', 'lower-case'],
+		'type-empty': [2, 'never'],
+		'type-enum': [
+			2,
+			'always',
+			[
+				'build',
+				'chore',
+				'ci',
+				'docs',
+				'feat',
+				'fix',
+				'perf',
+				'refactor',
+				'revert',
+				'test',
+			],
+		],
+	},
+};
