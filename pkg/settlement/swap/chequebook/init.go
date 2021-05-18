99c127396cd468ec6be864caa60425315d485df0
eknir <rinkehendriksen@gmail.com>
2021-05-18 08:52:59 +0200

    fix: better instructions to user

--- pkg/settlement/swap/chequebook/init.go
+++ pkg/settlement/swap/chequebook/init.go
@@ -73,7 +73,7 @@ func checkBalance(
 				logger.Warningf("cannot continue until there is at least %d BZZ available on %x", neededERC20, overlayEthAddress)
 			}
 			if chainId == 5 {
-				logger.Warningf("get your Goerli ETH and Goerli BZZ now via https://docs.ethswarm.org/docs/installation/fund-your-node")
+				logger.Warningf("learn how to fund your node by visiting our docs at https://docs.ethswarm.org/docs/installation/fund-your-node")
 			}
 			select {
 			case <-time.After(balanceCheckBackoffDuration):
