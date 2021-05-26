7ca0bbe1023c6d6d69d9b05ec65a749364211c46
Metacertain <metacertain@gmail.com>
2021-05-26 16:21:57 +0200

    fix: temporary state

--- pkg/retrieval/skip_peers.go
+++ pkg/retrieval/skip_peers.go
@@ -11,8 +11,9 @@ import (
 )
 
 type skipPeers struct {
-	addresses []swarm.Address
-	mu        sync.Mutex
+	overdraftAddresses []swarm.Address
+	addresses          []swarm.Address
+	mu                 sync.Mutex
 }
 
 func newSkipPeers() *skipPeers {
@@ -23,7 +24,14 @@ func (s *skipPeers) All() []swarm.Address {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	return append(s.addresses[:0:0], s.addresses...)
+	return append(append(s.addresses[:0:0], s.addresses...), s.overdraftAddresses...)
+}
+
+func (s *skipPeers) Reset() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.overdraftAddresses = []swarm.Address{}
 }
 
 func (s *skipPeers) Add(address swarm.Address) {
@@ -37,4 +45,17 @@ func (s *skipPeers) Add(address swarm.Address) {
 	}
 
 	s.addresses = append(s.addresses, address)
+}
+
+func (s *skipPeers) AddOverdraft(address swarm.Address) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	for _, a := range s.overdraftAddresses {
+		if a.Equal(address) {
+			return
+		}
+	}
+
+	s.overdraftAddresses = append(s.overdraftAddresses, address)
 }
