a8765e8b2daaf501fe95c9645c9b62bdd4b7411f
kayryu <lukayryu@gmail.com>
2021-05-29 05:53:55 -0700

    Add cashout

---
+++ cashout.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env sh
+DEBUG_API=http://localhost:1635
+MIN_AMOUNT=10000000000
+
+function getPeers() {
+  curl -s "$DEBUG_API/chequebook/cheque" | jq -r '.lastcheques | .[].peer'
+}
+
+function getCumulativePayout() {
+  local peer=$1
+  local cumulativePayout=$(curl -s "$DEBUG_API/chequebook/cheque/$peer" | jq '.lastreceived.payout')
+  if [ $cumulativePayout == null ]
+  then
+    echo 0
+  else
+    echo $cumulativePayout
+  fi
+}
+
+function getLastCashedPayout() {
+  local peer=$1
+  local cashout=$(curl -s "$DEBUG_API/chequebook/cashout/$peer" | jq '.cumulativePayout')
+  if [ $cashout == null ]
+  then
+    echo 0
+  else
+    echo $cashout
+  fi
+}
+
+function getUncashedAmount() {
+  local peer=$1
+  local cumulativePayout=$(getCumulativePayout $peer)
+  if [ $cumulativePayout == 0 ]
+  then
+    echo 0
+    return
+  fi
+
+  cashedPayout=$(getLastCashedPayout $peer)
+  let uncashedAmount=$cumulativePayout-$cashedPayout
+  echo $uncashedAmount
+}
+
+function cashout() {
+  local peer=$1
+  txHash=$(curl -s -XPOST "$DEBUG_API/chequebook/cashout/$peer" | jq -r .transactionHash)
+
+  echo cashing out cheque for $peer in transaction $txHash >&2
+
+  result="$(curl -s $DEBUG_API/chequebook/cashout/$peer | jq .result)"
+  while [ "$result" == "null" ]
+  do
+    sleep 5
+    result=$(curl -s $DEBUG_API/chequebook/cashout/$peer | jq .result)
+  done
+}
+
+function cashoutAll() {
+  local minAmount=$1
+  for peer in $(getPeers)
+  do
+    local uncashedAmount=$(getUncashedAmount $peer)
+    if (( "$uncashedAmount" > $minAmount ))
+    then
+      echo "uncashed cheque for $peer ($uncashedAmount uncashed)" >&2
+      cashout $peer
+    fi
+  done
+}
+
+function listAllUncashed() {
+  local totalAmount=0
+  for peer in $(getPeers)
+  do
+    local uncashedAmount=$(getUncashedAmount $peer)
+    if (( "$uncashedAmount" > 0 ))
+    then
+      echo $peer $uncashedAmount
+      let amount=$totalAmount+$uncashedAmount
+      totalAmount=$amount
+    fi
+  done
+  echo "$totalAmount"
+}
+
+case $1 in
+cashout)
+  cashout $2
+  ;;
+cashout-all)
+  cashoutAll $MIN_AMOUNT
+  ;;
+list-uncashed|*)
+  listAllUncashed
+  ;;
+esac
