93968e37b326525a1c27176cf86a32ce48cbbffd
Metacertain <metacertain@gmail.com>
2021-06-01 14:52:39 +0200

    feat: test swapprotocol

--- pkg/settlement/swap/swapprotocol/swapprotocol_test.go
+++ pkg/settlement/swap/swapprotocol/swapprotocol_test.go
@@ -24,21 +24,21 @@ import (
         "testing"
 )
 
-/*
-
 func TestInit(t *testing.T) {
         logger := logging.New(ioutil.Discard, 0)
         commonAddr := common.HexToAddress("0xab")
-        swapHsReceiver := swapmock.NewProtocolService()
-        swapHsInitiator := swapmock.NewProtocolService()
-        swappHsReceiver := swapprotocol.New(nil, logger, commonAddr)
+        swapHsReceiver := swapmock.NewSwap()
+        swapHsInitiator := swapmock.NewSwap()
+        exchange := exchangemock.New(big.NewInt(50), big.NewInt(500))
+        swappHsReceiver := swapprotocol.New(nil, logger, commonAddr, exchange)
         swappHsReceiver.SetSwap(swapHsReceiver)
         recorder := streamtest.New(
                 streamtest.WithProtocols(swappHsReceiver.Protocol()),
         )
         commonAddr2 := common.HexToAddress("0xdc")
-        swappHsInitiator := swapprotocol.New(recorder, logger, commonAddr2)
+        swappHsInitiator := swapprotocol.New(recorder, logger, commonAddr2, exchange)
         swappHsInitiator.SetSwap(swapHsInitiator)
+
         peerID := swarm.MustParseHexAddress("9ee7add7")
         peer := p2p.Peer{Address: peerID}
         if err := swappHsInitiator.Init(context.Background(), peer); err != nil {
@@ -60,7 +60,7 @@ func TestInit(t *testing.T) {
                 t.Fatal(err)
         }
         gotBeneficiary := messages[0].(*pb.Handshake).Beneficiary
-        if bytes.Equal(gotBeneficiary, commonAddr2.Bytes()) {
+        if !bytes.Equal(gotBeneficiary, commonAddr2.Bytes()) {
                 t.Fatalf("got %v bytes, want %v bytes", gotBeneficiary, commonAddr2.Bytes())
         }
         if len(messages) != 1 {
@@ -74,15 +74,13 @@ func TestInit(t *testing.T) {
                 t.Fatal(err)
         }
         gotBeneficiary = messages[0].(*pb.Handshake).Beneficiary
-        if bytes.Equal(gotBeneficiary, commonAddr.Bytes()) {
+        if !bytes.Equal(gotBeneficiary, commonAddr.Bytes()) {
                 t.Fatalf("got %v bytes, want %v bytes", gotBeneficiary, commonAddr.Bytes())
         }
         if len(messages) != 1 {
                 t.Fatalf("got %v messages, want %v", len(messages), 1)
         }
 }
-
-*/
 
 func TestEmitCheque(t *testing.T) {
         logger := logging.New(ioutil.Discard, 0)
