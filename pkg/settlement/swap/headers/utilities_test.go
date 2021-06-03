24ba8efeb4c81f04e9c7c2bb8697c012fae914ec
Metacertain <metacertain@gmail.com>
2021-06-03 12:07:30 +0200

    feat: add utilities tests

--- pkg/settlement/swap/headers/utilities_test.go
+++ pkg/settlement/swap/headers/utilities_test.go
@@ -6,29 +6,75 @@ package swap_test
 
 import (
 	"math/big"
+	"reflect"
 	"testing"
 
 	"github.com/ethersphere/bee/pkg/p2p"
-	swap "github.com/ethersphere/bee/pkg/settlement/swap/headers"
+	"github.com/ethersphere/bee/pkg/settlement/swap/headers"
 )
 
 func TestParseSettlementResponseHeaders(t *testing.T) {
 	headers := p2p.Headers{
-		"exchange":  []byte{10},
-		"deduction": []byte{20},
+		swap.ExchangeRateFieldName: []byte{10},
+		swap.DeductionFieldName:    []byte{20},
 	}
 
-	ex, ded, err := swap.ParseSettlementResponseHeaders(headers)
-
+	exchange, deduction, err := swap.ParseSettlementResponseHeaders(headers)
 	if err != nil {
-		t.Fatal("unexpected error", err)
+		t.Fatal(err)
 	}
 
-	if new(big.Int).SetInt64(10).Cmp(ex) != 0 {
-		t.Fatal("expected 0, got", ex)
+	if exchange.Cmp(big.NewInt(10)) != 0 {
+		t.Fatalf("Exchange rate mismatch, got %v, want %v", exchange, 10)
 	}
 
-	if new(big.Int).SetInt64(20).Cmp(ded) != 0 {
-		t.Fatal("expected 0, got", ded)
+	if deduction.Cmp(big.NewInt(20)) != 0 {
+		t.Fatalf("Deduction mismatch, got %v, want %v", deduction, 20)
 	}
+}
+
+func TestMakeSettlementHeaders(t *testing.T) {
+
+	makeHeaders := swap.MakeSettlementHeaders(big.NewInt(906000), big.NewInt(5348))
+
+	expectedHeaders := p2p.Headers{
+		swap.ExchangeRateFieldName: []byte{13, 211, 16},
+		swap.DeductionFieldName:    []byte{20, 228},
+	}
+
+	if !reflect.DeepEqual(makeHeaders, expectedHeaders) {
+		t.Fatalf("Made headers not as expected, got %+v, want %+v", makeHeaders, expectedHeaders)
+	}
+}
+
+func TestParseExchangeHeader(t *testing.T) {
+	toReadHeaders := p2p.Headers{
+		swap.ExchangeRateFieldName: []byte{13, 211, 16},
+	}
+
+	parsedExchange, err := swap.ParseExchangeHeader(toReadHeaders)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if parsedExchange.Cmp(big.NewInt(906000)) != 0 {
+		t.Fatalf("Allowance mismatch, got %v, want %v", parsedExchange, big.NewInt(906000))
+	}
+
+}
+
+func TestParseDeductionHeader(t *testing.T) {
+	toReadHeaders := p2p.Headers{
+		swap.DeductionFieldName: []byte{20, 228},
+	}
+
+	parsedDeduction, err := swap.ParseDeductionHeader(toReadHeaders)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if parsedDeduction.Cmp(big.NewInt(5348)) != 0 {
+		t.Fatalf("Allowance mismatch, got %v, want %v", parsedDeduction, big.NewInt(5348))
+	}
+
 }
