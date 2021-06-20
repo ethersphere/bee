98308ae5d950c002dd2cab0404156a1387898d7a
Anatol Lupacescu <anatollupacescu@gmail.com>
2021-06-20 20:01:37 +0200

    feat: main and test net flags

--- cmd/bee/cmd/start.go
+++ cmd/bee/cmd/start.go
@@ -127,7 +127,16 @@ inability to use, or your interaction with other nodes or the software.`)
 				return errors.New("boot node must be started as a full node")
 			}
 
-			b, err := node.NewBee(c.config.GetString(optionNameP2PAddr), signerConfig.publicKey, signerConfig.signer, c.config.GetUint64(optionNameNetworkID), logger, signerConfig.libp2pPrivateKey, signerConfig.pssPrivateKey, &node.Options{
+			mainnet := c.config.GetBool(optionNameMainNet)
+			testnet := c.config.GetBool(optionNameTestNet)
+
+			networkID := c.config.GetUint64(optionNameNetworkID)
+			networkID, err = parseNet(mainnet, testnet, networkID)
+			if err != nil {
+				return err
+			}
+
+			b, err := node.NewBee(c.config.GetString(optionNameP2PAddr), signerConfig.publicKey, signerConfig.signer, networkID, logger, signerConfig.libp2pPrivateKey, signerConfig.pssPrivateKey, &node.Options{
 				DataDir:                    c.config.GetString(optionNameDataDir),
 				CacheCapacity:              c.config.GetUint64(optionNameCacheCapacity),
 				DBOpenFilesLimit:           c.config.GetUint64(optionNameDBOpenFilesLimit),
@@ -404,4 +413,21 @@ func (c *command) configureSigner(cmd *cobra.Command, logger logging.Logger) (co
 		libp2pPrivateKey: libp2pPrivateKey,
 		pssPrivateKey:    pssPrivateKey,
 	}, nil
+}
+
+func parseNet(main, test bool, networkID uint64) (uint64, error) {
+	switch {
+	case main && test:
+		return 0, errors.New("please provide either mainnet or testnet option")
+	case main && networkID != 1:
+		return 0, errors.New("provided network ID does not match mainnet")
+	case test && networkID != 10:
+		return 0, errors.New("provided network ID does not match testnet")
+	case main:
+		return 1, nil
+	case test:
+		return 10, nil
+	default:
+		return networkID, nil
+	}
 }
