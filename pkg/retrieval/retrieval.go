dc2acbcfc63e9ff424ae210a2b9c69deaabf9e0e
Metacertain <metacertain@gmail.com>
2021-06-28 14:34:24 +0200

    fix: increase buffer

--- pkg/retrieval/retrieval.go
+++ pkg/retrieval/retrieval.go
@@ -126,7 +126,7 @@ func (s *Service) RetrieveChunk(ctx context.Context, addr swarm.Address, origin 
 		var (
 			peerAttempt  int
 			peersResults int
-			resultC      = make(chan retrievalResult, 1)
+			resultC      = make(chan retrievalResult, 2)
 		)
 
 		requestAttempt := 0
