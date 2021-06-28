a2314b79042ceec27cf931a69553cd09618c8451
Metacertain <metacertain@gmail.com>
2021-06-28 11:00:19 +0200

    fix: reduce retrieval result chan buffer

--- pkg/retrieval/retrieval.go
+++ pkg/retrieval/retrieval.go
@@ -126,7 +126,7 @@ func (s *Service) RetrieveChunk(ctx context.Context, addr swarm.Address, origin 
 		var (
 			peerAttempt  int
 			peersResults int
-			resultC      = make(chan retrievalResult, maxSelects)
+			resultC      = make(chan retrievalResult, 1)
 		)
 
 		requestAttempt := 0
