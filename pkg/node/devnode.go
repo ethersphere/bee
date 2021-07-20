6aafaa9fa0f5d88ba5ee5600ca5ca5c871e68e0a
Anatolie Lupacescu <anatol@ethswarm.org>
2021-07-20 16:13:57 +0300

    feat: dev mode command

---
+++ pkg/node/devnode.go
@@ -0,0 +1,355 @@
+package node
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"log"
+	"math/big"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethersphere/bee/pkg/accounting"
+	"github.com/ethersphere/bee/pkg/addressbook"
+	"github.com/ethersphere/bee/pkg/api"
+	"github.com/ethersphere/bee/pkg/crypto"
+	"github.com/ethersphere/bee/pkg/debugapi"
+	"github.com/ethersphere/bee/pkg/feeds/factory"
+	"github.com/ethersphere/bee/pkg/hive"
+	"github.com/ethersphere/bee/pkg/localstore"
+	"github.com/ethersphere/bee/pkg/logging"
+	"github.com/ethersphere/bee/pkg/metrics"
+	"github.com/ethersphere/bee/pkg/p2p/libp2p"
+	"github.com/ethersphere/bee/pkg/pingpong"
+	"github.com/ethersphere/bee/pkg/pinning"
+	"github.com/ethersphere/bee/pkg/postage"
+	"github.com/ethersphere/bee/pkg/postage/batchstore"
+	"github.com/ethersphere/bee/pkg/postage/listener"
+	"github.com/ethersphere/bee/pkg/postage/postagecontract"
+	"github.com/ethersphere/bee/pkg/pricing"
+	"github.com/ethersphere/bee/pkg/pss"
+	"github.com/ethersphere/bee/pkg/resolver/multiresolver"
+	"github.com/ethersphere/bee/pkg/settlement/pseudosettle"
+	"github.com/ethersphere/bee/pkg/settlement/swap/chequebook"
+	"github.com/ethersphere/bee/pkg/shed"
+	"github.com/ethersphere/bee/pkg/statestore/leveldb"
+	"github.com/ethersphere/bee/pkg/storage"
+	"github.com/ethersphere/bee/pkg/swarm"
+	"github.com/ethersphere/bee/pkg/tags"
+	"github.com/ethersphere/bee/pkg/topology/kademlia"
+	"github.com/ethersphere/bee/pkg/topology/lightnode"
+	"github.com/ethersphere/bee/pkg/tracing"
+	"github.com/ethersphere/bee/pkg/transaction"
+	"github.com/ethersphere/bee/pkg/traversal"
+	ma "github.com/multiformats/go-multiaddr"
+	"github.com/sirupsen/logrus"
+)
+
+func NewDevBee(addr string, publicKey *ecdsa.PublicKey, signer crypto.Signer, logger logging.Logger, libp2pPrivateKey, pssPrivateKey *ecdsa.PrivateKey, o *Options) (b *Bee, err error) {
+	tracer, tracerCloser, err := tracing.NewTracer(&tracing.Options{
+		Enabled:     o.TracingEnabled,
+		Endpoint:    o.TracingEndpoint,
+		ServiceName: o.TracingServiceName,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("tracer: %w", err)
+	}
+
+	p2pCtx, p2pCancel := context.WithCancel(context.Background())
+	defer func() {
+		// if there's been an error on this function
+		// we'd like to cancel the p2p context so that
+		// incoming connections will not be possible
+		if err != nil {
+			p2pCancel()
+		}
+	}()
+
+	b = &Bee{
+		p2pCancel:      p2pCancel,
+		errorLogWriter: logger.WriterLevel(logrus.ErrorLevel),
+		tracerCloser:   tracerCloser,
+	}
+
+	stateStore, err := leveldb.NewInMemoryStateStore(logger)
+	if err != nil {
+		return nil, err
+	}
+	b.stateStoreCloser = stateStore
+
+	addressbook := addressbook.New(stateStore)
+
+	var (
+		transactionService transaction.Service
+		chequebookService  chequebook.Service
+	)
+
+	var debugAPIService *debugapi.Service
+	if o.DebugAPIAddr != "" {
+		overlayEthAddress, err := signer.EthereumAddress()
+		if err != nil {
+			return nil, fmt.Errorf("eth address: %w", err)
+		}
+		// set up basic debug api endpoints for debugging and /health endpoint
+		debugAPIService = debugapi.New(*publicKey, pssPrivateKey.PublicKey, overlayEthAddress, logger, tracer, o.CORSAllowedOrigins, big.NewInt(0), transactionService)
+
+		debugAPIListener, err := net.Listen("tcp", o.DebugAPIAddr)
+		if err != nil {
+			return nil, fmt.Errorf("debug api listener: %w", err)
+		}
+
+		debugAPIServer := &http.Server{
+			IdleTimeout:       30 * time.Second,
+			ReadHeaderTimeout: 3 * time.Second,
+			Handler:           debugAPIService,
+			ErrorLog:          log.New(b.errorLogWriter, "", 0),
+		}
+
+		go func() {
+			logger.Infof("debug api address: %s", debugAPIListener.Addr())
+
+			if err := debugAPIServer.Serve(debugAPIListener); err != nil && err != http.ErrServerClosed {
+				logger.Debugf("debug api server: %v", err)
+				logger.Error("unable to serve debug api")
+			}
+		}()
+
+		b.debugAPIServer = debugAPIServer
+	}
+
+	var (
+		// TO BE MOCKED
+		networkID     uint64
+		swarmAddress  swarm.Address
+		lightNodes    *lightnode.Container
+		senderMatcher *transaction.Matcher
+		swapBackend   listener.BlockHeightContractFilterer
+		ns            storage.Storer
+	)
+
+	p2ps, err := libp2p.New(p2pCtx, signer, networkID, swarmAddress, addr, addressbook, stateStore, lightNodes, senderMatcher, logger, tracer, libp2p.Options{
+		PrivateKey:     libp2pPrivateKey,
+		NATAddr:        o.NATAddr,
+		EnableWS:       o.EnableWS,
+		EnableQUIC:     o.EnableQUIC,
+		WelcomeMessage: o.WelcomeMessage,
+		FullNode:       true,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("p2p service: %w", err)
+	}
+	b.p2pService = p2ps
+	b.p2pHalter = p2ps
+
+	var unreserveFn func([]byte, uint8) (uint64, error)
+	var evictFn = func(b []byte) error {
+		_, err := unreserveFn(b, swarm.MaxPO+1)
+		return err
+	}
+
+	batchStore, err := batchstore.New(stateStore, evictFn, logger)
+	if err != nil {
+		return nil, fmt.Errorf("batchstore: %w", err)
+	}
+
+	// localstore depends on batchstore
+	var path string
+
+	lo := &localstore.Options{
+		Capacity:               o.CacheCapacity,
+		ReserveCapacity:        uint64(batchstore.Capacity),
+		UnreserveFunc:          batchStore.Unreserve,
+		OpenFilesLimit:         o.DBOpenFilesLimit,
+		BlockCacheCapacity:     o.DBBlockCacheCapacity,
+		WriteBufferSize:        o.DBWriteBufferSize,
+		DisableSeeksCompaction: o.DBDisableSeeksCompaction,
+	}
+
+	storer, err := localstore.New(path, swarmAddress.Bytes(), stateStore, lo, logger)
+	if err != nil {
+		return nil, fmt.Errorf("localstore: %w", err)
+	}
+	b.localstoreCloser = storer
+	unreserveFn = storer.UnreserveBatch
+
+	post, err := postage.NewService(stateStore, batchStore, 0)
+	if err != nil {
+		return nil, fmt.Errorf("postage service load: %w", err)
+	}
+	b.postageServiceCloser = post
+
+	var (
+		postageContractService postagecontract.Interface
+		eventListener          postage.Listener
+		postageContractAddress common.Address
+	)
+
+	eventListener = listener.New(logger, swapBackend, postageContractAddress, o.BlockTime, &pidKiller{node: b})
+	b.listenerCloser = eventListener
+
+	if natManager := p2ps.NATManager(); natManager != nil {
+		// wait for nat manager to init
+		logger.Debug("initializing NAT manager")
+		select {
+		case <-natManager.Ready():
+			// this is magic sleep to give NAT time to sync the mappings
+			// this is a hack, kind of alchemy and should be improved
+			time.Sleep(3 * time.Second)
+			logger.Debug("NAT manager initialized")
+		case <-time.After(10 * time.Second):
+			logger.Warning("NAT manager init timeout")
+		}
+	}
+
+	// Construct protocols.
+	pingPong := pingpong.New(p2ps, logger, tracer)
+
+	if err = p2ps.AddProtocol(pingPong.Protocol()); err != nil {
+		return nil, fmt.Errorf("pingpong service: %w", err)
+	}
+
+	hive := hive.New(p2ps, addressbook, networkID, logger)
+	if err = p2ps.AddProtocol(hive.Protocol()); err != nil {
+		return nil, fmt.Errorf("hive service: %w", err)
+	}
+
+	var bootnodes []ma.Multiaddr
+
+	metricsDB, err := shed.NewDBWrap(stateStore.DB())
+	if err != nil {
+		return nil, fmt.Errorf("unable to create metrics storage for kademlia: %w", err)
+	}
+
+	kad := kademlia.New(swarmAddress, addressbook, hive, p2ps, metricsDB, logger, kademlia.Options{Bootnodes: bootnodes, BootnodeMode: o.BootnodeMode})
+	b.topologyCloser = kad
+	b.topologyHalter = kad
+	hive.SetAddPeersHandler(kad.AddPeers)
+	p2ps.SetPickyNotifier(kad)
+	batchStore.SetRadiusSetter(kad)
+
+	var (
+		paymentTolerance = big.NewInt(1000)
+		paymentEarly     = big.NewInt(1000)
+		paymentThreshold = big.NewInt(10000)
+	)
+
+	var pricing pricing.Interface
+
+	acc, err := accounting.NewAccounting(
+		paymentThreshold,
+		paymentTolerance,
+		paymentEarly,
+		logger,
+		stateStore,
+		pricing,
+		big.NewInt(refreshRate),
+		p2ps,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("accounting: %w", err)
+	}
+	b.accountingCloser = acc
+
+	pseudosettleService := pseudosettle.New(p2ps, logger, stateStore, acc, big.NewInt(refreshRate), p2ps)
+	if err = p2ps.AddProtocol(pseudosettleService.Protocol()); err != nil {
+		return nil, fmt.Errorf("pseudosettle service: %w", err)
+	}
+
+	acc.SetRefreshFunc(pseudosettleService.Pay)
+
+	tagService := tags.NewTags(stateStore, logger)
+	b.tagsCloser = tagService
+
+	pssService := pss.New(pssPrivateKey, logger)
+	b.pssCloser = pssService
+
+	traversalService := traversal.New(nil) //fixme
+
+	pinningService := pinning.NewService(storer, stateStore, traversalService)
+
+	multiResolver := multiresolver.NewMultiResolver(
+		multiresolver.WithConnectionConfigs(o.ResolverConnectionCfgs),
+		multiresolver.WithLogger(o.Logger),
+	)
+	b.resolverCloser = multiResolver
+
+	var apiService api.Service
+	if o.APIAddr != "" {
+		// API server
+		feedFactory := factory.New(ns)
+		apiService = api.New(tagService, ns, multiResolver, pssService, traversalService, pinningService, feedFactory, nil, nil, nil, signer, logger, tracer, api.Options{
+			CORSAllowedOrigins: o.CORSAllowedOrigins,
+			GatewayMode:        o.GatewayMode,
+			WsPingPeriod:       60 * time.Second,
+		})
+		apiListener, err := net.Listen("tcp", o.APIAddr)
+		if err != nil {
+			return nil, fmt.Errorf("api listener: %w", err)
+		}
+
+		apiServer := &http.Server{
+			IdleTimeout:       30 * time.Second,
+			ReadHeaderTimeout: 3 * time.Second,
+			Handler:           apiService,
+			ErrorLog:          log.New(b.errorLogWriter, "", 0),
+		}
+
+		go func() {
+			logger.Infof("api address: %s", apiListener.Addr())
+
+			if err := apiServer.Serve(apiListener); err != nil && err != http.ErrServerClosed {
+				logger.Debugf("api server: %v", err)
+				logger.Error("unable to serve api")
+			}
+		}()
+
+		b.apiServer = apiServer
+		b.apiCloser = apiService
+	}
+
+	if debugAPIService != nil {
+		// register metrics from components
+		debugAPIService.MustRegisterMetrics(p2ps.Metrics()...)
+		debugAPIService.MustRegisterMetrics(pingPong.Metrics()...)
+		debugAPIService.MustRegisterMetrics(acc.Metrics()...)
+		debugAPIService.MustRegisterMetrics(storer.Metrics()...)
+		debugAPIService.MustRegisterMetrics(kad.Metrics()...)
+		debugAPIService.MustRegisterMetrics(lightNodes.Metrics()...)
+
+		if bs, ok := batchStore.(metrics.Collector); ok {
+			debugAPIService.MustRegisterMetrics(bs.Metrics()...)
+		}
+
+		if eventListener != nil {
+			if ls, ok := eventListener.(metrics.Collector); ok {
+				debugAPIService.MustRegisterMetrics(ls.Metrics()...)
+			}
+		}
+
+		if pssServiceMetrics, ok := pssService.(metrics.Collector); ok {
+			debugAPIService.MustRegisterMetrics(pssServiceMetrics.Metrics()...)
+		}
+
+		if apiService != nil {
+			debugAPIService.MustRegisterMetrics(apiService.Metrics()...)
+		}
+		if l, ok := logger.(metrics.Collector); ok {
+			debugAPIService.MustRegisterMetrics(l.Metrics()...)
+		}
+
+		debugAPIService.MustRegisterMetrics(pseudosettleService.Metrics()...)
+
+		// inject dependencies and configure full debug api http path routes
+		debugAPIService.Configure(swarmAddress, p2ps, pingPong, kad, lightNodes, storer, tagService, acc, pseudosettleService, o.SwapEnable, nil, chequebookService, batchStore, post, postageContractService)
+	}
+
+	if err := kad.Start(p2pCtx); err != nil {
+		return nil, err
+	}
+
+	p2ps.Ready()
+
+	return b, nil
+}
