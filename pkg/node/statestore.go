6bb26a48331a599a1c5a7020b23000ce454b6f57
Anatolie Lupacescu <anatol@ethswarm.org>
2021-07-20 12:11:19 +0300

    fix: reusing store

--- pkg/node/statestore.go
+++ pkg/node/statestore.go
@@ -18,40 +18,20 @@ import (
 	ldbs "github.com/syndtr/goleveldb/leveldb/storage"
 )
 
-type MockStore struct {
-	ldb *ldb.DB
-}
-
-func (m *MockStore) Get(key string, i interface{}) (err error) {
-	return nil
-}
-func (m *MockStore) Put(key string, i interface{}) (err error) {
-	return nil
-}
-func (m *MockStore) Delete(key string) (err error) {
-	return nil
-}
-func (m *MockStore) Iterate(prefix string, iterFunc storage.StateIterFunc) (err error) {
-	return nil
-}
-
-// DB returns the underlying DB storage.
-func (m *MockStore) DB() *ldb.DB {
-	return m.ldb
-}
-func (m *MockStore) Close() error {
-	return nil
-}
-
 // InitStateStore will initialize the stateStore with the given path to the
 // data directory. When given an empty directory path, the function will instead
 // initialize an in-memory state store that will not be persisted.
 func InitStateStore(log logging.Logger, dataDir string) (ret storage.StateStorer, err error) {
 	if dataDir == "" {
 		ldb, err := ldb.Open(ldbs.NewMemStorage(), nil)
+		if err != nil {
+			return nil, err
+		}
 		log.Warning("using in-mem state store, no node state will be persisted")
-		return &MockStore{ldb: ldb}, err
+
+		return leveldb.NewStateStoreFromDB(ldb, log)
 	}
+
 	return leveldb.NewStateStore(filepath.Join(dataDir, "statestore"), log)
 }
 
