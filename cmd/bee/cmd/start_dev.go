002c2dd5963cca6a3b00a73bfe80bd44d552da66
Anatolie Lupacescu <anatol@ethswarm.org>
2021-07-22 10:09:49 +0300

    fix: comment out, mocks pending

--- cmd/bee/cmd/start_dev.go
+++ cmd/bee/cmd/start_dev.go
@@ -15,9 +15,8 @@ import (
 	"time"
 
 	"github.com/ethersphere/bee"
+	"github.com/ethersphere/bee/pkg/crypto"
 	"github.com/ethersphere/bee/pkg/node"
-	"github.com/ethersphere/bee/pkg/resolver/multiresolver"
-	"github.com/ethersphere/bee/pkg/swarm"
 	"github.com/kardianos/service"
 	"github.com/spf13/cobra"
 )
@@ -51,17 +50,6 @@ func (c *command) initStartDevCmd() (err error) {
 				}
 			}
 
-			// If the resolver is specified, resolve all connection strings
-			// and fail on any errors.
-			var resolverCfgs []multiresolver.ConnectionConfig
-			resolverEndpoints := c.config.GetStringSlice(optionNameResolverEndpoints)
-			if len(resolverEndpoints) > 0 {
-				resolverCfgs, err = multiresolver.ParseConnectionStrings(resolverEndpoints)
-				if err != nil {
-					return err
-				}
-			}
-
 			beeASCII := `
 Welcome to Swarm.... Bzzz Bzzzz Bzzzz
                 \     /
@@ -87,36 +75,20 @@ Welcome to Swarm.... Bzzz Bzzzz Bzzzz
 				debugAPIAddr = ""
 			}
 
-			signerConfig, err := c.configureSigner(cmd, logger)
+			var signer crypto.Signer
+			// signer := crypto.NewDefaultSigner(swarmPrivateKey)
+			// signerConfig, err := configureDevSigner(cmd, logger)
 			if err != nil {
 				return err
 			}
 
 			logger.Infof("version: %v", bee.Version)
 
-			tracingEndpoint := c.config.GetString(optionNameTracingEndpoint)
-
-			if c.config.IsSet(optionNameTracingHost) && c.config.IsSet(optionNameTracingPort) {
-				tracingEndpoint = strings.Join([]string{c.config.GetString(optionNameTracingHost), c.config.GetString(optionNameTracingPort)}, ":")
-			}
-
-			b, err := node.NewDevBee(c.config.GetString(optionNameP2PAddr), signerConfig.publicKey, signerConfig.signer, logger, signerConfig.libp2pPrivateKey, signerConfig.pssPrivateKey, &node.Options{
-				APIAddr:                c.config.GetString(optionNameAPIAddr),
-				DebugAPIAddr:           debugAPIAddr,
-				Addr:                   c.config.GetString(optionNameP2PAddr),
-				NATAddr:                c.config.GetString(optionNameNATAddr),
-				EnableWS:               c.config.GetBool(optionNameP2PWSEnable),
-				EnableQUIC:             c.config.GetBool(optionNameP2PQUICEnable),
-				WelcomeMessage:         c.config.GetString(optionWelcomeMessage),
-				CORSAllowedOrigins:     c.config.GetStringSlice(optionCORSAllowedOrigins),
-				TracingEnabled:         c.config.GetBool(optionNameTracingEnabled),
-				TracingEndpoint:        tracingEndpoint,
-				TracingServiceName:     c.config.GetString(optionNameTracingServiceName),
-				Logger:                 logger,
-				GlobalPinningEnabled:   c.config.GetBool(optionNameGlobalPinningEnabled),
-				ResolverConnectionCfgs: resolverCfgs,
-				Transaction:            c.config.GetString(optionNameTransactionHash),
-				RetrievalCaching:       c.config.GetBool(optionNameRetrievalCaching),
+			// generate signer in here
+			b, err := node.NewDevBee(signer, logger, &node.Options{
+				APIAddr:      c.config.GetString(optionNameAPIAddr),
+				DebugAPIAddr: debugAPIAddr,
+				Logger:       logger,
 			})
 			if err != nil {
 				return err
@@ -185,30 +157,10 @@ Welcome to Swarm.... Bzzz Bzzzz Bzzzz
 		},
 	}
 
-	cmd.Flags().String(optionNameDataDir, "", "data directory")
-	cmd.Flags().Uint64(optionNameCacheCapacity, 1000000, fmt.Sprintf("cache capacity in chunks, multiply by %d to get approximate capacity in bytes", swarm.ChunkSize))
-	cmd.Flags().String(optionNamePassword, "", "password for decrypting keys")
-	cmd.Flags().String(optionNamePasswordFile, "", "path to a file that contains password for decrypting keys")
 	cmd.Flags().String(optionNameAPIAddr, ":1633", "HTTP API listen address")
-	cmd.Flags().String(optionNameP2PAddr, ":1634", "P2P listen address")
-	cmd.Flags().String(optionNameNATAddr, "", "NAT exposed address")
-	cmd.Flags().Bool(optionNameP2PWSEnable, false, "enable P2P WebSocket transport")
-	cmd.Flags().Bool(optionNameP2PQUICEnable, false, "enable P2P QUIC transport")
-	cmd.Flags().Bool(optionNameDebugAPIEnable, true, "enable debug HTTP API")
 	cmd.Flags().String(optionNameDebugAPIAddr, ":1635", "debug HTTP API listen address")
-	cmd.Flags().Bool(optionNameTracingEnabled, false, "enable tracing")
-	cmd.Flags().String(optionNameTracingEndpoint, "127.0.0.1:6831", "endpoint to send tracing data")
-	cmd.Flags().String(optionNameTracingHost, "", "host to send tracing data")
-	cmd.Flags().String(optionNameTracingPort, "", "port to send tracing data")
-	cmd.Flags().String(optionNameTracingServiceName, "bee", "service name identifier for tracing")
 	cmd.Flags().String(optionNameVerbosity, "info", "log verbosity level 0=silent, 1=error, 2=warn, 3=info, 4=debug, 5=trace")
-	cmd.Flags().String(optionWelcomeMessage, "", "send a welcome message string during handshakes")
-	cmd.Flags().Bool(optionNameGlobalPinningEnabled, false, "enable global pinning")
-	cmd.Flags().StringSlice(optionNameResolverEndpoints, []string{}, "ENS compatible API endpoint for a TLD and with contract address, can be repeated, format [tld:][contract-addr@]url")
-	cmd.Flags().Bool(optionNameClefSignerEnable, false, "enable clef signer")
-	cmd.Flags().String(optionNameClefSignerEndpoint, "", "clef signer endpoint")
-	cmd.Flags().String(optionNameClefSignerEthereumAddress, "", "ethereum address to use from clef signer")
-	cmd.Flags().Bool(optionNameRetrievalCaching, true, "enable forwarded content caching")
+	cmd.Flags().Bool(optionNameDebugAPIEnable, true, "enable debug HTTP API")
 
 	c.root.AddCommand(cmd)
 	return nil
