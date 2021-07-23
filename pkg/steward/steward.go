79d7bc2f99aadd019c750828ba167b983813d103
mrekucci <peter@ethswarm.org>
2021-07-23 16:50:20 +0200

    feat: add an API endpoint that checks if content is available

--- pkg/steward/steward.go
+++ pkg/steward/steward.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 
 	"github.com/ethersphere/bee/pkg/pushsync"
+	"github.com/ethersphere/bee/pkg/retrieval"
 	"github.com/ethersphere/bee/pkg/storage"
 	"github.com/ethersphere/bee/pkg/swarm"
 	"github.com/ethersphere/bee/pkg/topology"
@@ -22,20 +23,30 @@ import (
 // how many parallel push operations
 const parallelPush = 5
 
-type Reuploader interface {
+type Interface interface {
 	// Reupload root hash and all of its underlying
 	// associated chunks to the network.
 	Reupload(context.Context, swarm.Address) error
+
+	// IsRetrievable checks whether the content
+	// on the given address is retrievable.
+	IsRetrievable(context.Context, swarm.Address) (bool, error)
 }
 
 type steward struct {
-	getter    storage.Getter
-	push      pushsync.PushSyncer
-	traverser traversal.Traverser
+	getter       storage.Getter
+	push         pushsync.PushSyncer
+	traverser    traversal.Traverser
+	netTraverser traversal.Traverser
 }
 
-func New(getter storage.Getter, t traversal.Traverser, p pushsync.PushSyncer) Reuploader {
-	return &steward{getter: getter, push: p, traverser: t}
+func New(getter storage.Getter, t traversal.Traverser, r retrieval.Interface, p pushsync.PushSyncer) Interface {
+	return &steward{
+		getter:       getter,
+		push:         p,
+		traverser:    t,
+		netTraverser: traversal.New(&netGetter{r}),
+	}
 }
 
 // Reupload content with the given root hash to the network.
@@ -75,4 +86,33 @@ func (s *steward) Reupload(ctx context.Context, root swarm.Address) error {
 		return fmt.Errorf("push error during reupload: %w", err)
 	}
 	return nil
+}
+
+// IsRetrievable implements Interface.IsRetrievable method.
+func (s *steward) IsRetrievable(ctx context.Context, root swarm.Address) (bool, error) {
+	noop := func(leaf swarm.Address) error { return nil }
+	switch err := s.netTraverser.Traverse(ctx, root, noop); {
+	case errors.Is(err, storage.ErrNotFound):
+		return false, nil
+	case err != nil:
+		return false, fmt.Errorf("traversal of %q failed: %w", root, err)
+	default:
+		return true, nil
+	}
+}
+
+// netGetter implements the storage Getter.Get method in a way
+// that it will try to retrieve the chunk only from the network.
+type netGetter struct {
+	retrieval retrieval.Interface
+}
+
+// Get implements the storage Getter.Get interface.
+func (ng *netGetter) Get(ctx context.Context, _ storage.ModeGet, addr swarm.Address) (swarm.Chunk, error) {
+	return ng.retrieval.RetrieveChunk(ctx, addr, true)
+}
+
+// Put implements the storage Putter.Put interface.
+func (ng *netGetter) Put(_ context.Context, _ storage.ModePut, _ ...swarm.Chunk) ([]bool, error) {
+	panic(errors.New("operation is not supported"))
 }
