5b2e2da8d564f18a3d172b724f31eb81624f21c5
Anatolie Lupacescu <anatol@ethswarm.org>
2021-08-13 13:15:04 +0300

    fix: password hash, simpler key

--- pkg/node/node.go
+++ pkg/node/node.go
@@ -246,9 +246,10 @@ func NewBee(addr string, publicKey *ecdsa.PublicKey, signer crypto.Signer, netwo
 
 	username := os.Getenv("BEE_AUTH_USERNAME")
 	password := os.Getenv("BEE_AUTH_PASSWORD")
+	expiry := 1 * time.Hour
 
 	if o.Restricted {
-		if authenticator, err = auth.New(username, password); err != nil {
+		if authenticator, err = auth.New(username, password, expiry); err != nil {
 			return nil, fmt.Errorf("authenticator: %w", err)
 		}
 	}
