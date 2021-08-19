436df578afe066be19e9f49c45827c099206ea79
Ivan Vandot <ivan@vandot.rs>
2021-08-19 15:49:57 +0200

    fix: fix beekeeper_artifacts

--- .github/bin/beekeeper_artifacts.sh
+++ .github/bin/beekeeper_artifacts.sh
@@ -15,13 +15,12 @@ do
 done
 kubectl -n local get pods > dump/kubectl_get_pods
 kubectl -n local logs -l app.kubernetes.io/part-of=bee --tail -1 --prefix -c bee > dump/kubectl_logs
-vertag=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 15)
 endpoint=$AWS_ENDPOINT
 if [[ "$endpoint" != http* ]]
 then
   endpoint=https://$endpoint
 fi
-fname=artifacts_$vertag.tar.gz
+fname=artifacts_${VERTAG}.tar.gz
 tar -cz dump | aws --endpoint-url "$endpoint" s3 cp - s3://"$BUCKET_NAME"/"$fname"
 aws --endpoint-url "$endpoint" s3api put-object-acl --bucket "$BUCKET_NAME" --acl public-read --key "$fname"
 out="== Uploaded debugging artifacts to https://${BUCKET_NAME}.${AWS_ENDPOINT}/$fname =="
