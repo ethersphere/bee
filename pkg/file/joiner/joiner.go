76bb99869881f6ea39936b9747ddec47d85e5429
Anatol <87016465+notanatol@users.noreply.github.com>
2021-09-09 17:43:44 +0300

    fix: recursive file joiner read (#2481)

--- pkg/file/joiner/joiner.go
+++ pkg/file/joiner/joiner.go
@@ -68,19 +68,19 @@ func (j *joiner) Read(b []byte) (n int, err error) {
 	return read, err
 }
 
-func (j *joiner) ReadAt(b []byte, off int64) (read int, err error) {
+func (j *joiner) ReadAt(buffer []byte, off int64) (read int, err error) {
 	// since offset is int64 and swarm spans are uint64 it means we cannot seek beyond int64 max value
 	if off >= j.span {
 		return 0, io.EOF
 	}
 
-	readLen := int64(cap(b))
+	readLen := int64(cap(buffer))
 	if readLen > j.span-off {
 		readLen = j.span - off
 	}
 	var bytesRead int64
 	var eg errgroup.Group
-	j.readAtOffset(b, j.rootData, 0, j.span, off, 0, readLen, &bytesRead, &eg)
+	j.readAtOffset(buffer, j.rootData, 0, j.span, off, 0, readLen, &bytesRead, &eg)
 
 	err = eg.Wait()
 	if err != nil {
@@ -89,6 +89,8 @@ func (j *joiner) ReadAt(b []byte, off int64) (read int, err error) {
 
 	return int(atomic.LoadInt64(&bytesRead)), nil
 }
+
+var ErrMalformedTrie = errors.New("malformed tree")
 
 func (j *joiner) readAtOffset(b, data []byte, cur, subTrieSize, off, bufferOffset, bytesToRead int64, bytesRead *int64, eg *errgroup.Group) {
 	// we are at a leaf data chunk
@@ -120,7 +122,10 @@ func (j *joiner) readAtOffset(b, data []byte, cur, subTrieSize, off, bufferOffse
 
 		// if we are here it means that we are within the bounds of the data we need to read
 		address := swarm.NewAddress(data[cursor : cursor+j.refLength])
+
 		subtrieSpan := sec
+		subtrieSpanLimit := sec
+
 		currentReadSize := subtrieSpan - (off - cur) // the size of the subtrie, minus the offset from the start of the trie
 
 		// upper bound alignments
@@ -131,7 +136,7 @@ func (j *joiner) readAtOffset(b, data []byte, cur, subTrieSize, off, bufferOffse
 			currentReadSize = subtrieSpan
 		}
 
-		func(address swarm.Address, b []byte, cur, subTrieSize, off, bufferOffset, bytesToRead int64) {
+		func(address swarm.Address, b []byte, cur, subTrieSize, off, bufferOffset, bytesToRead, subtrieSpanLimit int64) {
 			eg.Go(func() error {
 				ch, err := j.getter.Get(j.ctx, storage.ModeGetRequest, address)
 				if err != nil {
@@ -140,10 +145,15 @@ func (j *joiner) readAtOffset(b, data []byte, cur, subTrieSize, off, bufferOffse
 
 				chunkData := ch.Data()[8:]
 				subtrieSpan := int64(chunkToSpan(ch.Data()))
+
+				if subtrieSpan > subtrieSpanLimit {
+					return ErrMalformedTrie
+				}
+
 				j.readAtOffset(b, chunkData, cur, subtrieSpan, off, bufferOffset, currentReadSize, bytesRead, eg)
 				return nil
 			})
-		}(address, b, cur, subtrieSpan, off, bufferOffset, currentReadSize)
+		}(address, b, cur, subtrieSpan, off, bufferOffset, currentReadSize, subtrieSpanLimit)
 
 		bufferOffset += currentReadSize
 		bytesToRead -= currentReadSize
--- pkg/file/joiner/joiner_test.go
+++ pkg/file/joiner/joiner_test.go
@@ -7,6 +7,7 @@ package joiner_test
 import (
 	"bytes"
 	"context"
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"io"
@@ -16,6 +17,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/ethersphere/bee/pkg/cac"
 	"github.com/ethersphere/bee/pkg/encryption/store"
 	"github.com/ethersphere/bee/pkg/file/joiner"
 	"github.com/ethersphere/bee/pkg/file/pipeline/builder"
@@ -23,6 +25,7 @@ import (
 	filetest "github.com/ethersphere/bee/pkg/file/testing"
 	"github.com/ethersphere/bee/pkg/storage"
 	"github.com/ethersphere/bee/pkg/storage/mock"
+	testingc "github.com/ethersphere/bee/pkg/storage/testing"
 	"github.com/ethersphere/bee/pkg/swarm"
 	"gitlab.com/nolash/go-mockbytes"
 )
@@ -159,6 +162,51 @@ func TestJoinerWithReference(t *testing.T) {
 	}
 	if !bytes.Equal(resultBuffer, firstChunk.Data()[8:]) {
 		t.Fatalf("expected resultbuffer %v, got %v", resultBuffer, firstChunk.Data()[:len(resultBuffer)])
+	}
+}
+
+func TestJoinerMalformed(t *testing.T) {
+	store := mock.NewStorer()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	subTrie := []byte{8085: 1}
+	pb := builder.NewPipelineBuilder(ctx, store, storage.ModePutUpload, false)
+	c1addr, _ := builder.FeedPipeline(ctx, pb, bytes.NewReader(subTrie))
+
+	chunk2 := testingc.GenerateTestRandomChunk()
+	_, err := store.Put(ctx, storage.ModePutUpload, chunk2)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// root chunk
+	rootChunkData := make([]byte, 8+64)
+	binary.LittleEndian.PutUint64(rootChunkData[:8], uint64(swarm.ChunkSize*2))
+
+	copy(rootChunkData[8:], c1addr.Bytes())
+	copy(rootChunkData[8+32:], chunk2.Address().Bytes())
+
+	rootChunk, err := cac.NewWithDataSpan(rootChunkData)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = store.Put(ctx, storage.ModePutUpload, rootChunk)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	joinReader, _, err := joiner.New(ctx, store, rootChunk.Address())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resultBuffer := make([]byte, swarm.ChunkSize)
+	_, err = joinReader.Read(resultBuffer)
+	if !errors.Is(err, joiner.ErrMalformedTrie) {
+		t.Fatalf("expected %v, got %v", joiner.ErrMalformedTrie, err)
 	}
 }
 
