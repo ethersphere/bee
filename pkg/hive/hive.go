0fd4cc4d94b8c3b1354d1c20dcb571a6b3b40c2c
istae <14264581+esadakar@users.noreply.github.com>
2021-09-14 13:30:38 +0300

    fix: metric order

--- pkg/hive/hive.go
+++ pkg/hive/hive.go
@@ -288,9 +288,9 @@ func (s *Service) checkAndAddPeers(ctx context.Context, peers pb.Peers) {
 
 			// check if the underlay is usable by doing a raw ping using libp2p
 			if _, err = s.streamer.Ping(ctx, multiUnderlay); err != nil {
+				s.metrics.PingTime.Observe(float64(time.Since(start).Milliseconds()))
 				s.metrics.UnreachablePeers.Inc()
 				s.logger.Debugf("hive: peer %s: underlay %s not reachable", hex.EncodeToString(newPeer.Overlay), multiUnderlay)
-				s.metrics.PingTime.Observe(float64(time.Since(start).Milliseconds()))
 				return
 			}
 			s.metrics.PingTime.Observe(float64(time.Since(start).Milliseconds()))
