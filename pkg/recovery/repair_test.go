2a7ffd66fd85c2f57e4c5b47a0200f8c5120c8c8
alok <aloknerurkar@no-reply.com>
2021-10-05 12:03:05 +0530

    fix: test defs

--- pkg/recovery/repair_test.go
+++ pkg/recovery/repair_test.go
@@ -251,10 +251,10 @@ type mockPeerSuggester struct {
 	eachPeerRevFunc func(f topology.EachPeerFunc) error
 }
 
-func (s mockPeerSuggester) EachPeer(topology.EachPeerFunc) error {
+func (s mockPeerSuggester) EachPeer(_ topology.EachPeerFunc, _ ...topology.IteratorOpt) error {
 	return errors.New("not implemented")
 }
-func (s mockPeerSuggester) EachPeerRev(f topology.EachPeerFunc) error {
+func (s mockPeerSuggester) EachPeerRev(f topology.EachPeerFunc, _ ...topology.IteratorOpt) error {
 	return s.eachPeerRevFunc(f)
 }
 
