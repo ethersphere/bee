749b06d762474d410cb391632fbaac40eb9eb714
mrekucci <peter@ethswarm.org>
2021-10-05 12:20:27 +0200

    fix: increase p2p test coverage

--- pkg/p2p/p2p_test.go
+++ pkg/p2p/p2p_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/ethersphere/bee/pkg/p2p"
+	"github.com/libp2p/go-libp2p-core/network"
 )
 
 func TestNewSwarmStreamName(t *testing.T) {
@@ -16,5 +17,18 @@ func TestNewSwarmStreamName(t *testing.T) {
 
 	if got != want {
 		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func TestReachabilityStatus_String(t *testing.T) {
+	mapping := map[string]string{
+		p2p.ReachabilityStatusUnknown.String(): network.ReachabilityUnknown.String(),
+		p2p.ReachabilityStatusPrivate.String(): network.ReachabilityPrivate.String(),
+		p2p.ReachabilityStatusPublic.String():  network.ReachabilityPublic.String(),
+	}
+	for have, want := range mapping {
+		if have != want {
+			t.Fatalf("have reachability status %q; want %q", have, want)
+		}
 	}
 }
