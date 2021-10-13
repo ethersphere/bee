0aa0640a55b329b12ded4d3a3e2c8f6d47b6c2f4
acud <12988138+acud@users.noreply.github.com>
2021-10-13 10:22:15 +0100

    fix: panic before notifier set

--- pkg/p2p/libp2p/libp2p.go
+++ pkg/p2p/libp2p/libp2p.go
@@ -312,6 +312,11 @@ func (s *Service) startReachabilityTracker() error {
 				return
 			case e := <-sub.Out():
 				if r, ok := e.(event.EvtLocalReachabilityChanged); ok {
+					select {
+					case <-s.ready:
+					case <-s.halt:
+						return
+					}
 					s.notifier.UpdateReachability(p2p.ReachabilityStatus(r.Reachability))
 				}
 			}
