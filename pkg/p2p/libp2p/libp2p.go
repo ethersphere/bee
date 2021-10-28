3d99759794827713c47146f3ddb42d5e4973942c
istae <14264581+esadakar@users.noreply.github.com>
2021-10-28 11:42:59 +0300

    fix: reacher options and unit test

--- pkg/p2p/libp2p/libp2p.go
+++ pkg/p2p/libp2p/libp2p.go
@@ -480,7 +480,7 @@ func (s *Service) handleIncoming(stream network.Stream) {
 
 func (s *Service) SetPickyNotifier(n p2p.PickyNotifier) {
 	s.handshakeService.SetPicker(n)
-	s.reacher = reacher.New(s, n)
+	s.reacher = reacher.New(s, n, nil)
 	s.notifier = n
 }
 
