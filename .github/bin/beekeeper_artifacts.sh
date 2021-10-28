0b71ae65c1330fb407a51f270e281c9c591cb27e
istae <14264581+esadakar@users.noreply.github.com>
2021-10-28 19:45:07 +0300

    fix: artifact logs

--- .github/bin/beekeeper_artifacts.sh
+++ .github/bin/beekeeper_artifacts.sh
@@ -14,7 +14,9 @@ do
   curl -s -o dump/"$i"/stamps.json "$i"-debug.localhost/stamps
 done
 kubectl -n local get pods > dump/kubectl_get_pods
-kubectl -n local logs -l app.kubernetes.io/part-of=bee --tail -1 --prefix -c bee > dump/kubectl_logs
+kubectl -n local logs bee-0 > dump/kubectl_logs_bee_0
+kubectl -n local logs bee-1 > dump/kubectl_logs_bee_1
+kubectl -n local logs bootnode-0 > dump/kubectl_logs_bootnode_0
 endpoint=$AWS_ENDPOINT
 if [[ "$endpoint" != http* ]]
 then
