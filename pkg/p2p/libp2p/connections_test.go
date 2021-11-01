1e639a970b93a0ee109d9197c156058a752271fa
istae <14264581+esadakar@users.noreply.github.com>
2021-11-01 14:07:26 +0300

    fix: unskip test

--- pkg/p2p/libp2p/connections_test.go
+++ pkg/p2p/libp2p/connections_test.go
@@ -832,8 +832,6 @@ func TestWithBlocklistStreams(t *testing.T) {
 
 func TestUserAgentLogging(t *testing.T) {
 
-	t.Skip("skipping temporarily")
-
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
