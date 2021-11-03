bbd1a52a6370660ff34a7040e34f99bc5c2abf06
metacertain <metacertain@gmail.com>
2021-11-03 14:07:22 +0100

    fix: metrics

--- pkg/settlement/pseudosettle/pseudosettle.go
+++ pkg/settlement/pseudosettle/pseudosettle.go
@@ -249,15 +249,13 @@ func (s *Service) handler(ctx context.Context, p p2p.Peer, stream p2p.Stream) (e
 }
 
 // Pay initiates a payment to the given peer
-func (s *Service) Pay(ctx context.Context, peer swarm.Address, amount, checkAllowance *big.Int) (*big.Int, int64, error) {
+func (s *Service) Pay(ctx context.Context, peer swarm.Address, amount, checkAllowance *big.Int) (payedAmount *big.Int, time int64, err error) {
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 
-	var err error
-
 	defer func() {
 		if err != nil {
-			s.metrics.ReceivedPseudoSettlementsErrors.Inc()
+			s.metrics.SentPseudoSettlementsErrors.Inc()
 		}
 	}()
 
