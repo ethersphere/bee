15b9f6594d687ee7308db38d246b4ca8c6ee23c5
istae <14264581+istae@users.noreply.github.com>
2021-11-16 18:39:24 +0300

    chore: upload and download api metrics middleware (#2655)

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -391,6 +391,30 @@ func (s *server) newTracingHandler(spanName string) func(h http.Handler) http.Ha
 	}
 }
 
+func (s *server) contentLengthMetricMiddleware() func(h http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			now := time.Now()
+			h.ServeHTTP(w, r)
+			switch r.Method {
+			case http.MethodGet:
+				contentLength, err := strconv.Atoi(w.Header().Get("Decompressed-Content-Length"))
+				if err != nil {
+					s.logger.Debugf("api: content length int conversation failed: %v", err)
+					return
+				}
+				if contentLength > 0 {
+					s.metrics.ContentApiDuration.WithLabelValues(fmt.Sprintf("%d", toFileSizeBucket(int64(contentLength))), r.Method).Observe(time.Since(now).Seconds())
+				}
+			case http.MethodPost:
+				if r.ContentLength > 0 {
+					s.metrics.ContentApiDuration.WithLabelValues(fmt.Sprintf("%d", toFileSizeBucket(r.ContentLength)), r.Method).Observe(time.Since(now).Seconds())
+				}
+			}
+		})
+	}
+}
+
 func lookaheadBufferSize(size int64) int {
 	if size <= largeBufferFilesizeThreshold {
 		return smallFileBufferSize
---
+++ pkg/api/metrics_test.go
@@ -0,0 +1,32 @@
+// Copyright 2021 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package api_test
+
+import (
+	"testing"
+
+	"github.com/ethersphere/bee/pkg/api"
+)
+
+func TestToFileSizeBucket(t *testing.T) {
+
+	var want int64 = 300000
+	bucket := api.ToFileSizeBucket(want)
+	if bucket < want {
+		t.Fatalf("bucket should be greater than filesize")
+	}
+
+	want = 5000000
+	bucket = api.ToFileSizeBucket(want)
+	if bucket != want {
+		t.Fatalf("bucket should be exactly 5000000")
+	}
+
+	overBound := api.FileSizeBucketsKBytes[len(api.FileSizeBucketsKBytes)-1]*1000 + 1
+	bucket = api.ToFileSizeBucket(overBound)
+	if bucket != api.FileSizeBucketsKBytes[len(api.FileSizeBucketsKBytes)-1]*1000 {
+		t.Fatalf("bucket should be the last bucket")
+	}
+}
