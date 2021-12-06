5c6418cdf304ec4ce4ebc83fbb425f7431a3fcb3
metacertain <metacertain@gmail.com>
2021-12-06 13:23:47 +0100

    fix: naming

--- pkg/retrieval/retrieval.go
+++ pkg/retrieval/retrieval.go
@@ -519,7 +519,7 @@ func (s *Service) CheckAvailableChunk(ctx context.Context, addr swarm.Address) (
 
 func (s *Service) checkAvailableChunk(ctx context.Context, addr swarm.Address, sp *skipPeers) (attempt bool, err error) {
 
-	ctx, cancel := context.WithTimeout(ctx, retrieveChunkTimeout)
+	ctx, cancel := context.WithTimeout(ctx, defaultTTL)
 	defer cancel()
 
 	peer, err := s.farthestPeer(addr, sp.All())
