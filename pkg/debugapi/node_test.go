20139dfd564275ec292abbf96fcbc345acf02254
nugaon <toth.viktor.levente@gmail.com>
2021-12-07 16:07:05 +0100

    fix: package namespace

--- pkg/debugapi/node_test.go
+++ pkg/debugapi/node_test.go
@@ -2,20 +2,22 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package debugapi
+package debugapi_test
 
 import (
 	"testing"
+
+	"github.com/ethersphere/bee/pkg/debugapi"
 )
 
 func TestBeeNodeMode_String(t *testing.T) {
-	const nonExistingMode BeeNodeMode = 4
+	const nonExistingMode debugapi.BeeNodeMode = 4
 
 	mapping := map[string]string{
-		LightMode.String():       "light",
-		FullMode.String():        "full",
-		DevMode.String():         "dev",
-		nonExistingMode.String(): "unknown",
+		debugapi.LightMode.String(): "light",
+		debugapi.FullMode.String():  "full",
+		debugapi.DevMode.String():   "dev",
+		nonExistingMode.String():    "unknown",
 	}
 
 	for have, want := range mapping {
