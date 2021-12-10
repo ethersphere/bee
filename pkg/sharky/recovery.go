664c7e39b007ed46e67a9e48006a79e8c6d66dc5
zelig <viktor.tron@gmail.com>
2021-12-10 12:21:34 +0100

    refactor(sharky): refactor shard loop to fix race

--- pkg/sharky/recovery.go
+++ pkg/sharky/recovery.go
@@ -4,14 +4,19 @@ import (
 	"fmt"
 	"os"
 	"path"
+
+	"github.com/hashicorp/go-multierror"
 )
 
+// Recovery allows disaster recovery
 type Recovery struct {
 	shards []*slots
+	files  []os.File
 }
 
-func NewRecovery(dir string, shardCnt int, shardSize uint32) (*Recovery, error) {
+func NewRecovery(dir string, shardCnt int, shardSize uint32, datasize int) (*Recovery, error) {
 	shards := make([]*slots, shardCnt)
+	files := []os.File{}
 	for i := 0; i < shardCnt; i++ {
 		file, err := os.OpenFile(path.Join(dir, fmt.Sprintf("shard_%03d", i)), os.O_RDWR|os.O_CREATE, 0644)
 		if err != nil {
@@ -21,7 +26,7 @@ func NewRecovery(dir string, shardCnt int, shardSize uint32) (*Recovery, error) 
 		if err != nil {
 			return nil, err
 		}
-		size := uint32(fi.Size() / DataSize)
+		size := uint32(fi.Size() / int64(datasize))
 		ffile, err := os.OpenFile(path.Join(dir, fmt.Sprintf("free_%03d", i)), os.O_RDWR|os.O_CREATE, 0644)
 		if err != nil {
 			return nil, err
@@ -32,9 +37,10 @@ func NewRecovery(dir string, shardCnt int, shardSize uint32) (*Recovery, error) 
 		sl.head = 0
 		shards[i] = sl
 	}
-	return &Recovery{shards}, nil
+	return &Recovery{shards, files}, nil
 }
 
+// Add marks a location as used (not free)
 func (r *Recovery) Add(loc Location) error {
 	sh := r.shards[loc.Shard]
 	l := len(sh.data)
@@ -48,14 +54,24 @@ func (r *Recovery) Add(loc Location) error {
 	return nil
 }
 
-func (r *Recovery) Save() error {
+// Save saves all free slots files of the recovery (without closing)
+func (r *Recovery) Save() (err error) {
 	for _, sh := range r.shards {
 		for i := range sh.data {
 			sh.data[i] ^= 0xff
 		}
-		if err := sh.save(); err != nil {
-			return err
-		}
+		err = multierror.Append(err, sh.save())
 	}
-	return nil
+	return err.(*multierror.Error).ErrorOrNil()
+}
+
+// Close closes data and free slots files of the recovery (without saving)
+func (r *Recovery) Close() (err error) {
+	for _, sh := range r.shards {
+		err = multierror.Append(err, sh.close())
+	}
+	for _, file := range r.files {
+		err = multierror.Append(err, file.Close())
+	}
+	return err.(*multierror.Error).ErrorOrNil()
 }
