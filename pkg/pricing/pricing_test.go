05c76b5f97f336029cff387ac3970844de5bd22a
metacertain <metacertain@gmail.com>
2021-12-15 16:00:12 +0100

    feat: add test for differentiated initial threshold

--- pkg/pricing/pricing_test.go
+++ pkg/pricing/pricing_test.go
@@ -158,3 +158,135 @@ func TestAnnouncePaymentWithInsufficientThreshold(t *testing.T) {
 		t.Fatal("unexpected call to the observer")
 	}
 }
+
+func TestInitialPaymentThreshold(t *testing.T) {
+	logger := logging.New(io.Discard, 0)
+	testThreshold := big.NewInt(100000)
+	testLightThreshold := big.NewInt(10000)
+
+	observer := &testThresholdObserver{}
+
+	recipient := pricing.New(nil, logger, testThreshold, testLightThreshold, big.NewInt(1000))
+	recipient.SetPaymentThresholdObserver(observer)
+
+	peerID := swarm.MustParseHexAddress("9ee7add7")
+	peer := p2p.Peer{Address: peerID, FullNode: true}
+
+	recorder := streamtest.New(
+		streamtest.WithProtocols(recipient.Protocol()),
+		streamtest.WithBaseAddr(peerID),
+	)
+
+	payer := pricing.New(recorder, logger, testThreshold, testLightThreshold, big.NewInt(1000))
+
+	err := payer.Init(context.Background(), peer)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	records, err := recorder.Records(peerID, "pricing", "1.0.0", "pricing")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if l := len(records); l != 1 {
+		t.Fatalf("got %v records, want %v", l, 1)
+	}
+
+	record := records[0]
+
+	messages, err := protobuf.ReadMessages(
+		bytes.NewReader(record.In()),
+		func() protobuf.Message { return new(pb.AnnouncePaymentThreshold) },
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(messages) != 1 {
+		t.Fatalf("got %v messages, want %v", len(messages), 1)
+	}
+
+	sentPaymentThreshold := big.NewInt(0).SetBytes(messages[0].(*pb.AnnouncePaymentThreshold).PaymentThreshold)
+	if sentPaymentThreshold.Cmp(testThreshold) != 0 {
+		t.Fatalf("got message with amount %v, want %v", sentPaymentThreshold, testThreshold)
+	}
+
+	if !observer.called {
+		t.Fatal("expected observer to be called")
+	}
+
+	if observer.paymentThreshold.Cmp(testThreshold) != 0 {
+		t.Fatalf("observer called with wrong paymentThreshold. got %d, want %d", observer.paymentThreshold, testThreshold)
+	}
+
+	if !observer.peer.Equal(peerID) {
+		t.Fatalf("observer called with wrong peer. got %v, want %v", observer.peer, peerID)
+	}
+}
+
+func TestInitialPaymentThresholdLightNode(t *testing.T) {
+	logger := logging.New(io.Discard, 0)
+	testThreshold := big.NewInt(100000)
+	testLightThreshold := big.NewInt(10000)
+
+	observer := &testThresholdObserver{}
+
+	recipient := pricing.New(nil, logger, testThreshold, testLightThreshold, big.NewInt(1000))
+	recipient.SetPaymentThresholdObserver(observer)
+
+	peerID := swarm.MustParseHexAddress("9ee7add7")
+	peer := p2p.Peer{Address: peerID, FullNode: false}
+
+	recorder := streamtest.New(
+		streamtest.WithProtocols(recipient.Protocol()),
+		streamtest.WithBaseAddr(peerID),
+	)
+
+	payer := pricing.New(recorder, logger, testThreshold, testLightThreshold, big.NewInt(1000))
+
+	err := payer.Init(context.Background(), peer)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	records, err := recorder.Records(peerID, "pricing", "1.0.0", "pricing")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if l := len(records); l != 1 {
+		t.Fatalf("got %v records, want %v", l, 1)
+	}
+
+	record := records[0]
+
+	messages, err := protobuf.ReadMessages(
+		bytes.NewReader(record.In()),
+		func() protobuf.Message { return new(pb.AnnouncePaymentThreshold) },
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(messages) != 1 {
+		t.Fatalf("got %v messages, want %v", len(messages), 1)
+	}
+
+	sentPaymentThreshold := big.NewInt(0).SetBytes(messages[0].(*pb.AnnouncePaymentThreshold).PaymentThreshold)
+	if sentPaymentThreshold.Cmp(testLightThreshold) != 0 {
+		t.Fatalf("got message with amount %v, want %v", sentPaymentThreshold, testLightThreshold)
+	}
+
+	if !observer.called {
+		t.Fatal("expected observer to be called")
+	}
+
+	if observer.paymentThreshold.Cmp(testLightThreshold) != 0 {
+		t.Fatalf("observer called with wrong paymentThreshold. got %d, want %d", observer.paymentThreshold, testLightThreshold)
+	}
+
+	if !observer.peer.Equal(peerID) {
+		t.Fatalf("observer called with wrong peer. got %v, want %v", observer.peer, peerID)
+	}
+}
