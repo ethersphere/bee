a587037c46ba350192bc7c1773441671fe482945
metacertain <metacertain@gmail.com>
2021-12-15 16:00:45 +0100

    fix: add export test

---
+++ pkg/pricing/export_test.go
@@ -0,0 +1,15 @@
+// Copyright 2021 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package pricing
+
+import (
+	"context"
+
+	"github.com/ethersphere/bee/pkg/p2p"
+)
+
+func (s *Service) Init(ctx context.Context, p p2p.Peer) error {
+	return s.init(ctx, p)
+}
