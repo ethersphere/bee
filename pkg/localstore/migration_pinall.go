13162cb5646ba967442601e6a24d056eee13defe
acud <12988138+acud@users.noreply.github.com>
2022-01-03 15:08:11 -0600

    fix: add statestore migration

--- pkg/localstore/migration_pinall.go
+++ pkg/localstore/migration_pinall.go
@@ -56,6 +56,38 @@ func migratePinAll(db *DB) error {
 		return err
 	}
 
+	gcIndex, err := db.shed.NewIndex("AccessTimestamp|BinID|Hash->BatchID|BatchIndex", shed.IndexFuncs{
+		EncodeKey: func(fields shed.Item) (key []byte, err error) {
+			b := make([]byte, 16, 16+len(fields.Address))
+			binary.BigEndian.PutUint64(b[:8], uint64(fields.AccessTimestamp))
+			binary.BigEndian.PutUint64(b[8:16], fields.BinID)
+			key = append(b, fields.Address...)
+			return key, nil
+		},
+		DecodeKey: func(key []byte) (e shed.Item, err error) {
+			e.AccessTimestamp = int64(binary.BigEndian.Uint64(key[:8]))
+			e.BinID = binary.BigEndian.Uint64(key[8:16])
+			e.Address = key[16:]
+			return e, nil
+		},
+		EncodeValue: func(fields shed.Item) (value []byte, err error) {
+			value = make([]byte, 40)
+			copy(value, fields.BatchID)
+			copy(value[32:], fields.Index)
+			return value, nil
+		},
+		DecodeValue: func(keyItem shed.Item, value []byte) (e shed.Item, err error) {
+			e.BatchID = make([]byte, 32)
+			copy(e.BatchID, value[:32])
+			e.Index = make([]byte, postage.IndexSize)
+			copy(e.Index, value[32:])
+			return e, nil
+		},
+	})
+	if err != nil {
+		return err
+	}
+
 	pinIndex, err := db.shed.NewIndex("Hash->PinCounter", shed.IndexFuncs{
 		EncodeKey: func(fields shed.Item) (key []byte, err error) {
 			return fields.Address, nil
@@ -75,40 +107,25 @@ func migratePinAll(db *DB) error {
 		},
 	})
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	pushIndex, err := db.shed.NewIndex("StoreTimestamp|Hash->Tags", shed.IndexFuncs{
-		EncodeKey: func(fields shed.Item) (key []byte, err error) {
-			key = make([]byte, 40)
-			binary.BigEndian.PutUint64(key[:8], uint64(fields.StoreTimestamp))
-			copy(key[8:], fields.Address)
-			return key, nil
-		},
-		DecodeKey: func(key []byte) (e shed.Item, err error) {
-			e.Address = key[8:]
-			e.StoreTimestamp = int64(binary.BigEndian.Uint64(key[:8]))
-			return e, nil
-		},
-		EncodeValue: func(fields shed.Item) (value []byte, err error) {
-			tag := make([]byte, 4)
-			binary.BigEndian.PutUint32(tag, fields.Tag)
-			return tag, nil
-		},
-		DecodeValue: func(keyItem shed.Item, value []byte) (e shed.Item, err error) {
-			if len(value) == 4 { // only values with tag should be decoded
-				e.Tag = binary.BigEndian.Uint32(value)
-			}
-			return e, nil
-		},
-	})
+	// Persist gc size.
+	gcSize, err := db.shed.NewUint64Field("gc-size")
 	if err != nil {
 		return err
 	}
+
+	// reserve size
+	reserveSize, err := db.shed.NewUint64Field("reserve-size")
+	if err != nil {
+		return err
+	}
+
 	const limit = 10000
 	i := 0
 
-	err = db.gcIndex.Iterate(func(item shed.Item) (stop bool, err error) {
+	err = gcIndex.Iterate(func(item shed.Item) (stop bool, err error) {
 		i++
 		// delete from retrieve, pull, gc
 		err = db.retrievalDataIndex.DeleteInBatch(batch, item)
@@ -158,6 +175,39 @@ func migratePinAll(db *DB) error {
 	err = db.shed.WriteBatch(batch)
 	if err != nil {
 		return fmt.Errorf("write last batch: %w", err)
+	}
+	batch.Reset()
+
+	if err = gcSize.Put(0); err != nil {
+		return err
+	}
+
+	i = 0
+	ii := uint64(0)
+	err = pinIndex.Iterate(func(item shed.Item) (stop bool, err error) {
+		i++
+		ii++
+		if item.PinCounter > 1 {
+			item.PinCounter = 1
+		}
+
+		if err = pinIndex.PutInBatch(batch, item); err != nil {
+			return true, err
+		}
+
+		if i == limit {
+			err = db.shed.WriteBatch(batch)
+			if err != nil {
+				return true, fmt.Errorf("write batch: %w", err)
+			}
+			i = 0
+			batch.Reset()
+		}
+		return false, nil
+	}, nil)
+
+	if err = reserveSize.Put(ii); err != nil {
+		return err
 	}
 
 	return nil
