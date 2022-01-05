c50c89b6c1d94c233e4aef2c2467afaa157effa7
Viktor Trón <viktor.tron@gmail.com>
2022-01-05 13:39:11 +0100

    refactor: sharky free slot handling and recovery support (#2708)
    * refactor(sharky): free slots handling with bitvectors
    * feat(sharky): recovery helper
    * refactor(sharky): refactor shard loop to fix race
    * fix(sharky): fix channel write flow control
    
    Co-authored-by: acud <12988138+acud@users.noreply.github.com>

---
+++ pkg/sharky/recovery_test.go
@@ -0,0 +1,175 @@
+// Copyright 2021 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package sharky_test
+
+import (
+	"bytes"
+	"context"
+	"encoding/binary"
+	"errors"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/ethersphere/bee/pkg/sharky"
+)
+
+func TestRecovery(t *testing.T) {
+	datasize := 4
+	shards := 8
+	shardSize := uint32(16)
+	limit := shards * int(shardSize)
+
+	dir := t.TempDir()
+	ctx := context.Background()
+	size := limit / 2
+	data := make([]byte, 4)
+	locs := make([]sharky.Location, size)
+	preserved := make(map[uint32]bool)
+
+	func() {
+		s, err := sharky.New(dir, shards, shardSize, datasize)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer func() {
+			if err := s.Close(); err != nil {
+				t.Fatal(err)
+			}
+		}()
+		for i := range locs {
+			binary.BigEndian.PutUint32(data, uint32(i))
+			loc, err := s.Write(ctx, data)
+			if err != nil {
+				t.Fatal(err)
+			}
+			locs[i] = loc
+		}
+		// extract locations to preserve / free in map
+		indexes := make([]uint32, size)
+		for i := range indexes {
+			indexes[i] = uint32(i)
+		}
+		rest := indexes[:]
+		for n := size; n > size/2; n-- {
+			i := rand.Intn(n)
+			preserved[rest[i]] = false
+			rest = append(rest[:i], rest[i+1:]...)
+		}
+		if len(rest) != len(preserved) {
+			t.Fatalf("incorrect set sizes: %d <> %d", len(rest), len(preserved))
+		}
+		for _, i := range rest {
+			preserved[i] = true
+		}
+	}()
+	// recover based on preserved map
+	func() {
+		r, err := sharky.NewRecovery(dir, shards, shardSize, datasize)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer func() {
+			if err := r.Close(); err != nil {
+				t.Fatal(err)
+			}
+		}()
+		for i, add := range preserved {
+			if add {
+				if err := r.Add(locs[i]); err != nil {
+					t.Fatal(err)
+				}
+			}
+		}
+		if err := r.Save(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+	// check integrity of reecovered sharky
+	func() {
+		s, err := sharky.New(dir, shards, shardSize, datasize)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer func() {
+			if err := s.Close(); err != nil {
+				t.Fatal(err)
+			}
+		}()
+		buf := make([]byte, datasize)
+		t.Run("preserved are found", func(t *testing.T) {
+			for i := range preserved {
+				loc := locs[int(i)]
+				if err := s.Read(ctx, loc, buf); err != nil {
+					t.Fatal(err)
+				}
+				j := binary.BigEndian.Uint32(buf)
+				if i != j {
+					t.Fatalf("data not preserved at location %v: want %d; got %d", loc, i, j)
+				}
+			}
+		})
+		var freelocs []sharky.Location
+		payload := []byte{0xff}
+		t.Run("correct number of free slots", func(t *testing.T) {
+			cctx, cancel := context.WithTimeout(ctx, 800*time.Millisecond)
+			defer cancel()
+			for {
+				loc, err := s.Write(cctx, payload)
+				if err != nil {
+					if errors.Is(err, context.DeadlineExceeded) {
+						break
+					}
+					t.Fatal(err)
+				}
+				freelocs = append(freelocs, loc)
+			}
+			if len(freelocs) != limit-size/2 {
+				t.Fatalf("incorrect number of free slots: wanted %d; got %d", limit-size/2, len(freelocs))
+			}
+		})
+		t.Run("added locs are still preserved", func(t *testing.T) {
+			for i, added := range preserved {
+				if !added {
+					continue
+				}
+				if err := s.Read(ctx, locs[int(i)], buf); err != nil {
+					t.Fatal(err)
+				}
+				j := binary.BigEndian.Uint32(buf)
+				if i != j {
+					t.Fatalf("data not preserved at location %v: want %d; got %d", locs[int(j)], i, j)
+				}
+			}
+		})
+		t.Run("not added preserved are overwritten", func(t *testing.T) {
+			for i, added := range preserved {
+				if added {
+					continue
+				}
+				loc := locs[int(i)]
+				loc.Length = 1
+				if err := s.Read(ctx, loc, buf); err != nil {
+					t.Fatal(err)
+				}
+				data := buf[:len(payload)]
+				if !bytes.Equal(data, payload) {
+					t.Fatalf("incorrect data on freed location %v: want %x; got %x", loc, payload, data)
+				}
+			}
+		})
+		t.Run("all other slots also overwritten", func(t *testing.T) {
+			for _, loc := range freelocs {
+				if err := s.Read(ctx, loc, buf); err != nil {
+					t.Fatal(err)
+				}
+				data := buf[:len(payload)]
+				if !bytes.Equal(data, payload) {
+					t.Fatalf("incorrect data on freed location %v: want %x; got %x", loc, payload, data)
+				}
+			}
+		})
+	}()
+}
--- pkg/sharky/shard.go
+++ pkg/sharky/shard.go
@@ -5,187 +5,140 @@
 package sharky
 
 import (
-	"encoding/json"
+	"context"
 	"os"
-	"sync"
 )
 
-// location models the location <shard, offset, length> of a chunk
+// Location models the location <shard, slot, length> of a chunk
 type Location struct {
 	Shard  uint8
-	Offset int64
-	Length int64
+	Slot   uint32
+	Length uint16
 }
 
-// operation models both read and write
-type operation struct {
-	location Location   // shard, offset, length combo - given for read, returned by write
-	buffer   []byte     // fixed size byte slice for data - copied to by read
-	data     []byte     // variable length byte slice - given for write
-	err      chan error // signal for end of operation
+// write models the input to a write operation
+type write struct {
+	buf []byte     // variable size read buffer
+	res chan entry // to put the result through
+}
+
+// entry models the output result of a write operation
+type entry struct {
+	loc Location // shard, slot, length combo
+	err error    // signal for end of operation
+}
+
+// read models the input to read operation (the output is an error)
+type read struct {
+	buf  []byte // variable size read buffer
+	slot uint32 // slot to read from
+	at   int    // within-chunk offset for partial reads
 }
 
 // shard models a shard writing to a file with periodic offsets due to fixed datasize
 type shard struct {
-	readOps  chan *operation // channel for reads
-	writeOps chan *operation // channel for writes
-	free     chan int64      // channel for offsets available to write
-	freed    chan int64      // channel for offsets freed by garbage collection
-	index    uint8           // index of the shard
-	limit    int64           // max number of items in the shard
-	fh       *os.File        // the file handle the shard is writing data to
-	ffh      *os.File        // the file handle the shard is writing free slats to
-	quit     chan struct{}   // channel to signal quitting
-	wg       *sync.WaitGroup // waitgroup to allow clean closing
-}
-
-// forever loop processing
-func (sh *shard) offer(size int64) {
-	var offset int64
-	defer sh.wg.Done()
-	for {
-		// first try to obtain a free offset from among freed ones
-		select {
-		case offset = <-sh.freed:
-		case <-sh.quit:
-			return
-			// fallback to extending shard until limit
-		default:
-			// if limit is reached we only allow free offsets via release
-			if size == sh.limit {
-				select {
-				case offset = <-sh.freed:
-				case <-sh.quit:
-					return
-				}
-			} else {
-				// shard is allowed to grow upto limit
-				offset = size * DataSize
-				size++
-			}
-		}
-		// push the obtained offset to the main process
-		select {
-		case sh.free <- offset:
-		case <-sh.quit:
-			// remember free offset in limbo
-			sh.freed <- offset
-			return
-		}
-	}
+	reads    chan read     // channel for reads
+	errc     chan error    // result for reads
+	writes   chan write    // channel for writes
+	index    uint8         // index of the shard
+	datasize int           // max size of blobs
+	file     *os.File      // the file handle the shard is writing data to
+	slots    *slots        // component keeping track of freed slots
+	quit     chan struct{} // channel to signal quitting
 }
 
 // forever loop processing
 func (sh *shard) process() {
-	var writeOps chan *operation
-	var offset int64
-	free := sh.free
-	defer sh.wg.Done()
+	var writes chan write
+	var slot uint32
+	free := sh.slots.out
+LOOP:
 	for {
 		select {
-		// prioritise read ops
-		case op := <-sh.readOps:
-			sh.read(op)
+		case op := <-sh.reads:
+			// prioritise read ops i.e., continue processing read ops (only) as long as any
+			// this will block any writes on this shard effectively making store-wide
+			// write op to use a differenct shard while this one is busy
+			for {
+				sh.errc <- sh.read(op)
+				select {
+				case op = <-sh.reads:
+				default:
+					continue LOOP
+				}
+			}
+
+			// only enabled if there is a free slot previously popped
+		case op := <-writes:
+			op.res <- sh.write(op.buf, slot)
+			free = sh.slots.out // reenable popping a free slot next time we can write
+			writes = nil        // disable popping a write operation until there is a free slot
+
+			// pop a free slot
+		case slot = <-free:
+			// only if there is one can we pop a chunk to write otherwise keep back pressure on writes
+			// effectively enforcing another shard to be chosen
+			writes = sh.writes // enable popping a write operation
+			free = nil         // disabling getting a new slot until a write is actually done
+
 		case <-sh.quit:
-			// this condition checks if an offset is in limbo (popped but not used for write op)
-			if writeOps != nil {
-				sh.freed <- offset
+			// this condition checks if an slot is in limbo (popped but not used for write op)
+			if writes != nil {
+				sh.slots.wg.Add(1) // Done after the slots process pops from slots.in
+				go func() {
+					sh.slots.in <- slot
+				}()
 			}
 			return
-		default:
-			select {
-
-			// pop a free offset
-			case offset = <-free:
-				// only if there is one can we pop a chunk to write otherwise keep back pressure on writes
-				// effectively enforcing another shard to be chosen
-				writeOps = sh.writeOps // enable popping a write operation
-				free = nil             // disabling getting a new position until a write is actually done
-
-				// only enabled if there is a free offset (pos) previously popped
-			case op := <-writeOps:
-				sh.write(op, offset)
-				free = sh.free // reenable popping a free slot next time we can write
-				writeOps = nil // disable popping a write operation until there is a free slot
-
-				// prioritise read operations
-			case op := <-sh.readOps:
-				sh.read(op)
-
-			case <-sh.quit:
-				// again put back offset in limbo
-				if writeOps != nil {
-					sh.freed <- offset
-				}
-				return
-			}
 		}
-
 	}
 }
 
+// close closes the shard:
+// wait for pending operations to finish then saves free slots and blobs on disk
 func (sh *shard) close() error {
-	go func() { // close free channel only after all relevant inflight change
-		sh.wg.Wait()
-		close(sh.freed)
-	}()
-	free := []int64{}
-	for offset := range sh.freed {
-		free = append(free, offset)
-	}
-	frees, err := json.Marshal(free)
-	if err != nil {
+	sh.slots.wg.Wait()
+	if err := sh.slots.save(); err != nil {
 		return err
 	}
-	if err = sh.ffh.Truncate(0); err != nil {
+	if err := sh.slots.file.Close(); err != nil {
 		return err
 	}
-	if _, err = sh.ffh.Seek(0, 0); err != nil {
-		return err
-	}
-	if _, err := sh.ffh.Write(frees); err != nil {
-		return err
-	}
-	if err := sh.ffh.Close(); err != nil {
-		return err
-	}
-	return sh.fh.Close()
+	return sh.file.Close()
 }
 
-// not  called concurrently
-func (sh *shard) read(op *operation) {
-	_, err := sh.fh.ReadAt(op.buffer[:op.location.Length], op.location.Offset)
+// offset calculates the offset from the slot
+// this is possible since all blobs are of fixed size
+func (sh *shard) offset(slot uint32) int64 {
+	return int64(slot) * int64(sh.datasize)
+}
+
+// read reads loc.Length bytes to the buffer from the blob slot loc.Slot
+func (sh *shard) read(r read) error {
+	_, err := sh.file.ReadAt(r.buf, sh.offset(r.slot)+int64(r.at))
+	return err
+}
+
+// write writes loc.Length bytes to the buffer from the blob slot loc.Slot
+func (sh *shard) write(buf []byte, slot uint32) entry {
+	n, err := sh.file.WriteAt(buf, sh.offset(slot))
+	return entry{
+		loc: Location{
+			Shard:  sh.index,
+			Slot:   slot,
+			Length: uint16(n),
+		},
+		err: err,
+	}
+}
+
+// release frees the slot allowing new entry to overwrite
+func (sh *shard) release(ctx context.Context, slot uint32) error {
 	select {
-	case op.err <- err:
-	case <-sh.quit:
+	case sh.slots.in <- slot:
+		return nil
+	case <-ctx.Done():
+		sh.slots.wg.Done()
+		return ctx.Err()
 	}
-}
-
-// not  called concurrently
-func (sh *shard) write(op *operation, offset int64) {
-	sh.wg.Add(1)
-	_, err := sh.fh.WriteAt(op.data, offset)
-	if err != nil {
-		go func() {
-			sh.freed <- offset
-			sh.wg.Done()
-		}()
-	} else {
-		defer sh.wg.Done()
-		op.location.Offset = offset
-		op.location.Shard = sh.index
-		op.location.Length = int64(len(op.data))
-	}
-	select {
-	case op.err <- err:
-	case <-sh.quit:
-	}
-}
-
-func (sh *shard) release(offset int64) {
-	sh.wg.Add(1)
-	go func() {
-		sh.freed <- offset
-		sh.wg.Done()
-	}()
 }
