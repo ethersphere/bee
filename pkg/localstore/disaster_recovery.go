a4bf3838fda717c6275375bb4c9bd575e2466752
aloknerurkar <aloknerurkar@gmail.com>
2022-01-17 12:32:48 +0530

    feat: sharky DRP (#2752)
    * feat: drp for locastore+sharky
    
    * fix: recovery locations chan
    
    * fix: license
    
    * fix: stop on context cancel
    
    * fix: linter errcheck
    
    * fix: header
    
    * feat: use new recovery sharky
    
    * fix: remove dirty file after recovery
    
    * fix: review comments
    
    * fix: review comments
    
    * fix: review comments
    
    * fix: hanging channel, add test
    
    * fix: remove ineffective ctx from recovery, stop on error
    
    * feat: sharky db migration
    
    Co-authored-by: acud <12988138+acud@users.noreply.github.com>
    Co-authored-by: Anatolie Lupacescu <anatol@ethswarm.org>
    Co-authored-by: alok <aloknerurkar@no-reply.com>
    Co-authored-by: mrekucci <mrekucci@gmail.com>

---
+++ pkg/localstore/disaster_recovery.go
@@ -0,0 +1,95 @@
+// Copyright 2021 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package localstore
+
+import (
+	"encoding/binary"
+	"fmt"
+
+	"github.com/ethersphere/bee/pkg/postage"
+	"github.com/ethersphere/bee/pkg/sharky"
+	"github.com/ethersphere/bee/pkg/shed"
+)
+
+type locOrErr struct {
+	err error
+	loc sharky.Location
+}
+
+// recovery tries to recover a dirty database
+func recovery(db *DB) (chan locOrErr, error) {
+	// - go through all retrieval data index entries
+	// - find all used locations in sharky
+	// - return them so that sharky can be initialized with them
+
+	// first define the index instance
+	headerSize := 16 + postage.StampSize
+
+	retrievalDataIndex, err := db.shed.NewIndex("Address->StoreTimestamp|BinID|BatchID|BatchIndex|Sig|Location", shed.IndexFuncs{
+		EncodeKey: func(fields shed.Item) (key []byte, err error) {
+			return fields.Address, nil
+		},
+		DecodeKey: func(key []byte) (e shed.Item, err error) {
+			e.Address = key
+			return e, nil
+		},
+		EncodeValue: func(fields shed.Item) (value []byte, err error) {
+			b := make([]byte, headerSize)
+			binary.BigEndian.PutUint64(b[:8], fields.BinID)
+			binary.BigEndian.PutUint64(b[8:16], uint64(fields.StoreTimestamp))
+			stamp, err := postage.NewStamp(fields.BatchID, fields.Index, fields.Timestamp, fields.Sig).MarshalBinary()
+			if err != nil {
+				return nil, err
+			}
+			copy(b[16:], stamp)
+			value = append(b, fields.Location...)
+			return value, nil
+		},
+		DecodeValue: func(keyItem shed.Item, value []byte) (e shed.Item, err error) {
+			e.StoreTimestamp = int64(binary.BigEndian.Uint64(value[8:16]))
+			e.BinID = binary.BigEndian.Uint64(value[:8])
+			stamp := new(postage.Stamp)
+			if err = stamp.UnmarshalBinary(value[16:headerSize]); err != nil {
+				return e, err
+			}
+			e.BatchID = stamp.BatchID()
+			e.Index = stamp.Index()
+			e.Timestamp = stamp.Timestamp()
+			e.Sig = stamp.Sig()
+			e.Location = value[headerSize:]
+			return e, nil
+		},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	usedLocations := make(chan locOrErr)
+
+	go func() {
+		defer close(usedLocations)
+
+		err := retrievalDataIndex.Iterate(func(item shed.Item) (stop bool, err error) {
+			loc, err := sharky.LocationFromBinary(item.Location)
+			if err != nil {
+				return true, fmt.Errorf("location from binary: %w", err)
+			}
+
+			usedLocations <- locOrErr{
+				loc: loc,
+			}
+
+			return false, nil
+		}, nil)
+		if err != nil {
+			usedLocations <- locOrErr{
+				err: fmt.Errorf("iterate index: %w", err),
+			}
+		}
+	}()
+
+	return usedLocations, nil
+}
---
+++ pkg/localstore/migration_sharky.go
@@ -0,0 +1,213 @@
+// Copyright 2021 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package localstore
+
+import (
+	"context"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/ethersphere/bee/pkg/postage"
+	"github.com/ethersphere/bee/pkg/sharky"
+	"github.com/ethersphere/bee/pkg/shed"
+	"github.com/hashicorp/go-multierror"
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+// DBSchemaSharky is the bee schema identifier for sharky.
+const DBSchemaSharky = "sharky"
+
+// migrateDeadPush cleans up dangling push index entries that make the pusher stop pushing entries
+func migrateSharky(db *DB) error {
+	db.logger.Debug("starting sharky migration; have patience, this might take a while...")
+	var (
+		start          = time.Now()
+		batch          = new(leveldb.Batch)
+		batchSize      = 10000
+		batchCount     = 0
+		headerSize     = 16 + postage.StampSize
+		compactionRate = 100
+		compactionSize = batchSize * compactionRate
+	)
+
+	compaction := func() (time.Duration, error) {
+		compactStart := time.Now()
+		if err := db.shed.Compact(); err != nil {
+			return 0, fmt.Errorf("leveldb compaction failed: %w", err)
+		}
+		return time.Since(compactStart), nil
+	}
+
+	retrievalDataIndex, err := db.shed.NewIndex("Address->StoreTimestamp|BinID|BatchID|BatchIndex|Sig|Data", shed.IndexFuncs{
+		EncodeKey: func(fields shed.Item) (key []byte, err error) {
+			return fields.Address, nil
+		},
+		DecodeKey: func(key []byte) (e shed.Item, err error) {
+			e.Address = key
+			return e, nil
+		},
+		EncodeValue: func(fields shed.Item) (value []byte, err error) {
+			b := make([]byte, headerSize)
+			binary.BigEndian.PutUint64(b[:8], fields.BinID)
+			binary.BigEndian.PutUint64(b[8:16], uint64(fields.StoreTimestamp))
+			stamp, err := postage.NewStamp(fields.BatchID, fields.Index, fields.Timestamp, fields.Sig).MarshalBinary()
+			if err != nil {
+				return nil, err
+			}
+			copy(b[16:], stamp)
+			value = append(b, fields.Data...)
+			return value, nil
+		},
+		DecodeValue: func(keyItem shed.Item, value []byte) (e shed.Item, err error) {
+			e.StoreTimestamp = int64(binary.BigEndian.Uint64(value[8:16]))
+			e.BinID = binary.BigEndian.Uint64(value[:8])
+			stamp := new(postage.Stamp)
+			if err = stamp.UnmarshalBinary(value[16:headerSize]); err != nil {
+				return e, err
+			}
+			e.BatchID = stamp.BatchID()
+			e.Index = stamp.Index()
+			e.Timestamp = stamp.Timestamp()
+			e.Sig = stamp.Sig()
+			e.Data = value[headerSize:]
+			return e, nil
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	newRetrievalDataIndex, err := db.shed.NewIndex("Address->StoreTimestamp|BinID|BatchID|BatchIndex|Sig|Location", shed.IndexFuncs{
+		EncodeKey: func(fields shed.Item) (key []byte, err error) {
+			return fields.Address, nil
+		},
+		DecodeKey: func(key []byte) (e shed.Item, err error) {
+			e.Address = key
+			return e, nil
+		},
+		EncodeValue: func(fields shed.Item) (value []byte, err error) {
+			b := make([]byte, headerSize)
+			binary.BigEndian.PutUint64(b[:8], fields.BinID)
+			binary.BigEndian.PutUint64(b[8:16], uint64(fields.StoreTimestamp))
+			stamp, err := postage.NewStamp(fields.BatchID, fields.Index, fields.Timestamp, fields.Sig).MarshalBinary()
+			if err != nil {
+				return nil, err
+			}
+			copy(b[16:], stamp)
+			value = append(b, fields.Location...)
+			return value, nil
+		},
+		DecodeValue: func(keyItem shed.Item, value []byte) (e shed.Item, err error) {
+			e.StoreTimestamp = int64(binary.BigEndian.Uint64(value[8:16]))
+			e.BinID = binary.BigEndian.Uint64(value[:8])
+			stamp := new(postage.Stamp)
+			if err = stamp.UnmarshalBinary(value[16:headerSize]); err != nil {
+				return e, err
+			}
+			e.BatchID = stamp.BatchID()
+			e.Index = stamp.Index()
+			e.Timestamp = stamp.Timestamp()
+			e.Sig = stamp.Sig()
+			e.Location = value[headerSize:]
+			return e, nil
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	db.gcSize, err = db.shed.NewUint64Field("gc-size")
+	if err != nil {
+		return err
+	}
+
+	db.reserveSize, err = db.shed.NewUint64Field("reserve-size")
+	if err != nil {
+		return err
+	}
+
+	gcSize, err := db.gcSize.Get()
+	if err != nil {
+		return err
+	}
+
+	reserveSize, err := db.reserveSize.Get()
+	if err != nil {
+		return err
+	}
+
+	if gcSize+reserveSize > sharkyMaxTotalChunks {
+		return errors.New("maximum allowed chunks exceeded")
+	}
+
+	var compactionTime time.Duration
+	var dirtyLocations []sharky.Location
+
+	db.logger.Debugf("starting to move entries with batch size %d", batchSize)
+	for {
+		isBatchEmpty := true
+		err = retrievalDataIndex.Iterate(func(item shed.Item) (stop bool, err error) {
+			loc, err := db.sharky.Write(context.TODO(), item.Data)
+			if err != nil {
+				return false, err
+			}
+			dirtyLocations = append(dirtyLocations, loc)
+			item.Location, err = loc.MarshalBinary()
+			if err != nil {
+				return false, err
+			}
+			if err = newRetrievalDataIndex.PutInBatch(batch, item); err != nil {
+				return false, err
+			}
+			if err = retrievalDataIndex.DeleteInBatch(batch, item); err != nil {
+				return false, err
+			}
+			batchCount++
+			isBatchEmpty = false
+			if batchCount%batchSize == 0 {
+				db.logger.Debugf("collected %d entries; trying to flush...", batchSize)
+				return true, nil
+			}
+			return false, nil
+		}, nil)
+		if err != nil {
+			return fmt.Errorf("iterate index: %w", err)
+		}
+
+		if isBatchEmpty {
+			break
+		}
+
+		if err := db.shed.WriteBatch(batch); err != nil {
+			for _, loc := range dirtyLocations {
+				err = multierror.Append(err, db.sharky.Release(context.TODO(), loc))
+			}
+			return fmt.Errorf("write batch: %w", err)
+		}
+		dirtyLocations = nil
+		db.logger.Debugf("flush ok; progress so far: %d chunks", batchCount)
+		batch.Reset()
+
+		if batchCount%compactionSize == 0 {
+			dur, err := compaction()
+			if err != nil {
+				return err
+			}
+			compactionTime += dur
+		}
+	}
+
+	dur, err := compaction()
+	if err != nil {
+		return err
+	}
+	compactionTime += dur
+
+	db.logger.Debugf("leveldb compaction took: %v", compactionTime)
+	db.logger.Debugf("done migrating to sharky; it took me %s to move %d chunks.", time.Since(start), batchCount)
+	return nil
+}
