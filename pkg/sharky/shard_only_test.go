018a3abe6a4076a3d783e50884e8073015f0fbc6
Anatolie Lupacescu <anatol@ethswarm.org>
2022-01-19 12:37:35 -0600

    fix: three rw

--- pkg/sharky/shard_only_test.go
+++ pkg/sharky/shard_only_test.go
@@ -15,16 +15,18 @@ func TestShard(t *testing.T) {
 	shard := newShard(t)
 
 	var loc Location
-	var write0 = write{buf: []byte{0xff}, res: make(chan entry)}
+	payload := write{buf: []byte{0xff}, res: make(chan entry)}
+
+	shard.slots.wg.Add(1)
 
 	select {
-	case shard.writes <- write0:
-		e := <-write0.res
+	case shard.writes <- payload:
+		e := <-payload.res
 		if e.err != nil {
 			t.Fatal("write entry", e.err)
 		}
 		loc = e.loc
-		t.Log("wrote", write0.buf, "to slot", loc.Slot)
+		t.Log("wrote", payload.buf, "to slot", loc.Slot)
 	case <-time.After(100 * time.Millisecond):
 		t.Fatal("write timeout")
 	}
@@ -49,30 +51,74 @@ func TestShard(t *testing.T) {
 	cancel()
 	t.Log("released slot", loc.Slot)
 
-	var write1 = write{buf: []byte{0x0e}, res: make(chan entry)}
+	payload = write{buf: []byte{0xff >> 1}, res: make(chan entry)}
+
+	shard.slots.wg.Add(1)
 
 	select {
-	case shard.writes <- write1:
-		e := <-write1.res
+	case shard.writes <- payload:
+		e := <-payload.res
 		if e.err != nil {
 			t.Fatal("write entry", e.err)
 		}
 		loc = e.loc
-		t.Log("wrote", write1.buf, "to slot", loc.Slot)
+		t.Log("wrote", payload.buf, "to slot", loc.Slot)
 	case <-time.After(100 * time.Millisecond):
 		t.Fatal("write timeout")
 	}
 
+	buf = make([]byte, loc.Length)
+
 	select {
 	case shard.reads <- read{buf[:loc.Length], loc.Slot, 0}:
-		t.Log("read", buf, "from slot", loc.Slot)
 		if err := <-shard.errc; err != nil {
 			t.Fatal("read", err)
 		}
+		t.Log("read", buf, "from slot", loc.Slot)
+	case <-time.After(100 * time.Millisecond):
+		t.Fatal("timeout reading")
+	}
+
+	payload = write{buf: []byte{0xff >> 2}, res: make(chan entry)}
+
+	shard.slots.wg.Add(1)
+
+	select {
+	case shard.writes <- payload:
+		e := <-payload.res
+		if e.err != nil {
+			t.Fatal("write entry", e.err)
+		}
+		loc = e.loc
+		t.Log("wrote", payload.buf, "to slot", loc.Slot)
+	case <-time.After(100 * time.Millisecond):
+		t.Fatal("write timeout")
+	}
+
+	ctx = context.Background()
+	cctx, cancel = context.WithTimeout(ctx, 500*time.Millisecond)
+	if err := shard.release(cctx, loc.Slot); err != nil {
+		t.Fatal("release slot", loc.Slot, "err", err)
+	}
+	cancel()
+	t.Log("released slot", loc.Slot)
+
+	buf = make([]byte, loc.Length)
+
+	select {
+	case shard.reads <- read{buf[:loc.Length], loc.Slot, 0}:
+		if err := <-shard.errc; err != nil {
+			t.Fatal("read", err)
+		}
+		t.Log("read", buf, "from slot", loc.Slot)
 	case <-time.After(100 * time.Millisecond):
 		t.Fatal("timeout reading")
 	}
 }
+
+// 10 writes - 9 chunks
+
+//  2 3 5 7
 
 type dirFS string
 
@@ -97,41 +143,40 @@ func newShard(t *testing.T) *shard {
 	}
 
 	var wg sync.WaitGroup
-	wg.Add(1)
 
-	sl := newSlots(ffile.(sharkyFile), &wg)
-	err = sl.load()
+	slots := newSlots(ffile.(sharkyFile), &wg)
+	err = slots.load()
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	quit := make(chan struct{})
-	sh := &shard{
+	shard := &shard{
 		reads:    make(chan read),
 		errc:     make(chan error),
 		writes:   make(chan write),
 		index:    uint8(index),
-		datasize: 8,
+		datasize: 1,
 		file:     file.(sharkyFile),
-		slots:    sl,
+		slots:    slots,
 		quit:     quit,
 	}
 
 	t.Cleanup(func() {
 		quit <- struct{}{}
-		if err := sh.close(); err != nil {
+		if err := shard.close(); err != nil {
 			t.Fatal("close shard", err)
 		}
 	})
 
 	terminated := make(chan struct{})
-	sh.slots.wg.Add(1)
+
 	go func() {
-		sh.process()
+		shard.process()
 		close(terminated)
 	}()
 
-	go sl.process(terminated)
+	go slots.process(terminated)
 
-	return sh
+	return shard
 }
