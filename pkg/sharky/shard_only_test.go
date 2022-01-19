180951759afc50c33b57aa1a81abeee1d7bc038a
Anatolie Lupacescu <anatol@ethswarm.org>
2022-01-19 08:37:06 -0600

    fix: consistent beh

--- pkg/sharky/shard_only_test.go
+++ pkg/sharky/shard_only_test.go
@@ -11,30 +11,20 @@ import (
 	"time"
 )
 
-type dirFS struct {
-	basedir string
-}
-
-func (d *dirFS) Open(path string) (fs.File, error) {
-	return os.OpenFile(filepath.Join(d.basedir, path), os.O_RDWR|os.O_CREATE, 0644)
-}
-
 func TestShard(t *testing.T) {
 	shard := newShard(t)
 
-	payload := []byte{0xff}
-	res := make(chan entry)
-
 	var loc Location
+	var write0 = write{buf: []byte{0xff}, res: make(chan entry)}
 
 	select {
-	case shard.writes <- write{buf: payload, res: res}:
-		t.Log("wrote", payload)
-		e := <-res
+	case shard.writes <- write0:
+		e := <-write0.res
 		if e.err != nil {
 			t.Fatal("write entry", e.err)
 		}
 		loc = e.loc
+		t.Log("wrote", write0.buf, "to slot", loc.Slot)
 	case <-time.After(100 * time.Millisecond):
 		t.Fatal("write timeout")
 	}
@@ -43,34 +33,32 @@ func TestShard(t *testing.T) {
 
 	select {
 	case shard.reads <- read{buf[:loc.Length], loc.Slot, 0}:
-		t.Log("read", buf)
 		if err := <-shard.errc; err != nil {
 			t.Fatal("read", err)
 		}
+		t.Log("read", buf, "from slot", loc.Slot)
 	case <-time.After(100 * time.Millisecond):
 		t.Fatal("timeout reading")
 	}
 
 	ctx := context.Background()
 	cctx, cancel := context.WithTimeout(ctx, 500*time.Millisecond)
-	t.Cleanup(func() {
-		cancel()
-	})
-
 	if err := shard.release(cctx, loc.Slot); err != nil {
-		t.Fatal("release slot", err)
+		t.Fatal("release slot", loc.Slot, "err", err)
 	}
+	cancel()
+	t.Log("released slot", loc.Slot)
 
-	payload = []byte{payload[0] << 7}
+	var write1 = write{buf: []byte{0x0e}, res: make(chan entry)}
 
 	select {
-	case shard.writes <- write{buf: payload, res: res}:
-		e := <-res
+	case shard.writes <- write1:
+		e := <-write1.res
 		if e.err != nil {
 			t.Fatal("write entry", e.err)
 		}
 		loc = e.loc
-		t.Log("wrote", payload, "to slot", loc.Slot)
+		t.Log("wrote", write1.buf, "to slot", loc.Slot)
 	case <-time.After(100 * time.Millisecond):
 		t.Fatal("write timeout")
 	}
@@ -86,21 +74,28 @@ func TestShard(t *testing.T) {
 	}
 }
 
+type dirFS string
+
+func (d *dirFS) Open(path string) (fs.File, error) {
+	return os.OpenFile(filepath.Join(string(*d), path), os.O_RDWR|os.O_CREATE, 0644)
+}
+
 func newShard(t *testing.T) *shard {
 	t.Helper()
 
-	dir := t.TempDir()
-	basedir := &dirFS{basedir: dir}
+	basedir := dirFS(t.TempDir())
 	index := 1
 
 	file, err := basedir.Open(fmt.Sprintf("shard_%03d", index))
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	ffile, err := basedir.Open(fmt.Sprintf("free_%03d", index))
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	var wg sync.WaitGroup
 	wg.Add(1)
 
@@ -109,8 +104,8 @@ func newShard(t *testing.T) *shard {
 	if err != nil {
 		t.Fatal(err)
 	}
-	quit := make(chan struct{})
 
+	quit := make(chan struct{})
 	sh := &shard{
 		reads:    make(chan read),
 		errc:     make(chan error),
@@ -121,18 +116,22 @@ func newShard(t *testing.T) *shard {
 		slots:    sl,
 		quit:     quit,
 	}
+
 	t.Cleanup(func() {
 		quit <- struct{}{}
 		if err := sh.close(); err != nil {
 			t.Fatal("close shard", err)
 		}
 	})
+
 	terminated := make(chan struct{})
 	sh.slots.wg.Add(1)
 	go func() {
 		sh.process()
 		close(terminated)
 	}()
+
 	go sl.process(terminated)
+
 	return sh
 }
