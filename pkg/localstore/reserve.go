cf8024d7fc76553bd16b22988f262c4fdd2aeaa1
acud <12988138+acud@users.noreply.github.com>
2022-01-24 19:49:00 -0600

    chore: lint

--- pkg/localstore/reserve.go
+++ pkg/localstore/reserve.go
@@ -22,7 +22,7 @@ func (db *DB) UnreserveBatch(id []byte, radius uint8) (evicted uint64, err error
 			BatchID: id,
 		}
 		batch     = new(leveldb.Batch)
-		oldRadius = radius
+		oldRadius uint8
 	)
 	i, err := db.postageRadiusIndex.Get(item)
 	if err != nil {
