7ac3a8a2b201556e12587979755448d9cfe353a4
alok <aloknerurkar@no-reply.com>
2022-02-10 19:35:58 +0530

    fix: lint

--- pkg/postage/listener/listener_test.go
+++ pkg/postage/listener/listener_test.go
@@ -8,6 +8,7 @@ import (
 	"bytes"
 	"context"
 	"errors"
+	"fmt"
 	"io"
 	"math/big"
 	"sync"
@@ -63,14 +64,14 @@ func TestListener(t *testing.T) {
 
 		select {
 		case e := <-evC:
-			e.(blockNumberCall).compare(t, blockNumber-uint64(listener.TailSize)) // event args should be equal
+			e.(blockNumberCall).compareF(t, blockNumber-uint64(listener.TailSize)) // event args should be equal
 		case <-time.After(timeout):
 			t.Fatal("timed out waiting for block number update")
 		}
 
 		select {
 		case e := <-evC:
-			e.(createArgs).compare(t, c) // event args should be equal
+			e.(createArgs).compareF(t, c) // event args should be equal
 		case <-time.After(timeout):
 			t.Fatal("timed out waiting for event")
 		}
@@ -94,14 +95,14 @@ func TestListener(t *testing.T) {
 
 		select {
 		case e := <-evC:
-			e.(blockNumberCall).compare(t, blockNumber-uint64(listener.TailSize)) // event args should be equal
+			e.(blockNumberCall).compareF(t, blockNumber-uint64(listener.TailSize)) // event args should be equal
 		case <-time.After(timeout):
 			t.Fatal("timed out waiting for block number update")
 		}
 
 		select {
 		case e := <-evC:
-			e.(topupArgs).compare(t, topup) // event args should be equal
+			e.(topupArgs).compareF(t, topup) // event args should be equal
 		case <-time.After(timeout):
 			t.Fatal("timed out waiting for event")
 		}
@@ -125,14 +126,14 @@ func TestListener(t *testing.T) {
 
 		select {
 		case e := <-evC:
-			e.(blockNumberCall).compare(t, blockNumber-uint64(listener.TailSize)) // event args should be equal
+			e.(blockNumberCall).compareF(t, blockNumber-uint64(listener.TailSize)) // event args should be equal
 		case <-time.After(timeout):
 			t.Fatal("timed out waiting for block number update")
 		}
 
 		select {
 		case e := <-evC:
-			e.(depthArgs).compare(t, depthIncrease) // event args should be equal
+			e.(depthArgs).compareF(t, depthIncrease) // event args should be equal
 		case <-time.After(timeout):
 			t.Fatal("timed out waiting for event")
 		}
@@ -153,14 +154,14 @@ func TestListener(t *testing.T) {
 		l.Listen(0, ev, nil)
 		select {
 		case e := <-evC:
-			e.(blockNumberCall).compare(t, blockNumber-uint64(listener.TailSize)) // event args should be equal
+			e.(blockNumberCall).compareF(t, blockNumber-uint64(listener.TailSize)) // event args should be equal
 		case <-time.After(timeout):
 			t.Fatal("timed out waiting for block number update")
 		}
 
 		select {
 		case e := <-evC:
-			e.(priceArgs).compare(t, priceUpdate) // event args should be equal
+			e.(priceArgs).compareF(t, priceUpdate) // event args should be equal
 		case <-time.After(timeout):
 			t.Fatal("timed out waiting for event")
 		}
@@ -206,59 +207,59 @@ func TestListener(t *testing.T) {
 
 		select {
 		case e := <-evC:
-			e.(blockNumberCall).compare(t, 495) // event args should be equal
+			e.(blockNumberCall).compareF(t, 495) // event args should be equal
 		case <-time.After(timeout):
 			t.Fatal("timed out waiting for block number update")
 		}
 
 		select {
 		case e := <-evC:
-			e.(createArgs).compare(t, c) // event args should be equal
+			e.(createArgs).compareF(t, c) // event args should be equal
 		case <-time.After(timeout):
 			t.Fatal("timed out waiting for event")
 		}
 		select {
 		case e := <-evC:
-			e.(blockNumberCall).compare(t, 496) // event args should be equal
+			e.(blockNumberCall).compareF(t, 496) // event args should be equal
 		case <-time.After(timeout):
 			t.Fatal("timed out waiting for block number update")
 		}
 		select {
 		case e := <-evC:
-			e.(topupArgs).compare(t, topup) // event args should be equal
+			e.(topupArgs).compareF(t, topup) // event args should be equal
 		case <-time.After(timeout):
 			t.Fatal("timed out waiting for event")
 		}
 		select {
 		case e := <-evC:
-			e.(blockNumberCall).compare(t, 497) // event args should be equal
-		case <-time.After(timeout):
-			t.Fatal("timed out waiting for block number update")
-		}
-
-		select {
-		case e := <-evC:
-			e.(depthArgs).compare(t, depthIncrease) // event args should be equal
-		case <-time.After(timeout):
-			t.Fatal("timed out waiting for event")
-		}
-		select {
-		case e := <-evC:
-			e.(blockNumberCall).compare(t, 498) // event args should be equal
+			e.(blockNumberCall).compareF(t, 497) // event args should be equal
 		case <-time.After(timeout):
 			t.Fatal("timed out waiting for block number update")
 		}
 
 		select {
 		case e := <-evC:
-			e.(priceArgs).compare(t, priceUpdate)
+			e.(depthArgs).compareF(t, depthIncrease) // event args should be equal
+		case <-time.After(timeout):
+			t.Fatal("timed out waiting for event")
+		}
+		select {
+		case e := <-evC:
+			e.(blockNumberCall).compareF(t, 498) // event args should be equal
+		case <-time.After(timeout):
+			t.Fatal("timed out waiting for block number update")
+		}
+
+		select {
+		case e := <-evC:
+			e.(priceArgs).compareF(t, priceUpdate)
 		case <-time.After(timeout):
 			t.Fatal("timed out waiting for event")
 		}
 
 		select {
 		case e := <-evC:
-			e.(blockNumberCall).compare(t, toBatchBlock(blockNumber-uint64(listener.TailSize))) // event args should be equal
+			e.(blockNumberCall).compareF(t, toBatchBlock(blockNumber-uint64(listener.TailSize))) // event args should be equal
 		case <-time.After(timeout):
 			t.Fatal("timed out waiting for block number update")
 		}
@@ -276,7 +277,7 @@ func TestListener(t *testing.T) {
 
 		select {
 		case e := <-evC:
-			e.(blockNumberCall).compare(t, toBatchBlock(blockNumber-uint64(listener.TailSize))) // event args should be equal
+			e.(blockNumberCall).compareF(t, toBatchBlock(blockNumber-uint64(listener.TailSize))) // event args should be equal
 		case <-time.After(timeout):
 			t.Fatal("timed out waiting for block number update")
 		}
@@ -315,7 +316,7 @@ func TestListener(t *testing.T) {
 
 		select {
 		case e := <-evC:
-			e.(blockNumberCall).compare(t, toBatchBlock(blockNumber-uint64(listener.TailSize))) // event args should be equal
+			e.(blockNumberCall).compareF(t, toBatchBlock(blockNumber-uint64(listener.TailSize))) // event args should be equal
 		case <-time.After(timeout):
 			t.Fatal("timed out waiting for block number update")
 		}
@@ -376,6 +377,7 @@ func TestListenerBatchState(t *testing.T) {
 
 	stop := make(chan struct{})
 	done := make(chan struct{})
+	errs := make(chan error)
 	noOfEvents := 0
 
 	go func() {
@@ -385,24 +387,36 @@ func TestListenerBatchState(t *testing.T) {
 				return
 			case e := <-evC:
 				noOfEvents++
-				switch e.(type) {
+				switch ev := e.(type) {
 				case blockNumberCall:
-					blkNum := e.(blockNumberCall).blockNumber
-					if blkNum < 497 && blkNum > 500 {
-						t.Fatal("invalid blocknumber call", blkNum)
+					if ev.blockNumber < 497 && ev.blockNumber > 500 {
+						errs <- fmt.Errorf("invalid blocknumber call %d", ev.blockNumber)
+						return
 					}
-					if blkNum == 500 {
+					if ev.blockNumber == 500 {
 						close(done)
 						return
 					}
 				case createArgs:
-					e.(createArgs).compare(t, create)
+					if err := ev.compare(create); err != nil {
+						errs <- err
+						return
+					}
 				case topupArgs:
-					e.(topupArgs).compare(t, topup)
+					if err := ev.compare(topup); err != nil {
+						errs <- err
+						return
+					}
 				case depthArgs:
-					e.(depthArgs).compare(t, depthIncrease)
+					if err := ev.compare(depthIncrease); err != nil {
+						errs <- err
+						return
+					}
 				case priceArgs:
-					e.(priceArgs).compare(t, priceUpdate)
+					if err := ev.compare(priceUpdate); err != nil {
+						errs <- err
+						return
+					}
 				}
 			}
 		}
@@ -411,10 +425,13 @@ func TestListenerBatchState(t *testing.T) {
 	l := listener.New(logger, mf, postageStampAddress, 1, nil, stallingTimeout, backoffTime)
 	l.Listen(0, ev, snapshot)
 
+	defer close(stop)
+
 	select {
 	case <-time.After(5 * time.Second):
 		t.Fatal("timedout waiting for events to be processed")
-		close(stop)
+	case err := <-errs:
+		t.Fatal(err)
 	case <-done:
 		if noOfEvents != 9 {
 			t.Fatal("invalid count of events on completion", noOfEvents)
@@ -604,15 +621,24 @@ type createArgs struct {
 	immutable        bool
 }
 
-func (c createArgs) compare(t *testing.T, want createArgs) {
+func (c createArgs) compare(want createArgs) error {
 	if !bytes.Equal(c.id, want.id) {
-		t.Fatalf("id mismatch. got %v want %v", c.id, want.id)
+		return fmt.Errorf("id mismatch. got %v want %v", c.id, want.id)
 	}
 	if !bytes.Equal(c.owner, want.owner) {
-		t.Fatalf("owner mismatch. got %v want %v", c.owner, want.owner)
+		return fmt.Errorf("owner mismatch. got %v want %v", c.owner, want.owner)
 	}
 	if c.normalisedAmount.Cmp(want.normalisedAmount) != 0 {
-		t.Fatalf("normalised amount mismatch. got %v want %v", c.normalisedAmount.String(), want.normalisedAmount.String())
+		return fmt.Errorf("normalised amount mismatch. got %v want %v", c.normalisedAmount.String(), want.normalisedAmount.String())
+	}
+	return nil
+}
+
+func (c createArgs) compareF(t *testing.T, want createArgs) {
+	t.Helper()
+	err := c.compare(want)
+	if err != nil {
+		t.Fatal(err)
 	}
 }
 
@@ -634,13 +660,21 @@ type topupArgs struct {
 	normalisedBalance *big.Int
 }
 
-func (ta topupArgs) compare(t *testing.T, want topupArgs) {
-	t.Helper()
+func (ta topupArgs) compare(want topupArgs) error {
 	if !bytes.Equal(ta.id, want.id) {
-		t.Fatalf("id mismatch. got %v want %v", ta.id, want.id)
+		return fmt.Errorf("id mismatch. got %v want %v", ta.id, want.id)
 	}
 	if ta.normalisedBalance.Cmp(want.normalisedBalance) != 0 {
-		t.Fatalf("normalised balance mismatch. got %v want %v", ta.normalisedBalance.String(), want.normalisedBalance.String())
+		return fmt.Errorf("normalised balance mismatch. got %v want %v", ta.normalisedBalance.String(), want.normalisedBalance.String())
+	}
+	return nil
+}
+
+func (ta topupArgs) compareF(t *testing.T, want topupArgs) {
+	t.Helper()
+	err := ta.compare(want)
+	if err != nil {
+		t.Fatal(err)
 	}
 }
 
@@ -662,16 +696,24 @@ type depthArgs struct {
 	normalisedBalance *big.Int
 }
 
-func (d depthArgs) compare(t *testing.T, want depthArgs) {
-	t.Helper()
+func (d depthArgs) compare(want depthArgs) error {
 	if !bytes.Equal(d.id, want.id) {
-		t.Fatalf("id mismatch. got %v want %v", d.id, want.id)
+		return fmt.Errorf("id mismatch. got %v want %v", d.id, want.id)
 	}
 	if d.depth != want.depth {
-		t.Fatalf("depth mismatch. got %d want %d", d.depth, want.depth)
+		return fmt.Errorf("depth mismatch. got %d want %d", d.depth, want.depth)
 	}
 	if d.normalisedBalance.Cmp(want.normalisedBalance) != 0 {
-		t.Fatalf("normalised balance mismatch. got %v want %v", d.normalisedBalance.String(), want.normalisedBalance.String())
+		return fmt.Errorf("normalised balance mismatch. got %v want %v", d.normalisedBalance.String(), want.normalisedBalance.String())
+	}
+	return nil
+}
+
+func (d depthArgs) compareF(t *testing.T, want depthArgs) {
+	t.Helper()
+	err := d.compare(want)
+	if err != nil {
+		t.Fatal(err)
 	}
 }
 
@@ -691,10 +733,18 @@ type priceArgs struct {
 	price *big.Int
 }
 
-func (p priceArgs) compare(t *testing.T, want priceArgs) {
-	t.Helper()
+func (p priceArgs) compare(want priceArgs) error {
 	if p.price.Cmp(want.price) != 0 {
-		t.Fatalf("price mismatch. got %s want %s", p.price.String(), want.price.String())
+		return fmt.Errorf("price mismatch. got %s want %s", p.price.String(), want.price.String())
+	}
+	return nil
+}
+
+func (p priceArgs) compareF(t *testing.T, want priceArgs) {
+	t.Helper()
+	err := p.compare(want)
+	if err != nil {
+		t.Fatal(err)
 	}
 }
 
@@ -714,10 +764,18 @@ type blockNumberCall struct {
 	blockNumber uint64
 }
 
-func (b blockNumberCall) compare(t *testing.T, want uint64) {
-	t.Helper()
+func (b blockNumberCall) compare(want uint64) error {
 	if b.blockNumber != want {
-		t.Fatalf("blockNumber mismatch. got %d want %d", b.blockNumber, want)
+		return fmt.Errorf("blockNumber mismatch. got %d want %d", b.blockNumber, want)
+	}
+	return nil
+}
+
+func (b blockNumberCall) compareF(t *testing.T, want uint64) {
+	t.Helper()
+	err := b.compare(want)
+	if err != nil {
+		t.Fatal(err)
 	}
 }
 
