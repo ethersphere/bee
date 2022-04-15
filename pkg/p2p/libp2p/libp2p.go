ea5884574f20f5bd6e6d8d3834e767e93b7b8d70
metacertain <63165462+metacertain@users.noreply.github.com>
2022-04-15 10:09:33 +0200

    fix: reset stream on addcontextheader fail (#2885)
    * fix: reset stream on addcontextheader fail

--- pkg/p2p/libp2p/libp2p.go
+++ pkg/p2p/libp2p/libp2p.go
@@ -840,7 +840,8 @@ func (s *Service) NewStream(ctx context.Context, overlay swarm.Address, headers 
 		headers = make(p2p.Headers)
 	}
 	if err := s.tracer.AddContextHeader(ctx, headers); err != nil && !errors.Is(err, tracing.ErrContextNotFound) {
-		return nil, err
+		_ = stream.Reset()
+		return nil, fmt.Errorf("new stream add context header fail: %w", err)
 	}
 
 	// exchange headers
