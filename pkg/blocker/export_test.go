227b90366a4a4ae73c0476d54887f65d61e7220f
mrekucci <peter@ethswarm.org>
2022-04-20 16:02:53 +0400

    feat: improve resilience on hibernation and network failure (#2887)

---
+++ pkg/blocker/export_test.go
@@ -0,0 +1,7 @@
+// Copyright 2022 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package blocker
+
+var SequencerResolution = &sequencerResolution
---
+++ pkg/p2p/libp2p/unreachable_errors_unix.go
@@ -0,0 +1,17 @@
+// Copyright 2022 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build aix || darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris
+// +build aix darwin dragonfly freebsd linux netbsd openbsd solaris
+
+package libp2p
+
+import "golang.org/x/sys/unix"
+
+// Collection of errors returned by the underlying
+// operating system that signals network unavailability.
+var (
+	errHostUnreachable    error = unix.EHOSTUNREACH
+	errNetworkUnreachable error = unix.ENETUNREACH
+)
