a74869c87b17d8afc8b0a6a6da838fe96d51d578
acud <12988138+acud@users.noreply.github.com>
2022-04-25 10:08:52 -0500

    chore: try debug

--- pkg/blocker/blocker_test.go
+++ pkg/blocker/blocker_test.go
@@ -72,7 +72,7 @@ func TestBlocksAfterFlagTimeout(t *testing.T) {
 	blockedTime, ok := blocked[addr.ByteString()]
 	mu.Unlock()
 	if !ok {
-		pprof.Lookup("goroutine").WriteTo(os.Stdout, 1)
+		_ = pprof.Lookup("goroutine").WriteTo(os.Stdout, 1)
 		t.Fatal("address should be blocked")
 	}
 
