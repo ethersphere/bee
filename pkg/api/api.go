627246485f47b52745eb0b1ace7f13042ec89724
notanatol <anatol@ethswarm.org>
2022-04-27 18:13:41 +0300

    fix: no conversion on empty header

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -414,15 +414,18 @@ func (s *server) contentLengthMetricMiddleware() func(h http.Handler) http.Handl
 			switch r.Method {
 			case http.MethodGet:
 				hdr := w.Header().Get("Decompressed-Content-Length")
-				contentLength, err := strconv.Atoi(hdr)
-				if err != nil {
-					s.logger.Debugf("api: decompressed content length: '%s', int conversion failed: %v", hdr, err)
+				if hdr == "" {
+					s.logger.Debug("api: decompressed content length header not found")
 					hdr = w.Header().Get("Content-Length")
-					contentLength, err = strconv.Atoi(hdr)
-					if err != nil {
-						s.logger.Debugf("api: content length: '%s', int conversion failed: %v", hdr, err)
+					if hdr == "" {
+						s.logger.Debug("api: content length header not found")
 						return
 					}
+				}
+				contentLength, err := strconv.Atoi(hdr)
+				if err != nil {
+					s.logger.Debugf("api: content length: '%s', int conversion failed: %v", hdr, err)
+					return
 				}
 				if contentLength > 0 {
 					s.metrics.ContentApiDuration.WithLabelValues(fmt.Sprintf("%d", toFileSizeBucket(int64(contentLength))), r.Method).Observe(time.Since(now).Seconds())
