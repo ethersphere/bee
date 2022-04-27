ef0793a932b319ddfa246aa8fdea4ec7ec4a37e2
istae <14264581+istae@users.noreply.github.com>
2022-04-27 19:38:25 +0300

    fix: node start up to continue on bootstrapper error

--- pkg/node/node.go
+++ pkg/node/node.go
@@ -481,7 +481,7 @@ func NewBee(addr string, publicKey *ecdsa.PublicKey, signer crypto.Signer, netwo
 		)
 		logger.Infof("bootstrapper took %s", time.Since(start))
 		if err != nil {
-			logger.Errorf("bootstrapper failed to fetch batch state: %w", err)
+			logger.Errorf("bootstrapper failed to fetch batch state: %v", err)
 		}
 	}
 
