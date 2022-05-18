edb1ea45ce21c316d3602193579ba6f6000f913b
aloknerurkar <aloknerurkar@gmail.com>
2022-05-18 20:35:15 +0530

    feat(bmt): segment inclusion Merkle proofs (#2952)

---
+++ pkg/bmt/proof.go
@@ -0,0 +1,78 @@
+// Copyright 2022 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package bmt
+
+// Prover wraps the Hasher to allow Merkle proof functionality
+type Prover struct {
+	*Hasher
+}
+
+// Proof represents a Merkle proof of segment
+type Proof struct {
+	Section []byte
+	Sisters [][]byte
+	Span    []byte
+}
+
+// Proof returns the inclusion proof of the i-th data segment
+func (p Prover) Proof(i int) Proof {
+	if i < 0 || i > 127 {
+		panic("segment index can only lie between 0-127")
+	}
+	i = i / 2
+	n := p.bmt.leaves[i]
+	isLeft := n.isLeft
+	var sisters [][]byte
+	for n = n.parent; n != nil; n = n.parent {
+		sisters = append(sisters, n.getSister(isLeft))
+		isLeft = n.isLeft
+	}
+
+	secsize := 2 * p.segmentSize
+	offset := i * secsize
+	section := p.bmt.buffer[offset : offset+secsize]
+	return Proof{section, sisters, p.span}
+}
+
+// Verify returns the bmt hash obtained from the proof which can then be checked against
+// the BMT hash of the chunk
+func (p Prover) Verify(i int, proof Proof) (root []byte, err error) {
+	i = i / 2
+	n := p.bmt.leaves[i]
+	isLeft := n.isLeft
+	root, err = doHash(n.hasher, proof.Section)
+	if err != nil {
+		return nil, err
+	}
+	n = n.parent
+
+	for _, sister := range proof.Sisters {
+		if isLeft {
+			root, err = doHash(n.hasher, root, sister)
+		} else {
+			root, err = doHash(n.hasher, sister, root)
+		}
+		if err != nil {
+			return nil, err
+		}
+		isLeft = n.isLeft
+		n = n.parent
+	}
+	return sha3hash(proof.Span, root)
+}
+
+func (n *node) getSister(isLeft bool) []byte {
+	var src []byte
+
+	if isLeft {
+		src = n.right
+	} else {
+		src = n.left
+	}
+
+	buf := make([]byte, len(src))
+	copy(buf, src)
+	return buf
+}
---
+++ pkg/bmt/proof_test.go
@@ -0,0 +1,206 @@
+// Copyright 2022 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package bmt_test
+
+import (
+	"bytes"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"testing"
+
+	"github.com/ethersphere/bee/pkg/bmt"
+	"github.com/ethersphere/bee/pkg/swarm"
+)
+
+func TestProofCorrectness(t *testing.T) {
+	testData := []byte("hello world")
+	testData = append(testData, make([]byte, 4096-len(testData))...)
+
+	verifySegments := func(t *testing.T, exp []string, found [][]byte) {
+		var expSegments [][]byte
+		for _, v := range exp {
+			decoded, err := hex.DecodeString(v)
+			if err != nil {
+				t.Fatal(err)
+			}
+			expSegments = append(expSegments, decoded)
+		}
+
+		if len(expSegments) != len(found) {
+			t.Fatal("incorrect no of proof segments", len(found))
+		}
+
+		for idx, v := range expSegments {
+			if !bytes.Equal(v, found[idx]) {
+				t.Fatal("incorrect segment in proof")
+			}
+		}
+
+	}
+
+	pool := bmt.NewPool(bmt.NewConf(swarm.NewHasher, 128, 128))
+	hh := pool.Get()
+	defer pool.Put(hh)
+	hh.SetHeaderInt64(4096)
+
+	_, err := hh.Write(testData)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rh, err := hh.Hash(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("proof for left most", func(t *testing.T) {
+		proof := bmt.Prover{hh}.Proof(0)
+
+		expSegmentStrings := []string{
+			"ad3228b676f7d3cd4284a5443f17f1962b36e491b30a40b2405849e597ba5fb5",
+			"b4c11951957c6f8f642c4af61cd6b24640fec6dc7fc607ee8206a99e92410d30",
+			"21ddb9a356815c3fac1026b6dec5df3124afbadb485c9ba5a3e3398a04b7ba85",
+			"e58769b32a1beaf1ea27375a44095a0d1fb664ce2dd358e7fcbfb78c26a19344",
+			"0eb01ebfc9ed27500cd4dfc979272d1f0913cc9f66540d7e8005811109e1cf2d",
+			"887c22bd8750d34016ac3c66b5ff102dacdd73f6b014e710b51e8022af9a1968",
+		}
+
+		verifySegments(t, expSegmentStrings, proof.Sisters)
+
+		if !bytes.Equal(proof.Section, testData[:2*hh.Size()]) {
+			t.Fatal("section incorrect")
+		}
+
+		if !bytes.Equal(proof.Span, bmt.LengthToSpan(4096)) {
+			t.Fatal("incorrect span")
+		}
+	})
+
+	t.Run("proof for right most", func(t *testing.T) {
+		proof := bmt.Prover{hh}.Proof(127)
+
+		expSegmentStrings := []string{
+			"ad3228b676f7d3cd4284a5443f17f1962b36e491b30a40b2405849e597ba5fb5",
+			"b4c11951957c6f8f642c4af61cd6b24640fec6dc7fc607ee8206a99e92410d30",
+			"21ddb9a356815c3fac1026b6dec5df3124afbadb485c9ba5a3e3398a04b7ba85",
+			"e58769b32a1beaf1ea27375a44095a0d1fb664ce2dd358e7fcbfb78c26a19344",
+			"0eb01ebfc9ed27500cd4dfc979272d1f0913cc9f66540d7e8005811109e1cf2d",
+			"745bae095b6ff5416b4a351a167f731db6d6f5924f30cd88d48e74261795d27b",
+		}
+
+		verifySegments(t, expSegmentStrings, proof.Sisters)
+
+		if !bytes.Equal(proof.Section, testData[126*hh.Size():]) {
+			t.Fatal("section incorrect")
+		}
+
+		if !bytes.Equal(proof.Span, bmt.LengthToSpan(4096)) {
+			t.Fatal("incorrect span")
+		}
+	})
+
+	t.Run("proof for middle", func(t *testing.T) {
+		proof := bmt.Prover{hh}.Proof(64)
+
+		expSegmentStrings := []string{
+			"ad3228b676f7d3cd4284a5443f17f1962b36e491b30a40b2405849e597ba5fb5",
+			"b4c11951957c6f8f642c4af61cd6b24640fec6dc7fc607ee8206a99e92410d30",
+			"21ddb9a356815c3fac1026b6dec5df3124afbadb485c9ba5a3e3398a04b7ba85",
+			"e58769b32a1beaf1ea27375a44095a0d1fb664ce2dd358e7fcbfb78c26a19344",
+			"0eb01ebfc9ed27500cd4dfc979272d1f0913cc9f66540d7e8005811109e1cf2d",
+			"745bae095b6ff5416b4a351a167f731db6d6f5924f30cd88d48e74261795d27b",
+		}
+
+		verifySegments(t, expSegmentStrings, proof.Sisters)
+
+		if !bytes.Equal(proof.Section, testData[64*hh.Size():66*hh.Size()]) {
+			t.Fatal("section incorrect")
+		}
+
+		if !bytes.Equal(proof.Span, bmt.LengthToSpan(4096)) {
+			t.Fatal("incorrect span")
+		}
+	})
+
+	t.Run("root hash calculation", func(t *testing.T) {
+		segmentStrings := []string{
+			"ad3228b676f7d3cd4284a5443f17f1962b36e491b30a40b2405849e597ba5fb5",
+			"b4c11951957c6f8f642c4af61cd6b24640fec6dc7fc607ee8206a99e92410d30",
+			"21ddb9a356815c3fac1026b6dec5df3124afbadb485c9ba5a3e3398a04b7ba85",
+			"e58769b32a1beaf1ea27375a44095a0d1fb664ce2dd358e7fcbfb78c26a19344",
+			"0eb01ebfc9ed27500cd4dfc979272d1f0913cc9f66540d7e8005811109e1cf2d",
+			"745bae095b6ff5416b4a351a167f731db6d6f5924f30cd88d48e74261795d27b",
+		}
+
+		var segments [][]byte
+		for _, v := range segmentStrings {
+			decoded, err := hex.DecodeString(v)
+			if err != nil {
+				t.Fatal(err)
+			}
+			segments = append(segments, decoded)
+		}
+
+		segment := testData[64*hh.Size() : 66*hh.Size()]
+
+		rootHash, err := bmt.Prover{hh}.Verify(64, bmt.Proof{
+			Section: segment,
+			Sisters: segments,
+			Span:    bmt.LengthToSpan(4096),
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !bytes.Equal(rootHash, rh) {
+			t.Fatal("incorrect root hash obtained")
+		}
+	})
+}
+
+func TestProof(t *testing.T) {
+
+	// BMT segment inclusion proofs
+	// Usage
+	buf := make([]byte, 4096)
+	_, err := io.ReadFull(rand.Reader, buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pool := bmt.NewPool(bmt.NewConf(swarm.NewHasher, 128, 128))
+	hh := pool.Get()
+	defer pool.Put(hh)
+	hh.SetHeaderInt64(4096)
+
+	_, err = hh.Write(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rh, err := hh.Hash(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for i := 0; i < 128; i++ {
+		t.Run(fmt.Sprintf("segmentIndex %d", i), func(t *testing.T) {
+			proof := bmt.Prover{hh}.Proof(i)
+
+			h := pool.Get()
+			defer pool.Put(h)
+
+			root, err := bmt.Prover{h}.Verify(i, proof)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !bytes.Equal(rh, root) {
+				t.Fatalf("incorrect hash. wanted %x, got %x.", rh, root)
+			}
+		})
+	}
+}
