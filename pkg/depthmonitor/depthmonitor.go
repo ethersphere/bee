66d07967f1c6f82e3e883286ff6d105b2b8159fd
alok <aloknerurkar@no-reply.com>
2022-06-29 16:12:59 +0530

    feat: implementing the service

--- pkg/depthmonitor/depthmonitor.go
+++ pkg/depthmonitor/depthmonitor.go
@@ -1,10 +1,18 @@
 package depthmonitor
 
 import (
+	"errors"
+	"sync"
 	"time"
 
 	"github.com/ethersphere/bee/pkg/logging"
 	"github.com/ethersphere/bee/pkg/storage"
+)
+
+const (
+	depthKey         string  = "storage_depth"
+	adaptationWindow float64 = 60 * 60
+	manageWait               = 5 * time.Minute
 )
 
 // ReserveReporter interface defines the functionality required from the local storage
@@ -34,11 +42,14 @@ type Topology interface {
 }
 
 type Service struct {
-	topo       Topology
-	syncer     SyncReporter
-	reserve    ReserveReporter
-	st         storage.StateStorer
-	warmupTime time.Time
+	topo         Topology
+	syncer       SyncReporter
+	reserve      ReserveReporter
+	st           storage.StateStorer
+	logger       logging.Logger
+	depthLock    sync.Mutex
+	storageDepth int
+	quit         chan struct{}
 }
 
 func New(
@@ -46,14 +57,91 @@ func New(
 	syncer SyncReporter,
 	reserve ReserveReporter,
 	st storage.StateStorer,
-	warmupTime time.Duration,
 	logger logging.Logger,
+	warmupTime time.Duration,
 ) *Service {
-	return &Service{
-		topo:       t,
-		syncer:     syncer,
-		reserve:    reserve,
-		st:         st,
-		warmupTime: time.Now().Add(warmupTime),
+	s := &Service{
+		topo:    t,
+		syncer:  syncer,
+		reserve: reserve,
+		st:      st,
+		logger:  logger,
+	}
+	go s.manage(warmupTime)
+	return s
+}
+
+func (s *Service) manage(warmupTime time.Duration) {
+	// wait for warmup
+	select {
+	case <-s.quit:
+		return
+	case <-time.After(warmupTime):
+	}
+
+	// check if we have already saved a depth value before shutting down previously.
+	initialDepth := 0
+	err := s.st.Get(depthKey, &initialDepth)
+	if err != nil && !errors.Is(err, storage.ErrNotFound) {
+		s.logger.Errorf("depthmonitor: failed reading stored depth: %s", err.Error())
+	}
+
+	// if we are starting from scratch, we can use the initial ConnectionDepth as a starting point.
+	if initialDepth == 0 {
+		initialDepth = s.topo.ConnectionDepth()
+	}
+
+	s.logger.Infof("depthmonitor: warmup period complete, starting worker with initial depth %d", initialDepth)
+
+	s.depthLock.Lock()
+	s.storageDepth = initialDepth
+	s.depthLock.Unlock()
+
+	isNotHalfFull := func(x, y float64) bool {
+		return x/y < 0.5
+	}
+
+	// will signal that we are in an adaptation period
+	adaptationPeriod := false
+	// will store the start of the adaptation window
+	var adaptationStart time.Time
+
+	for {
+		select {
+		case <-s.quit:
+			return
+		case <-time.After(manageWait):
+		}
+
+		// if
+		if time.Now().Sub(adaptationStart).Seconds() > adaptationWindow {
+			adaptationPeriod = false
+		}
+
+		currentSize := float64(s.reserve.Size())
+		capacity := float64(s.reserve.Capacity())
+		reserveNotHalfFull := isNotHalfFull(currentSize, capacity)
+
+		if reserveNotHalfFull && !adaptationPeriod {
+			adaptationPeriod = true
+			adaptationStart = time.Now()
+		}
+
+		if !reserveNotHalfFull {
+			adaptationPeriod = false
+		}
+
+		expectedSize := s.syncer.Rate()*(adaptationWindow-time.Now().Sub(adaptationStart).Seconds()) + currentSize
+
+		if adaptationPeriod && isNotHalfFull(expectedSize, capacity) {
+			s.depthLock.Lock()
+			s.storageDepth--
+			s.topo.SetRadius(s.storageDepth)
+			s.logger.Infof("depthmonitor: reducing storage depth to better utilize reserve, new depth: %d", s.storageDepth)
+			s.depthLock.Unlock()
+			adaptationPeriod = false
+		}
 	}
 }
+
+func (s *Service) SetRadius() {}
