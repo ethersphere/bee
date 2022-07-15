dcdb6655f93257e957a2db13bc7fe187a2958a30
metacertain <metacertain@gmail.com>
2022-07-15 20:47:50 +0200

    feat: sample commit

--- pkg/commitment/commitment.go
+++ pkg/commitment/commitment.go
@@ -2,23 +2,23 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package pullsync provides the pullsync protocol
+// Package commitment provides the commitment sampler
 // implementation.
 package commitment
 
 import (
-	// "context"
+	"context"
 	"crypto/hmac"
 	// "encoding/binary"
 	"errors"
 	// "fmt"
-	// "io"
-	// "sync"
-	// "time"
+	"io"
+	"sync"
+	"time"
 
 	// "github.com/ethersphere/bee/pkg/bitvector"
 	// "github.com/ethersphere/bee/pkg/cac"
-	// "github.com/ethersphere/bee/pkg/logging"
+	"github.com/ethersphere/bee/pkg/logging"
 	// "github.com/ethersphere/bee/pkg/p2p"
 	// "github.com/ethersphere/bee/pkg/p2p/protobuf"
 	// "github.com/ethersphere/bee/pkg/postage"
@@ -26,7 +26,7 @@ import (
 	// "github.com/ethersphere/bee/pkg/pullsync/pullstorage"
 	// "github.com/ethersphere/bee/pkg/soc"
 	"github.com/ethersphere/bee/pkg/storage"
-	// "github.com/ethersphere/bee/pkg/swarm"
+	"github.com/ethersphere/bee/pkg/swarm"
 )
 
 const ()
@@ -38,21 +38,15 @@ var (
 	maxlimit            = 5000000
 )
 
-const ()
-
-// Interface is the PullSync interface.
-type Interface interface {
-}
-
 type Sampler struct {
 	//	metrics    metrics
 	logger  logging.Logger
 	storage storage.Storer
-	//	quit       chan struct{}
-	//	wg         sync.WaitGroup
+	quit    chan struct{}
+	wg      sync.WaitGroup
 	//
 	//	Interface
-	//	io.Closer
+	io.Closer
 }
 
 func New(storage storage.Storer, logger logging.Logger) *Sampler {
@@ -63,8 +57,8 @@ func New(storage storage.Storer, logger logging.Logger) *Sampler {
 		// validStamp: validStamp,
 		logger: logger,
 		// ruidCtx:    make(map[string]map[uint32]func()),
-		// wg:         sync.WaitGroup{},
-		// quit:       make(chan struct{}),
+		wg:   sync.WaitGroup{},
+		quit: make(chan struct{}),
 	}
 }
 
@@ -79,21 +73,18 @@ type ReserveCommitment struct {
 }
 
 // makeOffer tries to assemble an offer for a given requested interval.
-func (s *Syncer) MakeSample(ctx context.Context, anchor []byte, storageDepth uint8) (taddrs []SampleItem, err error) {
+func (s *Sampler) MakeSample(ctx context.Context, anchor []byte, storageDepth uint8) (ReserveCommitment, error) {
 
-	hmacr := hmac.New(swarm.NewHasher(), anchor)
+	hmacr := hmac.New(swarm.NewHasher, anchor)
 
-	zerobytes := make([]bytes, 32)
-	buffer := make([]SampleItem)
+	zerobytes := make([]byte, 32)
+	buffer := make([]SampleItem, 0, 128)
 
 	for bin := storageDepth; bin <= swarm.MaxPO; bin++ {
-		chs, top, err := s.storage.SubscribePull(ctx, bin, 0, 0, maxlimit)
-		if err != nil {
-			return o, nil, err
-		}
+		chs, _, stop := s.storage.SubscribePull(ctx, bin, 0, 0)
 
 		for ch := range chs {
-			chunk, err := s.storage.Get(ctx, storage.ModeGetRequest, ch)
+			chunk, err := s.storage.Get(ctx, storage.ModeGetRequest, ch.Address)
 			if err != nil {
 				s.logger.Error("reserve sampler: skipping missing chunk")
 				continue
@@ -109,7 +100,7 @@ func (s *Syncer) MakeSample(ctx context.Context, anchor []byte, storageDepth uin
 				}
 				if distance > 0 {
 					buffer = append(buffer[:i+1], buffer[i:]...)
-					buffer[i] = SampleItem{ChunkAddress: ch, TransformedChunkAddress: swarm.NewAddress(taddr)}
+					buffer[i] = SampleItem{ChunkAddress: ch.Address, TransformedChunkAddress: swarm.NewAddress(taddr)}
 					break
 				}
 				if distance == 0 {
@@ -122,29 +113,30 @@ func (s *Syncer) MakeSample(ctx context.Context, anchor []byte, storageDepth uin
 			}
 
 			if len(buffer) < 16 {
-				buffer[len(buffer)] = SampleItem{ChunkAddress: ch, TransformedChunkAddress: swarm.NewAddress(taddr)}
+				buffer[len(buffer)] = SampleItem{ChunkAddress: ch.Address, TransformedChunkAddress: swarm.NewAddress(taddr)}
 			}
 
 		}
 
+		stop()
+
 	}
 
 	if len(buffer) < 16 {
-		return nil, error
+		return ReserveCommitment{}, errors.New("not enough items")
 	}
 	hasher := swarm.NewHasher()
 
-	for item := range buffer {
+	for _, item := range buffer {
 		hasher.Write(item.TransformedChunkAddress.Bytes())
 	}
-
 	hash := hasher.Sum(nil)
 
-	return ReserveCommitment{Hash: hash, Items: buffer}, nil
+	return ReserveCommitment{Hash: swarm.NewAddress(hash), Items: buffer}, nil
 
 }
 
-func (s *Syncer) Close() error {
+func (s *Sampler) Close() error {
 	s.logger.Info("pull syncer shutting down")
 	close(s.quit)
 	cc := make(chan struct{})
