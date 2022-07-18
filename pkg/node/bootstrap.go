8393689b056353b37239d85d94471f62b71eef79
istae <14264581+istae@users.noreply.github.com>
2022-07-18 13:33:59 +0300

    fix: new context timeout for each retry

--- pkg/node/bootstrap.go
+++ pkg/node/bootstrap.go
@@ -58,7 +58,7 @@ var (
 const (
 	getSnapshotRetries = 3
 	retryWait          = time.Second * 5
-	timeout            = time.Minute
+	timeout            = time.Minute * 2
 )
 
 func bootstrapNode(
@@ -204,9 +204,6 @@ func bootstrapNode(
 		return nil, errors.New("timed out waiting for kademlia peers")
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-
 	logger.Info("bootstrap: trying to fetch stamps snapshot")
 
 	var (
@@ -220,6 +217,10 @@ func bootstrapNode(
 		if err != nil {
 			time.Sleep(retryWait)
 		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
 		snapshotReference, err = getLatestSnapshot(ctx, ns, snapshotFeed)
 		if err != nil {
 			logger.Warningf("bootstrap: fetching snapshot: %v", err)
@@ -235,6 +236,10 @@ func bootstrapNode(
 		if err != nil {
 			time.Sleep(retryWait)
 		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
 		reader, l, err = joiner.New(ctx, ns, snapshotReference)
 		if err != nil {
 			logger.Warningf("bootstrap: file joiner: %v", err)
