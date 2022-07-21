fc1ce1a9754cef30eb14ab562e57555b0b41d29e
acud <12988138+acud@users.noreply.github.com>
2022-07-21 09:08:26 -0500

    chore(version): bump to v1.7.0 (#3110)

--- version.go
+++ version.go
@@ -10,7 +10,7 @@ import (
 )
 
 var (
-	version    = "1.6.4" // manually set semantic version number
+	version    = "1.7.0" // manually set semantic version number
 	commitHash string    // automatically set git commit hash
 	commitTime string    // automatically set git commit time
 
