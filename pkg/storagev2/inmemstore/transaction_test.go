f6953cc4e21e1fddb8693ac672f6b02d59abe65e
mrekucci <peter@ethswarm.org>
2022-08-26 15:34:09 +0200

    feat: add in-memory store transaction

---
+++ pkg/storagev2/inmemstore/transaction_test.go
@@ -0,0 +1,36 @@
+// Copyright 2022 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package inmem_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/ethersphere/bee/pkg/storagev2"
+	inmem "github.com/ethersphere/bee/pkg/storagev2/inmemstore"
+	"github.com/ethersphere/bee/pkg/storagev2/storagetest"
+)
+
+func TestTxStore(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+
+	store := inmem.NewTxStore(&storage.TxStoreBase{
+		TxState: storage.NewTxState(ctx),
+		Store:   inmem.New(),
+	})
+
+	// We need to call Commit() so the store.Close() method won't block.
+	time.AfterFunc(100*time.Millisecond, func() {
+		if err := store.Commit(); err != nil {
+			t.Fatalf("Commit(): unexpected error: %v", err)
+		}
+	})
+
+	storagetest.TestStore(t, store)
+}
+
+// TODO: test the Rollback functionality.
