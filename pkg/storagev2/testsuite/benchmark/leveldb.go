f43c7ad8d9f896c015917ff97a88b929dea6339b
notanatol <anatol@ethswarm.org>
2022-08-31 09:35:05 +0300

    feat: benchmarks

---
+++ pkg/storagev2/testsuite/benchmark/leveldb.go
@@ -0,0 +1,131 @@
+package test
+
+import (
+	"encoding/binary"
+	"errors"
+	"strings"
+	"sync"
+
+	storage "github.com/ethersphere/bee/pkg/storagev2"
+	leveldbstore "github.com/ethersphere/bee/pkg/storagev2/leveldb"
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/opt"
+)
+
+type LevelDB struct {
+	path  string
+	fsync bool
+	db    storage.Store
+	mu    sync.RWMutex
+}
+
+type obj1 struct {
+	Id      string
+	SomeInt uint64
+	Buf     []byte
+}
+
+func (obj1) Namespace() string { return "obj1" }
+
+func (o *obj1) ID() string { return o.Id }
+
+// ID is 32 bytes
+func (o *obj1) Marshal() ([]byte, error) {
+	buf := make([]byte, 40)
+	copy(buf[:32], []byte(o.Id))
+	binary.LittleEndian.PutUint64(buf[32:], o.SomeInt)
+	buf = append(buf, o.Buf[:]...)
+	return buf, nil
+}
+
+func (o *obj1) Unmarshal(buf []byte) error {
+	if len(buf) < 40 {
+		return errors.New("invalid length")
+	}
+	o.Id = strings.TrimRight(string(buf[:32]), string([]byte{0}))
+	o.SomeInt = binary.LittleEndian.Uint64(buf[32:])
+	o.Buf = buf[40:]
+	return nil
+}
+
+func NewLevelDB(path string, fsync bool) (DB, error) {
+	db := &LevelDB{
+		path:  path,
+		fsync: fsync,
+	}
+
+	if err := db.init(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func (db *LevelDB) init() error {
+	beeDB, err := leveldbstore.New(db.path, &opt.Options{NoSync: !db.fsync})
+	if err != nil {
+		return err
+	}
+	db.db = beeDB
+	return nil
+}
+
+func (db *LevelDB) Set(key, value []byte) error {
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+
+	item := &obj1{
+		Id:  string(key),
+		Buf: value,
+	}
+
+	return db.db.Put(item)
+}
+
+func (db *LevelDB) Get(key []byte) ([]byte, error) {
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+
+	item := &obj1{
+		Id: string(key),
+	}
+
+	err := db.db.Get(item)
+
+	switch {
+	case err != nil && errors.Is(err, leveldb.ErrNotFound):
+		return nil, nil
+	case err != nil:
+		return nil, err
+	}
+
+	return item.Buf, nil
+}
+
+func (db *LevelDB) Del(key []byte) error {
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+
+	item := &obj1{
+		Id: string(key),
+	}
+
+	ok, err := db.db.Has(item)
+	if !ok || err != nil {
+		return err
+	}
+
+	err = db.db.Delete(item)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (db *LevelDB) Close() error {
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+
+	return db.db.Close()
+}
