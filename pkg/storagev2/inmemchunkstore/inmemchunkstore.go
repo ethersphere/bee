1f94768e28f5b36ce4b67db07627838201afbcac
mrekucci <peter@ethswarm.org>
2022-09-01 11:53:07 +0400

    refactor: cleanups (#3239)

--- pkg/storagev2/inmemchunkstore/inmemchunkstore.go
+++ pkg/storagev2/inmemchunkstore/inmemchunkstore.go
@@ -8,19 +8,19 @@ import (
 	"context"
 	"sync"
 
-	storage "github.com/ethersphere/bee/pkg/storagev2"
+	"github.com/ethersphere/bee/pkg/storagev2"
 	"github.com/ethersphere/bee/pkg/swarm"
 )
 
-type chunkStore struct {
+type ChunkStore struct {
 	chunks sync.Map
 }
 
-func New() storage.ChunkStore {
-	return &chunkStore{}
+func New() *ChunkStore {
+	return &ChunkStore{}
 }
 
-func (c *chunkStore) Get(_ context.Context, addr swarm.Address) (swarm.Chunk, error) {
+func (c *ChunkStore) Get(_ context.Context, addr swarm.Address) (swarm.Chunk, error) {
 	val, found := c.chunks.Load(addr.ByteString())
 	if !found {
 		return nil, storage.ErrNotFound
@@ -28,22 +28,22 @@ func (c *chunkStore) Get(_ context.Context, addr swarm.Address) (swarm.Chunk, er
 	return val.(swarm.Chunk), nil
 }
 
-func (c *chunkStore) Put(_ context.Context, ch swarm.Chunk) (bool, error) {
+func (c *ChunkStore) Put(_ context.Context, ch swarm.Chunk) (bool, error) {
 	_, loaded := c.chunks.LoadOrStore(ch.Address().ByteString(), ch)
 	return loaded, nil
 }
 
-func (c *chunkStore) Has(_ context.Context, addr swarm.Address) (bool, error) {
+func (c *ChunkStore) Has(_ context.Context, addr swarm.Address) (bool, error) {
 	_, exists := c.chunks.Load(addr.ByteString())
 	return exists, nil
 }
 
-func (c *chunkStore) Delete(_ context.Context, addr swarm.Address) error {
+func (c *ChunkStore) Delete(_ context.Context, addr swarm.Address) error {
 	c.chunks.Delete(addr.ByteString())
 	return nil
 }
 
-func (c *chunkStore) Iterate(_ context.Context, fn storage.IterateChunkFn) error {
+func (c *ChunkStore) Iterate(_ context.Context, fn storage.IterateChunkFn) error {
 	var retErr error
 	c.chunks.Range(func(_, val interface{}) bool {
 		stop, err := fn(val.(swarm.Chunk))
@@ -56,6 +56,6 @@ func (c *chunkStore) Iterate(_ context.Context, fn storage.IterateChunkFn) error
 	return retErr
 }
 
-func (c *chunkStore) Close() error {
+func (c *ChunkStore) Close() error {
 	return nil
 }
