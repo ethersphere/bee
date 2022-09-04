3004da770633f04ca219ab64d2b1d3928757569f
notanatol <anatol@ethswarm.org>
2022-09-04 19:49:41 +0300

    feat: read hot read benchmark

--- pkg/storagev2/leveldbstore/store_test.go
+++ pkg/storagev2/leveldbstore/store_test.go
@@ -36,33 +36,35 @@ pkg: github.com/ethersphere/bee/pkg/storagev2/leveldb
 cpu: Intel(R) Core(TM) i7-7820HQ CPU @ 2.90GHz
 BenchmarkLevelDB
 BenchmarkLevelDB/WriteSequential
-BenchmarkLevelDB/WriteSequential-8         	  740870	      1674 ns/op	    1043 B/op	       8 allocs/op
+BenchmarkLevelDB/WriteSequential-8         	  681466	      1520 ns/op	    1041 B/op	       8 allocs/op
 BenchmarkLevelDB/WriteRandom
 BenchmarkLevelDB/WriteRandom/parallelism-1
-BenchmarkLevelDB/WriteRandom/parallelism-1-8         	  746833	      4561 ns/op	    3165 B/op	       9 allocs/op
+BenchmarkLevelDB/WriteRandom/parallelism-1-8         	  675507	      4390 ns/op	    2932 B/op	       9 allocs/op
 BenchmarkLevelDB/WriteRandom/parallelism-2
-BenchmarkLevelDB/WriteRandom/parallelism-2-8         	  223671	      7636 ns/op	    4745 B/op	      17 allocs/op
+BenchmarkLevelDB/WriteRandom/parallelism-2-8         	  196368	      7741 ns/op	    4609 B/op	      17 allocs/op
 BenchmarkLevelDB/WriteRandom/parallelism-4
-BenchmarkLevelDB/WriteRandom/parallelism-4-8         	  102396	     12449 ns/op	    6230 B/op	      31 allocs/op
+BenchmarkLevelDB/WriteRandom/parallelism-4-8         	   96027	     12658 ns/op	    5927 B/op	      31 allocs/op
 BenchmarkLevelDB/WriteRandom/parallelism-8
-BenchmarkLevelDB/WriteRandom/parallelism-8-8         	   46778	     25570 ns/op	   10652 B/op	      58 allocs/op
+BenchmarkLevelDB/WriteRandom/parallelism-8-8         	   46778	     24776 ns/op	   10757 B/op	      58 allocs/op
 BenchmarkLevelDB/ReadSequential
-BenchmarkLevelDB/ReadSequential-8                    	  305744	      3307 ns/op	    1634 B/op	      19 allocs/op
+BenchmarkLevelDB/ReadSequential-8                    	 1000000	      3663 ns/op	    1827 B/op	      20 allocs/op
 BenchmarkLevelDB/ReadRandom
-BenchmarkLevelDB/ReadRandom-8                        	  392686	      5487 ns/op	    1222 B/op	      23 allocs/op
+BenchmarkLevelDB/ReadRandom-8                        	  389064	      5524 ns/op	    1248 B/op	      23 allocs/op
 BenchmarkLevelDB/ReadRandomMissing
-BenchmarkLevelDB/ReadRandomMissing-8                 	  398671	      5391 ns/op	    1194 B/op	      21 allocs/op
+BenchmarkLevelDB/ReadRandomMissing-8                 	  377690	      5380 ns/op	    1188 B/op	      21 allocs/op
 BenchmarkLevelDB/ReadReverse
-BenchmarkLevelDB/ReadReverse-8                       	  768434	      3521 ns/op	    1826 B/op	      21 allocs/op
+BenchmarkLevelDB/ReadReverse-8                       	  978600	      3758 ns/op	    1842 B/op	      21 allocs/op
+BenchmarkLevelDB/ReadRedHot
+BenchmarkLevelDB/ReadRedHot-8                        	 1000000	      3488 ns/op	    1802 B/op	      19 allocs/op
 BenchmarkLevelDB/IterateSequential
-BenchmarkLevelDB/IterateSequential-8                 	  370545	      4061 ns/op	    2611 B/op	      15 allocs/op
+BenchmarkLevelDB/IterateSequential-8                 	  406318	      4393 ns/op	    3087 B/op	      15 allocs/op
 BenchmarkLevelDB/IterateReverse
-BenchmarkLevelDB/IterateReverse-8                    	  377454	      3768 ns/op	    2747 B/op	      15 allocs/op
+BenchmarkLevelDB/IterateReverse-8                    	  405747	      3697 ns/op	    2705 B/op	      15 allocs/op
 BenchmarkLevelDB/DeleteRandom:_1
-BenchmarkLevelDB/DeleteRandom:_1-8                   	  682662	      1979 ns/op	     911 B/op	       6 allocs/op
+BenchmarkLevelDB/DeleteRandom:_1-8                   	  819007	      2179 ns/op	     978 B/op	       6 allocs/op
 BenchmarkLevelDB/DeleteSequential:_1
-BenchmarkLevelDB/DeleteSequential:_1-8               	  820357	      1274 ns/op	     555 B/op	       6 allocs/op
+BenchmarkLevelDB/DeleteSequential:_1-8               	  860095	      1305 ns/op	     455 B/op	       6 allocs/op
 PASS
 coverage: 60.6% of statements
-ok  	github.com/ethersphere/bee/pkg/storagev2/leveldb	32.747s
+ok  	github.com/ethersphere/bee/pkg/storagev2/leveldb	54.031s
 */
