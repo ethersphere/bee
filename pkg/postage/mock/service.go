451f008150998b0d462eeac991589779a2ae8cc1
swarmHaseeb <haseeb@ethswarm.org>
2022-09-05 12:27:28 +0500

    fix: removed functionality for depth increase

--- pkg/postage/mock/service.go
+++ pkg/postage/mock/service.go
@@ -114,7 +114,9 @@ func (m *mockPostage) HandleTopUp(batchID []byte, amount *big.Int) {
 	}
 }
 
-func (m *mockPostage) HandleDepthIncrease(_ []byte, _ uint8) {}
+func (m *mockPostage) HandleDepthIncrease(_ []byte, _ uint8) {
+	panic("not implemented")
+}
 
 func (m *mockPostage) Close() error {
 	return nil
