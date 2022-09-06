642499ee1cfb94288b7ef82822c4c54b7006105c
Vlado Pajic <vlado@brrrrr.at>
2022-09-06 22:41:04 +0200

    feat: set unhealthy when node construction failed

--- pkg/node/node.go
+++ pkg/node/node.go
@@ -321,7 +321,11 @@ func NewBee(interrupt chan struct{}, addr string, publicKey *ecdsa.PublicKey, si
 	probe := api.NewProbe()
 	probe.SetHealthy(api.ProbeStatusOK)
 	defer func(probe *api.Probe) {
-		probe.SetReady(api.ProbeStatusOK)
+		if err != nil {
+			probe.SetHealthy(api.ProbeStatusNOK)
+		} else {
+			probe.SetReady(api.ProbeStatusOK)
+		}
 	}(probe)
 
 	var debugService *api.Service
