c973f752fd0cb79bb1e7be71c46788dd5393f665
Umer <umerm.work@gmail.com>
2022-09-12 17:51:31 +0500

    refactor(chunk): add validation

--- pkg/api/chunk_address.go
+++ pkg/api/chunk_address.go
@@ -10,21 +10,22 @@ import (
 	"github.com/ethersphere/bee/pkg/jsonhttp"
 	"github.com/ethersphere/bee/pkg/storage"
 	"github.com/ethersphere/bee/pkg/swarm"
-	"github.com/gorilla/mux"
 )
 
 func (s *Service) hasChunkHandler(w http.ResponseWriter, r *http.Request) {
-	str := mux.Vars(r)["address"]
-	addr, err := swarm.ParseHexAddress(str)
-	if err != nil {
-		s.logger.Debug("has chunk: parse chunk address string failed", "string", str, "error", err)
-		jsonhttp.BadRequest(w, "bad address")
+	path := struct {
+		Address []byte `parse:"address,addressToString" name:"address" errMessage:"bad address"`
+	}{}
+
+	if err := s.parseAndValidate(r, &path); err != nil {
+		s.logger.Debug("create batch: parse and validate url path params failed", "error", err)
+		s.logger.Error(nil, "create batch: parse and validate url path params failed")
+		jsonhttp.BadRequest(w, err.Error())
 		return
 	}
-
-	has, err := s.storer.Has(r.Context(), addr)
+	has, err := s.storer.Has(r.Context(), swarm.NewAddress(path.Address))
 	if err != nil {
-		s.logger.Debug("has chunk: has chunk failed", "chunk_address", addr, "error", err)
+		s.logger.Debug("has chunk: has chunk failed", "chunk_address", swarm.NewAddress(path.Address), "error", err)
 		jsonhttp.BadRequest(w, err)
 		return
 	}
@@ -37,17 +38,20 @@ func (s *Service) hasChunkHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Service) removeChunk(w http.ResponseWriter, r *http.Request) {
-	str := mux.Vars(r)["address"]
-	addr, err := swarm.ParseHexAddress(str)
-	if err != nil {
-		s.logger.Debug("remove chunk: parse chunk address string failed", "string", str, "error", err)
-		jsonhttp.BadRequest(w, "bad address")
+	path := struct {
+		Address []byte `parse:"address,addressToString" name:"address" errMessage:"invalid address"`
+	}{}
+
+	if err := s.parseAndValidate(r, &path); err != nil {
+		s.logger.Debug("create batch: parse and validate url path params failed", "error", err)
+		s.logger.Error(nil, "create batch: parse and validate url path params failed")
+		jsonhttp.BadRequest(w, err.Error())
 		return
 	}
 
-	has, err := s.storer.Has(r.Context(), addr)
+	has, err := s.storer.Has(r.Context(), swarm.NewAddress(path.Address))
 	if err != nil {
-		s.logger.Debug("remove chunk: has chunk failed", "chunk_address", addr, "error", err)
+		s.logger.Debug("remove chunk: has chunk failed", "chunk_address", swarm.NewAddress(path.Address), "error", err)
 		jsonhttp.BadRequest(w, err)
 		return
 	}
@@ -57,9 +61,9 @@ func (s *Service) removeChunk(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = s.storer.Set(r.Context(), storage.ModeSetRemove, addr)
+	err = s.storer.Set(r.Context(), storage.ModeSetRemove, swarm.NewAddress(path.Address))
 	if err != nil {
-		s.logger.Debug("remove chunk: remove chunk failed", "chunk_address", addr, "error", err)
+		s.logger.Debug("remove chunk: remove chunk failed", "chunk_address", swarm.NewAddress(path.Address), "error", err)
 		jsonhttp.InternalServerError(w, err)
 		return
 	}
