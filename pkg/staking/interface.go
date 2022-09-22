38b013dd7d8fd4434075c080fc8966eb8c94d050
swarmHaseeb <haseeb@ethswarm.org>
2022-09-22 17:56:30 +0500

    feat(stakingAgent): implemented listner for staking

---
+++ pkg/staking/interface.go
@@ -0,0 +1,31 @@
+package staking
+
+import (
+	"io"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+// Listener provides a blockchain event iterator.
+type Listener interface {
+	io.Closer
+	Listen(from uint64, updater EventUpdater, initState *ChainSnapshot) <-chan error
+}
+
+// ChainSnapshot represents the snapshot of all the staking events between the
+// FirstBlockNumber and LastBlockNumber. The timestamp stores the time at which the
+// snapshot was generated. This snapshot can be used to sync the staking package
+// to prevent large no. of chain backend calls.
+type ChainSnapshot struct {
+	Events           []types.Log `json:"events"`
+	LastBlockNumber  uint64      `json:"lastBlockNumber"`
+	FirstBlockNumber uint64      `json:"firstBlockNumber"`
+	Timestamp        int64       `json:"timestamp"`
+}
+
+type EventUpdater interface {
+	DepositStake(overlay []byte, stakeAmount *big.Int, addr common.Address, lastUpdatedBlock *big.Int) error
+	SlashedNotRevealed(overlay []byte) error
+}
--- pkg/staking/listener/listener.go
+++ pkg/staking/listener/listener.go
@@ -1,1 +1,149 @@
+// Copyright 2021 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package listener
+
+import (
+	"context"
+	"fmt"
+	"math/big"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethersphere/bee/pkg/log"
+	"github.com/ethersphere/bee/pkg/staking"
+	"github.com/ethersphere/bee/pkg/transaction"
+	"github.com/ethersphere/bee/pkg/util"
+	"github.com/ethersphere/go-storage-incentives-abi/postageabi"
+)
+
+// loggerName is the tree path name of the logger for this package.
+const loggerName = "listener"
+
+var (
+	stakingABI = parseABI(postageabi.PostageStampABIv0_3_0)
+	// stakeUpdatedTopic is the staking contract's deposit event topic
+	stakeUpdatedTopic = stakingABI.Events["StakeUpdated"].ID
+	// stakeUpdatedTopic is the staking contract's deposit event topic
+	slashedTopic = stakingABI.Events["SlashedNotRevealed"].ID
+)
+
+type BlockHeightContractFilterer interface {
+	FilterLogs(ctx context.Context, query ethereum.FilterQuery) ([]types.Log, error)
+	BlockNumber(context.Context) (uint64, error)
+}
+
+type listener struct {
+	logger    log.Logger
+	ev        BlockHeightContractFilterer
+	blockTime uint64
+
+	stakingAddress common.Address
+	quit           chan struct{}
+	wg             sync.WaitGroup
+	//metrics             metrics
+	stallingTimeout time.Duration
+	backoffTime     time.Duration
+	syncingStopped  *util.Signaler
+}
+
+func New(
+	syncingStopped *util.Signaler,
+	logger log.Logger,
+	ev BlockHeightContractFilterer,
+	stakingAddress common.Address,
+	blockTime uint64,
+	stallingTimeout time.Duration,
+	backoffTime time.Duration,
+) staking.Listener {
+	return &listener{
+		syncingStopped: syncingStopped,
+		logger:         logger.WithName(loggerName).Register(),
+		ev:             ev,
+		blockTime:      blockTime,
+		stakingAddress: stakingAddress,
+		quit:           make(chan struct{}),
+		//metrics:             newMetrics(),
+		stallingTimeout: stallingTimeout,
+		backoffTime:     backoffTime,
+	}
+}
+
+func (l *listener) filterQuery(from, to *big.Int) ethereum.FilterQuery {
+	return ethereum.FilterQuery{
+		FromBlock: from,
+		ToBlock:   to,
+		Addresses: []common.Address{
+			l.stakingAddress,
+		},
+		Topics: [][]common.Hash{
+			{
+				stakeUpdatedTopic,
+				slashedTopic,
+			},
+		},
+	}
+}
+
+func (l *listener) processEvent(e types.Log, updater staking.EventUpdater) error {
+	defer l.metrics.EventsProcessed.Inc()
+	switch e.Topics[0] {
+	case stakeUpdatedTopic:
+		c := &stakingDeposit{}
+		err := transaction.ParseEvent(&stakingABI, "StakeUpdated", c, e)
+		if err != nil {
+			return err
+		}
+		l.metrics.DepositedCounter.Inc()
+		return updater.DepositStake(
+			c.Overlay[:],
+			c.stakeAmount,
+			c.address,
+			c.lastUpdatedBlock,
+		)
+	case slashedTopic:
+		c := &slashed{}
+		err := transaction.ParseEvent(&stakingABI, "SlashedNotRevealed", c, e)
+		if err != nil {
+			return err
+		}
+		l.metrics.SlashingCounter.Inc()
+		return updater.SlashedNotRevealed(c.Overlay[:])
+	}
+
+}
+
+func (l *listener) Close() error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l *listener) Listen(from uint64, updater staking.EventUpdater, initState *staking.ChainSnapshot) <-chan error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func parseABI(json string) abi.ABI {
+	cabi, err := abi.JSON(strings.NewReader(json))
+	if err != nil {
+		panic(fmt.Sprintf("error creating ABI for postage contract: %v", err))
+	}
+	return cabi
+}
+
+type stakingDeposit struct {
+	Overlay          [32]byte
+	stakeAmount      *big.Int
+	address          common.Address
+	lastUpdatedBlock *big.Int
+}
+
+type slashed struct {
+	Overlay [32]byte
+}
