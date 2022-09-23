3e57e1199bb96241a0d89244e72c3ff4cd92128f
alok <aloknerurkar@no-reply.com>
2022-09-23 18:45:11 +0530

    chore: bump version to 1.8.3

--- version.go
+++ version.go
@@ -10,7 +10,7 @@ import (
 )
 
 var (
-	version    = "1.8.2" // manually set semantic version number
+	version    = "1.8.3" // manually set semantic version number
 	commitHash string    // automatically set git commit hash
 	commitTime string    // automatically set git commit time
 
