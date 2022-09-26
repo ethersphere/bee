3fead375ccf0ec3873578ec595effc0b47a1aa95
Vlado Pajic <vlado@brrrrr.at>
2022-09-26 16:04:32 +0200

    fix: data race in test

--- pkg/soc/validator_test.go
+++ pkg/soc/validator_test.go
@@ -37,12 +37,11 @@ func TestInvalid(t *testing.T) {
 	t.Parallel()
 
 	socAddress := swarm.MustParseHexAddress("9d453ebb73b2fedaaf44ceddcf7a0aa37f3e3d6453fea5841c31f0ea6d61dc85")
-
 	// signed soc chunk of:
 	// id: 0
 	// wrapped chunk of: `foo`
 	// owner: 0x8d3766440f0d7b949a5e32995d09619a7f86e632
-	sch := swarm.NewChunk(socAddress, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 90, 205, 56, 79, 235, 193, 51, 183, 178, 69, 229, 221, 198, 45, 130, 210, 205, 237, 145, 130, 210, 113, 97, 38, 205, 136, 68, 80, 154, 246, 90, 5, 61, 235, 65, 130, 8, 2, 127, 84, 142, 62, 136, 52, 58, 246, 248, 74, 135, 114, 251, 60, 235, 192, 161, 131, 58, 14, 167, 236, 12, 19, 72, 49, 27, 3, 0, 0, 0, 0, 0, 0, 0, 102, 111, 111})
+	data := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 90, 205, 56, 79, 235, 193, 51, 183, 178, 69, 229, 221, 198, 45, 130, 210, 205, 237, 145, 130, 210, 113, 97, 38, 205, 136, 68, 80, 154, 246, 90, 5, 61, 235, 65, 130, 8, 2, 127, 84, 142, 62, 136, 52, 58, 246, 248, 74, 135, 114, 251, 60, 235, 192, 161, 131, 58, 14, 167, 236, 12, 19, 72, 49, 27, 3, 0, 0, 0, 0, 0, 0, 0, 102, 111, 111}
 
 	for _, c := range []struct {
 		name  string
@@ -51,6 +50,7 @@ func TestInvalid(t *testing.T) {
 		{
 			name: "wrong soc address",
 			chunk: func() swarm.Chunk {
+				sch := swarm.NewChunk(socAddress, data)
 				wrongAddressBytes := sch.Address().Bytes()
 				wrongAddressBytes[0] = 255 - wrongAddressBytes[0]
 				wrongAddress := swarm.NewAddress(wrongAddressBytes)
@@ -60,6 +60,7 @@ func TestInvalid(t *testing.T) {
 		{
 			name: "invalid data",
 			chunk: func() swarm.Chunk {
+				sch := swarm.NewChunk(socAddress, data)
 				data := make([]byte, len(sch.Data()))
 				copy(data, sch.Data())
 				cursor := swarm.HashSize + swarm.SocSignatureSize
@@ -71,6 +72,7 @@ func TestInvalid(t *testing.T) {
 		{
 			name: "invalid id",
 			chunk: func() swarm.Chunk {
+				sch := swarm.NewChunk(socAddress, data)
 				data := make([]byte, len(sch.Data()))
 				copy(data, sch.Data())
 				id := data[:swarm.HashSize]
@@ -81,6 +83,7 @@ func TestInvalid(t *testing.T) {
 		{
 			name: "invalid signature",
 			chunk: func() swarm.Chunk {
+				sch := swarm.NewChunk(socAddress, data)
 				data := make([]byte, len(sch.Data()))
 				copy(data, sch.Data())
 				// modify signature
