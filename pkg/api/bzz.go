33851e8753c9ea671d171d9673b80350b79d2004
swarmHaseeb <haseeb@ethswarm.org>
2022-09-26 15:55:50 +0500

    fix: headers

--- pkg/api/bzz.go
+++ pkg/api/bzz.go
@@ -1,7 +1,3 @@
-// Copyright 2020 The Swarm Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package api
 
 import (
