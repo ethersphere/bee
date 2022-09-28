6ec381df79590329f93dd806b3e5e05ca34b68cd
aloknerurkar <aloknerurkar@gmail.com>
2022-09-28 14:04:30 +0530

    feat(localstorev2): add chunkstore (#3315)

---
+++ pkg/localstorev2/internal/chunkstore/export_test.go
@@ -0,0 +1,19 @@
+// Copyright 2022 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package chunkstore
+
+type RetrievalIndexItem = retrievalIndexItem
+
+type ChunkStampItem = chunkStampItem
+
+var (
+	ErrMarshalInvalidRetrievalIndexItemAddress = errMarshalInvalidRetrievalIndexAddress
+	ErrUnmarshalInvalidRetrievalIndexItemSize  = errUnmarshalInvalidRetrievalIndexSize
+
+	ErrMarshalInvalidChunkStampItemAddress   = errMarshalInvalidChunkStampItemAddress
+	ErrUnmarshalInvalidChunkStampItemAddress = errUnmarshalInvalidChunkStampItemAddress
+	ErrMarshalInvalidChunkStampItemStamp     = errMarshalInvalidChunkStampItemStamp
+	ErrUnmarshalInvalidChunkStampItemSize    = errUnmarshalInvalidChunkStampItemSize
+)
