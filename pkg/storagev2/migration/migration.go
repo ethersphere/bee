4fe560a48b4ee4f20ec714b03308c208e6222231
Umer <umerm.work@gmail.com>
2022-10-12 19:13:12 +0500

    fix: pointer

--- pkg/storagev2/migration/migration.go
+++ pkg/storagev2/migration/migration.go
@@ -36,7 +36,7 @@ type storageVersionItem struct {
 
 const storageVersionItemSize = unsafe.Sizeof(&storageVersionItem{})
 
-func (s storageVersionItem) ID() string {
+func (s *storageVersionItem) ID() string {
 	return "storage_version"
 }
 
@@ -44,23 +44,23 @@ func (s storageVersionItem) Namespace() string {
 	return "migration"
 }
 
-func (s storageVersionItem) Marshal() ([]byte, error) {
+func (s *storageVersionItem) Marshal() ([]byte, error) {
 	buf := make([]byte, storageVersionItemSize)
 	binary.LittleEndian.PutUint64(buf, s.Version)
 	return buf, nil
 }
 
-func (s storageVersionItem) Unmarshal(bytes []byte) error {
+func (s *storageVersionItem) Unmarshal(bytes []byte) error {
 	ni := storageVersionItem{}
 	ni.Version = binary.LittleEndian.Uint64(bytes)
-	s = ni
+	s = &ni
 	return nil
 }
 
 func GetVersion(s storage.Store) (Version, error) {
 
 	item := storageVersionItem{}
-	err := s.Get(item)
+	err := s.Get(&item)
 	if err != nil {
 		return 0, err
 	}
