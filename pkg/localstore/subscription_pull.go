ff7688f0505972a051c94c4a66deb2ef5d5bf38a
istae <14264581+istae@users.noreply.github.com>
2022-10-20 13:32:54 +0300

    revert: falling edge

--- pkg/localstore/subscription_pull.go
+++ pkg/localstore/subscription_pull.go
@@ -22,6 +22,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/ethersphere/bee/pkg/flipflop"
 	"github.com/ethersphere/bee/pkg/shed"
 	"github.com/ethersphere/bee/pkg/storage"
 	"github.com/ethersphere/bee/pkg/swarm"
@@ -43,15 +44,17 @@ func (db *DB) SubscribePull(ctx context.Context, bin uint8, since, until uint64)
 
 	chunkDescriptors := make(chan storage.Descriptor)
 
-	trigger := make(chan struct{}, 1)
-	trigger <- struct{}{}
+	in, out, clean := flipflop.NewFallingEdge(flipFlopBufferDuration, flipFlopWorstCaseDuration)
 
 	db.pullTriggersMu.Lock()
 	if _, ok := db.pullTriggers[bin]; !ok {
 		db.pullTriggers[bin] = make([]chan<- struct{}, 0)
 	}
-	db.pullTriggers[bin] = append(db.pullTriggers[bin], trigger)
+	db.pullTriggers[bin] = append(db.pullTriggers[bin], in)
 	db.pullTriggersMu.Unlock()
+
+	// send signal for the initial iteration
+	in <- struct{}{}
 
 	stopChan := make(chan struct{})
 	var stopChanOnce sync.Once
@@ -62,6 +65,7 @@ func (db *DB) SubscribePull(ctx context.Context, bin uint8, since, until uint64)
 
 	db.subscriptionsWG.Add(1)
 	go func() {
+		defer clean()
 		defer db.subscriptionsWG.Done()
 		defer db.metrics.SubscribePullStop.Inc()
 		// close the returned store.Descriptor channel at the end to
@@ -79,7 +83,7 @@ func (db *DB) SubscribePull(ctx context.Context, bin uint8, since, until uint64)
 		first := true // first iteration flag for SkipStartFromItem
 		for {
 			select {
-			case <-trigger:
+			case <-out:
 				// iterate until:
 				// - last index Item is reached
 				// - subscription stop is called
@@ -167,9 +171,9 @@ func (db *DB) SubscribePull(ctx context.Context, bin uint8, since, until uint64)
 		defer db.pullTriggersMu.Unlock()
 
 		for i, t := range db.pullTriggers[bin] {
-			if t == trigger {
+			if t == in {
 				db.pullTriggers[bin] = append(db.pullTriggers[bin][:i], db.pullTriggers[bin][i+1:]...)
-				return
+				break
 			}
 		}
 	}
