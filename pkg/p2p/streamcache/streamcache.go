c757460e66ae0e8ff5a6b3639df8efe8ab842cb1
notanatol <anatol@ethswarm.org>
2022-10-24 11:53:22 +0100

    fix: header

--- pkg/p2p/streamcache/streamcache.go
+++ pkg/p2p/streamcache/streamcache.go
@@ -1,3 +1,9 @@
+// Copyright 2022 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package pushsync provides the pushsync protocol
+// implementation.
 package streamcache
 
 import (
