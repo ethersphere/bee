eaa6955d2ff4a617874aa138143e5c6cf27b6b43
vladopajic <vladopajic@users.noreply.github.com>
2022-10-26 11:54:56 +0200

    feat(migration): store migration helpers (#3421)

--- pkg/storagev2/migration/export_test.go
+++ pkg/storagev2/migration/export_test.go
@@ -7,3 +7,29 @@ package migration
 var (
 	ErrStorageVersionItemUnmarshalInvalidSize = errStorageVersionItemUnmarshalInvalidSize
 )
+
+func DefaultOptions() *options {
+	return defaultOptions()
+}
+
+func (o *options) DeleteFn() ItemDeleteFn {
+	return o.deleteFn
+}
+
+func (o *options) UpdateFn() ItemUpdateFn {
+	return o.updateFn
+}
+
+func (o *options) OpPerBatch() int {
+	return o.opPerBatch
+}
+
+func (o *options) ApplyAll(opt ...option) {
+	o.applyAll(opt)
+}
+
+func WithOpPerBatch(count int) option {
+	return func(o *options) {
+		o.opPerBatch = count
+	}
+}
