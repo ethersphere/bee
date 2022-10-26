92b54af3ed21b1f8684a3afb0c49e2041f8984eb
metacertain <metacertain@gmail.com>
2022-10-26 13:23:21 +0200

    fix: comment

--- pkg/config/chain.go
+++ pkg/config/chain.go
@@ -26,13 +26,9 @@ var (
 	goerliPostageStampAddress = common.HexToAddress("0x7aac0f092f7b961145900839ed6d54b1980f200c")
 	xdaiPostageStampAddress   = common.HexToAddress("0xa9c84e9ccC0A0bC9B8C8E948F24E024bC2607c9A")
 
-	//TODO: add correct staking addresses
-
 	//staking contract
 	goerliStakingAddress = common.HexToAddress("0x18391158435582D5bE5ac1640ab5E2825F68d3a4")
 	xdaiStakingAddress   = common.HexToAddress("0x52e86336210bB8F1FDe11EB8bc664a20AfC0a614")
-
-	//TODO: add correct redistribution addresses
 
 	// redistribution contract
 	goerliRedistributionAddress = common.HexToAddress("0x52afef6E33308668806d48F286bb92271b3D9180")
