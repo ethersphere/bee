73ca7f15cf0f43aa0a6256e22d05029ef1c3d4aa
istae <14264581+istae@users.noreply.github.com>
2022-10-30 00:54:25 +0300

    fix: better comments

--- pkg/storageincentives/agent.go
+++ pkg/storageincentives/agent.go
@@ -240,15 +240,13 @@ func (a *Agent) start(blockTime time.Duration, blocksPerRound, blocksPerPhase ui
 		round = block / blocksPerRound
 		a.metrics.Round.Set(float64(round))
 
-		// TODO: to be changed for the mainnet
-		// compute the current phase
 		p := block % blocksPerRound
 		if p < blocksPerPhase {
-			currentPhase = commit // [0, 38)
-		} else if p >= blocksPerPhase && p < 2*blocksPerPhase { // [38, 76)
+			currentPhase = commit // [0, 37]
+		} else if p >= blocksPerPhase && p < 2*blocksPerPhase { // [38, 75]
 			currentPhase = reveal
 		} else if p >= 2*blocksPerPhase {
-			currentPhase = claim // [76, 152)
+			currentPhase = claim // [76, 151]
 		}
 
 		// write the current phase only once
