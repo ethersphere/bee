1f94e508f32db2161e16328bb12a1cb6961877c7
Anatol <87016465+notanatol@users.noreply.github.com>
2022-11-01 16:47:28 +0000

    fix: stream related metrics (#3474)

--- pkg/p2p/libp2p/stream.go
+++ pkg/p2p/libp2p/stream.go
@@ -23,14 +23,11 @@ type stream struct {
 	network.Stream
 	headers         map[string][]byte
 	responseHeaders map[string][]byte
+	metrics         metrics
 }
 
-func NewStream(s network.Stream) p2p.Stream {
-	return &stream{Stream: s}
-}
-
-func newStream(s network.Stream) *stream {
-	return &stream{Stream: s}
+func newStream(s network.Stream, metrics metrics) *stream {
+	return &stream{Stream: s, metrics: metrics}
 }
 func (s *stream) Headers() p2p.Headers {
 	return s.headers
@@ -40,12 +37,18 @@ func (s *stream) ResponseHeaders() p2p.Headers {
 	return s.responseHeaders
 }
 
+func (s *stream) Reset() error {
+	defer s.metrics.StreamResetCount.Inc()
+	return s.Stream.Reset()
+}
+
 func (s *stream) FullClose() error {
+	defer s.metrics.ClosedStreamCount.Inc()
 	// close the stream to make sure it is gc'd
 	defer s.Close()
 
 	if err := s.CloseWrite(); err != nil {
-		_ = s.Reset()
+		_ = s.Stream.Reset()
 		return err
 	}
 
@@ -60,11 +63,11 @@ func (s *stream) FullClose() error {
 	// protocol the other side is speaking.
 	n, err := s.Read([]byte{0})
 	if n > 0 || err == nil {
-		_ = s.Reset()
+		_ = s.Stream.Reset()
 		return errExpectedEof
 	}
 	if !errors.Is(err, io.EOF) {
-		_ = s.Reset()
+		_ = s.Stream.Reset()
 		return err
 	}
 	return nil
