1b8885f1b437b0a689a5d2d6b66790c0b8977be4
istae <14264581+istae@users.noreply.github.com>
2022-11-05 23:12:05 +0300

    chore: head

--- pkg/sharky/slots_test.go
+++ pkg/sharky/slots_test.go
@@ -1,3 +1,7 @@
+// Copyright 2022 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package sharky
 
 import (
@@ -6,6 +10,7 @@ import (
 )
 
 func TestFreeSlotsPersist(t *testing.T) {
+	t.Parallel()
 
 	basedir := dirFS(t.TempDir())
 	var err error
