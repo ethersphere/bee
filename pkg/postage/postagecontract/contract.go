8199e43de4d580a5d3d01f58d6df9a539eb59d4c
alok <aloknerurkar@no-reply.com>
2022-11-11 17:11:40 +0530

    fix: disable gc test

--- pkg/postage/postagecontract/contract.go
+++ pkg/postage/postagecontract/contract.go
@@ -209,6 +209,7 @@ func (c *postageContract) getBalance(ctx context.Context) (*big.Int, error) {
 func (c *postageContract) CreateBatch(ctx context.Context, initialBalance *big.Int, depth uint8, immutable bool, label string) ([]byte, error) {
 
 	if depth <= BucketDepth {
+		fmt.Printf("========== MIN DEPTH ======== %d\n", BucketDepth)
 		return nil, ErrInvalidDepth
 	}
 
