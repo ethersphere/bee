e27aff299c552f2224cdffdd1c8b21a76ee80492
aloknerurkar <aloknerurkar@gmail.com>
2022-11-28 21:06:29 +0530

    fix: new localstore migration to reset gcSize (#3595)

--- pkg/localstore/schema.go
+++ pkg/localstore/schema.go
@@ -22,4 +22,4 @@ const DBSchemaCode = "code"
 
 // DBSchemaCurrent represents the DB schema we want to use.
 // The actual/current DB schema might differ until migrations are run.
-var DBSchemaCurrent = DBSchemaSharky
+var DBSchemaCurrent = DBSchemaCatharsis
