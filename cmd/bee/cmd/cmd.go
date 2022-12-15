6436d9e5a3391d89becd10bfe0daa26e4a954c6d
istae <14264581+istae@users.noreply.github.com>
2022-12-15 23:55:47 +0300

    chore: enable storage incentives by default

--- cmd/bee/cmd/cmd.go
+++ cmd/bee/cmd/cmd.go
@@ -283,7 +283,7 @@ func (c *command) setAllFlags(cmd *cobra.Command) {
 	cmd.Flags().String(optionNameTokenEncryptionKey, "", "admin username to get the security token")
 	cmd.Flags().String(optionNameAdminPasswordHash, "", "bcrypt hash of the admin password to get the security token")
 	cmd.Flags().Bool(optionNameUsePostageSnapshot, false, "bootstrap node using postage snapshot from the network")
-	cmd.Flags().Bool(optionNameStorageIncentivesEnable, false, "enable storage incentives feature")
+	cmd.Flags().Bool(optionNameStorageIncentivesEnable, true, "enable storage incentives feature")
 
 }
 
