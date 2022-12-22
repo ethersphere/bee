44a16dfd7d523607e7cd5eb85458ccbc19bc6407
vladopajic <vladopajic@users.noreply.github.com>
2022-12-22 12:45:56 +0100

    chore(p2p/protobuf): goroutine leaks ignored (#3592)

--- pkg/p2p/protobuf/main_test.go
+++ pkg/p2p/protobuf/main_test.go
@@ -6,8 +6,16 @@ package protobuf_test
 
 import (
 	"testing"
+
+	"go.uber.org/goleak"
 )
 
 func TestMain(m *testing.M) {
-	// goleak.VerifyTestMain(m)
+	goleak.VerifyTestMain(
+		m,
+		goleak.IgnoreTopFunction("time.Sleep"),
+		goleak.IgnoreTopFunction("io.(*pipe).Read"),
+		goleak.IgnoreTopFunction("io.(*pipe).Write"),
+		goleak.IgnoreTopFunction("github.com/ethersphere/bee/pkg/p2p/protobuf_test.newMessageWriter.func1"),
+	)
 }
