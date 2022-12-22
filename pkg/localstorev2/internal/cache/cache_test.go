15881f59aa02214827a43b545901e83038e67255
alok <aloknerurkar@no-reply.com>
2022-12-22 19:43:04 +0530

    fix: lint

--- pkg/localstorev2/internal/cache/cache_test.go
+++ pkg/localstorev2/internal/cache/cache_test.go
@@ -72,6 +72,7 @@ func TestCacheStateItem(t *testing.T) {
 	}}
 
 	for _, tc := range tests {
+		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
@@ -124,6 +125,7 @@ func TestCacheEntryItem(t *testing.T) {
 	}}
 
 	for _, tc := range tests {
+		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
