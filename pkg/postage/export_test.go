0908af4a25ec98bfc8616a0c261d7ba041a69044
istae <14264581+istae@users.noreply.github.com>
2023-01-10 19:31:46 +0300

    fix: mutable batches do not stop uploads that reach max utilization (#3685)

--- pkg/postage/export_test.go
+++ pkg/postage/export_test.go
@@ -4,8 +4,14 @@
 
 package postage
 
+import "github.com/ethersphere/bee/pkg/swarm"
+
 var (
 	IndexToBytes   = indexToBytes
 	BytesToIndex   = bytesToIndex
 	BlockThreshold = blockThreshold
 )
+
+func (si *StampIssuer) Inc(addr swarm.Address) ([]byte, error) {
+	return si.inc(addr)
+}
