7df6c7345f4931a959bb4c7d42538b6be92675ed
aloknerurkar <aloknerurkar@gmail.com>
2023-01-12 17:44:56 +0530

    refactor: upload store with tags (#3690)

--- pkg/storagev2/chunkstore.go
+++ pkg/storagev2/chunkstore.go
@@ -17,7 +17,18 @@ type Getter interface {
 	// Get a chunk by its swarm.Address. Returns the chunk associated with
 	// the address alongside with its postage stamp, or a storage.ErrNotFound
 	// if the chunk is not found.
+	// If the chunk has multiple stamps, then the first stamp is returned in this
+	// query. In order to deterministically get the stamp, use the GetterWithStamp
+	// interface. If the chunk is not found storage.ErrNotFound will be returned.
 	Get(context.Context, swarm.Address) (swarm.Chunk, error)
+}
+
+// GetterWithStamp is the interface that provides a way to specify a postage
+// stamp to filter the Get query. This is required as we will support multiple stamps
+// on a chunk. As a result some components would need to query chunk and provide
+// the stamp to return by using the argument.
+type GetterWithStamp interface {
+	GetWithStamp(context.Context, swarm.Address, []byte) (swarm.Chunk, error)
 }
 
 // Putter is the interface that wraps the basic Put method.
@@ -95,4 +106,26 @@ type PullSubscriber interface {
 
 type PushSubscriber interface {
 	SubscribePush(ctx context.Context, skipFn func([]byte) bool) (c <-chan swarm.Chunk, repeat, stop func())
+}
+
+type ChunkState = int
+
+const (
+	// ChunkSent is used by the pusher component to notify about successful push of chunk from
+	// the node. A chunk could be retried on failure so, this sent count is maintained to
+	// understand how many attempts were made by the node while pushing. The attempts are
+	// registered only when an actual request was sent from this node.
+	ChunkSent ChunkState = iota
+	// ChunkStored is used by the pusher component to notify that the uploader node is
+	// the closest node and has stored the chunk.
+	ChunkStored
+	// ChunkSynced is used by the pusher component to notify that the chunk is synced to the
+	// network. This is reported when a valid receipt was received after the chunk was
+	// pushed.
+	ChunkSynced
+)
+
+// PushReporter is used to report chunk state.
+type PushReporter interface {
+	Report(context.Context, swarm.Chunk, ChunkState) error
 }
