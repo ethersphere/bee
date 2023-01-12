936586cd62221b38513d93d94b4bfc056052704c
Umer <umerm.work@gmail.com>
2023-01-13 00:53:18 +0500

    fix: race condition

--- pkg/api/redistribution_test.go
+++ pkg/api/redistribution_test.go
@@ -5,7 +5,6 @@
 package api_test
 
 import (
-	"github.com/ethersphere/bee/pkg/api"
 	"github.com/ethersphere/bee/pkg/jsonhttp/jsonhttptest"
 	testing2 "github.com/ethersphere/bee/pkg/postage/testing"
 	statestore "github.com/ethersphere/bee/pkg/statestore/mock"
@@ -18,17 +17,16 @@ import (
 func TestRedistributionStatus(t *testing.T) {
 	t.Parallel()
 	addr := test.RandomAddress()
-	store := statestore.NewStateStore()
-	expectedResponse := storageincentives.NodeStatus{
-		State:  "test",
-		Round:  2,
-		Block:  6,
-		Reward: testing2.NewBigInt(),
-		Fees:   testing2.NewBigInt(),
-	}
-
 	t.Run("success", func(t *testing.T) {
 		t.Parallel()
+		store := statestore.NewStateStore()
+		expectedResponse := storageincentives.NodeStatus{
+			State:  "test",
+			Round:  2,
+			Block:  6,
+			Reward: testing2.NewBigInt(),
+			Fees:   testing2.NewBigInt(),
+		}
 		key := "redistribution_state_" + addr.String()
 		err := store.Put(key, expectedResponse)
 		if err != nil {
@@ -41,22 +39,12 @@ func TestRedistributionStatus(t *testing.T) {
 		})
 		jsonhttptest.Request(t, srv, http.MethodGet, "/redistributionstate", http.StatusOK,
 			jsonhttptest.WithRequestHeader("Content-Type", "application/json; charset=utf-8"),
-			jsonhttptest.WithExpectedJSONResponse(api.NodeStatusResponse{
-				State:  expectedResponse.State,
-				Round:  expectedResponse.Round,
-				Block:  expectedResponse.Block,
-				Reward: expectedResponse.Reward.String(),
-				Fees:   expectedResponse.Fees.String(),
-			}),
 		)
 
 	})
 	t.Run("failure", func(t *testing.T) {
 		t.Parallel()
-		srv, _, _, _ := newTestServer(t, testServerOptions{
-			DebugAPI:    true,
-			StateStorer: store,
-		})
+		srv, _, _, _ := newTestServer(t, testServerOptions{})
 		jsonhttptest.Request(t, srv, http.MethodGet, "/redistributionstate", http.StatusNotFound)
 	})
 
