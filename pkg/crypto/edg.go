e50764d2be558d55e794849e5bdb776ceca18a19
notanatol <anatol@ethswarm.org>
2023-01-16 17:01:37 +0000

    fix: unexport

--- pkg/crypto/edg.go
+++ pkg/crypto/edg.go
@@ -6,36 +6,34 @@ package crypto
 
 import (
 	"crypto/ecdsa"
-
-	"github.com/ethersphere/bee/pkg/keystore"
 )
 
-// EDGSecp256_k1 aggregates private key cryptography functions that employ secp256k1
-type EDGSecp256_k1 struct{}
+// edgSecp256_k1 aggregates private key cryptography functions that employ secp256k1
+type edgSecp256_k1 struct{}
 
-var EDGSecp256_K1 keystore.EDG = new(EDGSecp256_k1)
+var EDGSecp256_K1 = new(edgSecp256_k1)
 
-func (s *EDGSecp256_k1) Generate() (*ecdsa.PrivateKey, error) {
+func (s *edgSecp256_k1) Generate() (*ecdsa.PrivateKey, error) {
 	return GenerateSecp256k1Key()
 }
-func (s *EDGSecp256_k1) Encode(k *ecdsa.PrivateKey) ([]byte, error) {
+func (s *edgSecp256_k1) Encode(k *ecdsa.PrivateKey) ([]byte, error) {
 	return EncodeSecp256k1PrivateKey(k)
 }
-func (s *EDGSecp256_k1) Decode(data []byte) (*ecdsa.PrivateKey, error) {
+func (s *edgSecp256_k1) Decode(data []byte) (*ecdsa.PrivateKey, error) {
 	return DecodeSecp256k1PrivateKey(data)
 }
 
-// EDGSecp256_r1 aggregates private key cryptography functions that employ secp256r1
-type EDGSecp256_r1 struct{}
+// edg aggregates private key cryptography functions that employ secp256r1
+type edg struct{}
 
-var EDGSecp256_R1 keystore.EDG = new(EDGSecp256_r1)
+var EDGSecp256_R1 = new(edg)
 
-func (s *EDGSecp256_r1) Generate() (*ecdsa.PrivateKey, error) {
+func (s *edg) Generate() (*ecdsa.PrivateKey, error) {
 	return GenerateSecp256r1Key()
 }
-func (s *EDGSecp256_r1) Encode(k *ecdsa.PrivateKey) ([]byte, error) {
+func (s *edg) Encode(k *ecdsa.PrivateKey) ([]byte, error) {
 	return EncodeSecp256r1PrivateKey(k)
 }
-func (s *EDGSecp256_r1) Decode(data []byte) (*ecdsa.PrivateKey, error) {
+func (s *edg) Decode(data []byte) (*ecdsa.PrivateKey, error) {
 	return DecodeSecp256r1PrivateKey(data)
 }
