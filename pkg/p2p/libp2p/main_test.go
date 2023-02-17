d0db94efbf96431b1e24e5402c9f1d4523271620
Vlado Pajic <vlado@brrrrr.at>
2023-02-17 16:44:35 +0100

    chore: ignore more functions

--- pkg/p2p/libp2p/main_test.go
+++ pkg/p2p/libp2p/main_test.go
@@ -15,9 +15,11 @@ func TestMain(m *testing.M) {
 		m,
 		goleak.IgnoreTopFunction("internal/poll.runtime_pollWait"),
 		goleak.IgnoreTopFunction("sync.runtime_Semacquire"),
+		goleak.IgnoreTopFunction("net.ParseCIDR"),
 		goleak.IgnoreTopFunction("github.com/ipfs/go-log/writer.(*MirrorWriter).logRoutine"),
 		goleak.IgnoreTopFunction("github.com/huin/goupnp/httpu.(*MultiClient).Do.func2"),
 		goleak.IgnoreTopFunction("github.com/libp2p/go-flow-metrics.(*sweeper).run"),
 		goleak.IgnoreTopFunction("github.com/libp2p/go-cidranger.(*prefixTrie).insert"),
+		goleak.IgnoreTopFunction("github.com/libp2p/go-cidranger.newPathprefixTrie"),
 	)
 }
