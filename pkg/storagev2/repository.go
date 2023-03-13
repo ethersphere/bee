72619ca807aab3ef47cb5c90bc4a25d0b00f25d4
mrekucci <peter@ethswarm.org>
2023-03-13 15:05:14 +0100

    feat: add metrics for the new storage (#3877)

--- pkg/storagev2/repository.go
+++ pkg/storagev2/repository.go
@@ -6,8 +6,12 @@ package storage
 
 import (
 	"context"
+	"errors"
+	"time"
 
+	m "github.com/ethersphere/bee/pkg/metrics"
 	"github.com/hashicorp/go-multierror"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 // Repository is a collection of stores that provides a unified interface
@@ -20,36 +24,47 @@ type Repository interface {
 }
 
 type repository struct {
-	indexStore TxStore
-	chunkStore TxChunkStore
+	metrics metrics
+	txStart time.Time
+
+	txIndexStore TxStore
+	txChunkStore TxChunkStore
 }
 
 // IndexStore returns Store.
 func (r *repository) IndexStore() Store {
-	return r.indexStore
+	return r.txIndexStore
 }
 
 // ChunkStore returns ChunkStore.
 func (r *repository) ChunkStore() ChunkStore {
-	return r.chunkStore
+	return r.txChunkStore
 }
 
 // NewTx returns a new transaction that guards all the Repository
 // stores. The transaction must be committed or rolled back.
 func (r *repository) NewTx(ctx context.Context) (Repository, func() error, func() error) {
-	repo := new(repository)
-	repo.indexStore = r.indexStore.NewTx(NewTxState(ctx))
-	repo.chunkStore = r.chunkStore.NewTx(NewTxState(ctx))
+	repo := &repository{
+		metrics:      r.metrics,
+		txStart:      time.Now(),
+		txIndexStore: txIndexStoreWithMetrics{r.txIndexStore.NewTx(NewTxState(ctx)), r.metrics},
+		txChunkStore: txChunkStoreWithMetrics{r.txChunkStore.NewTx(NewTxState(ctx)), r.metrics},
+	}
 
-	txs := []Tx{repo.indexStore, repo.chunkStore}
+	txs := []Tx{repo.txIndexStore, repo.txChunkStore}
 
 	commit := func() error {
+		var err error
 		for _, tx := range txs {
-			if err := tx.Commit(); err != nil {
-				return err
+			err = tx.Commit()
+			if err != nil {
+				break
 			}
 		}
-		return nil
+		if !errors.Is(err, ErrTxDone) {
+			repo.metrics.TxTotalDuration.Observe(captureDuration(repo.txStart)())
+		}
+		return err
 	}
 
 	rollback := func() error {
@@ -59,16 +74,27 @@ func (r *repository) NewTx(ctx context.Context) (Repository, func() error, func(
 				errs = multierror.Append(errs, err)
 			}
 		}
+		if !errors.Is(errs.ErrorOrNil(), ErrTxDone) {
+			repo.metrics.TxTotalDuration.Observe(captureDuration(repo.txStart)())
+		}
 		return errs.ErrorOrNil()
 	}
 
 	return repo, commit, rollback
 }
 
+// Metrics returns set of prometheus collectors.
+// TODO: register metrics in the node.go.
+func (r *repository) Metrics() []prometheus.Collector {
+	return m.PrometheusCollectorsFromFields(r.metrics)
+}
+
 // NewRepository returns a new Repository instance.
-func NewRepository(indexStore TxStore, chunkStore TxChunkStore) Repository {
+func NewRepository(txIndexStore TxStore, txChunkStore TxChunkStore) Repository {
+	metrics := newMetrics()
 	return &repository{
-		indexStore: indexStore,
-		chunkStore: chunkStore,
+		metrics:      metrics,
+		txIndexStore: txIndexStoreWithMetrics{txIndexStore, metrics},
+		txChunkStore: txChunkStoreWithMetrics{txChunkStore, metrics},
 	}
 }
