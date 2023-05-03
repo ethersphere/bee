f9a680a990fa7518df3478cb3aabd74281af346a
vladopajic <vladopajic@users.noreply.github.com>
2023-05-03 11:34:01 +0200

    feat: add chunk type information to reserve (#4021)

--- pkg/storage/testing/chunk.go
+++ pkg/storage/testing/chunk.go
@@ -22,8 +22,11 @@ import (
 	"testing"
 
 	"github.com/ethersphere/bee/pkg/cac"
+	"github.com/ethersphere/bee/pkg/crypto"
 	postagetesting "github.com/ethersphere/bee/pkg/postage/testing"
+	"github.com/ethersphere/bee/pkg/soc"
 	"github.com/ethersphere/bee/pkg/swarm"
+	"github.com/ethersphere/bee/pkg/util/testutil"
 )
 
 // GenerateTestRandomChunk generates a valid content addressed chunk.
@@ -33,6 +36,18 @@ func GenerateTestRandomChunk() swarm.Chunk {
 	ch, _ := cac.New(data)
 	stamp := postagetesting.MustNewStamp()
 	return ch.WithStamp(stamp)
+}
+
+// GenerateTestRandomSoChunk generates a valid single owner chunk
+// using supplied content addressed chunk.
+func GenerateTestRandomSoChunk(tb testing.TB, cac swarm.Chunk) swarm.Chunk {
+	tb.Helper()
+
+	id := testutil.RandBytes(tb, swarm.HashSize)
+	key, _ := crypto.GenerateSecp256k1Key()
+	signer := crypto.NewDefaultSigner(key)
+	ch, _ := soc.New(id, cac).Sign(signer)
+	return ch.WithStamp(postagetesting.MustNewStamp())
 }
 
 // GenerateTestRandomInvalidChunk generates a random, however invalid, content
