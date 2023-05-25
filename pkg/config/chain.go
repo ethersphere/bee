12b2ce76232efd4789508847cfd7182e0da12838
notanatol <anatol@ethswarm.org>
2023-05-25 12:52:30 +0300

    fix: price oracle addr

--- pkg/config/chain.go
+++ pkg/config/chain.go
@@ -44,7 +44,7 @@ var (
 		StakingAddress:         common.HexToAddress(abi.TestnetStakingAddress),
 		PostageStampAddress:    common.HexToAddress(abi.TestnetPostageStampAddress),
 		RedistributionAddress:  common.HexToAddress(abi.TestnetRedistributionAddress),
-		SwapPriceOracleAddress: common.HexToAddress("0x0c9de531dcb38b758fe8a2c163444a5e54ee0db2"),
+		SwapPriceOracleAddress: common.HexToAddress(abi.TestnetPriceOracleAddress),
 		CurrentFactoryAddress:  common.HexToAddress("0x73c412512E1cA0be3b89b77aB3466dA6A1B9d273"),
 		LegacyFactoryAddresses: []common.Address{
 			common.HexToAddress("0xf0277caffea72734853b834afc9892461ea18474"),
@@ -64,7 +64,7 @@ var (
 		StakingAddress:         common.HexToAddress(abi.MainnetStakingAddress),
 		PostageStampAddress:    common.HexToAddress(abi.MainnetPostageStampStampAddress),
 		RedistributionAddress:  common.HexToAddress(abi.MainnetRedistributionAddress),
-		SwapPriceOracleAddress: common.HexToAddress("0x0FDc5429C50e2a39066D8A94F3e2D2476fcc3b85"),
+		SwapPriceOracleAddress: common.HexToAddress(abi.MainnetPriceOracleAddress),
 		CurrentFactoryAddress:  common.HexToAddress("0xc2d5a532cf69aa9a1378737d8ccdef884b6e7420"),
 
 		StakingABI:        abi.MainnetStakingABI,
