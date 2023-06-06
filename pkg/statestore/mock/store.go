03381ae82912dd54a868ca5c78a73ff71f5e8fd5
Acha Bill <achabill12@gmail.com>
2023-06-06 11:42:46 +0100

    fix: mock statestore

--- pkg/statestore/mock/store.go
+++ pkg/statestore/mock/store.go
@@ -102,3 +102,7 @@ func (s *store) Iterate(prefix string, iterFunc storage.StateIterFunc) (err erro
 func (s *store) Close() (err error) {
 	return nil
 }
+
+func (s *store) Nuke(bool) error {
+	return nil
+}
