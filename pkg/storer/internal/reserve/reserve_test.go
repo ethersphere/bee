0e30c87878dadc95701899329740e620df6513e9
alok <aloknerurkar@no-reply.com>
2023-06-28 13:45:44 +0530

    perf: reduce locking context during evictions

--- pkg/storer/internal/reserve/reserve_test.go
+++ pkg/storer/internal/reserve/reserve_test.go
@@ -182,7 +182,7 @@ func TestReplaceOldIndex(t *testing.T) {
 	}
 }
 
-func TestEvict(t *testing.T) {
+func TestIterateDeleteBatchBin(t *testing.T) {
 	t.Parallel()
 
 	baseAddr := swarm.RandAddress(t)
@@ -218,17 +218,26 @@ func TestEvict(t *testing.T) {
 		}
 	}
 
-	totalEvicted := 0
+	chsToDelete := make([]swarm.Chunk, 0)
 	for i := 0; i < 3; i++ {
-		evicted, err := r.EvictBatchBin(context.Background(), ts, uint8(i), evictBatch.ID, func(swarm.Chunk) {})
+		err := r.IterateBatchBin(context.Background(), ts, uint8(i), evictBatch.ID, func(ch swarm.Chunk) (bool, error) {
+			chsToDelete = append(chsToDelete, ch)
+			return false, nil
+		})
 		if err != nil {
 			t.Fatal(err)
 		}
-		totalEvicted += evicted
 	}
 
-	if totalEvicted != chunksPerBatch {
-		t.Fatalf("got %d, want %d", totalEvicted, chunksPerBatch)
+	if len(chsToDelete) != chunksPerBatch {
+		t.Fatalf("got %d, want %d", len(chsToDelete), chunksPerBatch)
+	}
+
+	for _, ch := range chsToDelete {
+		err := r.DeleteChunk(context.Background(), ts, ch)
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 
 	for i, ch := range chunks {
