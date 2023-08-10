d896b7ffada2b619eaaa2f5c153375932ded6238
istae <14264581+istae@users.noreply.github.com>
2023-08-10 11:12:37 +0300

    fix(salud): remove reserve size self health check (#4255)

--- pkg/salud/salud.go
+++ pkg/salud/salud.go
@@ -8,7 +8,6 @@ package salud
 
 import (
 	"context"
-	"math"
 	"sort"
 	"sync"
 	"time"
@@ -178,8 +177,6 @@ func (s *service) salud(mode string, minPeersPerbin int) {
 	pDur := percentileDur(peers, percentile)
 	pConns := percentileConns(peers, percentile)
 	commitment := commitment(peers)
-	reserveSize := reserveSize(peers, networkRadius, (1-percentile)/2)
-	reserveSizePercentErr := percentageErr(float64(s.reserve.ReserveSize()), float64(reserveSize))
 
 	s.metrics.AvgDur.Set(avgDur)
 	s.metrics.PDur.Set(pDur)
@@ -187,7 +184,6 @@ func (s *service) salud(mode string, minPeersPerbin int) {
 	s.metrics.NetworkRadius.Set(float64(networkRadius))
 	s.metrics.NeighborhoodRadius.Set(float64(nHoodRadius))
 	s.metrics.Commitment.Set(float64(commitment))
-	s.metrics.ReserveSizePercentErr.Set(reserveSizePercentErr)
 
 	s.logger.Debug("computed", "average", avgDur, "percentile", percentile, "pDur", pDur, "pConns", pConns, "network_radius", networkRadius, "neighborhood_radius", nHoodRadius, "batch_commitment", commitment)
 
@@ -226,9 +222,6 @@ func (s *service) salud(mode string, minPeersPerbin int) {
 	if s.reserve.StorageRadius() != networkRadius {
 		selfHealth = false
 		s.logger.Warning("node is unhealthy due to storage radius discrepency", "self_radius", s.reserve.StorageRadius(), "network_radius", networkRadius)
-	} else if reserveSize > 0 && reserveSizePercentErr > maxReserveSizePercentageErr {
-		s.logger.Warning("node is unhealthy due to reserve size discrepency", "self_size", s.reserve.ReserveSize(), "network_size", reserveSize)
-		selfHealth = false
 	}
 
 	s.isSelfHealthy.Store(selfHealth)
@@ -339,40 +332,6 @@ func commitment(peers []peer) uint64 {
 	}
 
 	return maxCommitment
-}
-
-// reserveSize returns the avg reserveSize, trimmed by p percent
-func reserveSize(peers []peer, radius uint8, p float64) uint64 {
-
-	startIndex := int(float64(len(peers)) * p)
-	endIndex := int(float64(len(peers)) * (1 - p))
-
-	sort.Slice(peers, func(i, j int) bool {
-		return peers[i].status.ReserveSize < peers[j].status.ReserveSize // ascendings
-	})
-
-	var avg uint64
-	var count uint64
-
-	for _, peer := range peers[startIndex:endIndex] {
-		if peer.status.StorageRadius == uint32(radius) {
-			avg += peer.status.ReserveSize
-			count++
-		}
-	}
-
-	if count > 0 {
-		return avg / count
-	}
-
-	return 0
-
-}
-
-// percentageErr returns the absolute value of the percentage difference between x and y,
-// in the range of [0, 1].
-func percentageErr(x, y float64) float64 {
-	return math.Abs((x - y) / y)
 }
 
 func maxIndex(n []int) int {
