953633d787a9319aef9a2aa5fabf1ee03e65dd13
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2023-09-25 13:02:02 +0200

    refactor: postage interface change

--- pkg/storageincentives/redistribution/inclusionproof.go
+++ pkg/storageincentives/redistribution/inclusionproof.go
@@ -28,20 +28,23 @@ type ChunkInclusionProofs struct {
 // github.com/ethersphere/storage-incentives/blob/ph_f2/src/Redistribution.sol
 // github.com/ethersphere/storage-incentives/blob/master/src/Redistribution.sol (when merged to master)
 type ChunkInclusionProof struct {
-	ProofSegments  []string `json:"proofSegments"`
-	ProveSegment   string   `json:"proveSegment"`
-	ProofSegments2 []string `json:"proofSegments2"`
-	ProveSegment2  string   `json:"proveSegment2"`
-	ChunkSpan      uint64   `json:"chunkSpan"`
-	ProofSegments3 []string `json:"proofSegments3"`
+	ProofSegments  []string     `json:"proofSegments"`
+	ProveSegment   string       `json:"proveSegment"`
+	ProofSegments2 []string     `json:"proofSegments2"`
+	ProveSegment2  string       `json:"proveSegment2"`
+	ChunkSpan      uint64       `json:"chunkSpan"`
+	ProofSegments3 []string     `json:"proofSegments3"`
+	PostageProof   PostageProof `json:"postageProof"`
+	SocProof       []SOCProof   `json:"socProofAttached"`
+}
 
+// SOCProof structure must exactly match
+// corresponding structure (of the same name) in Redistribution.sol smart contract.
+type PostageProof struct {
 	Signature string `json:"signature"`
-	ChunkAddr string `json:"chunkAddr"`
 	PostageId string `json:"postageId"`
 	Index     string `json:"index"`
 	TimeStamp string `json:"timeStamp"`
-
-	SocProofAttached []SOCProof `json:"socProofAttached"`
 }
 
 // SOCProof structure must exactly match
@@ -65,18 +68,19 @@ func NewChunkInclusionProof(proofp1, proofp2 bmt.Proof, proofp3 bmt.Proof, sampl
 	}
 
 	return ChunkInclusionProof{
-		ProofSegments:    proofp1Hex.ProofSegments,
-		ProveSegment:     proofp1Hex.ProveSegment,
-		ProofSegments2:   proofp2Hex.ProofSegments,
-		ProveSegment2:    proofp2Hex.ProveSegment,
-		ChunkSpan:        binary.LittleEndian.Uint64(proofp2.Span[:swarm.SpanSize]), // should be uint64 on the other size; copied from pkg/api/bytes.go
-		ProofSegments3:   proofp3Hex.ProofSegments,
-		Signature:        hex.EncodeToString(sampleItem.Stamp.Sig()),
-		ChunkAddr:        proofp1Hex.ProveSegment, // TODO refactor ABI
-		PostageId:        hex.EncodeToString(sampleItem.Stamp.BatchID()),
-		Index:            hex.EncodeToString(sampleItem.Stamp.Index()),
-		TimeStamp:        hex.EncodeToString(sampleItem.Stamp.Timestamp()),
-		SocProofAttached: socProof,
+		ProofSegments:  proofp1Hex.ProofSegments,
+		ProveSegment:   proofp1Hex.ProveSegment,
+		ProofSegments2: proofp2Hex.ProofSegments,
+		ProveSegment2:  proofp2Hex.ProveSegment,
+		ChunkSpan:      binary.LittleEndian.Uint64(proofp2.Span[:swarm.SpanSize]), // should be uint64 on the other size; copied from pkg/api/bytes.go
+		ProofSegments3: proofp3Hex.ProofSegments,
+		PostageProof: PostageProof{
+			Signature: hex.EncodeToString(sampleItem.Stamp.Sig()),
+			PostageId: hex.EncodeToString(sampleItem.Stamp.BatchID()),
+			Index:     hex.EncodeToString(sampleItem.Stamp.Index()),
+			TimeStamp: hex.EncodeToString(sampleItem.Stamp.Timestamp()),
+		},
+		SocProof: socProof,
 	}, nil
 }
 
