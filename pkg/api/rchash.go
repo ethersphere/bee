c8c595724d7307343a401158108ac7c72089eced
istae <14264581+istae@users.noreply.github.com>
2023-10-03 17:14:27 +0300

    revert: "feat(storageincentives): storage incentives phase 4 (#4345)"
    This reverts commit 08904602476619187091f826e7b12344ca2e623b.

--- pkg/api/rchash.go
+++ pkg/api/rchash.go
@@ -6,135 +6,40 @@ package api
 import (
 	"encoding/hex"
 	"net/http"
-	"strconv"
-	"time"
 
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethersphere/bee/pkg/jsonhttp"
-	"github.com/ethersphere/bee/pkg/storageincentives/redistribution"
-	"github.com/ethersphere/bee/pkg/swarm"
+	"github.com/ethersphere/bee/pkg/storageincentives"
 	"github.com/gorilla/mux"
 )
 
-type RCHashResponse struct {
-	Hash     swarm.Address        `json:"hash"`
-	Proofs   ChunkInclusionProofs `json:"proofs"`
-	Duration time.Duration        `json:"duration"`
-}
-
-type ChunkInclusionProofs struct {
-	A ChunkInclusionProof `json:"proof1"`
-	B ChunkInclusionProof `json:"proof2"`
-	C ChunkInclusionProof `json:"proofLast"`
-}
-
-// ChunkInclusionProof structure must exactly match
-// corresponding structure (of the same name) in Redistribution.sol smart contract.
-// github.com/ethersphere/storage-incentives/blob/ph_f2/src/Redistribution.sol
-// github.com/ethersphere/storage-incentives/blob/master/src/Redistribution.sol (when merged to master)
-type ChunkInclusionProof struct {
-	ProofSegments  []string     `json:"proofSegments"`
-	ProveSegment   string       `json:"proveSegment"`
-	ProofSegments2 []string     `json:"proofSegments2"`
-	ProveSegment2  string       `json:"proveSegment2"`
-	ChunkSpan      uint64       `json:"chunkSpan"`
-	ProofSegments3 []string     `json:"proofSegments3"`
-	PostageProof   PostageProof `json:"postageProof"`
-	SocProof       []SOCProof   `json:"socProof"`
-}
-
-// SOCProof structure must exactly match
-// corresponding structure (of the same name) in Redistribution.sol smart contract.
-type PostageProof struct {
-	Signature string `json:"signature"`
-	PostageId string `json:"postageId"`
-	Index     string `json:"index"`
-	TimeStamp string `json:"timeStamp"`
-}
-
-// SOCProof structure must exactly match
-// corresponding structure (of the same name) in Redistribution.sol smart contract.
-type SOCProof struct {
-	Signer     string `json:"signer"`
-	Signature  string `json:"signature"`
-	Identifier string `json:"identifier"`
-	ChunkAddr  string `json:"chunkAddr"`
-}
-
-func renderChunkInclusionProofs(proofs redistribution.ChunkInclusionProofs) ChunkInclusionProofs {
-	return ChunkInclusionProofs{
-		A: renderChunkInclusionProof(proofs.A),
-		B: renderChunkInclusionProof(proofs.B),
-		C: renderChunkInclusionProof(proofs.C),
-	}
-}
-
-func renderChunkInclusionProof(proof redistribution.ChunkInclusionProof) ChunkInclusionProof {
-	var socProof []SOCProof
-	if len(proof.SocProof) == 1 {
-		socProof = []SOCProof{{
-			Signer:     hex.EncodeToString(proof.SocProof[0].Signer.Bytes()),
-			Signature:  hex.EncodeToString(proof.SocProof[0].Signature[:]),
-			Identifier: hex.EncodeToString(proof.SocProof[0].Identifier.Bytes()),
-			ChunkAddr:  hex.EncodeToString(proof.SocProof[0].ChunkAddr.Bytes()),
-		}}
-	}
-
-	return ChunkInclusionProof{
-		ProveSegment:   hex.EncodeToString(proof.ProveSegment.Bytes()),
-		ProofSegments:  renderCommonHash(proof.ProofSegments),
-		ProveSegment2:  hex.EncodeToString(proof.ProveSegment2.Bytes()),
-		ProofSegments2: renderCommonHash(proof.ProofSegments2),
-		ProofSegments3: renderCommonHash(proof.ProofSegments3),
-		ChunkSpan:      proof.ChunkSpan,
-		PostageProof: PostageProof{
-			Signature: hex.EncodeToString(proof.PostageProof.Signature[:]),
-			PostageId: hex.EncodeToString(proof.PostageProof.PostageId[:]),
-			Index:     strconv.FormatUint(proof.PostageProof.Index, 16),
-			TimeStamp: strconv.FormatUint(proof.PostageProof.TimeStamp, 16),
-		},
-		SocProof: socProof,
-	}
-}
-
-func renderCommonHash(proofSegments []common.Hash) []string {
-	output := make([]string, len(proofSegments))
-	for i, s := range proofSegments {
-		output[i] = hex.EncodeToString(s.Bytes())
-	}
-	return output
-}
+type RCHashResponse storageincentives.SampleWithProofs
 
 // This API is kept for testing the sampler. As a result, no documentation or tests are added here.
 func (s *Service) rchash(w http.ResponseWriter, r *http.Request) {
 	logger := s.logger.WithName("get_rchash").Build()
 
 	paths := struct {
-		Depth   uint8  `map:"depth"`
-		Anchor1 string `map:"anchor1,decHex" validate:"required"`
-		Anchor2 string `map:"anchor2,decHex" validate:"required"`
+		Depth   *uint8 `map:"depth" validate:"required"`
+		Anchor1 string `map:"anchor1" validate:"required"`
 	}{}
 	if response := s.mapStructure(mux.Vars(r), &paths); response != nil {
 		response("invalid path params", logger, w)
 		return
 	}
 
-	anchor1 := []byte(paths.Anchor1)
+	anchor1, err := hex.DecodeString(paths.Anchor1)
+	if err != nil {
+		logger.Error(err, "invalid hex params")
+		jsonhttp.InternalServerError(w, "invalid hex params")
+		return
+	}
 
-	anchor2 := []byte(paths.Anchor2)
-
-	swp, err := s.redistributionAgent.SampleWithProofs(r.Context(), anchor1, anchor2, paths.Depth)
+	resp, err := s.redistributionAgent.SampleWithProofs(r.Context(), anchor1, *paths.Depth)
 	if err != nil {
 		logger.Error(err, "failed making sample with proofs")
 		jsonhttp.InternalServerError(w, "failed making sample with proofs")
 		return
 	}
 
-	resp := RCHashResponse{
-		Hash:     swp.Hash,
-		Duration: swp.Duration,
-		Proofs:   renderChunkInclusionProofs(swp.Proofs),
-	}
-
-	jsonhttp.OK(w, resp)
+	jsonhttp.OK(w, RCHashResponse(resp))
 }
--- pkg/storageincentives/redistribution/redistribution.go
+++ pkg/storageincentives/redistribution/redistribution.go
@@ -23,8 +23,8 @@ type Contract interface {
 	ReserveSalt(context.Context) ([]byte, error)
 	IsPlaying(context.Context, uint8) (bool, error)
 	IsWinner(context.Context) (bool, error)
-	Claim(context.Context, ChunkInclusionProofs) (common.Hash, error)
-	Commit(context.Context, []byte, uint32) (common.Hash, error)
+	Claim(context.Context) (common.Hash, error)
+	Commit(context.Context, []byte, *big.Int) (common.Hash, error)
 	Reveal(context.Context, uint8, []byte, []byte) (common.Hash, error)
 }
 
@@ -92,8 +92,8 @@ func (c *contract) IsWinner(ctx context.Context) (isWinner bool, err error) {
 }
 
 // Claim sends a transaction to blockchain if a win is claimed.
-func (c *contract) Claim(ctx context.Context, proofs ChunkInclusionProofs) (common.Hash, error) {
-	callData, err := c.incentivesContractABI.Pack("claim", proofs.A, proofs.B, proofs.C)
+func (c *contract) Claim(ctx context.Context) (common.Hash, error) {
+	callData, err := c.incentivesContractABI.Pack("claim")
 	if err != nil {
 		return common.Hash{}, err
 	}
@@ -115,7 +115,7 @@ func (c *contract) Claim(ctx context.Context, proofs ChunkInclusionProofs) (comm
 }
 
 // Commit submits the obfusHash hash by sending a transaction to the blockchain.
-func (c *contract) Commit(ctx context.Context, obfusHash []byte, round uint32) (common.Hash, error) {
+func (c *contract) Commit(ctx context.Context, obfusHash []byte, round *big.Int) (common.Hash, error) {
 	callData, err := c.incentivesContractABI.Pack("commit", common.BytesToHash(obfusHash), common.BytesToHash(c.overlay.Bytes()), round)
 	if err != nil {
 		return common.Hash{}, err
