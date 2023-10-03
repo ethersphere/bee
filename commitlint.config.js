d5748a18e96a89f6a5688d907a3b1973a1a727b0
istae <14264581+istae@users.noreply.github.com>
2023-10-03 17:23:38 +0300

    chore: increase max commit length

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@ module.exports = {
 		'body-case': [0, 'never'],
 		'footer-leading-blank': [1, 'always'],
 		'footer-max-line-length': [2, 'always', 72],
-		'header-max-length': [2, 'always', 72],
+		'header-max-length': [2, 'always', 100],
 		'subject-case': [
 			2,
 			'never',
