7d1d6aa4ba1ab0a1a3c1b199b9a0294b0df2ff36
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2023-10-05 12:41:09 +0200

    chore: add license header

--- pkg/storageincentives/redistribution/abi_types_test.go
+++ pkg/storageincentives/redistribution/abi_types_test.go
@@ -1,3 +1,7 @@
+// Copyright 2023 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package redistribution_test
 
 // must include tests for just the contract calldata abi packaging
