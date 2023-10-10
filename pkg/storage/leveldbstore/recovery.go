414fc3b9d19b1d53b26175c3d7f8bde3e1ffc4ce
mrekucci <peter@ethswarm.org>
2023-10-10 19:26:52 +0400

    feat: add logs to the txstore and txchunkstore recovery method (#4387)

--- pkg/storage/leveldbstore/recovery.go
+++ pkg/storage/leveldbstore/recovery.go
@@ -7,6 +7,7 @@ package leveldbstore
 import (
 	"fmt"
 
+	"github.com/ethersphere/bee/pkg/log"
 	"github.com/ethersphere/bee/pkg/storage"
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/opt"
@@ -35,13 +36,18 @@ func (p *pendingTx) Unmarshal(bytes []byte) error {
 // Recover attempts to recover from a previous
 // crash by reverting all uncommitted transactions.
 func (s *TxStore) Recover() error {
+	logger := log.NewLogger("node").WithName("tx_store_recovery").Register() // "node" - copies the node.LoggerName in order to avoid circular import.
+
 	batch := new(leveldb.Batch)
 
+	logger.Info("checking for uncommitted transactions")
 	err := s.Iterate(storage.Query{
 		Factory:      func() storage.Item { return new(pendingTx) },
 		ItemProperty: storage.QueryItem,
 	}, func(r storage.Result) (bool, error) {
+		logger.Info("uncommitted transaction found", "id", r.ID)
 		if err := r.Entry.(*pendingTx).val.Replay(batch); err != nil {
+			logger.Debug("unable to replay uncommitted transaction", "id", r.ID, "err", err)
 			return true, fmt.Errorf("unable to replay batch for %s: %w", r.ID, err)
 		}
 		batch.Delete(id(r.ID))
@@ -51,8 +57,15 @@ func (s *TxStore) Recover() error {
 		return fmt.Errorf("leveldbstore: recovery: iteration failed: %w", err)
 	}
 
+	if batch.Len() == 0 {
+		logger.Info("no uncommitted transactions found")
+		return nil
+	}
+
+	logger.Info("reversing uncommitted transactions", "count", batch.Len())
 	if err := s.BatchedStore.(*Store).db.Write(batch, &opt.WriteOptions{Sync: true}); err != nil {
 		return fmt.Errorf("leveldbstore: recovery: unable to write batch: %w", err)
 	}
+	logger.Info("recovery successful")
 	return nil
 }
