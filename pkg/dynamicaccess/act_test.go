087bff668d0b6b13dd40f2eb4948d7e883ec3d86
Ferenc Sárai <sarai.ferenc@gmail.com>
2024-03-19 15:50:12 +0100

    Act swarm address (#15)
    * (refactor): ACT interface params + add doc comments
    
    * Revert "(refactor): ACT interface params + add doc comments"
    
    This reverts commit ee8da04fe7468a4fa65bd390fa17f72f2e93d301.
    
    * (refactor): ACT interface params + add doc comments
    
    * (refactor): Add error to ACT interface methods
    
    * Add in-memory storage and implement Store and Load methods
    
    * Move and refactor ACT diffieHellman to Session. Add Key and NewFromKeystore functions.
    
    ---------
    
    Co-authored-by: Ferenc Sárai <ferenc.sarai@solarpunk.buzz>
    Co-authored-by: Bálint Ujvári <balint.ujvari@solarpunk.buzz>

--- pkg/dynamicaccess/act_test.go
+++ pkg/dynamicaccess/act_test.go
@@ -6,18 +6,10 @@ package dynamicaccess_test
 
 import (
 	"bytes"
-	"context"
 	"encoding/hex"
 	"testing"
 
 	"github.com/ethersphere/bee/pkg/dynamicaccess"
-	"github.com/ethersphere/bee/pkg/file/loadsave"
-	"github.com/ethersphere/bee/pkg/file/pipeline"
-	"github.com/ethersphere/bee/pkg/file/pipeline/builder"
-	"github.com/ethersphere/bee/pkg/file/redundancy"
-	"github.com/ethersphere/bee/pkg/manifest"
-	"github.com/ethersphere/bee/pkg/storage"
-	mockstorer "github.com/ethersphere/bee/pkg/storer/mock"
 	"github.com/ethersphere/bee/pkg/swarm"
 )
 
@@ -36,88 +28,25 @@ func TestActAddLookup(t *testing.T) {
 	}
 }
 
-func TestActWithManifest(t *testing.T) {
-
-	storer := mockstorer.New()
-	encrypt := false
-	ctx := context.Background()
-	ls := loadsave.New(storer.ChunkStore(), storer.Cache(), pipelineFactory(storer.Cache(), false, 0))
-	rootManifest, err := manifest.NewDefaultManifest(ls, encrypt)
-	if err != nil {
-		t.Error("DefaultManifest should not return an error")
-	}
+func TestActStoreLoad(t *testing.T) {
 
 	act := dynamicaccess.NewDefaultAct()
 	lookupKey := swarm.RandAddress(t).Bytes()
 	encryptedAccesskey := swarm.RandAddress(t).Bytes()
-	err = act.Add(lookupKey, encryptedAccesskey)
+	err := act.Add(lookupKey, encryptedAccesskey)
 	if err != nil {
 		t.Error("Add() should not return an error")
 	}
 
-	actManifEntry, _ := act.Load(lookupKey)
-	if actManifEntry == nil {
-		t.Error("Load() should return a manifest.Entry")
-	}
-
-	err = rootManifest.Add(ctx, hex.EncodeToString(lookupKey), actManifEntry)
+	swarm_ref, err := act.Store()
 	if err != nil {
-		t.Error("rootManifest.Add() should not return an error")
-	}
-
-	_, err = rootManifest.Store(ctx)
-	if err != nil {
-		t.Error("rootManifest.Store() should not return an error")
-	}
-
-	actualMe, err := rootManifest.Lookup(ctx, hex.EncodeToString(lookupKey))
-	if err != nil {
-		t.Error("rootManifest.Lookup() should not return an error")
+		t.Error("Store() should not return an error")
 	}
 
 	actualAct := dynamicaccess.NewDefaultAct()
-	actualAct.Store(actualMe)
+	actualAct.Load(swarm_ref)
 	actualEak, _ := actualAct.Lookup(lookupKey)
 	if !bytes.Equal(actualEak, encryptedAccesskey) {
-		t.Errorf("actualAct.Store() value is not the expected %s != %s", actualEak, encryptedAccesskey)
-	}
-}
-
-func TestActStore(t *testing.T) {
-	mp := make(map[string]string)
-
-	lookupKey := swarm.RandAddress(t).Bytes()
-	encryptedAccesskey := swarm.RandAddress(t).Bytes()
-	mp[hex.EncodeToString(lookupKey)] = hex.EncodeToString(encryptedAccesskey)
-
-	me := manifest.NewEntry(swarm.NewAddress(lookupKey), mp)
-	act := dynamicaccess.NewDefaultAct()
-	act.Store(me)
-	eak, _ := act.Lookup(lookupKey)
-
-	if !bytes.Equal(eak, encryptedAccesskey) {
-		t.Errorf("Store() value is not the expected %s != %s", eak, encryptedAccesskey)
-	}
-
-}
-
-func TestActLoad(t *testing.T) {
-	act := dynamicaccess.NewDefaultAct()
-	lookupKey := swarm.RandAddress(t).Bytes()
-	encryptedAccesskey := swarm.RandAddress(t).Bytes()
-	act.Add(lookupKey, encryptedAccesskey)
-	me, _ := act.Load(lookupKey)
-
-	eak := me.Metadata()[hex.EncodeToString(lookupKey)]
-
-	if eak != hex.EncodeToString(encryptedAccesskey) {
-		t.Errorf("Load() value is not the expected %s != %s", eak, encryptedAccesskey)
-	}
-
-}
-
-func pipelineFactory(s storage.Putter, encrypt bool, rLevel redundancy.Level) func() pipeline.Interface {
-	return func() pipeline.Interface {
-		return builder.NewPipelineBuilder(context.Background(), s, encrypt, rLevel)
+		t.Errorf("actualAct.Load() value is not the expected %s != %s", hex.EncodeToString(actualEak), hex.EncodeToString(encryptedAccesskey))
 	}
 }
