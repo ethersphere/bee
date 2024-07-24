c27eb4c112a304fe69964228f5baf25cdeb1c077
Acha Bill <achabill12@gmail.com>
2024-07-24 23:51:37 +0100

    fix: race

--- pkg/storer/migration/step_06.go
+++ pkg/storer/migration/step_06.go
@@ -37,10 +37,11 @@ func step_06(st transaction.Storage) func() error {
 }
 
 func addStampHash(logger log.Logger, st transaction.Storage) (int64, int64, error) {
-	itemC := make(chan *reserve.BatchRadiusItemV1, 1)
+	itemC := make(chan *reserve.BatchRadiusItemV1)
 	errC := make(chan error)
 	doneC := make(chan any)
 	defer close(doneC)
+	defer close(errC)
 
 	var eg errgroup.Group
 	p := runtime.NumCPU()
@@ -63,101 +64,98 @@ func addStampHash(logger log.Logger, st transaction.Storage) (int64, int64, erro
 	}()
 
 	go func() {
-		for item := range itemC {
-			batchRadiusItemV1 := item
-			eg.Go(func() error {
-				err := st.Run(context.Background(), func(s transaction.Store) error {
-					idxStore := s.IndexStore()
-					stamp, err := chunkstamp.LoadWithBatchID(idxStore, "reserve", batchRadiusItemV1.Address, batchRadiusItemV1.BatchID)
-					if err != nil {
-						return err
-					}
-					stampHash, err := stamp.Hash()
-					if err != nil {
-						return err
-					}
-
-					// Since the ID format has changed, we should delete the old item and put a new one with the new ID format.
-					err = idxStore.Delete(batchRadiusItemV1)
-					if err != nil {
-						return err
-					}
-					err = idxStore.Put(&reserve.BatchRadiusItem{
-						Bin:       batchRadiusItemV1.Bin,
-						BatchID:   batchRadiusItemV1.BatchID,
-						StampHash: stampHash,
-						Address:   batchRadiusItemV1.Address,
-						BinID:     batchRadiusItemV1.BinID,
-					})
-					if err != nil {
-						return err
-					}
-
-					chunkBinItemV1 := &reserve.ChunkBinItemV1{
-						Bin:   batchRadiusItemV1.Bin,
-						BinID: batchRadiusItemV1.BinID,
-					}
-					err = idxStore.Get(chunkBinItemV1)
-					if err != nil {
-						return err
-					}
-
-					// same id. Will replace.
-					err = idxStore.Put(&reserve.ChunkBinItem{
-						Bin:       chunkBinItemV1.Bin,
-						BinID:     chunkBinItemV1.BinID,
-						Address:   chunkBinItemV1.Address,
-						BatchID:   chunkBinItemV1.BatchID,
-						StampHash: stampHash,
-						ChunkType: chunkBinItemV1.ChunkType,
-					})
-					if err != nil {
-						return err
-					}
-
-					// same id. Will replace.
-					stampIndexItem := &stampindex.Item{
-						BatchID:        chunkBinItemV1.BatchID,
-						StampIndex:     stamp.Index(),
-						StampHash:      stampHash,
-						StampTimestamp: stamp.Timestamp(),
-						ChunkAddress:   chunkBinItemV1.Address,
-					}
-					stampIndexItem.SetNamespace([]byte("reserve"))
-					err = idxStore.Put(stampIndexItem)
-					if err != nil {
-						return err
-					}
-					doneCount.Add(1)
-					return nil
-				})
-				if err != nil {
-					errC <- err
-					return err
-				}
-				return nil
-			})
-		}
+		_ = st.IndexStore().Iterate(storage.Query{
+			Factory: func() storage.Item { return new(reserve.BatchRadiusItemV1) },
+		}, func(result storage.Result) (bool, error) {
+			seenCount++
+			item := result.Entry.(*reserve.BatchRadiusItemV1)
+			select {
+			case itemC <- item:
+			case err := <-errC:
+				return true, err
+			}
+			return false, nil
+		})
+		close(itemC)
 	}()
 
-	err := st.IndexStore().Iterate(storage.Query{
-		Factory: func() storage.Item { return new(reserve.BatchRadiusItemV1) },
-	}, func(result storage.Result) (bool, error) {
-		seenCount++
-		item := result.Entry.(*reserve.BatchRadiusItemV1)
-		select {
-		case itemC <- item:
-		case err := <-errC:
-			return true, err
-		}
-		return false, nil
-	})
-	close(itemC)
-	if err != nil {
-		return 0, 0, err
+	for item := range itemC {
+		batchRadiusItemV1 := item
+		eg.Go(func() error {
+			err := st.Run(context.Background(), func(s transaction.Store) error {
+				idxStore := s.IndexStore()
+				stamp, err := chunkstamp.LoadWithBatchID(idxStore, "reserve", batchRadiusItemV1.Address, batchRadiusItemV1.BatchID)
+				if err != nil {
+					return err
+				}
+				stampHash, err := stamp.Hash()
+				if err != nil {
+					return err
+				}
+
+				// Since the ID format has changed, we should delete the old item and put a new one with the new ID format.
+				err = idxStore.Delete(batchRadiusItemV1)
+				if err != nil {
+					return err
+				}
+				err = idxStore.Put(&reserve.BatchRadiusItem{
+					Bin:       batchRadiusItemV1.Bin,
+					BatchID:   batchRadiusItemV1.BatchID,
+					StampHash: stampHash,
+					Address:   batchRadiusItemV1.Address,
+					BinID:     batchRadiusItemV1.BinID,
+				})
+				if err != nil {
+					return err
+				}
+
+				chunkBinItemV1 := &reserve.ChunkBinItemV1{
+					Bin:   batchRadiusItemV1.Bin,
+					BinID: batchRadiusItemV1.BinID,
+				}
+				err = idxStore.Get(chunkBinItemV1)
+				if err != nil {
+					return err
+				}
+
+				// same id. Will replace.
+				err = idxStore.Put(&reserve.ChunkBinItem{
+					Bin:       chunkBinItemV1.Bin,
+					BinID:     chunkBinItemV1.BinID,
+					Address:   chunkBinItemV1.Address,
+					BatchID:   chunkBinItemV1.BatchID,
+					StampHash: stampHash,
+					ChunkType: chunkBinItemV1.ChunkType,
+				})
+				if err != nil {
+					return err
+				}
+
+				// same id. Will replace.
+				stampIndexItem := &stampindex.Item{
+					BatchID:        chunkBinItemV1.BatchID,
+					StampIndex:     stamp.Index(),
+					StampHash:      stampHash,
+					StampTimestamp: stamp.Timestamp(),
+					ChunkAddress:   chunkBinItemV1.Address,
+				}
+				stampIndexItem.SetNamespace([]byte("reserve"))
+				err = idxStore.Put(stampIndexItem)
+				if err != nil {
+					return err
+				}
+				doneCount.Add(1)
+				return nil
+			})
+			if err != nil {
+				errC <- err
+				return err
+			}
+			return nil
+		})
 	}
 
-	err = eg.Wait()
+	err := eg.Wait()
 	if err != nil {
 		return 0, 0, err
 	}
