eb0a89d49f7d0b0d87d13ac1b27fe3a24cc69357
istae <14264581+istae@users.noreply.github.com>
2024-10-15 00:35:19 +0300

    feat(staking): change height on restart (#4856)

--- pkg/node/node.go
+++ pkg/node/node.go
@@ -1004,18 +1004,44 @@ func NewBee(
 
 	stakingContract := staking.New(overlayEthAddress, stakingContractAddress, abiutil.MustParseABI(chainCfg.StakingABI), bzzTokenAddress, transactionService, common.BytesToHash(nonce), o.TrxDebugMode, uint8(o.ReserveCapacityDoubling))
 
-	if chainEnabled && changedOverlay {
-		stake, err := stakingContract.GetPotentialStake(ctx)
-		if err != nil {
-			return nil, errors.New("getting stake balance")
-		}
-		if stake.Cmp(big.NewInt(0)) > 0 {
-			logger.Debug("changing overlay address in staking contract")
-			tx, err := stakingContract.ChangeStakeOverlay(ctx, common.BytesToHash(nonce))
+	if chainEnabled {
+
+		if changedOverlay {
+			stake, err := stakingContract.GetPotentialStake(ctx)
 			if err != nil {
-				return nil, fmt.Errorf("cannot change staking overlay address: %v", err.Error())
+				return nil, err
 			}
-			logger.Info("overlay address changed in staking contract", "transaction", tx)
+			if stake.Cmp(big.NewInt(0)) > 0 {
+				logger.Debug("changing overlay address in staking contract")
+				tx, err := stakingContract.ChangeStakeOverlay(ctx, common.BytesToHash(nonce))
+				if err != nil {
+					return nil, fmt.Errorf("cannot change staking overlay address: %v", err.Error())
+				}
+				logger.Info("overlay address changed in staking contract", "transaction", tx)
+			}
+		}
+
+		// make sure that the staking contract has the up to date height
+		tx, updated, err := stakingContract.UpdateHeight(ctx)
+		if err != nil {
+			return nil, err
+		}
+		if updated {
+			logger.Info("updated new reserve capacity doubling height in the staking contract", "transaction", tx, "new_height", o.ReserveCapacityDoubling)
+		}
+
+		if o.ReserveCapacityDoubling > 0 {
+			stake, err := stakingContract.GetPotentialStake(ctx)
+			if err != nil {
+				return nil, err
+			}
+			if stake.Cmp(big.NewInt(0)) > 0 {
+				// Check if the staked amount is sufficient to cover the additional neighborhoods.
+				// The staked amount must be at least 2^h * MinimumStake.
+				if stake.Cmp(big.NewInt(0).Mul(big.NewInt(1<<o.ReserveCapacityDoubling), staking.MinimumStakeAmount)) < 0 {
+					logger.Warning("staked amount does not sufficiently cover the additional reserve capacity. Stake should be at least 2^h * 10 BZZ, where h is the number extra doublings.")
+				}
+			}
 		}
 	}
 
