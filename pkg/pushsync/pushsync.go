df2e12c0f53fe7b23364ba6bd7413ff62aa0c35c
istae <14264581+istae@users.noreply.github.com>
2025-01-21 01:21:02 +0300

    fix: debug log

--- pkg/pushsync/pushsync.go
+++ pkg/pushsync/pushsync.go
@@ -528,6 +528,7 @@ func (ps *PushSync) push(parentCtx context.Context, resultChan chan<- receiptRes
 		select {
 		case resultChan <- receiptResult{pushTime: now, peer: peer, err: err, receipt: receipt}:
 		case <-parentCtx.Done():
+			ps.logger.Debug("push result parent context canceled", "chunk_address", ch.Address(), "peer_address", peer)
 		}
 	}()
 
