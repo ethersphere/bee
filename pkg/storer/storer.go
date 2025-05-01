d42d918cbf0b0b56c65ce55d7a8cd34a8ada87f9
Acha Bill <achabill12@gmail.com>
2025-05-01 23:31:17 +0100

    revert: defaultBgCacheWorkers from 128 to 16

--- pkg/storer/storer.go
+++ pkg/storer/storer.go
@@ -243,7 +243,7 @@ const (
 	defaultWriteBufferSize        = uint64(32 * 1024 * 1024)
 	defaultDisableSeeksCompaction = false
 	defaultCacheCapacity          = uint64(1_000_000)
-	defaultBgCacheWorkers         = 128
+	defaultBgCacheWorkers         = 16
 	DefaultReserveCapacity        = 1 << 22 // 4194304 chunks
 
 	indexPath  = "indexstore"
