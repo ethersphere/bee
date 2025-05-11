ee1f14ce3c3620195be338abd3c130561affe7b2
v1rtl <hi@v1rtl.site>
2025-05-11 14:14:57 +0300

    move windows-only usage to separate files

--- cmd/bee/cmd/start.go
+++ cmd/bee/cmd/start.go
@@ -13,14 +13,10 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"os/signal"
 	"path/filepath"
 	"strings"
-	"sync/atomic"
-	"syscall"
 	"time"
 
-	"github.com/ethersphere/bee/v2"
 	"github.com/ethersphere/bee/v2/pkg/accesscontrol"
 	chaincfg "github.com/ethersphere/bee/v2/pkg/config"
 	"github.com/ethersphere/bee/v2/pkg/crypto"
@@ -42,143 +38,6 @@ const (
 
 //go:embed bee-welcome-message.txt
 var beeWelcomeMessage string
-
-func (c *command) initStartCmd() (err error) {
-	cmd := &cobra.Command{
-		Use:               "start",
-		Short:             "Start a Swarm node",
-		PersistentPreRunE: c.CheckUnknownParams,
-		RunE: func(cmd *cobra.Command, args []string) (err error) {
-			if len(args) > 0 {
-				return cmd.Help()
-			}
-
-			v := strings.ToLower(c.config.GetString(optionNameVerbosity))
-
-			logger, err := newLogger(cmd, v)
-			if err != nil {
-				return fmt.Errorf("new logger: %w", err)
-			}
-
-			if c.isWindowsService {
-				logger, err = createWindowsEventLogger(serviceName, logger)
-				if err != nil {
-					return fmt.Errorf("failed to create windows logger %w", err)
-				}
-			}
-
-			fmt.Print(beeWelcomeMessage)
-			time.Sleep(5 * time.Second)
-			fmt.Printf("\n\nversion: %v - planned to be supported until %v, please follow https://ethswarm.org/\n\n", bee.Version, endSupportDate())
-			logger.Info("bee version", "version", bee.Version)
-
-			go startTimeBomb(logger)
-
-			// ctx is global context of bee node; which is canceled after interrupt signal is received.
-			ctx, cancel := context.WithCancel(context.Background())
-			sysInterruptChannel := make(chan os.Signal, 1)
-			signal.Notify(sysInterruptChannel, syscall.SIGINT, syscall.SIGTERM)
-
-			go func() {
-				select {
-				case <-sysInterruptChannel:
-					logger.Info("received interrupt signal")
-					cancel()
-				case <-ctx.Done():
-				}
-			}()
-
-			// Building bee node can take up some time (because node.NewBee(...) is compute have function )
-			// Because of this we need to do it in background so that program could be terminated when interrupt signal is received
-			// while bee node is being constructed.
-			respC := buildBeeNodeAsync(ctx, c, cmd, logger)
-			var beeNode atomic.Value
-
-			p := &program{
-				start: func() {
-					// Wait for bee node to fully build and initialized
-					select {
-					case resp := <-respC:
-						if resp.err != nil {
-							logger.Error(resp.err, "failed to build bee node")
-							return
-						}
-						beeNode.Store(resp.bee)
-					case <-ctx.Done():
-						return
-					}
-
-					// Bee has fully started at this point, from now on we
-					// block main goroutine until it is interrupted or stopped
-					select {
-					case <-ctx.Done():
-					case <-beeNode.Load().(*node.Bee).SyncingStopped():
-						logger.Debug("syncing has stopped")
-					}
-
-					logger.Info("shutting down...")
-				},
-				stop: func() {
-					// Whenever program is being stopped we need to cancel main context
-					// beforehand so that node could be stopped via Shutdown method
-					cancel()
-
-					// Shutdown node (if node was fully started)
-					val := beeNode.Load()
-					if val == nil {
-						return
-					}
-
-					done := make(chan struct{})
-					go func(beeNode *node.Bee) {
-						defer close(done)
-
-						if err := beeNode.Shutdown(); err != nil {
-							logger.Error(err, "shutdown failed")
-						}
-					}(val.(*node.Bee))
-
-					// If shutdown function is blocking too long,
-					// allow process termination by receiving another signal.
-					select {
-					case <-sysInterruptChannel:
-						logger.Info("node shutdown terminated")
-					case <-done:
-						logger.Info("node shutdown")
-					}
-				},
-			}
-
-			if c.isWindowsService {
-				s, err := service.New(p, &service.Config{
-					Name:        serviceName,
-					DisplayName: "Bee",
-					Description: "Bee, Swarm client.",
-				})
-				if err != nil {
-					return err
-				}
-
-				if err = s.Run(); err != nil {
-					return err
-				}
-			} else {
-				// start blocks until some interrupt is received
-				p.start()
-				p.stop()
-			}
-
-			return nil
-		},
-		PreRunE: func(cmd *cobra.Command, args []string) error {
-			return c.config.BindPFlags(cmd.Flags())
-		},
-	}
-
-	c.setAllFlags(cmd)
-	c.root.AddCommand(cmd)
-	return nil
-}
 
 type buildBeeNodeResp struct {
 	bee *node.Bee
--- cmd/bee/cmd/start_windows.go
+++ cmd/bee/cmd/start_windows.go
@@ -7,13 +7,24 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"sync/atomic"
+	"syscall"
+	"time"
 
 	"golang.org/x/sys/windows/svc"
 	"golang.org/x/sys/windows/svc/debug"
 	"golang.org/x/sys/windows/svc/eventlog"
 
+	"github.com/ethersphere/bee/v2"
 	"github.com/ethersphere/bee/v2/pkg/log"
+	"github.com/ethersphere/bee/v2/pkg/node"
+	"github.com/kardianos/service"
+	"github.com/spf13/cobra"
 )
 
 func isWindowsService() (bool, error) {
@@ -54,4 +65,133 @@ func (l windowsEventLogger) Error(err error, msg string, keysAndValues ...interf
 		keysAndValues = append(keysAndValues, "error", err)
 	}
 	_ = l.winlog.Error(1633, fmt.Sprintf("%s %s", msg, fmt.Sprintln(keysAndValues...)))
+}
+
+func (c *command) initStartCmd() (err error) {
+	cmd := &cobra.Command{
+		Use:               "start",
+		Short:             "Start a Swarm node",
+		PersistentPreRunE: c.CheckUnknownParams,
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			if len(args) > 0 {
+				return cmd.Help()
+			}
+
+			v := strings.ToLower(c.config.GetString(optionNameVerbosity))
+
+			logger, err := newLogger(cmd, v)
+			if err != nil {
+				return fmt.Errorf("new logger: %w", err)
+			}
+
+			logger, err = createWindowsEventLogger(serviceName, logger)
+			if err != nil {
+				return fmt.Errorf("failed to create windows logger %w", err)
+			}
+
+			fmt.Print(beeWelcomeMessage)
+			time.Sleep(5 * time.Second)
+			fmt.Printf("\n\nversion: %v - planned to be supported until %v, please follow https://ethswarm.org/\n\n", bee.Version, endSupportDate())
+			logger.Info("bee version", "version", bee.Version)
+
+			go startTimeBomb(logger)
+
+			// ctx is global context of bee node; which is canceled after interrupt signal is received.
+			ctx, cancel := context.WithCancel(context.Background())
+			sysInterruptChannel := make(chan os.Signal, 1)
+			signal.Notify(sysInterruptChannel, syscall.SIGINT, syscall.SIGTERM)
+
+			go func() {
+				select {
+				case <-sysInterruptChannel:
+					logger.Info("received interrupt signal")
+					cancel()
+				case <-ctx.Done():
+				}
+			}()
+
+			// Building bee node can take up some time (because node.NewBee(...) is compute have function )
+			// Because of this we need to do it in background so that program could be terminated when interrupt signal is received
+			// while bee node is being constructed.
+			respC := buildBeeNodeAsync(ctx, c, cmd, logger)
+			var beeNode atomic.Value
+
+			p := &program{
+				start: func() {
+					// Wait for bee node to fully build and initialized
+					select {
+					case resp := <-respC:
+						if resp.err != nil {
+							logger.Error(resp.err, "failed to build bee node")
+							return
+						}
+						beeNode.Store(resp.bee)
+					case <-ctx.Done():
+						return
+					}
+
+					// Bee has fully started at this point, from now on we
+					// block main goroutine until it is interrupted or stopped
+					select {
+					case <-ctx.Done():
+					case <-beeNode.Load().(*node.Bee).SyncingStopped():
+						logger.Debug("syncing has stopped")
+					}
+
+					logger.Info("shutting down...")
+				},
+				stop: func() {
+					// Whenever program is being stopped we need to cancel main context
+					// beforehand so that node could be stopped via Shutdown method
+					cancel()
+
+					// Shutdown node (if node was fully started)
+					val := beeNode.Load()
+					if val == nil {
+						return
+					}
+
+					done := make(chan struct{})
+					go func(beeNode *node.Bee) {
+						defer close(done)
+
+						if err := beeNode.Shutdown(); err != nil {
+							logger.Error(err, "shutdown failed")
+						}
+					}(val.(*node.Bee))
+
+					// If shutdown function is blocking too long,
+					// allow process termination by receiving another signal.
+					select {
+					case <-sysInterruptChannel:
+						logger.Info("node shutdown terminated")
+					case <-done:
+						logger.Info("node shutdown")
+					}
+				},
+			}
+
+			s, err := service.New(p, &service.Config{
+				Name:        serviceName,
+				DisplayName: "Bee",
+				Description: "Bee, Swarm client.",
+			})
+			if err != nil {
+				return err
+			}
+
+			if err = s.Run(); err != nil {
+				return err
+			}
+
+			return nil
+		},
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return c.config.BindPFlags(cmd.Flags())
+		},
+	}
+
+	c.setAllFlags(cmd)
+	c.root.AddCommand(cmd)
+	return nil
 }
