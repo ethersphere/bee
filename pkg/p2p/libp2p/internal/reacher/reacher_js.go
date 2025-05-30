80e706ac9e6f3de8270eaa8c4bd1a650247265a7
v1rtl <hi@v1rtl.site>
2025-05-30 21:17:27 +0300

    remove metrics from reacher

--- pkg/p2p/libp2p/internal/reacher/reacher_js.go
+++ pkg/p2p/libp2p/internal/reacher/reacher_js.go
@@ -1,12 +1,11 @@
-//go:build !js
-// +build !js
+//go:build js
+// +build js
 
 package reacher
 
 import (
 	"context"
 	"sync"
-	"time"
 
 	"github.com/ethersphere/bee/v2/pkg/p2p"
 )
@@ -21,8 +20,7 @@ type reacher struct {
 	pinger   p2p.Pinger
 	notifier p2p.ReachableNotifier
 
-	wg      sync.WaitGroup
-	metrics metrics
+	wg sync.WaitGroup
 
 	options *Options
 }
@@ -35,7 +33,6 @@ func New(streamer p2p.Pinger, notifier p2p.ReachableNotifier, o *Options) *reach
 		pinger:   streamer,
 		peers:    make(map[string]*peer),
 		notifier: notifier,
-		metrics:  newMetrics(),
 	}
 
 	if o == nil {
@@ -59,20 +56,16 @@ func (r *reacher) ping(c chan *peer, ctx context.Context) {
 
 	for p := range c {
 
-		now := time.Now()
-
 		ctxt, cancel := context.WithTimeout(ctx, r.options.PingTimeout)
 		_, err := r.pinger.Ping(ctxt, p.addr)
 		cancel()
 
 		// ping was successful
 		if err == nil {
-			r.metrics.Pings.WithLabelValues("success").Inc()
-			r.metrics.PingTime.WithLabelValues("success").Observe(time.Since(now).Seconds())
+
 			r.notifier.Reachable(p.overlay, p2p.ReachabilityStatusPublic)
 		} else {
-			r.metrics.Pings.WithLabelValues("failure").Inc()
-			r.metrics.PingTime.WithLabelValues("failure").Observe(time.Since(now).Seconds())
+
 			r.notifier.Reachable(p.overlay, p2p.ReachabilityStatusPrivate)
 		}
 	}
