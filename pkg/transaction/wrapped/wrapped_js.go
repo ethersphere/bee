1686f63caa2b1ae55b1b30232033956091abcf0f
v1rtl <hi@v1rtl.site>
2025-05-30 17:18:06 +0300

    remove wrapped tx metrics from wasm build

--- pkg/transaction/wrapped/wrapped_js.go
+++ pkg/transaction/wrapped/wrapped_js.go
@@ -1,5 +1,5 @@
-//go:build !js
-// +build !js
+//go:build js
+// +build js
 
 package wrapped
 
@@ -11,29 +11,25 @@ import (
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-
 	"github.com/ethersphere/bee/v2/pkg/transaction"
 )
 
 type wrappedBackend struct {
 	backend transaction.Backend
-	metrics metrics
 }
 
 func NewBackend(backend transaction.Backend) transaction.Backend {
 	return &wrappedBackend{
 		backend: backend,
-		metrics: newMetrics(),
 	}
 }
 
 func (b *wrappedBackend) TransactionReceipt(ctx context.Context, txHash common.Hash) (*types.Receipt, error) {
-	b.metrics.TotalRPCCalls.Inc()
-	b.metrics.TransactionReceiptCalls.Inc()
+
 	receipt, err := b.backend.TransactionReceipt(ctx, txHash)
 	if err != nil {
 		if !errors.Is(err, ethereum.NotFound) {
-			b.metrics.TotalRPCErrors.Inc()
+
 		}
 		return nil, err
 	}
@@ -41,12 +37,11 @@ func (b *wrappedBackend) TransactionReceipt(ctx context.Context, txHash common.H
 }
 
 func (b *wrappedBackend) TransactionByHash(ctx context.Context, hash common.Hash) (*types.Transaction, bool, error) {
-	b.metrics.TotalRPCCalls.Inc()
-	b.metrics.TransactionCalls.Inc()
+
 	tx, isPending, err := b.backend.TransactionByHash(ctx, hash)
 	if err != nil {
 		if !errors.Is(err, ethereum.NotFound) {
-			b.metrics.TotalRPCErrors.Inc()
+
 		}
 		return nil, false, err
 	}
@@ -54,23 +49,21 @@ func (b *wrappedBackend) TransactionByHash(ctx context.Context, hash common.Hash
 }
 
 func (b *wrappedBackend) BlockNumber(ctx context.Context) (uint64, error) {
-	b.metrics.TotalRPCCalls.Inc()
-	b.metrics.BlockNumberCalls.Inc()
+
 	blockNumber, err := b.backend.BlockNumber(ctx)
 	if err != nil {
-		b.metrics.TotalRPCErrors.Inc()
+
 		return 0, err
 	}
 	return blockNumber, nil
 }
 
 func (b *wrappedBackend) HeaderByNumber(ctx context.Context, number *big.Int) (*types.Header, error) {
-	b.metrics.TotalRPCCalls.Inc()
-	b.metrics.BlockHeaderCalls.Inc()
+
 	header, err := b.backend.HeaderByNumber(ctx, number)
 	if err != nil {
 		if !errors.Is(err, ethereum.NotFound) {
-			b.metrics.TotalRPCErrors.Inc()
+
 		}
 		return nil, err
 	}
@@ -78,121 +71,110 @@ func (b *wrappedBackend) HeaderByNumber(ctx context.Context, number *big.Int) (*
 }
 
 func (b *wrappedBackend) BalanceAt(ctx context.Context, address common.Address, block *big.Int) (*big.Int, error) {
-	b.metrics.TotalRPCCalls.Inc()
-	b.metrics.BalanceCalls.Inc()
+
 	balance, err := b.backend.BalanceAt(ctx, address, block)
 	if err != nil {
-		b.metrics.TotalRPCErrors.Inc()
+
 		return nil, err
 	}
 	return balance, nil
 }
 
 func (b *wrappedBackend) NonceAt(ctx context.Context, account common.Address, blockNumber *big.Int) (uint64, error) {
-	b.metrics.TotalRPCCalls.Inc()
-	b.metrics.NonceAtCalls.Inc()
+
 	nonce, err := b.backend.NonceAt(ctx, account, blockNumber)
 	if err != nil {
-		b.metrics.TotalRPCErrors.Inc()
+
 		return 0, err
 	}
 	return nonce, nil
 }
 
 func (b *wrappedBackend) CodeAt(ctx context.Context, contract common.Address, blockNumber *big.Int) ([]byte, error) {
-	b.metrics.TotalRPCCalls.Inc()
-	b.metrics.CodeAtCalls.Inc()
+
 	code, err := b.backend.CodeAt(ctx, contract, blockNumber)
 	if err != nil {
-		b.metrics.TotalRPCErrors.Inc()
+
 		return nil, err
 	}
 	return code, nil
 }
 
 func (b *wrappedBackend) CallContract(ctx context.Context, call ethereum.CallMsg, blockNumber *big.Int) ([]byte, error) {
-	b.metrics.TotalRPCCalls.Inc()
-	b.metrics.CallContractCalls.Inc()
+
 	result, err := b.backend.CallContract(ctx, call, blockNumber)
 	if err != nil {
-		b.metrics.TotalRPCErrors.Inc()
+
 		return nil, err
 	}
 	return result, nil
 }
 
 func (b *wrappedBackend) PendingNonceAt(ctx context.Context, account common.Address) (uint64, error) {
-	b.metrics.TotalRPCCalls.Inc()
-	b.metrics.PendingNonceCalls.Inc()
+
 	nonce, err := b.backend.PendingNonceAt(ctx, account)
 	if err != nil {
-		b.metrics.TotalRPCErrors.Inc()
+
 		return 0, err
 	}
 	return nonce, nil
 }
 
 func (b *wrappedBackend) SuggestGasPrice(ctx context.Context) (*big.Int, error) {
-	b.metrics.TotalRPCCalls.Inc()
-	b.metrics.SuggestGasPriceCalls.Inc()
+
 	gasPrice, err := b.backend.SuggestGasPrice(ctx)
 	if err != nil {
-		b.metrics.TotalRPCErrors.Inc()
+
 		return nil, err
 	}
 	return gasPrice, nil
 }
 
 func (b *wrappedBackend) SuggestGasTipCap(ctx context.Context) (*big.Int, error) {
-	b.metrics.TotalRPCCalls.Inc()
-	b.metrics.SuggestGasPriceCalls.Inc()
+
 	gasTipCap, err := b.backend.SuggestGasTipCap(ctx)
 	if err != nil {
-		b.metrics.TotalRPCErrors.Inc()
+
 		return nil, err
 	}
 	return gasTipCap, nil
 }
 
 func (b *wrappedBackend) EstimateGas(ctx context.Context, call ethereum.CallMsg) (gas uint64, err error) {
-	b.metrics.TotalRPCCalls.Inc()
-	b.metrics.EstimateGasCalls.Inc()
+
 	gas, err = b.backend.EstimateGas(ctx, call)
 	if err != nil {
-		b.metrics.TotalRPCErrors.Inc()
+
 		return 0, err
 	}
 	return gas, nil
 }
 
 func (b *wrappedBackend) SendTransaction(ctx context.Context, tx *types.Transaction) error {
-	b.metrics.TotalRPCCalls.Inc()
-	b.metrics.SendTransactionCalls.Inc()
+
 	err := b.backend.SendTransaction(ctx, tx)
 	if err != nil {
-		b.metrics.TotalRPCErrors.Inc()
+
 		return err
 	}
 	return nil
 }
 
 func (b *wrappedBackend) FilterLogs(ctx context.Context, query ethereum.FilterQuery) ([]types.Log, error) {
-	b.metrics.TotalRPCCalls.Inc()
-	b.metrics.FilterLogsCalls.Inc()
+
 	logs, err := b.backend.FilterLogs(ctx, query)
 	if err != nil {
-		b.metrics.TotalRPCErrors.Inc()
+
 		return nil, err
 	}
 	return logs, nil
 }
 
 func (b *wrappedBackend) ChainID(ctx context.Context) (*big.Int, error) {
-	b.metrics.TotalRPCCalls.Inc()
-	b.metrics.ChainIDCalls.Inc()
+
 	chainID, err := b.backend.ChainID(ctx)
 	if err != nil {
-		b.metrics.TotalRPCErrors.Inc()
+
 		return nil, err
 	}
 	return chainID, nil
