9843847d97c5a0a3e9d0a43dc34ff0b561f6d218
akrem-chabchoub <chabchoub.akrem@gmail.com>
2025-10-19 23:57:24 +0200

    test(gsoc): integrate synctest for websocket handler tests

--- pkg/api/gsoc_test.go
+++ pkg/api/gsoc_test.go
@@ -10,6 +10,7 @@ import (
 	"net/url"
 	"strings"
 	"testing"
+	"testing/synctest"
 	"time"
 
 	"github.com/ethersphere/bee/v2/pkg/cac"
@@ -28,75 +29,79 @@ import (
 func TestGsocWebsocketSingleHandler(t *testing.T) {
 	t.Parallel()
 
-	var (
-		id               = make([]byte, 32)
-		g, cl, signer, _ = newGsocTest(t, id, 0)
-		respC            = make(chan error, 1)
-		payload          = []byte("hello there!")
-	)
+	synctest.Test(t, func(t *testing.T) {
+		var (
+			id               = make([]byte, 32)
+			g, cl, signer, _ = newGsocTest(t, id, 0)
+			respC            = make(chan error, 1)
+			payload          = []byte("hello there!")
+		)
 
-	err := cl.SetReadDeadline(time.Now().Add(2 * time.Second))
-	if err != nil {
-		t.Fatal(err)
-	}
-	cl.SetReadLimit(swarm.ChunkSize)
+		err := cl.SetReadDeadline(time.Now().Add(2 * time.Second))
+		if err != nil {
+			t.Fatal(err)
+		}
+		cl.SetReadLimit(swarm.ChunkSize)
 
-	ch, _ := cac.New(payload)
-	socCh := soc.New(id, ch)
-	ch, _ = socCh.Sign(signer)
-	socCh, _ = soc.FromChunk(ch)
-	g.Handle(socCh)
+		ch, _ := cac.New(payload)
+		socCh := soc.New(id, ch)
+		ch, _ = socCh.Sign(signer)
+		socCh, _ = soc.FromChunk(ch)
+		g.Handle(socCh)
 
-	go expectMessage(t, cl, respC, payload)
-	if err := <-respC; err != nil {
-		t.Fatal(err)
-	}
+		go expectMessage(t, cl, respC, payload)
+		if err := <-respC; err != nil {
+			t.Fatal(err)
+		}
+	})
 }
 
 func TestGsocWebsocketMultiHandler(t *testing.T) {
 	t.Parallel()
 
-	var (
-		id                      = make([]byte, 32)
-		g, cl, signer, listener = newGsocTest(t, make([]byte, 32), 0)
-		owner, _                = signer.EthereumAddress()
-		chunkAddr, _            = soc.CreateAddress(id, owner.Bytes())
-		u                       = url.URL{Scheme: "ws", Host: listener, Path: fmt.Sprintf("/gsoc/subscribe/%s", hex.EncodeToString(chunkAddr.Bytes()))}
-		cl2, _, err             = websocket.DefaultDialer.Dial(u.String(), nil)
-		respC                   = make(chan error, 2)
-	)
-	if err != nil {
-		t.Fatalf("dial: %v. url %v", err, u.String())
-	}
-	testutil.CleanupCloser(t, cl2)
+	synctest.Test(t, func(t *testing.T) {
+		var (
+			id                      = make([]byte, 32)
+			g, cl, signer, listener = newGsocTest(t, make([]byte, 32), 0)
+			owner, _                = signer.EthereumAddress()
+			chunkAddr, _            = soc.CreateAddress(id, owner.Bytes())
+			u                       = url.URL{Scheme: "ws", Host: listener, Path: fmt.Sprintf("/gsoc/subscribe/%s", hex.EncodeToString(chunkAddr.Bytes()))}
+			cl2, _, err             = websocket.DefaultDialer.Dial(u.String(), nil)
+			respC                   = make(chan error, 2)
+		)
+		if err != nil {
+			t.Fatalf("dial: %v. url %v", err, u.String())
+		}
+		testutil.CleanupCloser(t, cl2)
 
-	err = cl.SetReadDeadline(time.Now().Add(2 * time.Second))
-	if err != nil {
-		t.Fatal(err)
-	}
-	cl.SetReadLimit(swarm.ChunkSize)
+		err = cl.SetReadDeadline(time.Now().Add(2 * time.Second))
+		if err != nil {
+			t.Fatal(err)
+		}
+		cl.SetReadLimit(swarm.ChunkSize)
 
-	ch, _ := cac.New(payload)
-	socCh := soc.New(id, ch)
-	ch, _ = socCh.Sign(signer)
-	socCh, _ = soc.FromChunk(ch)
+		ch, _ := cac.New(payload)
+		socCh := soc.New(id, ch)
+		ch, _ = socCh.Sign(signer)
+		socCh, _ = soc.FromChunk(ch)
 
-	// close the websocket before calling GSOC with the message
-	err = cl.WriteMessage(websocket.CloseMessage, []byte{})
-	if err != nil {
-		t.Fatal(err)
-	}
+		// close the websocket before calling GSOC with the message
+		err = cl.WriteMessage(websocket.CloseMessage, []byte{})
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	g.Handle(socCh)
+		g.Handle(socCh)
 
-	go expectMessage(t, cl, respC, payload)
-	go expectMessage(t, cl2, respC, payload)
-	if err := <-respC; err != nil {
-		t.Fatal(err)
-	}
-	if err := <-respC; err != nil {
-		t.Fatal(err)
-	}
+		go expectMessage(t, cl, respC, payload)
+		go expectMessage(t, cl2, respC, payload)
+		if err := <-respC; err != nil {
+			t.Fatal(err)
+		}
+		if err := <-respC; err != nil {
+			t.Fatal(err)
+		}
+	})
 }
 
 // TestGsocPong tests that the websocket api adheres to the websocket standard
@@ -104,34 +109,37 @@ func TestGsocWebsocketMultiHandler(t *testing.T) {
 // The test opens a websocket, keeps it alive for 500ms, then receives a GSOC message.
 func TestGsocPong(t *testing.T) {
 	t.Parallel()
-	id := make([]byte, 32)
 
-	var (
-		g, cl, signer, _ = newGsocTest(t, id, 90*time.Millisecond)
+	synctest.Test(t, func(t *testing.T) {
+		id := make([]byte, 32)
 
-		respC    = make(chan error, 1)
-		pongWait = 1 * time.Millisecond
-	)
+		var (
+			g, cl, signer, _ = newGsocTest(t, id, 90*time.Millisecond)
 
-	cl.SetReadLimit(swarm.ChunkSize)
-	err := cl.SetReadDeadline(time.Now().Add(pongWait))
-	if err != nil {
-		t.Fatal(err)
-	}
+			respC    = make(chan error, 1)
+			pongWait = 1 * time.Millisecond
+		)
 
-	time.Sleep(500 * time.Millisecond) // wait to see that the websocket is kept alive
-	ch, _ := cac.New([]byte("hello there!"))
-	socCh := soc.New(id, ch)
-	ch, _ = socCh.Sign(signer)
-	socCh, _ = soc.FromChunk(ch)
+		cl.SetReadLimit(swarm.ChunkSize)
+		err := cl.SetReadDeadline(time.Now().Add(pongWait))
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	g.Handle(socCh)
+		time.Sleep(500 * time.Millisecond) // wait to see that the websocket is kept alive
+		ch, _ := cac.New([]byte("hello there!"))
+		socCh := soc.New(id, ch)
+		ch, _ = socCh.Sign(signer)
+		socCh, _ = soc.FromChunk(ch)
 
-	go expectMessage(t, cl, respC, nil)
-	if err := <-respC; err == nil || !strings.Contains(err.Error(), "i/o timeout") {
-		// note: error has *websocket.netError type so we need to check error by checking message
-		t.Fatal("want timeout error")
-	}
+		g.Handle(socCh)
+
+		go expectMessage(t, cl, respC, nil)
+		if err := <-respC; err == nil || !strings.Contains(err.Error(), "i/o timeout") {
+			// note: error has *websocket.netError type so we need to check error by checking message
+			t.Fatal("want timeout error")
+		}
+	})
 }
 
 func newGsocTest(t *testing.T, socId []byte, pingPeriod time.Duration) (gsoc.Listener, *websocket.Conn, crypto.Signer, string) {
