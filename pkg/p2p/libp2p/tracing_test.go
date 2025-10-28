12b0e275d8c5387b93358a1c212fd6cc380042b7
akrem-chabchoub <chabchoub.akrem@gmail.com>
2025-10-28 14:12:08 +0100

    refactor(tracing_test):  remove synctest from TestTracing

--- pkg/p2p/libp2p/tracing_test.go
+++ pkg/p2p/libp2p/tracing_test.go
@@ -10,86 +10,80 @@ import (
 	"testing"
 	"time"
 
-	"testing/synctest"
-
 	"github.com/ethersphere/bee/v2/pkg/p2p"
 	"github.com/ethersphere/bee/v2/pkg/p2p/libp2p"
 	"github.com/ethersphere/bee/v2/pkg/tracing"
 )
 
 func TestTracing(t *testing.T) {
-	t.Parallel()
+	tracer1, closer1, err := tracing.NewTracer(&tracing.Options{
+		Enabled:     true,
+		ServiceName: "bee-test",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer closer1.Close()
 
-	synctest.Test(t, func(t *testing.T) {
-		tracer1, closer1, err := tracing.NewTracer(&tracing.Options{
-			Enabled:     true,
-			ServiceName: "bee-test",
-		})
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer closer1.Close()
+	tracer2, closer2, err := tracing.NewTracer(&tracing.Options{
+		Enabled:     true,
+		ServiceName: "bee-test",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer closer2.Close()
 
-		tracer2, closer2, err := tracing.NewTracer(&tracing.Options{
-			Enabled:     true,
-			ServiceName: "bee-test",
-		})
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer closer2.Close()
+	s1, overlay1 := newService(t, 1, libp2pServiceOpts{libp2pOpts: libp2p.Options{
+		FullNode: true,
+	}})
 
-		s1, overlay1 := newService(t, 1, libp2pServiceOpts{libp2pOpts: libp2p.Options{
-			FullNode: true,
-		}})
+	s2, _ := newService(t, 1, libp2pServiceOpts{})
 
-		s2, _ := newService(t, 1, libp2pServiceOpts{})
+	var handledTracingSpan string
+	handled := make(chan struct{})
+	if err := s1.AddProtocol(newTestProtocol(func(ctx context.Context, _ p2p.Peer, _ p2p.Stream) error {
 
-		var handledTracingSpan string
-		handled := make(chan struct{})
-		if err := s1.AddProtocol(newTestProtocol(func(ctx context.Context, _ p2p.Peer, _ p2p.Stream) error {
-
-			span, _, _ := tracer1.StartSpanFromContext(ctx, "test-p2p-handler", nil)
-			defer span.Finish()
-
-			handledTracingSpan = fmt.Sprint(span.Context())
-			close(handled)
-			return nil
-		})); err != nil {
-			t.Fatal(err)
-		}
-
-		addr := serviceUnderlayAddress(t, s1)
-
-		connectContext := t.Context()
-
-		if _, err := s2.Connect(connectContext, addr); err != nil {
-			t.Fatal(err)
-		}
-
-		ctx := t.Context()
-
-		span, _, ctx := tracer2.StartSpanFromContext(ctx, "test-p2p-client", nil)
+		span, _, _ := tracer1.StartSpanFromContext(ctx, "test-p2p-handler", nil)
 		defer span.Finish()
 
-		if fmt.Sprint(span.Context()) == "" {
-			t.Error("not tracing span context to send")
-		}
+		handledTracingSpan = fmt.Sprint(span.Context())
+		close(handled)
+		return nil
+	})); err != nil {
+		t.Fatal(err)
+	}
 
-		stream, err := s2.NewStream(ctx, overlay1, nil, testProtocolName, testProtocolVersion, testStreamName)
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer stream.Close()
+	addr := serviceUnderlayAddress(t, s1)
 
-		select {
-		case <-handled:
-		case <-time.After(30 * time.Second):
-			t.Fatal("timeout waiting for handler")
-		}
+	connectContext := t.Context()
 
-		if handledTracingSpan == "" {
-			t.Error("got not tracing span context in handler")
-		}
-	})
+	if _, err := s2.Connect(connectContext, addr); err != nil {
+		t.Fatal(err)
+	}
+
+	ctx := t.Context()
+
+	span, _, ctx := tracer2.StartSpanFromContext(ctx, "test-p2p-client", nil)
+	defer span.Finish()
+
+	if fmt.Sprint(span.Context()) == "" {
+		t.Error("not tracing span context to send")
+	}
+
+	stream, err := s2.NewStream(ctx, overlay1, nil, testProtocolName, testProtocolVersion, testStreamName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer stream.Close()
+
+	select {
+	case <-handled:
+	case <-time.After(30 * time.Second):
+		t.Fatal("timeout waiting for handler")
+	}
+
+	if handledTracingSpan == "" {
+		t.Error("got not tracing span context in handler")
+	}
 }
