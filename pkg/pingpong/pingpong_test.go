4a95156ccaecca77616fbaa64877ab97252618bd
akrem-chabchoub <chabchoub.akrem@gmail.com>
2025-10-29 23:14:16 +0100

    fix(test): correct typo in RTT validation comment

--- pkg/pingpong/pingpong_test.go
+++ pkg/pingpong/pingpong_test.go
@@ -43,7 +43,7 @@ func TestPing(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		// check that RTT is a same value (rtt can be 0 in synctest virtual time)
+		// check that RTT is a sane value (rtt can be 0 in synctest virtual time)
 		if rtt < 0 {
 			t.Errorf("invalid RTT value %v", rtt)
 		}
