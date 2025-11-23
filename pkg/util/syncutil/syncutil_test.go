2bf8755e15f19032d470e546ce0bb0d9278ba35f
Akrem Chabchoub <121046693+akrem-chabchoub@users.noreply.github.com>
2025-11-23 22:50:21 +0100

    test(synctest): add synctest to syncutil test (#5259)

--- pkg/util/syncutil/syncutil_test.go
+++ pkg/util/syncutil/syncutil_test.go
@@ -7,23 +7,26 @@ package syncutil
 import (
 	"sync"
 	"testing"
+	"testing/synctest"
 	"time"
 )
 
 func TestWaitWithTimeout(t *testing.T) {
 	var wg sync.WaitGroup
 
-	if !WaitWithTimeout(&wg, 10*time.Millisecond) {
-		t.Fatal("want timeout; have none")
-	}
+	synctest.Test(t, func(t *testing.T) {
+		if !WaitWithTimeout(&wg, 10*time.Millisecond) {
+			t.Fatal("want timeout; have none")
+		}
 
-	wg.Add(1)
-	if WaitWithTimeout(&wg, 10*time.Millisecond) {
-		t.Fatal("have timeout; want none")
-	}
+		wg.Add(1)
+		if WaitWithTimeout(&wg, 10*time.Millisecond) {
+			t.Fatal("have timeout; want none")
+		}
 
-	wg.Done()
-	if !WaitWithTimeout(&wg, 10*time.Millisecond) {
-		t.Fatal("want no timeout; have none")
-	}
+		wg.Done()
+		if !WaitWithTimeout(&wg, 10*time.Millisecond) {
+			t.Fatal("want no timeout; have none")
+		}
+	})
 }
