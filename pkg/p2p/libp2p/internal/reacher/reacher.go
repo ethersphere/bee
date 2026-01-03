a39c7e00cf140e6cdb6c5a58585d89f8554b95f7
acud <12988138+acud@users.noreply.github.com>
2026-01-03 12:40:51 -0600

    woip

--- pkg/p2p/libp2p/internal/reacher/reacher.go
+++ pkg/p2p/libp2p/internal/reacher/reacher.go
@@ -18,7 +18,7 @@ import (
 )
 
 const (
-	pingTimeout        = time.Second * 1
+	pingTimeout        = time.Second * 15
 	workers            = 16
 	retryAfterDuration = time.Minute * 5
 )
@@ -77,6 +77,7 @@ func New(streamer p2p.Pinger, notifier p2p.ReachableNotifier, o *Options, log lo
 }
 
 func (r *reacher) manage() {
+
 	defer r.wg.Done()
 
 	c := make(chan *peer)
