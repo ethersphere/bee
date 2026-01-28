bc411849d1e46862421682ec86ba9853c8af31e3
Ljubisa Gacevic <ljubisa.rs@gmail.com>
2026-01-28 17:01:28 +0100

    fix(p2p): add remote multiaddress as fallback

--- pkg/p2p/libp2p/internal/handshake/handshake.go
+++ pkg/p2p/libp2p/internal/handshake/handshake.go
@@ -306,6 +306,8 @@ func (s *Service) Handle(ctx context.Context, stream p2p.Stream, peerMultiaddrs 
 		return a.Equal(b)
 	})
 
+	s.logger.Debug("INVESTIGATION", "advertisableUnderlays", advertisableUnderlays)
+
 	advertisableUnderlays = p2p.FilterBee260CompatibleUnderlays(o.bee260compatibility, advertisableUnderlays)
 
 	bzzAddress, err := bzz.NewAddress(s.signer, advertisableUnderlays, s.overlay, s.networkID, s.nonce)
@@ -314,6 +316,8 @@ func (s *Service) Handle(ctx context.Context, stream p2p.Stream, peerMultiaddrs 
 	}
 
 	welcomeMessage := s.GetWelcomeMessage()
+
+	s.logger.Debug("INVESTIGATION", "peerMultiaddrs", peerMultiaddrs)
 
 	peerMultiaddrs = p2p.FilterBee260CompatibleUnderlays(o.bee260compatibility, peerMultiaddrs)
 
