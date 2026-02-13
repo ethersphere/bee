73e102a62718dcf7ae461f6b057436d8ca58d13e
Ljubisa Gacevic <ljubisa.rs@gmail.com>
2026-02-13 17:56:10 +0100

    fix(p2p): add header to heap.go in reacher package

--- pkg/p2p/libp2p/internal/reacher/heap.go
+++ pkg/p2p/libp2p/internal/reacher/heap.go
@@ -1,3 +1,9 @@
+// Copyright 2026 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package reacher runs a background worker that will ping peers
+// from an internal queue and report back the reachability to the notifier.
 package reacher
 
 // peerHeap is a min-heap of peers ordered by retryAfter time.
