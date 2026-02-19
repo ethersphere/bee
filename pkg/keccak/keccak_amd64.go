e48f88212b258bfdf8ddda8410bcec8745ed31c5
acud <12988138+acud@users.noreply.github.com>
2026-02-18 21:14:58 -0600

    clean

--- pkg/keccak/keccak_amd64.go
+++ pkg/keccak/keccak_amd64.go
@@ -2,8 +2,10 @@
 
 package keccak
 
-var hasAVX512 = detectAVX512()
-var hasAVX2 = detectAVX2()
+var (
+	hasAVX512 = detectAVX512()
+	hasAVX2   = detectAVX2()
+)
 
 //go:noescape
 func keccak256x4(inputs *[4][]byte, outputs *[4]Hash256)
@@ -15,7 +17,7 @@ func detectAVX512() bool {
 	eax, ebx, _, _ := cpuid(7, 0)
 	_ = eax
 	// EBX bit 16 = AVX-512F, bit 31 = AVX-512VL
-	return (ebx & (1 << 16)) != 0 && (ebx & (1 << 31)) != 0
+	return (ebx&(1<<16)) != 0 && (ebx&(1<<31)) != 0
 }
 
 func detectAVX2() bool {
