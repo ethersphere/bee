fe5fffa683b4322fc62e21d29f1c8b4821129203
acud <12988138+acud@users.noreply.github.com>
2026-02-18 20:51:35 -0600

    stack size

--- pkg/keccak/keccak_times4_amd64.s
+++ pkg/keccak/keccak_times4_amd64.s
@@ -3,7 +3,7 @@
 #include "textflag.h"
 
 // func keccak256x4(inputs *[4][]byte, outputs *[4]Hash256)
-TEXT ·keccak256x4(SB), $15192-16
+TEXT ·keccak256x4(SB), $16384-16
     MOVQ inputs+0(FP), DI
     MOVQ outputs+8(FP), SI
     CALL go_keccak256x4(SB)
