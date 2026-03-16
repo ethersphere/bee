9e3f2fe3ba728c5e5c04ff774045fa53248aa232
tsinglua <tsinglua@outlook.com>
2026-03-16 18:42:16 +0800

    chore: fix comment for TestBytesDirectUpload function
    Signed-off-by: tsinglua <tsinglua@outlook.com>

--- pkg/api/bytes_test.go
+++ pkg/api/bytes_test.go
@@ -376,7 +376,7 @@ func TestBytesGetHandlerInvalidInputs(t *testing.T) {
 	}
 }
 
-// TestDirectUploadBytes tests that the direct upload endpoint give correct error message in dev mode
+// TestBytesDirectUpload tests that the direct upload endpoint gives correct error message in dev mode
 func TestBytesDirectUpload(t *testing.T) {
 	t.Parallel()
 	const (
