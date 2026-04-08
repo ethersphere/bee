0c407df494d4622d37eb959bf3e02b70ffb05031
Ljubisa Gacevic <ljubisa.rs@gmail.com>
2026-04-08 18:02:10 +0200

    fix: rename base token symbols

--- pkg/config/chain.go
+++ pkg/config/chain.go
@@ -84,14 +84,14 @@ var (
 		ChainID:                8453,
 		NetworkID:              2,
 		PostageStampStartBlock: 41062386,
-		NativeTokenSymbol:      "ETH",
+		NativeTokenSymbol:      "bETH",
 		SwarmTokenSymbol:       "bBZZ",
 
 		StakingAddress:         common.HexToAddress("0x491075e789DBdbb7d08D95946E665eFB2751eE1E"),
 		PostageStampAddress:    common.HexToAddress("0x8613A18717E30be14852846eC6D45F5010339451"),
 		RedistributionAddress:  common.HexToAddress("0x6a02826e2a56092F56e0ba4dB766c5f4540414C2"),
 		SwapPriceOracleAddress: common.HexToAddress("0x0fF044F6bB4F684a5A149B46D7eC03ea659F98A1"),
-		CurrentFactoryAddress:  common.HexToAddress("0xc2d5a532cf69aa9a1378737d8ccdef884b6e7420"),
+		CurrentFactoryAddress:  common.HexToAddress("0xe4620F49ebDEF146366E63B08Eb66cAe32d51c8f"),
 
 		StakingABI:        abi.MainnetStakingABI,
 		PostageStampABI:   abi.MainnetPostageStampABI,
