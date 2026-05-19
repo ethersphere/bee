da8ccfd0637003f6c217eddf2d7344c5c514b38b
acud <12988138+acud@users.noreply.github.com>
2026-05-19 17:34:02 -0600

    perf: bmt SIMD hasher (#5381)

--- cmd/bee/cmd/cmd.go
+++ cmd/bee/cmd/cmd.go
@@ -341,6 +341,7 @@ func (c *command) setAllFlags(cmd *cobra.Command) {
 	cmd.Flags().String(optionAutoTLSRegistrationEndpoint, p2pforge.DefaultForgeEndpoint, "autotls registration endpoint")
 	cmd.Flags().String(optionAutoTLSCAEndpoint, p2pforge.DefaultCAEndpoint, "autotls certificate authority endpoint")
 	cmd.Flags().Bool(optionNamePubsubBrokerMode, true, "enable pubsub broker mode")
+	cmd.Flags().Bool(optionUseSIMD, false, "use SIMD BMT hasher (available only on linux amd64 platforms)")
 }
 
 // preRun must be called from every command's PreRunE, after which c.logger is
