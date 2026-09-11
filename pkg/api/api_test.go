e2593181240df0bc54ba9eb5ee1a6e6e916dc4b7
acud <12988138+acud@users.noreply.github.com>
2026-09-11 06:50:33 -0600

    chore: revert #5497

--- pkg/api/api_test.go
+++ pkg/api/api_test.go
@@ -137,10 +137,6 @@ type testServerOptions struct {
 	ChequebookDisabled  bool
 	SwapDisabled        bool
 	Erc20ServiceNil     bool
-	// ServiceOut, when set, receives the constructed *api.Service so tests
-	// can drive it directly (e.g. via a custom net.Listener) instead of
-	// through the httptest.Server this function also sets up.
-	ServiceOut **api.Service
 }
 
 func newTestServer(t *testing.T, o testServerOptions) (*http.Client, *websocket.Conn, string, *chanStorer) {
@@ -253,10 +249,6 @@ func newTestServer(t *testing.T, o testServerOptions) (*http.Client, *websocket.
 	s.Mount()
 	if !o.FullAPIDisabled {
 		s.EnableFullAPI()
-	}
-
-	if o.ServiceOut != nil {
-		*o.ServiceOut = s
 	}
 
 	if o.DirectUpload {
